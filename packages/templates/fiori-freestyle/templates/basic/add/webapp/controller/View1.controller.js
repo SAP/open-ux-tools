a3a56a24b81a712025273a6cb99f94f9dcf12fbb
Tobias Queck <tobias.queck@sap.com>
2021-06-11 10:47:10 -0700

    feat: added freestyle templates

---
+++ packages/templates/fiori-freestyle/templates/basic/add/webapp/controller/View1.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("<%=app.id%>.controller.View1", {
+			onInit: function () {
+
+			}
+		});
+	});
