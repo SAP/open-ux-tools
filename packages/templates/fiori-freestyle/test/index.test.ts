c8a7752b250b6c93ebe1124cbc6e6369d244dc8f
Ullas Holla <ullas.holla@sap.com>
2021-06-25 14:00:29 -0700

    test(templates/ui5-application): add snapshot tests
    Upgrade jest to 27.0.5

--- packages/templates/fiori-freestyle/test/index.test.ts
+++ packages/templates/fiori-freestyle/test/index.test.ts
@@ -8,6 +8,7 @@ import { rmdirSync } from 'fs';
 describe('Fiori freestyle templates', () => {
     const debug = !!process.env['UX_DEBUG'];
     const outputDir = join(tmpdir(), '/templates/fiori-freestyle');
+    if (debug) console.log(outputDir);
 
     const commonConfig = {
         app: {
