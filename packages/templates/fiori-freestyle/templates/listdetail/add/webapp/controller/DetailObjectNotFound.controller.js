a6396d6e4139836d3b8afeb692bcf8601b2a8931
Tobias Queck <tobias.queck@sap.com>
2021-07-15 11:10:29 +0200

    chore: copied master-detail from tools-suite repo

---
+++ packages/templates/fiori-freestyle/templates/listdetail/add/webapp/controller/DetailObjectNotFound.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+	"./BaseController"
+], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("{{2masterdetail.parameters.AppId.value}}.controller.DetailObjectNotFound", {});
+});
