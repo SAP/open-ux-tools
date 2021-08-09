e4b126b0c28b1d7fedb0afd5c026060d151b62a7
Austin Devine <austin.devine@sap.com>
2021-08-09 11:21:41 +0100

    enforce pnpm usage and node versions, show test coverage

--- packages/lib/yaml/jest.config.js
+++ packages/lib/yaml/jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     transform: {
         '^.+\\.ts$': 'ts-jest'
-    }
+    },
+	collectCoverage: true,
+	coverageReporters: ['text'],
+	collectCoverageFrom: ['src/**/*.ts']
 };
