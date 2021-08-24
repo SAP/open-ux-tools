42e9025ab6e3d08787012d7b103e91ff9cd6f0f3
Austin Devine <austin.devine@sap.com>
2021-08-24 22:13:29 +0100

    Update branch from main

--- packages/templates/ui5-application/src/data/index.ts
+++ packages/templates/ui5-application/src/data/index.ts
@@ -8,10 +8,8 @@ export * from './types';
  * @param {Ui5App} data - the Ui5App instance
  * @returns {Ui5App} - the updated Ui5App instance
  */
-export function mergeWithDefaults(data: Ui5App): Ui5App {
-    return {
-        app: Object.assign(appDefaults(data.app.id), data.app),
-        ui5: mergeUi5(data.ui5),
-        "package": Object.assign(packageDefaults(data.app.version), data.package)
-    };
+export function mergeWithDefaults(data: Ui5App): void {
+	data.app = Object.assign(appDefaults(data.app.id), data.app);
+	data.ui5 = mergeUi5(data.ui5);
+	data.package = Object.assign(packageDefaults(data.app.version), data.package)
 }
