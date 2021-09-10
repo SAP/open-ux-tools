2a7138587d5f4fd2a9248afbdda282cc0c9553fc
Austin Devine <austin.devine@sap.com>
2021-09-10 02:35:30 +0100

    WIP writing test/flpSandbox.html and lisDetail tests. Index.html and test/flpSandbox.html work with npm start. npm start-local & start-mock do not work

--- packages/templates/fiori-freestyle/test/common.ts
+++ packages/templates/fiori-freestyle/test/common.ts
@@ -1,22 +1,31 @@
-import { OdataService, OdataVersion } from "@sap/ux-odata-service-template";
-import { tmpdir } from "os";
-import { join } from "path";
+import { OdataService, OdataVersion } from '@sap/ux-odata-service-template';
+import { tmpdir } from 'os';
+import { join } from 'path';
 
-export function prepareDebug(): { enabled: boolean, outputDir: string } {
+export function prepareDebug(): { enabled: boolean; outputDir: string } {
     const debug = !!process.env['UX_DEBUG'];
     const outputDir = join(tmpdir(), '/templates/fiori-freestyle');
     // eslint-disable-next-line no-console
-    if (debug) { console.log(outputDir); }
+    if (debug) {
+        console.log(outputDir);
+    }
     return { enabled: debug, outputDir };
 }
 
 export const commonConfig = {
     app: {
         id: 'test.me',
-        title: 'My Test App'
+        title: 'My Test App',
+        flpAppId: 'testme-app'
     },
-    "package": {
+    package: {
         name: 'test.me'
+    },
+    ui5: {
+        localVersion: '1.90.0',
+        version: '', // I.e Latest
+        ui5Theme: 'sap_fiori_3',
+        ui5Libs: 'sap.m,sap.ushell'
     }
 };
 
