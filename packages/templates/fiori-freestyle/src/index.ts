b8ab03f03986426bdbbb5f07da88124ecb2bac76
i057546 <iain.o-farrell@sap.com>
2021-09-13 18:21:26 +0100

    fix(ff): Removes duplicate deps in package.json

--- packages/templates/fiori-freestyle/src/index.ts
+++ packages/templates/fiori-freestyle/src/index.ts
@@ -18,36 +18,37 @@ import { getUI5Libs } from './data/ui5Libs';
  * @returns Reference to a mem-fs-editor
  */
 async function generate<T>(basePath: string, data: FreestyleApp<T>, fs?: Editor): Promise<Editor> {
+    // Clone rather than modifyng callers refs
+    const ffApp: FreestyleApp<T> = Object.assign({}, data);
     // generate base UI5 project
-    data.app.baseComponent = 'sap/ui/core/UIComponent';
-    fs = await generateUi5Project(basePath, data, fs);
+    ffApp.app.baseComponent = ffApp.app.baseComponent || 'sap/ui/core/UIComponent';
+    fs = await generateUi5Project(basePath, ffApp, fs);
 
     // add new and overwrite files from templates e.g.
     const tmpPath = join(__dirname, '..', 'templates');
     // Common files
-    fs.copyTpl(join(tmpPath, 'common', 'add', '**/*.*'), basePath, data);
+    fs.copyTpl(join(tmpPath, 'common', 'add', '**/*.*'), basePath, ffApp);
 
     // By template type
-    fs.copyTpl(join(tmpPath, data.template.type, 'add', '**/*.*'), basePath, data);
+    fs.copyTpl(join(tmpPath, ffApp.template.type, 'add', '**/*.*'), basePath, ffApp);
 
     // merge content into existing files
-    const extRoot = join(__dirname, '..', 'templates', data.template.type, 'extend', 'webapp');
+    const extRoot = join(__dirname, '..', 'templates', ffApp.template.type, 'extend', 'webapp');
 
     // manifest.json
     const manifestPath = join(basePath, 'webapp', 'manifest.json');
-    fs.extendJSON(manifestPath, JSON.parse(render(fs.read(join(extRoot, 'manifest.json')), data)));
+    fs.extendJSON(manifestPath, JSON.parse(render(fs.read(join(extRoot, 'manifest.json')), ffApp)));
 
     // i18n.properties
     fs.append(
         join(basePath, 'webapp', 'i18n', 'i18n.properties'),
-        render(fs.read(join(extRoot, 'i18n', 'i18n.properties')), data)
+        render(fs.read(join(extRoot, 'i18n', 'i18n.properties')), ffApp)
     );
 
     // package.json
     const packagePath = join(basePath, 'package.json');
-    fs.extendJSON(packagePath, JSON.parse(render(fs.read(join(tmpPath, 'common', 'extend', 'package.json')), data)));
+    fs.extendJSON(packagePath, JSON.parse(render(fs.read(join(tmpPath, 'common', 'extend', 'package.json')), ffApp)));
     const packageJson = JSON.parse(fs.read(packagePath));
-    packageJson.ui5.dependencies.push('@sap/ux-ui5-tooling');
     fs.writeJSON(packagePath, packageJson);
 
     // ui5.yaml
@@ -57,17 +58,17 @@ async function generate<T>(basePath: string, data: FreestyleApp<T>, fs?: Editor)
     fs.write(ui5ConfigPath, ui5Config.toString());
 
     // add service to the project if provided
-    if (data.service) {
-        await addOdataService(basePath, data.service, fs);
+    if (ffApp.service) {
+        await addOdataService(basePath, ffApp.service, fs);
     }
 
     // ui5-local.yaml
     const ui5LocalConfigPath = join(basePath, 'ui5-local.yaml');
     const ui5LocalConfig = await UI5Config.newInstance(fs.read(ui5LocalConfigPath));
     ui5LocalConfig.addCustomMiddleware(getMiddlewareConfig());
-    if (data?.ui5?.localVersion) {
-        // ui5Config.addUI5Framework(data.ui5.localVersion);
-        ui5Config.addLibraries(getUI5Libs(data?.ui5?.ui5Libs));
+    if (ffApp?.ui5?.localVersion) {
+        // ui5Config.addUI5Framework(ffApp.ui5.localVersion);
+        ui5Config.addLibraries(getUI5Libs(ffApp?.ui5?.ui5Libs));
     }
 
     fs.write(ui5LocalConfigPath, ui5Config.toString());
