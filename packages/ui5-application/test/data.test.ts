576c2024fd29934fbd75a0b3b25afc5d992d34f9
Austin Devine <austin.devine@sap.com>
2021-09-16 22:26:18 +0100

    lint

--- packages/ui5-application/test/data.test.ts
+++ packages/ui5-application/test/data.test.ts
@@ -1,27 +1,31 @@
-import { mergeUi5 } from "../src/data/defaults";
-import type { UI5 } from "@sap/open-ux-tools-types";
+import { mergeUi5 } from '../src/data/defaults';
+import type { UI5 } from '@sap/open-ux-tools-types';
 
 describe('Setting defaults', () => {
-
     it('mergeUi5', async () => {
-        const testData: { input: UI5, expected: UI5}[] = [
-            { input: {}, expected: {
-                version: '1.84.0',
-								minUI5Version: '1.60',
-                descriptorVersion: '1.12.0',
-                typesVersion: '1.84.0' }
+        const testData: { input: UI5; expected: UI5 }[] = [
+            {
+                input: {},
+                expected: {
+                    version: '1.84.0',
+                    minUI5Version: '1.60',
+                    descriptorVersion: '1.12.0',
+                    typesVersion: '1.84.0'
+                }
             },
-            { input: { version: '1.72.0'}, expected: {
-                version: '1.72.0',
-								minUI5Version: '1.60',
-                descriptorVersion: '1.12.0',
-                typesVersion: '1.71.18' }
+            {
+                input: { version: '1.72.0' },
+                expected: {
+                    version: '1.72.0',
+                    minUI5Version: '1.60',
+                    descriptorVersion: '1.12.0',
+                    typesVersion: '1.71.18'
+                }
             }
         ];
 
-        testData.forEach(test => {
+        testData.forEach((test) => {
             expect(mergeUi5(test.input)).toEqual(test.expected);
         });
-
     });
 });
