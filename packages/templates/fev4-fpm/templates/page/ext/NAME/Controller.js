508d9442b787d7cf17c6244a0ffbbc7c95b074c0
Tobias Queck <tobias.queck@sap.com>
2021-09-21 12:22:56 +0200

    fix: removed unnessary class, and renamed variable

--- packages/templates/fev4-fpm/templates/page/ext/NAME/Controller.js
+++ packages/templates/fev4-fpm/templates/page/ext/NAME/Controller.js
@@ -1,14 +1,11 @@
 sap.ui.define(
     [
-        'sap/fe/core/PageController',
-        'sap/fe/core/controllerextensions/RoutingListener'
+        'sap/fe/core/PageController'
     ],
-    function(Controller, RoutingListener) {
+    function(PageController) {
         'use strict';
 
-        return Controller.extend('<%= id %>.<%= name %>', {
-            routingListener: RoutingListener,
-
+        return PageController.extend('<%= id %>.<%= name %>', {
             /**
              * Called when a controller is instantiated and its View controls (if available) are already created.
              * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
