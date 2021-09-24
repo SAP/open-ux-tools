03f7c3e0adbae6a1a6695c3bb4d13d1997a26d71
Volker Buzek <volker.buzek@js-soft.com>
2021-09-24 15:24:44 +0200

    test: add assertions for scaffolding-/config time
    - dynamic view- and controller-name
    - manifest.json adjusting accordingly
    - ui5 cdn + local resources for bootstrapping

--- packages/fiori-freestyle/test/index.test.ts
+++ packages/fiori-freestyle/test/index.test.ts
@@ -1,6 +1,6 @@
 import { FreestyleApp, generate } from '../src';
 import { join } from 'path';
-import { TemplateType, OdataService, OdataVersion } from '@sap/open-ux-tools-types';
+import { TemplateType, OdataService, BootstrapSrc } from '@sap/open-ux-tools-types';
 import { rmdirSync } from 'fs';
 import { sample } from './sample/metadata';
 import { testOutputDir, debug, northwind } from './common';
@@ -91,8 +91,7 @@ describe(`Fiori freestyle templates: ${TEST_NAME}`, () => {
         });
     });
 
-    test("initial view- and controller-name can be adjusted by configuration", async () => {
-        const testPath = join(curTestOutPath, "initViewAndController")
+    describe("set view-and controller-name at scaffolding time", () => {
         const viewPrefix = "MainView"
         const contollerPrefix = "MainView" // well...
         const FreestyleApp: FreestyleApp<any> = {
@@ -112,8 +111,58 @@ describe(`Fiori freestyle templates: ${TEST_NAME}`, () => {
             }
         }
 
-        const fs = await generate(testPath, FreestyleApp)
-        expect(fs.exists(join(testPath, "webapp", "view", `${viewPrefix}.view.xml`))).toBeTruthy()
-        expect(fs.exists(join(testPath, "webapp", "controller", `${contollerPrefix}.controller.js`))).toBeTruthy()
+        test("initial view- and controller-name can be adjusted by configuration", async () => {
+            const testPath = join(curTestOutPath, "initViewAndController")
+            const fs = await generate(testPath, FreestyleApp)
+            expect(fs.exists(join(testPath, "webapp", "view", `${viewPrefix}.view.xml`))).toBeTruthy()
+            expect(fs.exists(join(testPath, "webapp", "controller", `${contollerPrefix}.controller.js`))).toBeTruthy()
+        })
+
+        test("manifest.json adheres to view-/controller-name set at scaffolding time", async () => {
+            const testPath = join(curTestOutPath, "mainfestJson")
+            const fs = await generate(testPath, FreestyleApp)
+            const manifest = { json: fs.readJSON(join(testPath, "webapp", "manifest.json")) as any }
+            expect(
+                [
+                    manifest.json["sap.ui5"].rootView.viewName,
+                    manifest.json["sap.ui5"].rootView.id,
+                    manifest.json["sap.ui5"].routing.routes[0].name,
+                    manifest.json["sap.ui5"].routing.routes[0].pattern,
+                    manifest.json["sap.ui5"].routing.routes[0].target[0],
+                    manifest.json["sap.ui5"].routing.targets[`Target${viewPrefix}`].viewId,
+                    manifest.json["sap.ui5"].routing.targets[`Target${viewPrefix}`].viewName,
+                ].every(entry => entry.includes(viewPrefix))
+            ).toBeTruthy()
+        })
     })
+
+
+    describe("index.html UI5 bootstrap location can be set dynamically", () => {
+        const FreestyleApp: FreestyleApp<any> = {
+            app: {
+                id: "someId",
+            },
+            package: {
+                name: "someId",
+            },
+            template: {
+                type: TemplateType.Basic,
+                settings: {}
+            }
+        }
+
+        const testData = [
+            [BootstrapSrc.CdnOpenUI5, { ...FreestyleApp, ui5: { bootstrapSrc: BootstrapSrc.CdnOpenUI5 } } as FreestyleApp<any>, "https://openui5.hana.ondemand.com/resources/sap-ui-core.js"],
+            [BootstrapSrc.CdnSAPUI5, { ...FreestyleApp, ui5: { bootstrapSrc: BootstrapSrc.CdnSAPUI5 } } as FreestyleApp<any>, "https://sapui5.hana.ondemand.com/resources/sap-ui-core.js"],
+            [BootstrapSrc.Local, { ...FreestyleApp, ui5: { bootstrapSrc: BootstrapSrc.Local } } as FreestyleApp<any>, "resources/sap-ui-core.js"],
+        ]
+
+        test.each(testData)("%s", async (_, appConfig, expectation) => {
+            const testPath = join(curTestOutPath, Date.now().toString())
+            const fs = await generate(testPath, appConfig as FreestyleApp<any>)
+            const index = { html: fs.read(join(testPath, "webapp", "index.html")) }
+            expect(index.html).toContain(expectation)
+        })
+    })
+
 });
