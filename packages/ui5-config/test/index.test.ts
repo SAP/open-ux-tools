e4314c8a7460ebcc91f7e5f3e9c84f545ae6e3a0
i057546 <iain.o-farrell@sap.com>
2021-10-06 18:00:17 +0100

    fix(ui5-config): Remove duplicate test

--- packages/ui5-config/test/index.test.ts
+++ packages/ui5-config/test/index.test.ts
@@ -174,9 +174,6 @@ describe('Fiori config utils', () => {
         `);
     });
 
-    /**
-     * Should we be generating ui5-config files at all if the path is not defined?
-     */
     test('getFioriToolsProxyMiddlewareConfig no path provided', async () => {
         let serviceData = {
             name: 'maintestService',
@@ -185,41 +182,41 @@ describe('Fiori config utils', () => {
         } as OdataService;
 
         expect(getFioriToolsProxyMiddlewareConfig(serviceData)).toMatchInlineSnapshot(`
-            Object {
-              "comments": Array [
-                Object {
-                  "comment": " If set to true, certificate errors will be ignored. E.g. self-signed certificates will be accepted",
-                  "path": "configuration.ignoreCertError",
-                },
-                Object {
-                  "comment": " The UI5 version, for instance, 1.78.1. null means latest version",
-                  "path": "configuration.ui5.version",
-                },
-              ],
-              "config": Array [
-                Object {
-                  "afterMiddleware": "compression",
-                  "configuration": Object {
-                    "backend": Array [
-                      Object {
-                        "path": "/",
-                        "url": "http://localhost:8080",
-                      },
-                    ],
-                    "ignoreCertError": false,
-                    "ui5": Object {
-                      "path": Array [
-                        "/resources",
-                        "/test-resources",
-                      ],
-                      "url": "https://ui5.sap.com",
-                      "version": "",
+          Object {
+            "comments": Array [
+              Object {
+                "comment": " If set to true, certificate errors will be ignored. E.g. self-signed certificates will be accepted",
+                "path": "configuration.ignoreCertError",
+              },
+              Object {
+                "comment": " The UI5 version, for instance, 1.78.1. null means latest version",
+                "path": "configuration.ui5.version",
+              },
+            ],
+            "config": Array [
+              Object {
+                "afterMiddleware": "compression",
+                "configuration": Object {
+                  "backend": Array [
+                    Object {
+                      "path": "/",
+                      "url": "http://localhost:8080",
                     },
+                  ],
+                  "ignoreCertError": false,
+                  "ui5": Object {
+                    "path": Array [
+                      "/resources",
+                      "/test-resources",
+                    ],
+                    "url": "https://ui5.sap.com",
+                    "version": "",
                   },
-                  "name": "fiori-tools-proxy",
                 },
-              ],
-            }
-        `);
+                "name": "fiori-tools-proxy",
+              },
+            ],
+          }
+      `);
     });
 });
