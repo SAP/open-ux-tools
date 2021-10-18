f537ea8efac3fa2e727c8958746d7f0a98d903a6
Ullas Holla <ullas.holla@sap.com>
2021-10-18 13:40:04 -0700

    refactor: rm common

--- packages/common/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    transform: {
-        '^.+\\.ts$': 'ts-jest'
-    },
-    collectCoverage: true,
-    coverageReporters: ['text', 'html'],
-    collectCoverageFrom: ['src/**/*.ts'],
-    modulePathIgnorePatterns: ['<rootDir>/dist']
-};
