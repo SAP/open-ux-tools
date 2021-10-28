3cebaf9be32da8fd9466f3e71a7e8225b7f22462
Tobias Queck <tobias.queck@sap.com>
2021-10-28 10:08:22 +0200

    fix: regression introduced with lint fixes

--- packages/ui5-application-writer/src/data/index.ts
+++ packages/ui5-application-writer/src/data/index.ts
@@ -16,10 +16,15 @@ export function mergeWithDefaults(ui5App: Ui5App): {
     package: Package;
 } {
     validate(ui5App);
-    return {
-        app: mergeApp(ui5App.app),
-        appOptions: ui5App.appOptions || {},
-        ui5: mergeUi5(ui5App.ui5 || {}),
-        package: Object.assign(packageDefaults(ui5App.package.version, ui5App.app.description), ui5App.package)
+    ui5App.app = mergeApp(ui5App.app);
+    ui5App.appOptions = ui5App.appOptions || {};
+    ui5App.ui5 = mergeUi5(ui5App.ui5 || {});
+    ui5App.package = Object.assign(packageDefaults(ui5App.package.version, ui5App.app.description), ui5App.package);
+
+    return ui5App as {
+        app: App;
+        appOptions: Partial<AppOptions>;
+        ui5: UI5;
+        package: Package;
     };
 }
