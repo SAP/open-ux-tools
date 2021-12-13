ecd1c7de21e179da5e18f982bdafdf4386ea55a1
Tobias Queck <tobias.queck@sap.com>
2021-12-13 10:03:53 +0100

    added manual test for steampunk.

--- test/manual/axios-extension/src/index.ts
+++ test/manual/axios-extension/src/index.ts
@@ -1,18 +1,7 @@
-import { config } from 'dotenv';
-import { createForAbap, ODataVersion } from '@sap-ux/axios-extension';
+import { AbapServiceProvider, createForAbap, createForAbapOnBtp, ODataVersion } from '@sap-ux/axios-extension';
+import { readFileSync } from 'fs';
 
-const url = 'https://sapes5.sapdevcenter.com/';
-
-const settings: any = config().parsed ?? {};
-
-async function checkAbapSystem(settings: { TEST_URL: string; TEST_USER: string; TEST_PASSWORD: string }) {
-    const provider = createForAbap({
-        baseURL: settings.TEST_URL,
-        auth: {
-            username: settings.TEST_USER,
-            password: settings.TEST_PASSWORD
-        }
-    });
+async function callAFewAbapServices(provider: AbapServiceProvider): Promise<void> {
     const catalog = await provider.catalog(ODataVersion.v2);
 
     const metadata = await catalog.metadata();
@@ -21,4 +10,37 @@ async function checkAbapSystem(settings: { TEST_URL: string; TEST_USER: string; 
     const services = await catalog.listServices();
 }
 
-checkAbapSystem(settings);
+async function checkAbapSystem(env: { TEST_URL: string; TEST_USER: string; TEST_PASSWORD: string }): Promise<void> {
+    const provider = createForAbap({
+        baseURL: env.TEST_URL,
+        auth: {
+            username: env.TEST_USER,
+            password: env.TEST_PASSWORD
+        }
+    });
+    return callAFewAbapServices(provider);
+}
+
+async function checkAbapBtpSystem(env: { TEST_SERVICE_INFO_PATH: string }): Promise<void> {
+    const serviceInfo = JSON.parse(readFileSync(env.TEST_SERVICE_INFO_PATH, 'utf-8'));
+    const provider = createForAbapOnBtp(serviceInfo);
+    return callAFewAbapServices(provider);
+}
+
+const args = process.argv.slice(3);
+const test = args.length > 0 ? args[0] : undefined;
+const env = process.env as any;
+switch (test) {
+    case 'abap':
+        checkAbapSystem(env);
+        break;
+    case 'btp':
+        checkAbapBtpSystem(env);
+        break;
+    case undefined:
+        console.log(`Test name missing, try 'pnpm test -- abap'`);
+        break;
+    default:
+        console.log(`Unknown manual test ${test}`);
+        break;
+}
