e83955332c406d4e36532a171c7885415e2f8d75
Ullas Holla <ullas.holla@sap.com>
2022-01-04 21:39:08 -0800

    chore(logger): fix linting errors

--- packages/logger/src/extend.ts
+++ packages/logger/src/extend.ts
@@ -3,7 +3,8 @@ import { ExtendedLogger, Logger } from 'types';
 
 /**
  * Extend the logger to add a debug method
- * @param logger: existing logger instance
+ * @param logger existing logger instance
+ * @param namespace namespace for the debug logger
  */
 export function extendLogger(logger: Logger, namespace: string): ExtendedLogger {
     debug.enable(namespace);
