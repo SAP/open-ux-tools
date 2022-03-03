a5665eaa8655a84f36578cdd805009fcf89e52e0
Andis <98035289+jna119@users.noreply.github.com>
2022-03-03 14:23:32 +0200

    fix: cleanup
    cleanup

--- packages/fe-fpm-writer/test/unit/section.test.ts
+++ packages/fe-fpm-writer/test/unit/section.test.ts
@@ -174,16 +174,8 @@ describe('CustomSection', () => {
         const folderVariants = ['extensions/custom', 'extensions\\custom'];
         for (const folderVariant of folderVariants) {
             test(`Existing folder variations - ${folderVariant}`, () => {
-                const testCustomSection: CustomSection = {
-                    target: 'dummy',
-                    name: 'DummySection',
-                    folder: folderVariant,
-                    title: 'Dummy Section',
-                    position: {
-                        placement: Placement.Before,
-                        anchor: 'NewDummyFacet'
-                    }
-                };
+                const testCustomSection = JSON.parse(JSON.stringify(customSection));
+                testCustomSection.folder = folderVariant;
                 generateCustomSection(testDir, { ...testCustomSection }, fs);
                 const updatedManifest: any = fs.readJSON(join(testDir, 'webapp/manifest.json'));
 
@@ -191,7 +183,9 @@ describe('CustomSection', () => {
                     updatedManifest['sap.ui5']['routing']['targets'][testCustomSection.target]['options']['settings'][
                         'content'
                     ]['body']['sections'][testCustomSection.name];
-                expect(section.template).toEqual('sapux.fe.fpm.writer.test.extensions.custom.DummySection');
+                expect(section.template).toEqual(
+                    `sapux.fe.fpm.writer.test.extensions.custom.${testCustomSection.name}`
+                );
             });
         }
     });
