78ca9f426d2e43a711138c6cfa5161d74c5bdf2c
Tobias Queck <tobias.queck@sap.com>
2022-03-28 15:15:28 +0200

    enhanced example to also use the mock server

--- examples/fpm-cli/src/index.ts
+++ examples/fpm-cli/src/index.ts
@@ -1,6 +1,8 @@
 import { join } from 'path';
 import { generate as generateApp } from '@sap-ux/ui5-application-writer';
+import { generate as generateService, OdataVersion } from '@sap-ux/odata-service-writer';
 import { generateCustomPage, enableFPM } from '@sap-ux/fe-fpm-writer';
+import { readFileSync } from 'fs';
 
 const createFPMExample = async function (appId: string): Promise<void> {
     const basePath = join('.tmp', appId);
@@ -14,13 +16,24 @@ const createFPMExample = async function (appId: string): Promise<void> {
         }
     });
 
+    await generateService(
+        basePath,
+        {
+            version: OdataVersion.v4,
+            url: 'http://my.sap.example',
+            path: '/mock/service/path',
+            metadata: readFileSync(join(__dirname, '../service/metadata.xml'), 'utf-8')
+        },
+        fs
+    );
+
     enableFPM(basePath, { fcl: true }, fs);
 
     generateCustomPage(
         basePath,
         {
             name: 'Main',
-            entity: 'MyEntity'
+            entity: 'RootEntity'
         },
         fs
     );
---
+++ packages/fe-fpm-writer/test/unit/app.test.ts
@@ -0,0 +1,3 @@
+describe('CustomApp', () => {
+    test('enableFPM');
+});
