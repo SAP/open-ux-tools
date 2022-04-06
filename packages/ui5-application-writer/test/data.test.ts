73d6a6beab8b96425eb82de49c3896fdfdec9a6b
IainSAP <46536134+IainSAP@users.noreply.github.com>
2022-04-06 20:46:43 +0100

    Fix for #438 incorrect manifest version (#440)
    * fix(ui5-applicaiton-writer): Fix incorrcet manifest version >= 1.100
    
    https://github.com/SAP/open-ux-tools/issues/438
    
    * fix(ui5-applicaiton-writer): Updates comment
    
    * fix(ui5-applicaiton-writer): Fix manifest version is correctly set
    
    * fix(ui-application-writer): Minor updates
    
    * fix(ui5-application-writer): Address review comments
    
    * fix(ui5-application-writer): JSDoc updates
    
    * chore(ui5-appplcaiton-writer): Adds changeset
    
    * fix(ui5-application-writer): Fixes typo in changeset

--- packages/ui5-application-writer/test/data.test.ts
+++ packages/ui5-application-writer/test/data.test.ts
@@ -159,6 +159,57 @@ describe('Setting defaults', () => {
                 ui5Theme: 'sap_fiori_3',
                 ui5Libs: defaultUI5Libs
             }
+        },
+        // 9 - Test that ui5 versions >= 1.100 resolve to the correct manifest version
+        {
+            input: {
+                version: '1.199.0'
+            },
+            expected: {
+                framework: 'SAPUI5',
+                frameworkUrl: 'https://ui5.sap.com',
+                version: '1.199.0',
+                localVersion: '1.199.0',
+                minUI5Version: '1.199.0',
+                descriptorVersion: '1.40.0',
+                typesVersion: '1.199.0',
+                ui5Theme: 'sap_fiori_3',
+                ui5Libs: defaultUI5Libs
+            }
+        },
+        // 10 - Test that non-matching ui5 versions return the closest manifest version
+        {
+            input: {
+                version: '1.97.2'
+            },
+            expected: {
+                framework: 'SAPUI5',
+                frameworkUrl: 'https://ui5.sap.com',
+                version: '1.97.2',
+                localVersion: '1.97.2',
+                minUI5Version: '1.97.2',
+                descriptorVersion: '1.37.0',
+                typesVersion: '1.97.2',
+                ui5Theme: 'sap_fiori_3',
+                ui5Libs: defaultUI5Libs
+            }
+        },
+        // 11 - Test that non-matching ui5 versions return the default manifest version if outside range
+        {
+            input: {
+                version: '1.28.0'
+            },
+            expected: {
+                framework: 'SAPUI5',
+                frameworkUrl: 'https://ui5.sap.com',
+                version: '1.28.0',
+                localVersion: '1.76.0',
+                minUI5Version: '1.28.0',
+                descriptorVersion: '1.12.0',
+                typesVersion: '1.76.0',
+                ui5Theme: 'sap_fiori_3',
+                ui5Libs: defaultUI5Libs
+            }
         }
     ];
 
