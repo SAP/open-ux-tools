0b8b5de23bccd3aa870d41faff353887b0db1a0c
Tobias Queck <tobias.queck@sap.com>
2022-04-08 11:18:43 +0200

    adding a test to specify the expected input

---
+++ packages/axios-extension/test/abap/designtime-adaptation-service.test.ts
@@ -0,0 +1,47 @@
+import nock from 'nock';
+import fs from 'fs';
+import { DesigntimeAdaptationService, createForAbap, DTAConfig } from '../../src';
+
+nock.disableNetConnect();
+
+describe('DesigntimeAdaptationService', () => {
+    const server = 'http://sap.example';
+    const service = createForAbap({ baseURL: server }).designtimeAdaptation();
+
+    beforeEach(() => {
+        jest.resetModules();
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    describe('deploy', () => {
+        const config: DTAConfig = {
+            name: 'apps/mybaseapp/appVariants/myvariantid/',
+            layer: 'CUSTOMER_BASE',
+            package: 'MY_PACKAGE',
+            transport: 'MYTRANSPORT'
+        };
+
+        // mock an existing and not existing app
+        nock(server)
+            .post(
+                `${DesigntimeAdaptationService.PATH}?name=${encodeURIComponent(config.name)}&layer=${
+                    config.layer
+                }&package=${config.package}&changeList=${config.transport}`
+            )
+            .reply(200);
+
+        const archivePath = './dist/my-app.zip';
+        const testData = Buffer.from('TestData');
+        beforeAll(() => {
+            jest.spyOn(fs, 'readFileSync').mockReturnValue(testData);
+        });
+
+        test('deploy new app', async () => {
+            const response = await service.deploy(archivePath, config);
+            expect(response.data).toBeDefined();
+        });
+    });
+});
