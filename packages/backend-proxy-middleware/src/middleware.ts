9f84d52bd3e9dc0fa8c3c297018b3367b5a3a201
Tobias Queck <tobias.queck@sap.com>
2022-05-18 16:58:08 +0200

    Feat/228/backend proxy (#471)
    * Added skeleton
    
    * enhanced store to support api-hub-keys
    
    * a bit more code cleanup
    
    * added explanation for BSP property
    
    * removed build errors
    
    * a bit of cleanup
    
    * tests for enhanceConfigsForDestination
    
    * added cred header for bas
    
    * removed unnessary codfe
    
    * cleanup of types and linitng
    
    * linting
    
    * removed comment
    
    * fixed unit test
    
    * tests for local system usage
    
    * removed code smells
    
    * EventHandlers: cleanup, sjsdoc, tests, small fix
    
    * changed ignoreCertErrors to secure
    
    * pathRewrite: cleanup, jsdoc, tests
    
    * more tests
    
    * extracted embedded flp logic to
    
    * fix: overwrite of old rules
    
    * fix: handle error if config file is not found
    
    * improved test coverage
    
    * more comments and linting
    
    * redefined middleware API to simplify usage
    
    * filled the README and added some more jsdoc comments in the code
    
    * changing reporter to fix sonar report
    
    * added temp example to help identify issues
    
    * fixed sonar config
    
    * added changeset
    
    * use relative path
    
    * ohne worte
    
    * fixed wrong path
    
    * enhanced example for simple test
    
    * removed temp example
    
    * fixes after merge
    
    * fixes based on review
    
    * linting
    
    * enhanced `options` section
    
    * trying to fix the wrong coverage reporting
    
    * remove unnessary property debug
    
    * corrected type of client
    
    * added destination for testing in app-studio
    
    * removed temp test project
    
    * modified the tests that they also run in BAS
    
    * removed todos
    
    * reverted file that should not have been touched
    
    * enabled skipped test
    
    * removed a change
    
    * fix: odd syntax in test
    
    * assigned logger
    
    * moved initI18n and fixed minore issues
    
    * fixed log of proxy
    
    * renamed pathPrefix to pathReplace
    
    * making sonar happy
    
    * reducing cognitive complexity
    
    * removed dep
    
    * forgot the lock file

---
+++ packages/backend-proxy-middleware/src/middleware.ts
@@ -0,0 +1,66 @@
+import dotenv from 'dotenv';
+import { ToolsLogger, UI5ToolingTransport } from '@sap-ux/logger';
+import type { RequestHandler } from 'express';
+import { Router as createRouter } from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import type { MiddlewareParameters, BackendMiddlewareConfig } from './base/types';
+import { generateProxyMiddlewareOptions, initI18n } from './base/proxy';
+import { addOptionsForEmbeddedBSP } from './ext/bsp';
+
+/**
+ * Hides the proxy credentials for displaying the proxy configuration in the console.
+ *
+ * @param proxy - user's proxy server
+ * @returns proxy with hidden credentials for displaying in the console
+ */
+function formatProxyForLogging(proxy: string | undefined): string | undefined {
+    if (proxy) {
+        const forwardSlashIndex = proxy.indexOf('//');
+        const atIndex = proxy.indexOf('@');
+
+        if (forwardSlashIndex !== -1 && atIndex !== -1) {
+            proxy = proxy.replace(proxy.slice(forwardSlashIndex + 2, atIndex), '***:***');
+        }
+    }
+    return proxy || 'none';
+}
+
+/**
+ * UI5 middleware allowing to to proxy backends.
+ *
+ * @param params input parameters for UI5 middleware
+ * @param params.options configuration options
+ */
+module.exports = async ({ options }: MiddlewareParameters<BackendMiddlewareConfig>): Promise<RequestHandler> => {
+    const logger = new ToolsLogger({
+        transports: [new UI5ToolingTransport({ moduleName: 'backend-proxy-middleware' })]
+    });
+
+    await initI18n();
+    dotenv.config();
+
+    const router = createRouter();
+    const backend = options.configuration.backend;
+    const configOptions = options.configuration.options ?? {};
+    configOptions.secure = configOptions.secure !== undefined ? !!configOptions.secure : true;
+
+    try {
+        const proxyOptions = await generateProxyMiddlewareOptions(options.configuration.backend, configOptions, logger);
+        if (backend.bsp) {
+            addOptionsForEmbeddedBSP(backend.bsp, proxyOptions, logger);
+        }
+        router.use(backend.path, createProxyMiddleware(proxyOptions));
+        logger.info(
+            `Starting backend-proxy-middleware using following configuration:\nbackend: ${JSON.stringify({
+                ...backend,
+                proxy: formatProxyForLogging(backend.proxy)
+            })}\noptions: ${JSON.stringify(configOptions)}'`
+        );
+    } catch (e) {
+        const message = `Failed to register backend for ${backend.path}. Check configuration in yaml file. \n\t${e}`;
+        logger.error(message);
+        throw new Error(message);
+    }
+
+    return router;
+};
---
+++ packages/backend-proxy-middleware/test/base/config.test.ts
@@ -0,0 +1,70 @@
+import { getCorporateProxyServer, isHostExcludedFromProxy } from '../../src/base/config';
+
+describe('config', () => {
+    const corporateProxy = 'https://myproxy.example:8443';
+
+    describe('getCorporateProxyServer', () => {
+        test('get value from CLI (wins over input)', () => {
+            process.argv.push(`proxy=${corporateProxy}`);
+            expect(getCorporateProxyServer('~not.used')).toEqual(corporateProxy);
+            process.argv.pop();
+        });
+        test('get value from input (wins over env)', () => {
+            const envProxy = process.env.FIORI_TOOLS_PROXY;
+            process.env.FIORI_TOOLS_PROXY = '~not.used';
+            expect(getCorporateProxyServer(corporateProxy)).toEqual(corporateProxy);
+            process.env.FIORI_TOOLS_PROXY = envProxy;
+        });
+        test('get value from env if there is no input', () => {
+            const envProxy = process.env.FIORI_TOOLS_PROXY;
+            process.env.FIORI_TOOLS_PROXY = corporateProxy;
+            expect(getCorporateProxyServer(undefined)).toEqual(corporateProxy);
+            process.env.FIORI_TOOLS_PROXY = envProxy;
+        });
+    });
+
+    describe('isHostExcludedFromProxy', () => {
+        const host = 'http://www.host.example';
+        const noProxyConfig = process.env.no_proxy;
+
+        afterEach(() => {
+            process.env.no_proxy = noProxyConfig;
+        });
+
+        test('no_proxy config does not exist', () => {
+            process.env.no_proxy = undefined;
+            expect(isHostExcludedFromProxy(host)).toBeFalsy();
+        });
+
+        test('host is not excluded via no_proxy config', () => {
+            process.env.no_proxy = 'host,www';
+            expect(isHostExcludedFromProxy(host)).toBeFalsy();
+        });
+
+        test('host is not excluded via no_proxy config but has similar ending', () => {
+            process.env.no_proxy = 'ample';
+            expect(isHostExcludedFromProxy(host)).toBeFalsy();
+            process.env.no_proxy = 'ost.example';
+            expect(isHostExcludedFromProxy(host)).toBeFalsy();
+        });
+
+        test('host is excluded via no_proxy config', () => {
+            process.env.no_proxy = 'host.example';
+            expect(isHostExcludedFromProxy(host)).toBeTruthy();
+            process.env.no_proxy = 'example';
+            expect(isHostExcludedFromProxy(host)).toBeTruthy();
+        });
+
+        test('host is excluded via no_proxy config, bit with leading .', () => {
+            process.env.no_proxy = '.host.example';
+            expect(isHostExcludedFromProxy(host)).toBeTruthy();
+            process.env.no_proxy = '.example';
+            expect(isHostExcludedFromProxy(host)).toBeTruthy();
+        });
+
+        test('all hosts are excluded from proxy', () => {
+            process.env.no_proxy = '*';
+            expect(isHostExcludedFromProxy(host)).toBeTruthy();
+        });
+    });
+});
