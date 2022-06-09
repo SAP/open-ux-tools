f2ac92de0b219a12418e52a0a3b1125538187851
C5226337 <davis.voicescuks@sap.com>
2022-06-09 11:28:03 +0300

    chore: remove unused types

--- packages/odata-annotation-core/src/annotation-file.ts
+++ packages/odata-annotation-core/src/annotation-file.ts
@@ -4,35 +4,6 @@ export interface Node {
     type: string;
     range?: Range;
 }
-
-export const DOCUMENT_TYPE = 'document';
-export interface AnnotationDocument extends Node {
-    type: typeof DOCUMENT_TYPE;
-    uri: string;
-    references: Reference[];
-    content: DocumentContent;
-}
-export const REFERENCE_TYPE = 'reference';
-export interface Reference extends Node {
-    type: typeof REFERENCE_TYPE;
-    uri?: string;
-    imports: Import[];
-}
-
-export const IMPORT_TYPE = 'import';
-export interface Import extends Node {
-    type: typeof IMPORT_TYPE;
-    name?: Attribute;
-    alias?: Attribute;
-}
-
-export const DOCUMENT_CONTENT_TYPE = 'document-content';
-export interface DocumentContent extends Node {
-    type: typeof DOCUMENT_CONTENT_TYPE;
-    namespace?: Attribute;
-    children: ElementChild[];
-}
-
 export const ATTRIBUTE_TYPE = 'attribute';
 
 export interface Attribute extends Node {
@@ -86,86 +57,7 @@ export interface Element extends Node {
     contentRange?: Range;
 }
 
-export type AnyNode = ElementChild | Document | DocumentContent | Import | Reference | AnnotationDocument;
-
-/**
- * Creates AnnotationDocument node.
- *
- * @param range Range for the whole document
- * @param uri Document URI
- * @param references References node
- * @param content Content node
- * @returns AnnotationDocument node
- */
-export function createAnnotationDocumentNode(
-    range: Range,
-    uri: string,
-    references: Reference[],
-    content: DocumentContent
-): AnnotationDocument {
-    return {
-        type: DOCUMENT_TYPE,
-        range,
-        uri,
-        references,
-        content
-    };
-}
-
-/**
- * Creates Reference node.
- *
- * @param range Reference node range
- * @param imports Import nodes
- * @param uri URI for the reference
- * @returns Reference node
- */
-export function createReferenceNode(range: Range, imports: Import[], uri?: string): Reference {
-    return {
-        type: REFERENCE_TYPE,
-        imports,
-        range,
-        uri
-    };
-}
-
-/**
- * Creates Import node.
- *
- * @param range Import node range
- * @param name Imported namespace
- * @param alias Alias of the namespace
- * @returns Import node
- */
-export function createImportNode(range: Range, name?: Attribute, alias?: Attribute): Import {
-    return {
-        type: IMPORT_TYPE,
-        range,
-        name,
-        alias
-    };
-}
-
-/**
- * Creates DocumentContent node.
- *
- * @param range DocumentContent node range
- * @param children Content nodes
- * @param namespace Namespace for the content nodes
- * @returns DocumentContent node
- */
-export function createDocumentContentNode(
-    range: Range,
-    children: ElementChild[],
-    namespace?: Attribute
-): DocumentContent {
-    return {
-        type: DOCUMENT_CONTENT_TYPE,
-        range,
-        children,
-        namespace
-    };
-}
+export type AnyNode = ElementChild;
 
 export function createAttributeNode(
     range: Range,
