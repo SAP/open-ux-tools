ba444a4e134ce7db7cc0851a75ed408c4421c24c
Tobias Queck <tobias.queck@sap.com>
2022-08-19 13:59:54 +0200

    set sapux=true as default for FE apps

--- packages/fiori-elements-writer/test/fpm.test.ts
+++ packages/fiori-elements-writer/test/fpm.test.ts
@@ -16,26 +16,42 @@ jest.mock('read-pkg-up', () => ({
 
 describe(`Flexible Programming Model template: ${TEST_NAME}`, () => {
     const curTestOutPath = join(testOutputDir, TEST_NAME);
+    const fpmTemplate = {
+        type: TemplateType.FlexibleProgrammingModel,
+        settings: {
+            entityConfig: {
+                mainEntityName: 'Bookings'
+            },
+            pageName: 'Main'
+        }
+    };
 
     const fpmConfigs: Array<{ name: string; config: FioriElementsApp<FPMSettings> }> = [
         {
             name: 'with-js',
             config: {
                 ...Object.assign(feBaseConfig('fefpmjs'), {
-                    template: {
-                        type: TemplateType.FlexibleProgrammingModel,
-                        settings: {
-                            entityConfig: {
-                                mainEntityName: 'Bookings'
-                            },
-                            pageName: 'Main'
-                        }
-                    }
+                    template: fpmTemplate
                 }),
                 ui5: {
                     minUI5Version: '1.96.11'
                 },
                 service: v4Service
+            } as FioriElementsApp<FPMSettings>
+        },
+        {
+            name: 'with-ts',
+            config: {
+                ...Object.assign(feBaseConfig('fefpmts'), {
+                    template: fpmTemplate
+                }),
+                ui5: {
+                    minUI5Version: '1.96.11'
+                },
+                service: v4Service,
+                appOptions: {
+                    typescript: true
+                }
             } as FioriElementsApp<FPMSettings>
         }
     ];
