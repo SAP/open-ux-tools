a38da63d1672e28bdece2aab52a86f957865ecdf
Tobias Queck <tobias.queck@sap.com>
2022-08-29 16:46:59 +0200

    removed commented lines

--- packages/fe-fpm-writer/templates/app/Component.ts
+++ packages/fe-fpm-writer/templates/app/Component.ts
@@ -8,13 +8,4 @@ export default class Component extends AppComponent {
 	public static metadata = {
 		manifest: "json"
 	};
-
-    /**
-     * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-     * @public
-     * @override
-     */
-	//public init() : void {
-    //    super.init();
-	//}
 }
