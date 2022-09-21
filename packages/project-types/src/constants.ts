a4fb37b1e54ea8ef143849a57ac0cb734eb977d9
Klaus Keller <klaus.keller@sap.com>
2022-09-21 15:31:04 -0700

    fix(project-types): add const for ui5 mock yaml

--- packages/project-types/src/constants.ts
+++ packages/project-types/src/constants.ts
@@ -1,6 +1,7 @@
-export enum FileName {
+export const enum FileName {
     Manifest = 'manifest.json',
     Package = 'package.json',
     Ui5Yaml = 'ui5.yaml',
-    Ui5LocalYaml = 'ui5-local.yaml'
+    Ui5LocalYaml = 'ui5-local.yaml',
+    Ui5MockYaml = 'ui5-mock.yaml'
 }
