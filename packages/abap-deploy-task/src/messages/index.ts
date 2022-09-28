ea0992ca8b255265cdad102e239766fe73864770
Tobias Queck <tobias.queck@sap.com>
2022-09-28 15:29:10 +0200

    simpler code

--- packages/abap-deploy-task/src/messages/index.ts
+++ packages/abap-deploy-task/src/messages/index.ts
@@ -6,12 +6,7 @@ import messages from './messages.json';
  * @param {...any} args
  * @returns
  */
-export function t(msg: keyof typeof messages, args?: { [key: string]: unknown }): string {
-    if (args) {
-        // eslint-disable-next-line no-new-func
-        return new Function(...Object.keys(args), 'return `' + messages[msg] + '`;')(...Object.values(args));
-    } else {
-        // eslint-disable-next-line no-new-func
-        return new Function('return `' + messages[msg] + '`;')();
-    }
+export function t(msg: keyof typeof messages, args: { [key: string]: unknown } = {}): string {
+    // eslint-disable-next-line no-new-func
+    return new Function(...Object.keys(args), 'return `' + messages[msg] + '`;')(...Object.values(args));
 }
