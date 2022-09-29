51eaff0b7aa53e4d0b2d86cdacc7c63a90fb03bb
Tobias Queck <tobias.queck@sap.com>
2022-09-29 18:58:01 +0200

    async

--- packages/project-access/src/file/findFiles.ts
+++ packages/project-access/src/file/findFiles.ts
@@ -2,7 +2,7 @@ import { create as createStorage } from 'mem-fs';
 import type { Editor } from 'mem-fs-editor';
 import { create } from 'mem-fs-editor';
 import { dirname, join } from 'path';
-import { sync as searchGlob } from 'glob';
+import { sync as searchGlob, glob } from 'glob';
 import { existsSync as exists } from 'fs';
 
 /**
@@ -25,30 +25,40 @@ declare module 'mem-fs-editor' {
  * @param fs - optional mem-fs-editor instance
  * @returns - array of path that contain the filename
  */
-export function findFiles(filename: string, root: string, stopFolders: string[], fs?: Editor): string[] {
-    const files = searchGlob(filename, {
-        cwd: root,
-        matchBase: true,
-        absolute: true,
-        ignore: stopFolders.map((folder) => `**/${folder}/**`)
+export function findFiles(filename: string, root: string, stopFolders: string[], fs?: Editor): Promise<string[]> {
+    return new Promise<string[]>((resolve, reject) => {
+        glob(
+            filename,
+            {
+                cwd: root,
+                matchBase: true,
+                absolute: true,
+                ignore: stopFolders.map((folder) => `**/${folder}/**`)
+            },
+            (err, files) => {
+                if (err) {
+                    reject(err);
+                } else if (fs) {
+                    const memFiles = fs.dump(root);
+                    const modified = Object.keys(memFiles)
+                        .filter((file) => memFiles[file].state === 'modified' && file.endsWith(filename))
+                        .map((file) => join(root, file));
+                    const ignore = Object.keys(memFiles)
+                        .filter((file) => memFiles[file].state === 'deleted')
+                        .map((file) => join(root, file))
+                        .concat(...modified);
+                    resolve(
+                        files
+                            .filter((match) => !ignore.includes(match))
+                            .map((match) => dirname(match))
+                            .concat(...modified.map((file) => dirname(file)))
+                    );
+                } else {
+                    resolve(files.map((match) => dirname(match)));
+                }
+            }
+        );
     });
-
-    if (fs) {
-        const memFiles = fs.dump(root);
-        const modified = Object.keys(memFiles)
-            .filter((file) => memFiles[file].state === 'modified' && file.endsWith(filename))
-            .map((file) => join(root, file));
-        const ignore = Object.keys(memFiles)
-            .filter((file) => memFiles[file].state === 'deleted')
-            .map((file) => join(root, file))
-            .concat(...modified);
-        return files
-            .filter((match) => !ignore.includes(match))
-            .map((match) => dirname(match))
-            .concat(...modified.map((file) => dirname(file)));
-    } else {
-        return files.map((match) => dirname(match));
-    }
 }
 
 /**
@@ -59,8 +69,8 @@ export function findFiles(filename: string, root: string, stopFolders: string[],
  * @param fs - optional mem-fs-editor instance
  * @returns - path to file name if found, otherwise undefined
  */
-export function findFileUp(fileName: string, startPath: string, fs?: Editor): string | undefined {
-    return findUp(fileName, startPath, fs ?? { exists });
+export function findFileUp(fileName: string, startPath: string, fs?: Editor): Promise<string | undefined> {
+    return Promise.resolve(findUp(fileName, startPath, fs ?? { exists }));
 }
 
 /**
--- packages/project-access/test/file/findFiles.test.ts
+++ packages/project-access/test/file/findFiles.test.ts
@@ -7,66 +7,66 @@ describe('file', () => {
     const root = join(__dirname, '../test-data/file');
 
     describe('findFiles', () => {
-        test('Find files in root', () => {
-            const result = findFiles('rootfile', root, []);
+        test('Find files in root', async () => {
+            const result = await findFiles('rootfile', root, []);
             expect(result).toEqual([root]);
         });
 
-        test('Find files', () => {
-            const result = findFiles('child', root, []);
+        test('Find files', async () => {
+            const result = await findFiles('child', root, []);
             expect(result.length).toBe(4);
         });
 
-        test('Find files with folder exclusion (stop folder)', () => {
-            const result = findFiles('child', root, ['childB', 'childC']);
+        test('Find files with folder exclusion (stop folder)', async () => {
+            const result = await findFiles('child', root, ['childB', 'childC']);
             expect(result).toEqual([join(root, 'childA')]);
         });
 
-        test('Find files with deleted folders in mem-fs', () => {
+        test('Find files with deleted folders in mem-fs', async () => {
             const fs = create(createStorage());
             fs.delete(join(root, 'childB'));
             fs.delete(join(root, 'childC'));
-            const result = findFiles('child', root, [], fs);
+            const result = await findFiles('child', root, [], fs);
             expect(result).toEqual([join(root, 'childA')]);
         });
 
-        test('Find files with added files in mem-fs', () => {
+        test('Find files with added files in mem-fs', async () => {
             const fs = create(createStorage());
             const addedFolder = join(root, 'childD');
             fs.write(join(addedFolder, 'child'), '');
-            const result = findFiles('child', root, ['childC'], fs);
+            const result = await findFiles('child', root, ['childC'], fs);
             expect(result.length).toBe(3);
             expect(result.includes(addedFolder)).toBe(true);
         });
 
-        test('Find files with modified existing files in mem-fs', () => {
+        test('Find files with modified existing files in mem-fs', async () => {
             const fs = create(createStorage());
             fs.append(join(root, 'childB/child'), '...');
-            const result = findFiles('child', root, [], fs);
+            const result = await findFiles('child', root, [], fs);
             expect(result.length).toBe(4);
         });
     });
 
     describe('findFileUp', () => {
         const file = 'child';
-        test('file in the start folder', () => {
+        test('file in the start folder', async () => {
             const start = join(root, 'childA');
-            expect(findFileUp(file, start)).toBe(join(start, file));
+            expect(await findFileUp(file, start)).toBe(join(start, file));
         });
 
-        test('file in parent folder', () => {
+        test('file in parent folder', async () => {
             const start = join(root, 'childC/nested1');
-            expect(findFileUp(file, start)).toBe(join(start, '..', file));
+            expect(await findFileUp(file, start)).toBe(join(start, '..', file));
         });
 
-        test('no match', () => {
-            expect(findFileUp(file, root)).toBeUndefined();
+        test('no match', async () => {
+            expect(await findFileUp(file, root)).toBeUndefined();
         });
 
-        test('file in mem-fs', () => {
+        test('file in mem-fs', async () => {
             const fs = create(createStorage());
             fs.write(join(root, file), '...');
-            expect(findFileUp(file, root, fs)).toBe(join(root, file));
+            expect(await findFileUp(file, root, fs)).toBe(join(root, file));
         });
     });
 });
