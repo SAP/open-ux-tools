b22a4f4fa4fca1fc6531218d095facf4f716be51
Tobias Queck <tobias.queck@sap.com>
2022-10-05 16:15:28 +0200

    exported a useful isAxiosError typecast

--- packages/axios-extension/src/index.ts
+++ packages/axios-extension/src/index.ts
@@ -1,6 +1,18 @@
-export { AxiosRequestConfig } from 'axios';
+import type { AxiosError, AxiosRequestConfig } from 'axios';
 export * from './base/odata-service';
 export * from './base/service-provider';
 export * from './abap';
 export * from './factory';
 export * from './auth';
+
+export { AxiosError, AxiosRequestConfig };
+
+/**
+ * Casts an unknown error to an AxiosError.
+ *
+ * @param e unknown error
+ * @returns exception casted to AxiosError if it is one
+ */
+export function isAxiosError(e: unknown): e is AxiosError {
+    return typeof e === 'object' && e !== null && 'isAxiosError' in e;
+}
