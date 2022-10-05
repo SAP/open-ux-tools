bac8e61646570dab6ce3481f681f80c2658b687a
Tobias Queck <tobias.queck@sap.com>
2022-10-05 08:02:19 +0200

    proposal for usage of mem-fs (#705)
    * proposal for usage of mem-fs
    
    * more comples tests
    
    * fix for findup
    
    * added another test file
    
    * added test for mem-fs
    
    * linting
    
    * removed obsolete deps
    
    * lockfile
    
    * mem-fs tryly optional
    
    * async
    
    * linting
    
    * changed back to find2 and switched base function to mem-fs as option
    
    * lock
    
    * consolidate code

--- packages/project-access/test/file/fileAccess.test.ts
+++ packages/project-access/test/file/fileAccess.test.ts
@@ -1,45 +1,69 @@
 import { join } from 'path';
 import type { Package } from '../../src';
 import { readFile, readJSON, fileExists } from '../../src/file';
+import { create as createStorage } from 'mem-fs';
+import { create } from 'mem-fs-editor';
 
-describe('readFile()', () => {
-    test('Read existing file, should return content', async () => {
-        const content = await readFile(join(__dirname, 'fileAccess.test.ts'));
-        expect(content).toContain('fileAccess.test.ts');
+describe('fileAccess', () => {
+    const memFs = create(createStorage());
+    const memFilePath = join(__dirname, 'fileAccess.test.ts.mem');
+    const memFileContent = { hello: 'world' };
+    memFs.writeJSON(memFilePath, memFileContent);
+
+    describe('readFile', () => {
+        test('Read existing file, should return content', async () => {
+            const content = await readFile(join(__dirname, 'fileAccess.test.ts'));
+            expect(content).toContain('fileAccess.test.ts');
+        });
+
+        test('Read non existing file, should throw error', async () => {
+            try {
+                await readFile('NOT_EXISTING_FILE');
+                fail(`readFile() should have thrown error but did not`);
+            } catch (error) {
+                expect(error.toString()).toContain('NOT_EXISTING_FILE');
+            }
+        });
+
+        test('Read existing file from mem-fs', async () => {
+            const content = await readFile(memFilePath, memFs);
+            expect(content).toContain(memFileContent.hello);
+        });
     });
 
-    test('Read non existing file, should throw error', async () => {
-        try {
-            await readFile('NOT_EXISTING_FILE');
-            fail(`readFile() should have thrown error but did not`);
-        } catch (error) {
-            expect(error.toString()).toContain('NOT_EXISTING_FILE');
-        }
-    });
-});
+    describe('readJSON', () => {
+        test('Read existing JSON file, should return JSON content', async () => {
+            const packageJson = await readJSON<Package>(join(__dirname, '../../package.json'));
+            expect(packageJson.name).toEqual('@sap-ux/project-access');
+        });
 
-describe('readJSON()', () => {
-    test('Read existing JSON file, should return JSON content', async () => {
-        const packageJson = await readJSON<Package>(join(__dirname, '..', '..', 'package.json'));
-        expect(packageJson.name).toEqual('@sap-ux/project-access');
-    });
+        test('Read non existing JSON file, should throw error', async () => {
+            try {
+                await readFile('NOT_EXISTING_JSON_FILE');
+                fail(`readJSON() should have thrown error but did not`);
+            } catch (error) {
+                expect(error.toString()).toContain('NOT_EXISTING_JSON_FILE');
+            }
+        });
 
-    test('Read non existing JSON file, should throw error', async () => {
-        try {
-            await readFile('NOT_EXISTING_JSON_FILE');
-            fail(`readJSON() should have thrown error but did not`);
-        } catch (error) {
-            expect(error.toString()).toContain('NOT_EXISTING_JSON_FILE');
-        }
-    });
-});
-
-describe('fileExists()', () => {
-    test('Check existing file, should return true', async () => {
-        expect(await fileExists(join(__dirname, 'fileAccess.test.ts'))).toBeTruthy();
+        test('Read existing JSON file from mem-fs', async () => {
+            const content = await readJSON<typeof memFileContent>(memFilePath, memFs);
+            expect(content.hello).toBe(memFileContent.hello);
+            expect(content).toEqual(memFileContent);
+        });
     });
 
-    test('Check non existing file, should return false', async () => {
-        expect(await fileExists('DOES_NOT_EXIST')).toBeFalsy();
+    describe('fileExists', () => {
+        test('Check existing file, should return true', () => {
+            expect(fileExists(join(__dirname, 'fileAccess.test.ts'))).resolves.toBe(true);
+        });
+
+        test('Check non existing file, should return false', () => {
+            expect(fileExists('DOES_NOT_EXIST')).resolves.toBe(false);
+        });
+
+        test('Check existing file in memf-fs, should return true', () => {
+            expect(fileExists(memFilePath, memFs)).resolves.toBe(true);
+        });
     });
 });
