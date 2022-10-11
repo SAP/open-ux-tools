ffef269015b203df99cfdd06a9b794ae7f9725bb
Tobias Queck <tobias.queck@sap.com>
2022-10-11 17:36:02 +0200

    more testing

--- packages/abap-deploy-task/test/unit/cli/config.test.ts
+++ packages/abap-deploy-task/test/unit/cli/config.test.ts
@@ -1,33 +1,57 @@
 import type { AbapDeployConfig, CliOptions } from '../../../src/types';
-import { mergeConfig } from '../../../src/cli/config';
+import { getDeploymentConfig, mergeConfig } from '../../../src/cli/config';
+import { join } from 'path';
 
-describe('mergeConfig', () => {
-    const config: AbapDeployConfig = {
-        app: {
-            name: '~name',
-            desription: '~description',
-            package: '~package',
-            transport: '~transport'
-        },
-        target: {
-            url: 'http://target.example'
-        }
-    };
-    const noOptions: CliOptions = {
-        config: 'ui5.yaml'
-    };
-
-    test('no overrides', async () => {
-        expect(await mergeConfig(config, noOptions)).toEqual(config);
+describe('cli/config', () => {
+    describe('getDeploymentConfig', () => {
+        test('valid config path', () => {
+            const getConfigPromise = getDeploymentConfig(join(__dirname, '../../test-input/ui5-deploy.yaml'));
+            expect(getConfigPromise).resolves.toBeDefined();
+        });
+        test('invalid config', () => {
+            const getConfigPromise = getDeploymentConfig(join(__dirname, '../../test-input/ui5.yaml'));
+            expect(getConfigPromise).rejects.toThrowError();
+        });
+        test('invalid path', () => {
+            const getConfigPromise = getDeploymentConfig(join(__dirname, '../../test-input/ui5-invalid.yaml'));
+            expect(getConfigPromise).rejects.toThrowError();
+        });
     });
 
-    test('some overrides', async () => {
-        const overrides: Partial<CliOptions> = {
-            name: '~new-name',
-            desription: '~new-description',
-            package: '~new-package',
-            transport: '~new-transport'
+    describe('mergeConfig', () => {
+        const config: AbapDeployConfig = {
+            app: {
+                name: '~name',
+                desription: '~description',
+                package: '~package',
+                transport: '~transport'
+            },
+            target: {
+                url: 'http://target.example'
+            }
         };
-        expect((await mergeConfig(config, { ...noOptions, ...overrides })).app).toEqual(overrides);
+        const noOptions: CliOptions = {
+            config: 'ui5.yaml'
+        };
+
+        test('no overrides', async () => {
+            expect(await mergeConfig(config, noOptions)).toEqual(config);
+        });
+
+        test('some overrides', async () => {
+            const appOverrides: Partial<CliOptions> = {
+                name: '~new-name',
+                desription: '~new-description',
+                package: '~new-package',
+                transport: '~new-transport'
+            };
+            const targetOverrides: Partial<CliOptions> = {
+                url: 'https://secure.target.example',
+                client: '001'
+            };
+            const merged = await mergeConfig(config, { ...noOptions, ...appOverrides, ...targetOverrides });
+            expect(merged.app).toEqual(appOverrides);
+            expect(merged.target).toEqual(targetOverrides);
+        });
     });
 });
