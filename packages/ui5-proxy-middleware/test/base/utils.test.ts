ee7f9a985342a597d676787ae3edf3267ad381db
Zdravko Georgiev <91968887+zdravko-georgiev@users.noreply.github.com>
2022-10-25 14:10:28 +0200

    fix(ui5-proxy-middleware): change setting of proxy env variables (#749)
    * fix(ui5-proxy-middleware): use getProxyForUrl directly
    
    * fix(ui5-proxy-middleware): changeset
    
    * fix(ui5-proxy-middleware): do not override config.proxy
    
    * fix(ui5-proxy-middleware): change setting of proxy environment
    
    * fix(backend-proxy-middleware): change setting of proxy env variables
    
    * fix(backend-proxy-middleware): remove undefined
    
    * fix: update changeset

--- packages/ui5-proxy-middleware/test/base/utils.test.ts
+++ packages/ui5-proxy-middleware/test/base/utils.test.ts
@@ -7,11 +7,11 @@ import {
     getYamlFile,
     hideProxyCredentials,
     injectUI5Url,
-    isProxyRequired,
     proxyRequestHandler,
     proxyResponseHandler,
     setHtmlResponse,
-    proxyErrorHandler
+    proxyErrorHandler,
+    updateProxyEnv
 } from '../../src/base/utils';
 import type { Response } from 'express';
 import YAML from 'yaml';
@@ -102,137 +102,72 @@ describe('Utils', () => {
     describe('getCorporateProxyServer', () => {
         const corporateProxy = 'https://myproxy.example:8443';
 
-        afterEach(() => {
-            delete process.env.http_proxy;
-            delete process.env.HTTP_PROXY;
-            delete process.env.https_proxy;
-            delete process.env.HTTPS_PROXY;
-            delete process.env.npm_config_proxy;
-            delete process.env.npm_config_https_proxy;
-        });
-
-        test('get value from CLI (wins over input)', () => {
+        test('get value from CLI (wins over input and env)', () => {
+            const envProxy = process.env.npm_config_proxy;
+            const envHttpsProxy = process.env.npm_config_https_proxy;
+            process.env.npm_config_proxy = '~not.used';
+            process.env.npm_config_https_proxy = '~not.used';
             process.argv.push(`proxy=${corporateProxy}`);
             expect(getCorporateProxyServer('~not.used')).toEqual(corporateProxy);
             process.argv.pop();
+            process.env.npm_config_proxy = envProxy;
+            process.env.npm_config_https_proxy = envHttpsProxy;
         });
-        test('get value from input (wins over env)', () => {
-            const envProxy = process.env.FIORI_TOOLS_PROXY;
-            process.env.FIORI_TOOLS_PROXY = '~not.used';
-            expect(getCorporateProxyServer(corporateProxy)).toEqual(corporateProxy);
-            process.env.FIORI_TOOLS_PROXY = envProxy;
+        test('get value from env (wins over input)', () => {
+            const envProxy = process.env.npm_config_proxy;
+            const envHttpsProxy = process.env.npm_config_https_proxy;
+            process.env.npm_config_proxy = corporateProxy;
+            process.env.npm_config_https_proxy = corporateProxy;
+            expect(getCorporateProxyServer('~not.used')).toEqual(corporateProxy);
+            process.env.npm_config_proxy = envProxy;
+            process.env.npm_config_https_proxy = envHttpsProxy;
         });
         test('get value from env if there is no input', () => {
-            const envProxy = process.env.FIORI_TOOLS_PROXY;
-            process.env.FIORI_TOOLS_PROXY = corporateProxy;
+            const envProxy = process.env.npm_config_proxy;
+            const envHttpsProxy = process.env.npm_config_https_proxy;
+            process.env.npm_config_proxy = corporateProxy;
+            process.env.npm_config_https_proxy = corporateProxy;
             expect(getCorporateProxyServer(undefined)).toEqual(corporateProxy);
-            process.env.FIORI_TOOLS_PROXY = envProxy;
+            process.env.npm_config_proxy = envProxy;
+            process.env.npm_config_https_proxy = envHttpsProxy;
+        });
+        test('get value from input if there is no env', () => {
+            const envProxy = process.env.npm_config_proxy;
+            const envHttpsProxy = process.env.npm_config_https_proxy;
+            delete process.env.npm_config_proxy;
+            delete process.env.npm_config_https_proxy;
+            expect(getCorporateProxyServer(corporateProxy)).toEqual(corporateProxy);
+            process.env.npm_config_proxy = envProxy;
+            process.env.npm_config_https_proxy = envHttpsProxy;
         });
     });
 
-    describe('isProxyRequired', () => {
-        const host = 'http://www.host.example';
-        const noProxyConfig = process.env.no_proxy;
+    describe('updateProxyEnv', () => {
         const corporateProxy = 'https://myproxy.example:8443';
-
-        beforeAll(() => {
-            process.env.npm_config_proxy = corporateProxy;
-            process.env.npm_config_https_proxy = corporateProxy;
-        });
-
         afterEach(() => {
-            process.env.no_proxy = noProxyConfig;
-        });
-
-        afterAll(() => {
             delete process.env.npm_config_proxy;
             delete process.env.npm_config_https_proxy;
         });
 
-        test('no_proxy config does not exist', () => {
-            delete process.env.no_proxy;
-            expect(isProxyRequired(host)).toBeTruthy();
+        test('set value from CLI (wins over input)', () => {
+            process.argv.push(`proxy=${corporateProxy}`);
+            updateProxyEnv('~not.used');
+            expect(process.env.npm_config_proxy).toEqual(corporateProxy);
+            expect(process.env.npm_config_https_proxy).toEqual(corporateProxy);
+            process.argv.pop();
         });
-
-        test('host is not excluded via no_proxy config', () => {
-            process.env.no_proxy = 'host,www';
-            expect(isProxyRequired(host)).toBeTruthy();
+        test('set value from input if there is no env)', () => {
+            updateProxyEnv(corporateProxy);
+            expect(process.env.npm_config_proxy).toEqual(corporateProxy);
+            expect(process.env.npm_config_https_proxy).toEqual(corporateProxy);
         });
-
-        test('host is not excluded via no_proxy config but has similar ending', () => {
-            process.env.no_proxy = 'ample';
-            expect(isProxyRequired(host)).toBeTruthy();
-            process.env.no_proxy = 'ost.example';
-            expect(isProxyRequired(host)).toBeTruthy();
-        });
-
-        test('host is not excluded via no_proxy config, because no leading dot', () => {
-            process.env.no_proxy = 'host.example';
-            expect(isProxyRequired(host)).toBeTruthy();
-            process.env.no_proxy = 'example';
-            expect(isProxyRequired(host)).toBeTruthy();
-        });
-
-        test('host is excluded via no_proxy config, with leading .', () => {
-            process.env.no_proxy = '.host.example';
-            expect(isProxyRequired(host)).toBeFalsy();
-            process.env.no_proxy = '.example';
-            expect(isProxyRequired(host)).toBeFalsy();
-        });
-
-        test('host is excluded via no_proxy config, full host', () => {
-            process.env.no_proxy = 'www.host.example';
-            expect(isProxyRequired(host)).toBeFalsy();
-        });
-
-        test('all hosts are excluded from proxy', () => {
-            process.env.no_proxy = '*';
-            expect(isProxyRequired(host)).toBeFalsy();
-        });
-
-        test('host with port is not excluded from proxy, no leading dot', () => {
-            process.env.no_proxy = 'host.example';
-            expect(isProxyRequired(`${host}:3333`)).toBeTruthy();
-        });
-
-        test('host with port is excluded from proxy, when all ports are excluded, with leading dot', () => {
-            process.env.no_proxy = '.host.example';
-            expect(isProxyRequired(`${host}:3333`)).toBeFalsy();
-        });
-
-        test('host with port is excluded from proxy, when all ports are excluded, full host', () => {
-            process.env.no_proxy = 'www.host.example';
-            expect(isProxyRequired(`${host}:3333`)).toBeFalsy();
-        });
-
-        test('only host with specific port is excluded from proxy, with leading dot', () => {
-            process.env.no_proxy = '.host.example:3333';
-            expect(isProxyRequired(`${host}:3333`)).toBeFalsy();
-            expect(isProxyRequired(host)).toBeTruthy();
-        });
-
-        test('only host with specific port is excluded from proxy, full host', () => {
-            process.env.no_proxy = 'www.host.example:3333';
-            expect(isProxyRequired(`${host}:3333`)).toBeFalsy();
-            expect(isProxyRequired(host)).toBeTruthy();
-        });
-
-        test('host with default port is excluded from proxy', () => {
-            process.env.no_proxy = '.host.example:80';
-            expect(isProxyRequired(`${host}:80`)).toBeFalsy();
-            expect(isProxyRequired(`${host}`)).toBeFalsy();
-        });
-
-        test('ip address is excluded from proxy', () => {
-            const host = 'http://123.156.255.101';
-            process.env.no_proxy = '123.156.255.101';
-            expect(isProxyRequired(host)).toBeFalsy();
-        });
-
-        test('ip address is not excluded from proxy', () => {
-            const host = 'http://123.156.255.101';
-            process.env.no_proxy = '127.0.0.1';
-            expect(isProxyRequired(host)).toBeTruthy();
+        test('set value from env if there is no input', () => {
+            const envProxy = process.env.FIORI_TOOLS_PROXY;
+            process.env.FIORI_TOOLS_PROXY = corporateProxy;
+            updateProxyEnv(undefined);
+            expect(process.env.npm_config_proxy).toEqual(corporateProxy);
+            expect(process.env.npm_config_https_proxy).toEqual(corporateProxy);
+            process.env.FIORI_TOOLS_PROXY = envProxy;
         });
     });
 
