c1f24805c0bdd0da855dcf5c592ed0b2fa21d151
Ouin, Edouard <edouard.ouin@sap.com>
2022-10-26 17:34:33 +0100

    feat(ui-components): add UIPersona

---
+++ packages/ui-components/stories/UIPersona.story.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import type { IStackTokens } from '@fluentui/react';
+import { Stack, MessageBarType } from '@fluentui/react';
+import { UIPersona, UIPersonaSize } from '../src/components/UIPersona';
+
+export default { title: 'Utilities/Persona' };
+const stackTokens: IStackTokens = { childrenGap: 40 };
+
+export const defaultUsage = (): JSX.Element => {
+    return (
+        <Stack tokens={stackTokens}>
+            <UIPersona
+                imageUrl={`https://static2.sharepointonline.com/files/fabric/office-ui-fabric-react-assets/persona-male.png`}
+                size={UIPersonaSize.size24}
+            />
+            <UIPersona
+                imageUrl={`https://static2.sharepointonline.com/files/fabric/office-ui-fabric-react-assets/persona-male.png`}
+                size={UIPersonaSize.size32}
+            />
+            <UIPersona
+                imageUrl={`https://static2.sharepointonline.com/files/fabric/office-ui-fabric-react-assets/persona-male.png`}
+                size={UIPersonaSize.size40}
+            />
+            <UIPersona
+                imageUrl={`https://static2.sharepointonline.com/files/fabric/office-ui-fabric-react-assets/persona-male.png`}
+                size={UIPersonaSize.size48}
+            />
+            <UIPersona
+                imageUrl={`https://static2.sharepointonline.com/files/fabric/office-ui-fabric-react-assets/persona-male.png`}
+                size={UIPersonaSize.size56}
+            />
+            <UIPersona
+                imageUrl={`https://static2.sharepointonline.com/files/fabric/office-ui-fabric-react-assets/persona-male.png`}
+                size={UIPersonaSize.size72}
+            />
+            <UIPersona
+                imageUrl={`https://static2.sharepointonline.com/files/fabric/office-ui-fabric-react-assets/persona-male.png`}
+                size={UIPersonaSize.size100}
+            />
+            <UIPersona
+                imageUrl={`https://static2.sharepointonline.com/files/fabric/office-ui-fabric-react-assets/persona-male.png`}
+                size={UIPersonaSize.size120}
+            />
+            <UIPersona text="John Doe" size={UIPersonaSize.size72} />
+        </Stack>
+    );
+};
