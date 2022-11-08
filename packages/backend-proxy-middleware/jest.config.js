d36af16ccb9f1d78c7bb285f0897da9d01d3601b
Tobias Queck <tobias.queck@sap.com>
2022-11-08 13:47:18 +0100

    added base jest config

--- packages/backend-proxy-middleware/jest.config.js
+++ packages/backend-proxy-middleware/jest.config.js
@@ -1,20 +1,6 @@
-module.exports = {
-    transform: {
-        '^.+\\.ts$': 'ts-jest'
-    },
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.ts'],
-    reporters: [
-        'default',
-        [
-            'jest-sonar',
-            {
-                reportedFilePath: 'relative',
-                relativeRootDir: '<rootDir>/../../../'
-            }
-        ]
-    ],
-    coverageReporters: ['text', ['lcov', { projectRoot: '../../' }]],
-    modulePathIgnorePatterns: ['<rootDir>/dist'],
-    verbose: true
-};
+const config = require('../../jest.base');
+config.coverageReporters = [
+    'text', 
+    ['lcov', { projectRoot: '../../' }]
+];    
+module.exports = config;
