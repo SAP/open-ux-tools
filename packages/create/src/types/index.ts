81d5d3298f0050bd7339b68c17c605914c86166f
Klaus Keller <klaus.keller@sap.com>
2022-11-19 01:13:10 -0800

    chore: consolidate types to index

--- packages/create/src/types/index.ts
+++ packages/create/src/types/index.ts
@@ -1,1 +1,12 @@
-export * from './types';
+import type { Editor } from 'mem-fs-editor';
+
+/**
+ * mem-fs-editor types do not expose dump
+ */
+export type EditorWithDump = Editor & {
+    dump: () => EditorChanges;
+};
+
+export interface EditorChanges {
+    [filepath: string]: { contents: string; state: 'modified' | 'deleted' };
+}
