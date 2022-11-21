214a59f95a87416576da46e38acf41a3ac1eafff
Nicolas Lunet <nicolas.lunet@sap.com>
2022-11-21 10:44:04 +0100

    besser

--- types/mem-fs-editor.d.ts
+++ types/mem-fs-editor.d.ts
@@ -1,10 +1,11 @@
+import {Editor as _Editor} from 'mem-fs-editor';
 /**
  * Add missing dump function declaration as mem-fs-editor types do not expose this.
  */
 declare module 'mem-fs-editor' {
     export type FileMap = { [key: string]: { contents: string; state: 'modified' | 'deleted' } };
 
-    export type Editor = Editor & {
+    export interface Editor extends _Editor {
         /**
          * Dump files to compare expected result. Provide a cwd for relative path.
          * See also https://github.com/SBoudrias/mem-fs-editor#dumpcwd-filter for further details.
