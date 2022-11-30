9158f81ff7d46c1ea9498b02b8eeb1cca6e89ef4
Klaus Keller <klaus.keller@sap.com>
2022-11-29 17:21:54 -0800

    fix: allow to select from all odata sources while prompting

--- packages/mockserver-config-writer/src/app-info.ts
+++ packages/mockserver-config-writer/src/app-info.ts
@@ -18,3 +18,20 @@ export function getMainServiceDataSource(manifest: Manifest): ManifestNamespace.
     }
     return dataSource;
 }
+
+/**
+ * Get the data sources of type 'OData' from manifest.
+ *
+ * @param manifest - the parsed manifest.json
+ * @returns data sources of type 'OData'
+ */
+export function getODataSources(manifest: Manifest): { [k: string]: ManifestNamespace.DataSource } {
+    const result: { [k: string]: ManifestNamespace.DataSource } = {};
+    const dataSources = manifest['sap.app'].dataSources || {};
+    for (const dataSource in dataSources) {
+        if (dataSources[dataSource].uri && dataSources[dataSource].type === 'OData') {
+            result[dataSource] = dataSources[dataSource];
+        }
+    }
+    return result;
+}
--- packages/mockserver-config-writer/src/prompt/index.ts
+++ packages/mockserver-config-writer/src/prompt/index.ts
@@ -2,9 +2,9 @@ import { join } from 'path';
 import { create as createStorage } from 'mem-fs';
 import { create } from 'mem-fs-editor';
 import type { Editor } from 'mem-fs-editor';
-import type { PromptObject } from 'prompts';
+import type { Choice, PromptObject } from 'prompts';
 import type { Manifest } from '@sap-ux/project-access';
-import { getMainServiceDataSource } from '../app-info';
+import { getODataSources, getMainServiceDataSource } from '../app-info';
 
 /**
  * Return the list of questions to configure mockserver.
@@ -15,18 +15,29 @@ import { getMainServiceDataSource } from '../app-info';
  * @returns - array of questions that serves as input for prompt module
  */
 export function getMockserverConfigQuestions(params?: { webappPath?: string; fs?: Editor }): PromptObject[] {
-    let path = '';
+    const question: Partial<PromptObject> = {
+        name: 'path',
+        message: 'Path to mocked service'
+    };
     if (params?.webappPath) {
         const fs = params.fs || create(createStorage());
         const manifest: Manifest = JSON.parse(fs.read(join(params.webappPath, 'manifest.json')));
-        path = getMainServiceDataSource(manifest)?.uri || '';
-    }
-    return [
-        {
-            name: 'path',
-            type: 'text',
-            message: 'Path to mocked service',
-            initial: path
+        const mainDataSourceUri = getMainServiceDataSource(manifest)?.uri || '';
+        const oDataSources = getODataSources(manifest);
+        const choices: Choice[] = [];
+        for (const dsName in oDataSources) {
+            choices.push({
+                title: `${dsName}: ${oDataSources[dsName].uri}`,
+                value: oDataSources[dsName].uri,
+                description: oDataSources[dsName].settings?.odataVersion || undefined
+            });
         }
-    ];
+        if (choices.length > 0) {
+            question.type = 'select';
+            question.choices = choices;
+            question.initial = choices.findIndex((c) => c.value === mainDataSourceUri);
+        }
+    }
+    question.type ||= 'text';
+    return [question as PromptObject];
 }
