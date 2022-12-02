42ef9827584cc645116920ba8d3236990a697715
Klaus Keller <klaus.keller@sap.com>
2022-12-02 15:57:48 -0800

    chore: consolidate

--- packages/mockserver-config-writer/src/types/index.ts
+++ packages/mockserver-config-writer/src/types/index.ts
@@ -1,1 +1,15 @@
-export * from './types';
+export interface MockserverConfig {
+    webappPath: string;
+    packageJsonConfig?: PackageJsonMockConfig;
+    ui5MockYamlConfig?: Ui5MockYamlConfig;
+}
+
+export interface PackageJsonMockConfig {
+    script?: string;
+    mockserverModule?: string;
+    mockserverVersion?: string;
+}
+
+export interface Ui5MockYamlConfig {
+    path?: string;
+}
