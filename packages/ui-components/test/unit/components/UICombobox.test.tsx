5abeddc2e9100efda2aafdbc5280d075f0682e62
Andis Redmans <andis.redmans@sap.com>
2023-01-05 13:35:22 +0200

    coverage
    coverage

--- packages/ui-components/test/unit/components/UICombobox.test.tsx
+++ packages/ui-components/test/unit/components/UICombobox.test.tsx
@@ -531,6 +531,19 @@ describe('<UIComboBox />', () => {
         }
     });
 
+    describe('Test "isForceEnabled" property', () => {
+        const testCases = [true, false];
+        for (const testCase of testCases) {
+            it(`"isForceEnabled=${testCase}"`, () => {
+                wrapper.setProps({
+                    options: [],
+                    isForceEnabled: testCase
+                });
+                expect(wrapper.find(ComboBox).prop('disabled')).toEqual(!testCase);
+            });
+        }
+    });
+
     describe('Test "readonly" property', () => {
         const testCases = [
             {
