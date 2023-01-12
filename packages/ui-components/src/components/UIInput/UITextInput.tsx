4f18fa3fd5413bb4238abdfa3478fbb6d55b89d2
Andis Redmans <andis.redmans@sap.com>
2023-01-12 10:50:32 +0200

    fix: sonar
    sonar

--- packages/ui-components/src/components/UIInput/UITextInput.tsx
+++ packages/ui-components/src/components/UIInput/UITextInput.tsx
@@ -70,6 +70,154 @@ export class UITextInput extends React.Component<UITextInputProps> {
     }
 
     /**
+     * Method returns styles for text field.
+     *
+     * @param {ITextFieldStyleProps} props Component properties.
+     * @returns {Partial<ITextFieldStyles>} Styles to apply for text field.
+     */
+    private getStyles = (props: ITextFieldStyleProps): Partial<ITextFieldStyles> => {
+        const messageInfo = getMessageInfo(this.props);
+        return {
+            ...{
+                root: {
+                    height: 'auto',
+                    fontFamily: 'var(--vscode-font-family)'
+                },
+                fieldGroup: [
+                    // Common styles
+                    {
+                        backgroundColor: COLOR_STYLES.regular.backgroundColor,
+                        borderWidth: 1,
+                        borderStyle: COLOR_STYLES.regular.borderStyle,
+                        borderColor: COLOR_STYLES.regular.borderColor,
+                        color: COLOR_STYLES.regular.color,
+                        borderRadius: 0,
+                        boxSizing: 'initial'
+                    },
+                    // Single line common styles
+                    !props.multiline && {
+                        height: 24,
+                        maxHeight: 24,
+                        minHeight: 24
+                    },
+                    // Hoverable input
+                    !props.disabled && {
+                        selectors: {
+                            '&:hover': {
+                                borderColor: COLOR_STYLES.hover.borderColor
+                            }
+                        }
+                    },
+                    // Disabled field
+                    props.disabled && {
+                        backgroundColor: COLOR_STYLES.disabled.backgroundColor,
+                        opacity: COLOR_STYLES.disabled.opacity,
+                        borderRadius: 0
+                    },
+                    // Read only container - disable hover style
+                    this.props.readOnly && {
+                        borderStyle: COLOR_STYLES.readOnly.borderStyle,
+                        // No hover efect on input without value
+                        selectors: !this.props.value
+                            ? {
+                                  '&:hover': {
+                                      borderColor: 'var(--vscode-editorWidget-border)'
+                                  }
+                              }
+                            : undefined
+                    },
+                    // Error message
+                    props.hasErrorMessage && {
+                        borderColor: messageInfo.style.borderColor
+                    },
+                    props.focused && {
+                        selectors: {
+                            ':after': {
+                                border: this.getFocusBorder(messageInfo)
+                            }
+                        }
+                    }
+                ],
+                field: [
+                    // Common styles
+                    {
+                        backgroundColor: COLOR_STYLES.regular.backgroundColor,
+                        color: COLOR_STYLES.regular.color,
+                        fontSize: '13px',
+                        fontWeight: 'normal',
+                        boxSizing: 'border-box',
+                        selectors: {
+                            '::placeholder': {
+                                fontSize: 13,
+                                fontFamily: 'var(--vscode-font-family)',
+                                color: 'var(--vscode-input-placeholderForeground)'
+                            }
+                        }
+                    },
+                    // Single line common styles
+                    !props.multiline && {
+                        lineHeight: 'normal'
+                    },
+                    // Multi line common styles
+                    props.multiline && {
+                        minHeight: '60px',
+                        height: 'auto',
+                        display: 'flex'
+                    },
+                    // Disabled input
+                    props.disabled && {
+                        backgroundColor: 'transparent'
+                    },
+                    // Readonly input
+                    this.props.readOnly && {
+                        fontStyle: 'italic',
+                        backgroundColor: COLOR_STYLES.readOnly.backgroundColor
+                    },
+                    // Input with icon
+                    props.hasIcon && {
+                        selectors: {
+                            '&:hover': {
+                                cursor: 'pointer'
+                            }
+                        }
+                    }
+                ],
+                suffix: {
+                    backgroundColor: 'var(--vscode-input-background)'
+                },
+                subComponentStyles: {
+                    label: {
+                        root: [
+                            {
+                                marginTop: 25,
+                                ...labelGlobalStyle
+                            },
+                            props.disabled && {
+                                opacity: COLOR_STYLES.disabled.opacity
+                            },
+                            props.required && {
+                                selectors: {
+                                    '::after': {
+                                        content: `' *'`,
+                                        color: 'var(--vscode-inputValidation-errorBorder)',
+                                        paddingRight: 12
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                },
+                errorMessage: [messageInfo.style],
+                icon: [
+                    {
+                        bottom: 2
+                    }
+                ]
+            }
+        };
+    };
+
+    /**
      * Method to render HTML input element.
      * Custom rendering is used to use "readonly" attribute instead of "disabled" to make disabled field focusable.
      *
@@ -95,147 +243,7 @@ export class UITextInput extends React.Component<UITextInputProps> {
      */
     render(): JSX.Element {
         const messageInfo = getMessageInfo(this.props);
-        const textFieldStyles = (props: ITextFieldStyleProps): Partial<ITextFieldStyles> => {
-            return {
-                ...{
-                    root: {
-                        height: 'auto',
-                        fontFamily: 'var(--vscode-font-family)'
-                    },
-                    fieldGroup: [
-                        // Common styles
-                        {
-                            backgroundColor: COLOR_STYLES.regular.backgroundColor,
-                            borderWidth: 1,
-                            borderStyle: COLOR_STYLES.regular.borderStyle,
-                            borderColor: COLOR_STYLES.regular.borderColor,
-                            color: COLOR_STYLES.regular.color,
-                            borderRadius: 0,
-                            boxSizing: 'initial'
-                        },
-                        // Single line common styles
-                        !props.multiline && {
-                            height: 24,
-                            maxHeight: 24,
-                            minHeight: 24
-                        },
-                        // Hoverable input
-                        !props.disabled && {
-                            selectors: {
-                                '&:hover': {
-                                    borderColor: COLOR_STYLES.hover.borderColor
-                                }
-                            }
-                        },
-                        // Disabled field
-                        props.disabled && {
-                            backgroundColor: COLOR_STYLES.disabled.backgroundColor,
-                            opacity: COLOR_STYLES.disabled.opacity,
-                            borderRadius: 0
-                        },
-                        // Read only container - disable hover style
-                        this.props.readOnly && {
-                            borderStyle: COLOR_STYLES.readOnly.borderStyle,
-                            // No hover efect on input without value
-                            selectors: !this.props.value
-                                ? {
-                                      '&:hover': {
-                                          borderColor: 'var(--vscode-editorWidget-border)'
-                                      }
-                                  }
-                                : undefined
-                        },
-                        // Error message
-                        props.hasErrorMessage && {
-                            borderColor: messageInfo.style.borderColor
-                        },
-                        props.focused && {
-                            selectors: {
-                                ':after': {
-                                    border: this.getFocusBorder(messageInfo)
-                                }
-                            }
-                        }
-                    ],
-                    field: [
-                        // Common styles
-                        {
-                            backgroundColor: COLOR_STYLES.regular.backgroundColor,
-                            color: COLOR_STYLES.regular.color,
-                            fontSize: '13px',
-                            fontWeight: 'normal',
-                            boxSizing: 'border-box',
-                            selectors: {
-                                '::placeholder': {
-                                    fontSize: 13,
-                                    fontFamily: 'var(--vscode-font-family)',
-                                    color: 'var(--vscode-input-placeholderForeground)'
-                                }
-                            }
-                        },
-                        // Single line common styles
-                        !props.multiline && {
-                            lineHeight: 'normal'
-                        },
-                        // Multi line common styles
-                        props.multiline && {
-                            minHeight: '60px',
-                            height: 'auto',
-                            display: 'flex'
-                        },
-                        // Disabled input
-                        props.disabled && {
-                            backgroundColor: 'transparent'
-                        },
-                        // Readonly input
-                        this.props.readOnly && {
-                            fontStyle: 'italic',
-                            backgroundColor: COLOR_STYLES.readOnly.backgroundColor
-                        },
-                        // Input with icon
-                        props.hasIcon && {
-                            selectors: {
-                                '&:hover': {
-                                    cursor: 'pointer'
-                                }
-                            }
-                        }
-                    ],
-                    suffix: {
-                        backgroundColor: 'var(--vscode-input-background)'
-                    },
-                    subComponentStyles: {
-                        label: {
-                            root: [
-                                {
-                                    marginTop: 25,
-                                    ...labelGlobalStyle
-                                },
-                                props.disabled && {
-                                    opacity: COLOR_STYLES.disabled.opacity
-                                },
-                                props.required && {
-                                    selectors: {
-                                        '::after': {
-                                            content: `' *'`,
-                                            color: 'var(--vscode-inputValidation-errorBorder)',
-                                            paddingRight: 12
-                                        }
-                                    }
-                                }
-                            ]
-                        }
-                    },
-                    errorMessage: [messageInfo.style],
-                    icon: [
-                        {
-                            bottom: 2
-                        }
-                    ]
-                }
-            };
-        };
-
+        const textFieldStyles = this.getStyles;
         return (
             <TextField
                 onRenderInput={this.props.disabled ? this.onRenderDisabledInput : undefined}
