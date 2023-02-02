f3664ee370942d9e0981e1c27cdf8eb6946571ad
Klaus Keller <klaus.keller@sap.com>
2023-02-02 15:45:45 -0800

    chore: changeset, remove additional /

--- packages/project-access/src/types/webapp/index.ts
+++ packages/project-access/src/types/webapp/index.ts
@@ -1,4 +1,4 @@
-import type * as ManifestNamespace from '@ui5/manifest/types//manifest';
+import type * as ManifestNamespace from '@ui5/manifest/types/manifest';
 
 export { ManifestNamespace };
 export type Manifest = ManifestNamespace.SAPJSONSchemaForWebApplicationManifestFile;
