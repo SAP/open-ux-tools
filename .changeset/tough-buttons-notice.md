c06de66b2b0ac5d1122af2ec695644712166204d
Austin Devine <devinea@users.noreply.github.com>
2023-04-05 19:45:10 +0100

    TBI - refactor locate-reuse-lib.js template to use modern API's and fix lint issues (#966)

---
+++ .changeset/tough-buttons-notice.md
@@ -0,0 +1,7 @@
+---
+'@sap-ux/fiori-elements-writer': patch
+'@sap-ux/fiori-freestyle-writer': patch
+'@sap-ux/ui5-application-writer': patch
+---
+
+TBI - refactor locate-reuse-lib.js template to use modern API's and fix lint issues
--- packages/ui5-application-writer/templates/optional/loadReuseLibs/webapp/test/locate-reuse-libs.js
+++ packages/ui5-application-writer/templates/optional/loadReuseLibs/webapp/test/locate-reuse-libs.js
@@ -1,6 +1,7 @@
-/*eslint-disable semi, no-console*/
 (function (sap) {
-    var getKeys = function (libOrComp, libOrCompKeysString) {
+    var fioriToolsGetManifestLibs = function (manifestPath) {
+        var url = manifestPath;
+        var result = "";
         // SAPUI5 delivered namespaces from https://ui5.sap.com/#/api/sap
         var ui5Libs = [
             "sap.apf",
@@ -26,72 +27,73 @@
             "sap.webanalytics",
             "sap.zen"
         ];
-        Object.keys(libOrComp).forEach(function (libOrCompKey) {
-            // ignore libs or Components that start with SAPUI5 delivered namespaces
-            if (
-                !ui5Libs.some(function (substring) {
-                    return libOrCompKey === substring || libOrCompKey.startsWith(substring + ".");
-                })
-            ) {
-                if (libOrCompKeysString.length > 0) {
-                    libOrCompKeysString = libOrCompKeysString + "," + libOrCompKey;
-                } else {
-                    libOrCompKeysString = libOrCompKey;
+        function getKeys(libOrComp, libOrCompKeysString) {
+            var libOrCompKeysStringTmp = libOrCompKeysString;
+            Object.keys(libOrComp).forEach(function (libOrCompKey) {
+                // ignore libs or Components that start with SAPUI5 delivered namespaces
+                if (!ui5Libs.some(function (substring) { return libOrCompKey === substring || libOrCompKey.startsWith(substring + "."); })) {
+                    if (libOrCompKeysStringTmp.length > 0) {
+                        libOrCompKeysStringTmp = libOrCompKeysStringTmp + "," + libOrCompKey;
+                    } else {
+                        libOrCompKeysStringTmp = libOrCompKey;
+                    }
                 }
-            }
-        });
-        return libOrCompKeysString;
-    };
-
-    var updateManifest = function (manifest) {
-        var result = "";
-        if (manifest) {
-            if (manifest["sap.ui5"] && manifest["sap.ui5"].dependencies) {
-                if (manifest["sap.ui5"].dependencies.libs) {
-                    result = getKeys(manifest["sap.ui5"].dependencies.libs, result);
-                }
-                if (manifest["sap.ui5"].dependencies.components) {
-                    result = getKeys(manifest["sap.ui5"].dependencies.components, result);
-                }
-            }
-            if (manifest["sap.ui5"] && manifest["sap.ui5"].componentUsages) {
-                result = getKeys(manifest["sap.ui5"].componentUsages, result);
-            }
+            });
+            return libOrCompKeysStringTmp;
         }
-        return result;
-    };
-
-    var fioriToolsGetManifestLibs = function (manifestPath) {
-        var url = manifestPath;
         return new Promise(function (resolve, reject) {
             $.ajax(url)
                 .done(function (manifest) {
-                    resolve(updateManifest(manifest));
+                    if (manifest) {
+                        if (
+                            manifest["sap.ui5"] &&
+                            manifest["sap.ui5"].dependencies
+                        ) {
+                            if (manifest["sap.ui5"].dependencies.libs) {
+                                result = getKeys(manifest["sap.ui5"].dependencies.libs, result);
+                            }
+                            if (manifest["sap.ui5"].dependencies.components) {
+                                result = getKeys(manifest["sap.ui5"].dependencies.components, result);
+                            }
+                        }
+                        if (
+                            manifest["sap.ui5"] &&
+                            manifest["sap.ui5"].componentUsages
+                        ) {
+                            result = getKeys(manifest["sap.ui5"].componentUsages, result);
+                        }
+                    }
+                    resolve(result);
                 })
-                .fail(function () {
+                .fail(function (error) {
                     reject(new Error("Could not fetch manifest at '" + manifestPath));
                 });
         });
     };
-
-    var updateModuleDefinition = function (data) {
-        if (data) {
-            Object.keys(data).forEach(function (moduleDefinitionKey) {
-                var moduleDefinition = data[moduleDefinitionKey];
-                if (moduleDefinition && moduleDefinition.dependencies) {
-                    moduleDefinition.dependencies.forEach(function (dependency) {
-                        if (dependency.url && dependency.url.length > 0 && dependency.type === "UI5LIB") {
-                            jQuery.sap.log.info(
-                                "Registering Library " + dependency.componentId + " from server " + dependency.url
-                            );
-                            jQuery.sap.registerModulePath(dependency.componentId, dependency.url);
-                        }
-                    });
-                }
-            });
-        }
-    };
-
+    function registerModules(dataFromAppIndex) {
+        Object.keys(dataFromAppIndex).forEach(function (moduleDefinitionKey) {
+            var moduleDefinition = dataFromAppIndex[moduleDefinitionKey];
+            if (moduleDefinition && moduleDefinition.dependencies) {
+                moduleDefinition.dependencies.forEach(function (dependency) {
+                    if (dependency.url && dependency.url.length > 0 && dependency.type === "UI5LIB") {
+                        sap.ui.require(["sap/base/Log"], function (Log) {
+                            Log.info("Registering Library " +
+                                dependency.componentId +
+                                " from server " +
+                                dependency.url);
+                        });
+                        var compId = dependency.componentId.replace(/\./g, "/");
+                        var config = {
+                            paths: {
+                            }
+                        };
+                        config.paths[compId] = dependency.url;
+                        sap.ui.loader.config(config);
+                    }
+                });
+            }
+        });
+    }
     /**
      * Registers the module paths for dependencies of the given component.
      * @param {string} manifestPath The the path to the app manifest path
@@ -100,22 +102,36 @@
      * the app-index was successful and the module paths were registered.
      */
     sap.registerComponentDependencyPaths = function (manifestPath) {
-        /*eslint-disable semi, consistent-return*/
+
         return fioriToolsGetManifestLibs(manifestPath).then(function (libs) {
             if (libs && libs.length > 0) {
                 var url = "/sap/bc/ui2/app_index/ui5_app_info?id=" + libs;
-                var sapClient = jQuery.sap.getUriParameters().get("sap-client");
-                if (sapClient && sapClient.length === 3) {
-                    url = url + "&sap-client=" + sapClient;
-                }
-                return $.ajax(url).done(updateModuleDefinition);
+                var sapClient = "";
+
+                return new Promise(
+                    function (resolve) {
+                        sap.ui.require(["sap/base/util/UriParameters"], function (UriParameters) {
+                            sapClient = UriParameters.fromQuery(window.location.search).get("sap-client");
+                            if (sapClient && sapClient.length === 3) {
+                                url = url + "&sap-client=" + sapClient;
+                            }
+                            resolve(url);
+                        });
+                    }).then(function (url2) {
+                        return $.ajax(url2).done(function (data) {
+                            if (data) {
+                                registerModules(data);
+                            }
+                        });
+                    });
+            } else {
+                return undefined;
             }
         });
     };
 })(sap);
 
 /*eslint-disable sap-browser-api-warning, sap-no-dom-access*/
-var scripts = document.getElementsByTagName("script");
 var currentScript = document.getElementById("locate-reuse-libs");
 if (!currentScript) {
     currentScript = document.currentScript;
@@ -123,22 +139,26 @@ if (!currentScript) {
 var manifestUri = currentScript.getAttribute("data-sap-ui-manifest-uri");
 var componentName = currentScript.getAttribute("data-sap-ui-componentName");
 var useMockserver = currentScript.getAttribute("data-sap-ui-use-mockserver");
-var bundleResources = function () {
-    jQuery.sap.require("jquery.sap.resources");
-    var sLocale = sap.ui.getCore().getConfiguration().getLanguage();
-    var oBundle = jQuery.sap.resources({
-        url: "i18n/i18n.properties",
-        locale: sLocale
-    });
-    document.title = oBundle.getText("appTitle");
-};
+
 sap.registerComponentDependencyPaths(manifestUri)
     .catch(function (error) {
-        jQuery.sap.log.error(error);
+        sap.ui.require(["sap/base/Log"], function (Log) {
+            Log.error(error);
+        });
     })
     .finally(function () {
-        // setting the app title with internationalization
-        sap.ui.getCore().attachInit(bundleResources);
+
+        // setting the app title with internationalization 
+        sap.ui.getCore().attachInit(function () {
+            var sLocale = sap.ui.getCore().getConfiguration().getLanguage();
+            sap.ui.require(["sap/base/i18n/ResourceBundle"], function (ResourceBundle) {
+                var oResourceBundle = ResourceBundle.create({
+                    url: "i18n/i18n.properties",
+                    locale: sLocale
+                });
+                document.title = oResourceBundle.getText("appTitle");
+            });
+        });
 
         if (componentName && componentName.length > 0) {
             if (useMockserver && useMockserver === "true") {
@@ -154,8 +174,17 @@ sap.registerComponentDependencyPaths(manifestUri)
                 // Requiring the ComponentSupport module automatically executes the component initialisation for all declaratively defined components
                 sap.ui.require(["sap/ui/core/ComponentSupport"]);
 
-                // setting the app title with the i18n text
-                sap.ui.getCore().attachInit(bundleResources);
+                // setting the app title with the i18n text 
+                sap.ui.getCore().attachInit(function () {
+                    var sLocale = sap.ui.getCore().getConfiguration().getLanguage();
+                    sap.ui.require(["sap/base/i18n/ResourceBundle"], function (ResourceBundle) {
+                        var oResourceBundle = ResourceBundle.create({
+                            url: "i18n/i18n.properties",
+                            locale: sLocale
+                        });
+                        document.title = oResourceBundle.getText("appTitle");
+                    });
+                });
             }
         } else {
             sap.ui.getCore().attachInit(function () {
@@ -164,5 +193,3 @@ sap.registerComponentDependencyPaths(manifestUri)
             });
         }
     });
-
-sap.registerComponentDependencyPaths(manifestUri);
