d3d79bd6021a46c3d0101be32b5aabe555bf8b18
Christos Koutsiaris <christos.koutsiaris@sap.com>
2023-04-17 18:08:51 +0100

    feat(app-gen): code smell

--- packages/ui5-application-writer/src/types.ts
+++ packages/ui5-application-writer/src/types.ts
@@ -77,7 +77,7 @@ export interface AppOptions {
     /**
      * Excludes the index.html from the template and does not add the `start-noflp` script in package.json
      */
-    generateIndex?: boolean | undefined;
+    generateIndex?: boolean;
 }
 
 export interface Ui5App {
