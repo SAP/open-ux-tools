bb274de9dc984888b984013c2af23f573bb4d245
AlinaGovoruhina <alina.govoruhina@sap.com>
2023-04-21 13:46:13 +0300

    fix: remove empty substring

--- packages/fe-fpm-writer/templates/filter/Controller.js
+++ packages/fe-fpm-writer/templates/filter/Controller.js
@@ -2,20 +2,20 @@ sap.ui.define(["sap/ui/model/Filter", "sap/ui/model/FilterOperator"], function(F
     "use strict";
     return {
         <%- (typeof eventHandlerFnName !== 'undefined' && eventHandlerFnName) || 'itemsFilter' %>: function(sValue) {
-          switch (sValue) {
-              case "0":
-                    return new Filter({ path: "<%- property %>", operator: FilterOperator.LT, value1: 100 });       
-              case "1":
-                    return new Filter({
-                      filters: [
-                          new Filter({ path: "<%- property %>", operator: FilterOperator.GT, value1: 100 }),
-                          new Filter({ path: "<%- property %>", operator: FilterOperator.LT, value1: 500 })
-                      ],
-                      and: true
-                  });           
-              case "2":
-                    return new Filter({ path: "<%- property %>", operator: FilterOperator.GT, value1: 500 });       
-          }
-      }
-   };
-  });
+            switch (sValue) {
+                case "0":
+                        return new Filter({ path: "<%- property %>", operator: FilterOperator.LT, value1: 100 });
+                case "1":
+                        return new Filter({
+                        filters: [
+                            new Filter({ path: "<%- property %>", operator: FilterOperator.GT, value1: 100 }),
+                            new Filter({ path: "<%- property %>", operator: FilterOperator.LT, value1: 500 })
+                        ],
+                        and: true
+                    });
+                case "2":
+                        return new Filter({ path: "<%- property %>", operator: FilterOperator.GT, value1: 500 });
+            }
+        }
+    };
+});
