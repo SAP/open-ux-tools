5afbb758df31759af80950df665f517aa6d1b068
AlinaGovoruhina <alina.govoruhina@sap.com>
2023-05-09 10:34:04 +0300

    fix: replace tabs with spaces in templates

--- packages/fe-fpm-writer/templates/filter/Controller.js
+++ packages/fe-fpm-writer/templates/filter/Controller.js
@@ -1,21 +1,21 @@
 sap.ui.define(["sap/ui/model/Filter", "sap/ui/model/FilterOperator"], function(Filter, FilterOperator) {
-	"use strict";
-	return {
-		<%- (typeof eventHandlerFnName !== 'undefined' && eventHandlerFnName) || 'itemsFilter' %>: function(sValue) {
-			switch (sValue) {
-				case "0":
-						return new Filter({ path: "<%- property %>", operator: FilterOperator.LT, value1: 100 });
-				case "1":
-						return new Filter({
-						filters: [
-							new Filter({ path: "<%- property %>", operator: FilterOperator.GT, value1: 100 }),
-							new Filter({ path: "<%- property %>", operator: FilterOperator.LT, value1: 500 })
-						],
-						and: true
-					});
-				case "2":
-						return new Filter({ path: "<%- property %>", operator: FilterOperator.GT, value1: 500 });
-			}
-		}
-	};
+    "use strict";
+    return {
+        <%- (typeof eventHandlerFnName !== 'undefined' && eventHandlerFnName) || 'itemsFilter' %>: function(sValue) {
+            switch (sValue) {
+                case "0":
+                        return new Filter({ path: "<%- property %>", operator: FilterOperator.LT, value1: 100 });
+                case "1":
+                        return new Filter({
+                        filters: [
+                            new Filter({ path: "<%- property %>", operator: FilterOperator.GT, value1: 100 }),
+                            new Filter({ path: "<%- property %>", operator: FilterOperator.LT, value1: 500 })
+                        ],
+                        and: true
+                    });
+                case "2":
+                        return new Filter({ path: "<%- property %>", operator: FilterOperator.GT, value1: 500 });
+            }
+        }
+    };
 });
