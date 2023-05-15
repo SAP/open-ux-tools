513968ea97a7c539a82c59748a194baf37f8e103
Austin Devine <austin.devine@sap.com>
2023-05-15 14:02:10 +0100

    feedback and fixes

--- packages/eslint-plugin-fiori-tools/src/utils.ts
+++ packages/eslint-plugin-fiori-tools/src/utils.ts
@@ -1,14 +1,14 @@
-import { promises as fs, existsSync } from 'fs';
+import { readFileSync, existsSync } from 'fs';
 import { join } from 'path';
 import { parseDocument } from 'yaml';
 import * as process from 'process';
 
-export const getWebAppPath = async (): Promise<string> => {
+export const getWebAppPath = (): string => {
     let webappPath = 'webapp';
     const projectRoot = process.cwd();
     const ui5YamlPath = join(projectRoot, 'ui5.yaml');
     if (existsSync(ui5YamlPath)) {
-        const ui5Yaml = parseDocument(await fs.readFile(ui5YamlPath, { encoding: 'utf8' })).toJSON();
+        const ui5Yaml = parseDocument(readFileSync(ui5YamlPath, { encoding: 'utf8' })).toJSON();
 
         if (ui5Yaml.resources?.configuration?.paths?.webapp) {
             webappPath = ui5Yaml.resources.configuration.paths.webapp;
