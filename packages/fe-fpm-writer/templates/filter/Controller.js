d7354dbabe0e143628c91dad3f4c46471ec78a75
AlinaGovoruhina <alina.govoruhina@sap.com>
2023-05-17 12:07:34 +0300

    refactor: update templates with fn name

--- packages/fe-fpm-writer/templates/filter/Controller.js
+++ packages/fe-fpm-writer/templates/filter/Controller.js
@@ -1,7 +1,7 @@
 sap.ui.define(["sap/ui/model/Filter", "sap/ui/model/FilterOperator"], function(Filter, FilterOperator) {
     "use strict";
     return {
-        <%- (typeof eventHandlerFnName !== 'undefined' && eventHandlerFnName) || 'itemsFilter' %>: function(sValue) {
+        <%- eventHandlerFnName %>: function(sValue) {
             switch (sValue) {
                 case "0":
                         return new Filter({ path: "<%- property %>", operator: FilterOperator.LT, value1: 100 });
