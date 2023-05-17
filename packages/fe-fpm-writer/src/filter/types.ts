450ddd61cb0011f39f122ecab12d087bd8d22133
AlinaGovoruhina <alina.govoruhina@sap.com>
2023-05-17 12:03:50 +0300

    chore: add jsoc for types

--- packages/fe-fpm-writer/src/filter/types.ts
+++ packages/fe-fpm-writer/src/filter/types.ts
@@ -1,12 +1,42 @@
 import type { CustomElement, InternalCustomElement, Position, EventHandler } from '../common/types';
 
+/**
+ * Properties to create a custom filter.
+ */
 export interface CustomFilter extends CustomElement, EventHandler {
+    /**
+     * Filter label in the application.
+     */
     label: string;
+    /**
+     * Property used to filter by.
+     */
     property: string;
+    /**
+     * ID of the embedded filter XML view.
+     */
     controlID: string;
+    /**
+     * Existing filter XML fragment file name without '.fragment.xml' suffix.
+     * If not set, is equal to the name specified in the config.
+     */
     fragmentFile?: string;
+    /**
+     * Name of the created filter function.
+     * If function name is not set in the eventHandler, is equal to 'itemsFilter'.
+     */
+    eventHandlerFnName?: string;
+    /**
+     * Sets the required property of the custom filter.
+     */
     required?: boolean;
+    /**
+     * Position of a custom filter relative to an anchor element.
+     */
     position?: Position;
+    /**
+     * Specifies whether the controller file is generated in Typescript instead of Javascript.
+     */
     typescript?: boolean;
 }
 
