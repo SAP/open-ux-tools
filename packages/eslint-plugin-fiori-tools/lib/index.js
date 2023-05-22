17f50fda7a6a07ae945ea72c23d90453132baccb
Austin Devine <austin.devine@sap.com>
2023-05-22 16:06:57 +0100

    Rename plugins configs for TS and JS

--- packages/eslint-plugin-fiori-tools/lib/index.js
+++ packages/eslint-plugin-fiori-tools/lib/index.js
@@ -3,8 +3,11 @@
 // Plugin Definition
 //------------------------------------------------------------------------------
 module.exports.configs = {
-    default: {
+    defaultTS: {
         extends: ['../eslintrc-common.js', '../eslintrc-typescript.js', '../eslintrc-prod.js', '../eslintrc-test.js']
+    },
+    defaultJS: {
+        extends: ['../eslintrc-common.js', '../eslintrc-prod.js', '../eslintrc-test.js']
     },
     testCode: {
         extends: ['../eslintrc-common.js', '../eslintrc-typescript.js', '../eslintrc-test.js']
