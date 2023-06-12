4d59b1ee717bb046c83833c64ad6ec91db4d96a8
Klaus Keller <klaus.keller@sap.com>
2023-06-12 15:20:42 -0700

    feat(cap-config-writer): export hasMinCdsVersion

--- packages/cap-config-writer/src/index.ts
+++ packages/cap-config-writer/src/index.ts
@@ -1,7 +1,1 @@
-export {
-    checkCdsUi5PluginEnabled,
-    enableCdsUi5Plugin,
-    getWorkspaceInfo,
-    hasCdsPluginUi5,
-    hasMinCdsVersion
-} from './cap-config';
+export { checkCdsUi5PluginEnabled, enableCdsUi5Plugin, hasMinCdsVersion } from './cap-config';
