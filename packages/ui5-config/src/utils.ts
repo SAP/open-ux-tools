28c95ae66456b4b206c8e15d61c293fdd49318fe
Tobias Queck <tobias.queck@sap.com>
2023-06-20 15:42:58 +0200

    Removing types version workaround

--- packages/ui5-config/src/utils.ts
+++ packages/ui5-config/src/utils.ts
@@ -23,21 +23,20 @@ export function mergeObjects<B, E>(base: B, extension: E): B & E {
 
 /**
  * Get the best types version for the given minUI5Version within a selective range, starting at 1.90.0
- * for https://www.npmjs.com/package/@sapui5/ts-types-esm
- * For the latest versions the LTS S/4 on-premise version (1.102.x) is used, for anything before we
- * match the versions as far back as available.
+ * for https://www.npmjs.com/package/@sapui5/ts-types-esm. For anything before use 1.90.0, and if no
+ * minVersion is provided, use the latest LTS version (1.108.x).
  *
  * @param minUI5Version the minimum UI5 version that needs to be supported
  * @returns semantic version representing the types version.
  */
 export function getEsmTypesVersion(minUI5Version?: string) {
     const version = semVer.coerce(minUI5Version);
-    if (!version || semVer.gte(version, UI5_DEFAULT.TYPES_VERSION_BEST_MIN)) {
+    if (!version) {
         return `~${UI5_DEFAULT.TYPES_VERSION_BEST}`;
+    } else if (semVer.lt(version, UI5_DEFAULT.ESM_TYPES_VERSION_SINCE)) {
+        return `~${UI5_DEFAULT.ESM_TYPES_VERSION_SINCE}`;
     } else {
-        return semVer.gte(version, UI5_DEFAULT.ESM_TYPES_VERSION_SINCE)
-            ? `~${semVer.major(version)}.${semVer.minor(version)}.0`
-            : `~${UI5_DEFAULT.ESM_TYPES_VERSION_SINCE}`;
+        return `~${semVer.major(version)}.${semVer.minor(version)}.${semVer.patch(version)}`;
     }
 }
 
@@ -51,11 +50,9 @@ export function getTypesVersion(minUI5Version?: string) {
     const version = semVer.coerce(minUI5Version);
     if (!version) {
         return `~${UI5_DEFAULT.TYPES_VERSION_BEST}`;
-    } else if (semVer.gte(version, UI5_DEFAULT.TYPES_VERSION_BEST)) {
-        return `~${UI5_DEFAULT.TYPES_VERSION_BEST}`;
+    } else if (semVer.lt(version, UI5_DEFAULT.TYPES_VERSION_SINCE)) {
+        return `~${UI5_DEFAULT.TYPES_VERSION_SINCE}`;
     } else {
-        return semVer.gte(version, UI5_DEFAULT.TYPES_VERSION_SINCE)
-            ? `~${semVer.major(version)}.${semVer.minor(version)}.${semVer.patch(version)}`
-            : UI5_DEFAULT.TYPES_VERSION_PREVIOUS;
+        return `~${semVer.major(version)}.${semVer.minor(version)}.${semVer.patch(version)}`;
     }
 }
