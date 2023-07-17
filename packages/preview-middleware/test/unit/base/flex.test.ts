25a52c4f088e71776ecd3d26f8ac71f859c9dae7
Tobias Queck <tobias.queck@sap.com>
2023-07-17 16:29:41 +0200

    unit test for deleteChange

--- packages/preview-middleware/test/unit/base/flex.test.ts
+++ packages/preview-middleware/test/unit/base/flex.test.ts
@@ -2,11 +2,18 @@ import type { ReaderCollection } from '@ui5/fs';
 import { readChanges, writeChange } from '../../../src/base/flex';
 import { ToolsLogger } from '@sap-ux/logger';
 import { tmpdir } from 'os';
-import { mkdirSync, readFileSync } from 'fs';
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
+import { deleteChange } from '../../../dist/base/flex';
 
 describe('flex', () => {
     const logger = new ToolsLogger();
+    const path = join(tmpdir(), Date.now().toString());
+    beforeAll(() => {
+        mkdirSync(path);
+        mkdirSync(join(path, 'changes'));
+    })
+
     describe('readChanges', () => {
         const byGlobMock = jest.fn();
         const project = {
@@ -43,9 +50,6 @@ describe('flex', () => {
     });
 
     describe('writeChange', () => {
-        const path = join(tmpdir(), Date.now().toString());
-        mkdirSync(path);
-
         test('valid change', () => {
             const change = { fileName: 'id', fileType: 'ctrl_variant' };
             const result = writeChange(change, path, logger);
@@ -59,6 +63,18 @@ describe('flex', () => {
         test('invalid change', () => {
             const result = writeChange({}, path, logger);
             expect(result.success).toBe(false);
+        });
+    });
+
+    describe('deleteChange', () => {
+        test('existing change', () => {
+            const changeId = 'mychange';
+            const fullPath = join(path, 'changes', `${changeId}.change`);
+            writeFileSync(fullPath, JSON.stringify({ hello: 'world'}));
+            const result = deleteChange({ fileName: changeId }, path, logger);
+            expect(result.success).toBe(true);
+            expect(result.message).toBeDefined();
+            expect(existsSync(fullPath)).toBe(false);
         });
     });
 });
