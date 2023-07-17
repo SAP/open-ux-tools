d24282736829ab5efb614fcd631bd53aae9bc66d
Tobias Queck <tobias.queck@sap.com>
2023-07-17 19:52:41 +0200

    Initial publication of the preview middleware (#1093)
    * initial version
    
    * consolidate ui5.d.ts
    
    * types cleanup
    
    * linting
    
    * changeset
    
    * first test
    
    * Linting auto fix commit
    
    * extracted flex logic
    
    * enhanced readmes
    
    * tests for flex
    
    * test coverage
    
    * liniting and pnpm.lock
    
    * better logging and comments
    
    * updated README and minor things
    
    * Linting auto fix commit
    
    * fixing the readme
    
    * better readme
    
    * missing readme info
    
    * Update README.md
    
    * fix: livereload
    
    * fix: added templates to included files
    
    * feat: delete change
    
    * better loging
    
    * fix: flex handling
    
    * Linting auto fix commit
    
    * fix: rta
    
    * only allow rta mode if layer is known
    
    * unit test for deleteChange
    
    * Linting auto fix commit
    
    * fix: prefix removal on delete
    
    * Linting auto fix commit
    
    * review feedback
    
    * better test coverage
    
    * reuselibs
    
    * Linting auto fix commit
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ packages/preview-middleware/test/unit/base/flex.test.ts
@@ -0,0 +1,80 @@
+import type { ReaderCollection } from '@ui5/fs';
+import { readChanges, writeChange } from '../../../src/base/flex';
+import { ToolsLogger } from '@sap-ux/logger';
+import { tmpdir } from 'os';
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
+import { deleteChange } from '../../../dist/base/flex';
+
+describe('flex', () => {
+    const logger = new ToolsLogger();
+    const path = join(tmpdir(), Date.now().toString());
+    beforeAll(() => {
+        mkdirSync(path);
+        mkdirSync(join(path, 'changes'));
+    });
+
+    describe('readChanges', () => {
+        const byGlobMock = jest.fn();
+        const project = {
+            byGlob: byGlobMock
+        } as unknown as ReaderCollection;
+        function mockChange(id: string, ext: string = 'change') {
+            return {
+                getPath: () => `test/changes/${id}.${ext}`,
+                getName: () => `${id}.${ext}`,
+                getString: () => Promise.resolve(JSON.stringify({ id }))
+            };
+        }
+        test('no changes available', async () => {
+            byGlobMock.mockResolvedValueOnce([]);
+            const changes = await readChanges(project, logger);
+            expect(changes).toEqual({});
+        });
+
+        test('valid changes', async () => {
+            byGlobMock.mockResolvedValueOnce([mockChange('id1'), mockChange('id2', 'ctrl_variant_management_change')]);
+            const changes = await readChanges(project, logger);
+            expect(Object.keys(changes)).toHaveLength(2);
+            expect(changes).toEqual({
+                'sap.ui.fl.id1': { id: 'id1' },
+                'sap.ui.fl.id2': { id: 'id2' }
+            });
+        });
+
+        test('mix of valid and invalid changes', async () => {
+            byGlobMock.mockResolvedValueOnce([mockChange('id1'), { invalid: 'change' }]);
+            const changes = await readChanges(project, logger);
+            expect(Object.keys(changes)).toHaveLength(1);
+        });
+    });
+
+    describe('writeChange', () => {
+        test('valid change', () => {
+            const change = { fileName: 'id', fileType: 'ctrl_variant' };
+            const result = writeChange(change, path, logger);
+            expect(result.success).toBe(true);
+            expect(result.message).toBeDefined();
+            expect(
+                JSON.parse(readFileSync(join(path, 'changes', `${change.fileName}.${change.fileType}`), 'utf-8'))
+            ).toEqual(change);
+        });
+
+        test('invalid change', () => {
+            const result = writeChange({}, path, logger);
+            expect(result.success).toBe(false);
+        });
+    });
+
+    describe('deleteChange', () => {
+        test('existing change', () => {
+            const changeId = 'mychange';
+            const fullPath = join(path, 'changes', `${changeId}.change`);
+            writeFileSync(fullPath, JSON.stringify({ hello: 'world' }));
+            const result = deleteChange({ fileName: `sap.ui.fl.${changeId}` }, path, logger);
+            expect(result.success).toBe(true);
+            expect(result.message).toBeDefined();
+            expect(existsSync(fullPath)).toBe(false);
+        });
+    });
+});
---
+++ packages/preview-middleware/test/unit/base/flp.test.ts
@@ -0,0 +1,178 @@
+import type { ReaderCollection } from '@ui5/fs';
+import type { TemplateConfig } from '../../../src/base/flp';
+import { FlpSandbox as FlpSandboxUnderTest } from '../../../src/base/flp';
+import type { FlpConfig } from '../../../src/types';
+import type { MiddlewareUtils } from '@ui5/server';
+import { ToolsLogger } from '@sap-ux/logger';
+import type { Manifest } from '@sap-ux/project-access';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import type { SuperTest, Test } from 'supertest';
+import supertest from 'supertest';
+import express from 'express';
+import { tmpdir } from 'os';
+
+class FlpSandbox extends FlpSandboxUnderTest {
+    public templateConfig: TemplateConfig;
+    public readonly config: FlpConfig;
+}
+
+describe('FlpSandbox', () => {
+    const mockProject = {
+        byGlob: jest.fn().mockImplementation((glob) =>
+            Promise.resolve(
+                glob.includes('changes')
+                    ? [
+                          {
+                              getPath: () => 'test/changes/myid.change',
+                              getName: () => 'myid.change',
+                              getString: () => Promise.resolve(JSON.stringify({ id: 'myId' }))
+                          }
+                      ]
+                    : []
+            )
+        )
+    } as unknown as ReaderCollection;
+    const mockUtils = {
+        getProject() {
+            return {
+                getSourcePath: () => tmpdir()
+            };
+        }
+    } as unknown as MiddlewareUtils;
+    const logger = new ToolsLogger();
+    const fixtures = join(__dirname, '../../fixtures');
+
+    describe('constructor', () => {
+        test('default (no) config', () => {
+            const flp = new FlpSandbox({}, mockProject, mockUtils, logger);
+            expect(flp.config.path).toBe('/test/flp.html');
+            expect(flp.config.apps).toBeDefined();
+            expect(flp.config.apps).toHaveLength(0);
+            expect(flp.router).toBeDefined();
+        });
+
+        test('advanced config', () => {
+            const config: FlpConfig = {
+                path: '/my/custom/path',
+                apps: [
+                    {
+                        target: '/other/app',
+                        local: './local/path'
+                    }
+                ]
+            };
+            const flp = new FlpSandbox(config, mockProject, mockUtils, logger);
+            expect(flp.config.path).toBe(config.path);
+            expect(flp.config.apps).toEqual(config.apps);
+            expect(flp.router).toBeDefined();
+        });
+    });
+
+    describe('init', () => {
+        test('minimal manifest', async () => {
+            const flp = new FlpSandbox({}, mockProject, mockUtils, logger);
+            const manifest = {
+                'sap.app': { id: 'my.id' }
+            } as Manifest;
+            await flp.init(manifest);
+            expect(flp.templateConfig).toMatchSnapshot();
+        });
+
+        test('optional configurations', async () => {
+            const flp = new FlpSandbox({}, mockProject, mockUtils, logger);
+            const manifest = JSON.parse(readFileSync(join(fixtures, 'simple-app/webapp/manifest.json'), 'utf-8'));
+            await flp.init(manifest);
+            expect(flp.templateConfig).toMatchSnapshot();
+        });
+
+        test('additional apps', async () => {
+            const flp = new FlpSandbox(
+                {
+                    apps: [
+                        {
+                            target: '/simple/app',
+                            local: join(fixtures, 'simple-app')
+                        },
+                        {
+                            target: '/yet/another/app',
+                            local: join(fixtures, 'multi-app'),
+                            intent: {
+                                object: 'myObject',
+                                action: 'action'
+                            }
+                        }
+                    ]
+                },
+                mockProject,
+                mockUtils,
+                logger
+            );
+            const manifest = {
+                'sap.app': { id: 'my.id' }
+            } as Manifest;
+            await flp.init(manifest);
+            expect(flp.templateConfig).toMatchSnapshot();
+        });
+    });
+
+    describe('router', () => {
+        let server!: SuperTest<Test>;
+
+        beforeAll(async () => {
+            const flp = new FlpSandbox(
+                {
+                    apps: [
+                        {
+                            target: '/yet/another/app',
+                            local: join(fixtures, 'multi-app')
+                        }
+                    ]
+                },
+                mockProject,
+                mockUtils,
+                logger
+            );
+            const manifest = JSON.parse(readFileSync(join(fixtures, 'simple-app/webapp/manifest.json'), 'utf-8'));
+            await flp.init(manifest);
+
+            const app = express();
+            app.use(flp.router);
+
+            server = await supertest(app);
+        });
+
+        test('test/flp.html', async () => {
+            const response = await server.get('/test/flp.html').expect(200);
+            expect(response.text).toMatchSnapshot();
+        });
+
+        test('WorkspaceConnector.js', async () => {
+            const response = await server.get('/resources/preview/WorkspaceConnector.js').expect(200);
+            expect(response.text).toMatchSnapshot();
+        });
+
+        test('GET /preview/api/changes', async () => {
+            const response = await server.get('/preview/api/changes').expect(200);
+            expect(response.text).toMatchSnapshot();
+        });
+
+        test('POST /preview/api/changes', async () => {
+            const response = await server
+                .post('/preview/api/changes')
+                .set('Content-Type', 'application/json')
+                .send({ fileName: 'id', fileType: 'ctrl_variant' })
+                .expect(200);
+            expect(response.text).toMatchInlineSnapshot(`"FILE_CREATED id.ctrl_variant"`);
+        });
+
+        test('DELETE /preview/api/changes', async () => {
+            const response = await server
+                .delete('/preview/api/changes')
+                .set('Content-Type', 'application/json')
+                .send({ fileName: 'id' })
+                .expect(200);
+            expect(response.text).toMatchInlineSnapshot(`"FILE_DELETED id.ctrl_variant"`);
+        });
+    });
+});
