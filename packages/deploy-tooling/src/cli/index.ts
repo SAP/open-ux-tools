987d57dad09ef719fe906d5c0668963799b1ce81
Cian Morrin <cian.morrin@sap.com>
2023-07-20 23:18:59 +0100

    feat(deploy-tooling): add conflict for create transport

--- packages/deploy-tooling/src/cli/index.ts
+++ packages/deploy-tooling/src/cli/index.ts
@@ -60,7 +60,9 @@ export function createCommand(name: 'deploy' | 'undeploy'): Command {
         command
             .option('--package <abap-package>', 'Package name for deploy target ABAP system')
             .option('--description <description>', 'Project description of the app')
-            .option('--create-transport <description>', 'Create transport request during deployment')
+            .addOption(
+                new Option('--create-transport', 'Create transport request during deployment').conflicts(['transport'])
+            )
             // SafeMode: Example: If the deployment would overwrite a repository that contains an app with a different sap.app/id and SafeMode is true, HTTP status code 412 (Precondition Failed) with further information would be returned.
             .option('--safe', 'Prevents accidentally breaking deployments.')
             .option('--keep', 'Keep a copy of the deployed archive in the project folder.');
