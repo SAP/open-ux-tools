b1a2e3a930ba5e024a4fa31b79533cf08f1b024e
Cian Morrin <99677697+cianmSAP@users.noreply.github.com>
2023-07-26 09:17:47 +0100

    feat(deploy-tooling): create transport request during deployment (#1113)
    * feat(deploy-tooling): creates new tr
    
    * feat(ui5-config): add method to ui5 config
    
    * feat(ui5-config): fix updating deploy taks
    
    * feat(deploy-tooling): writing yaml
    
    * feat(deploy-tooling): update messages
    
    * feat(deploy-tooling): changest lint
    
    * feat(deploy-tooling): fix lint
    
    * feat(deploy-tooling): sonar
    
    * feat(deploy-tooling): unit tests
    
    * feat(deploy-toolong): increase coverage
    
    * feat(deploy-toolong): fix windows test
    
    * feat(deploy-tooling): fix error handling
    
    * feat(deploy-tooling): remove writing to config
    
    * feat(deploy-tooling): changeset
    
    * feat(deploy-tooling): unit test
    
    * feat(deploy-tooling): fix jsdoc and readme
    
    * feat(deploy-tooling): export types
    
    * feat(deploy-tooling): add conflict for create transport
    
    * feat(deploy-tooling): error handling
    
    * feat(deploy-tooling): decouple functions
    
    * feat(deploy-tooling): update cli unit test
    
    * feat(deploy-tooling): jsdoc update
    
    * fix(deploy-tooling): cleanup how TR is handled if ADT service not found
    
    ---------
    
    Co-authored-by: J Long <john.long@sap.com>

--- packages/deploy-tooling/test/__mocks__/index.ts
+++ packages/deploy-tooling/test/__mocks__/index.ts
@@ -11,14 +11,19 @@ type MockedStore = {
 export const mockedStoreService = (mockedStore as unknown as MockedStore).mockedService;
 
 type MockedAxiosExtension = {
-    mockedProvider: { getUi5AbapRepository: jest.Mock };
+    mockedProvider: { getUi5AbapRepository: jest.Mock; getAdtService: jest.Mock };
     mockedUi5AbapRepositoryService: {
         deploy: jest.Mock;
         undeploy: jest.Mock;
     };
+    mockedAdtServiceMethod: {
+        createTransportRequest: jest.Mock;
+    };
 };
 export const mockedUi5RepoService = (mockedAxiosExtension as unknown as MockedAxiosExtension)
     .mockedUi5AbapRepositoryService;
+
+export const mockedAdtService = (mockedAxiosExtension as unknown as MockedAxiosExtension).mockedAdtServiceMethod;
 
 export const mockedProvider = (mockedAxiosExtension as unknown as MockedAxiosExtension).mockedProvider;
 
