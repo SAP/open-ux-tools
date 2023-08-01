18596fd77745561d8ab13af4351e863cd2c23dfa
Andis Redmans <andis.redmans@sap.com>
2023-08-01 22:50:42 +0300

    fix: workaround try
    workaround try

--- packages/project-access/src/project/module-loader.ts
+++ packages/project-access/src/project/module-loader.ts
@@ -1,3 +1,5 @@
+import { execSync } from 'child_process';
+
 /**
  * Load module from project or app. Throws error if module is not installed.
  *
@@ -8,6 +10,9 @@
 export async function loadModuleFromProject<T>(projectRoot: string, moduleName: string): Promise<T> {
     let module: T;
     try {
+        // Run resolve in separate process to avoid caching module with error state
+        execSync(`cd ${projectRoot} && node -e require.resolve('${moduleName}')`);
+        // Resolve module
         const modulePath = require.resolve(moduleName, { paths: [projectRoot] });
         module = (await import(modulePath)) as T;
     } catch (error) {
