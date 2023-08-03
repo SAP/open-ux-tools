fcd5c6bbdb8204b9fd0e5d5a0c7f5863898a5599
C5285809 <ainis.broks@sap.com>
2023-08-03 14:39:57 +0300

    test: custom header section

---
+++ packages/fe-fpm-writer/test/unit/headersection.test.ts
@@ -0,0 +1,113 @@
+import type { Editor } from 'mem-fs-editor';
+import { create } from 'mem-fs-editor';
+import { create as createStorage } from 'mem-fs';
+import { join } from 'path';
+import { generateCustomHeaderSection } from '../../src/section';
+import type { CustomHeaderSection } from '../../src/section/types';
+import type { Manifest } from '../../src/common/types';
+import { Placement } from '../../src/common/types';
+import * as manifestSections from './sample/section/webapp/manifest.json';
+
+const testDir = join(__dirname, 'sample/headersection');
+
+describe('CustomHeaderSection generateCustomHeaderSection', () => {
+    let fs: Editor;
+    const manifest = JSON.parse(JSON.stringify(manifestSections));
+    manifest['sap.ui5'].routing.targets.sample.options.settings.content.body.sections = {};
+    // Basic custom header section object
+    const customHeaderSection: CustomHeaderSection = {
+        target: 'sample',
+        name: 'NewCustomHeaderSection',
+        folder: 'extensions/custom',
+        title: 'New Custom Header Section',
+        subTitle: 'Custom Header section subtitle'
+    };
+    const expectedFragmentPath = join(
+        testDir,
+        `webapp/${customHeaderSection.folder}/${customHeaderSection.name}.fragment.xml`
+    );
+
+    beforeEach(() => {
+        fs = create(createStorage());
+        fs.delete(testDir);
+        fs.write(join(testDir, 'webapp/manifest.json'), JSON.stringify(manifest));
+    });
+
+    const testVersions = ['1.85', '1.86', '1.98'];
+    test.each(testVersions)('Versions %s', (minUI5Version) => {
+        const testCustomSubSection: CustomHeaderSection = {
+            ...customHeaderSection,
+            eventHandler: true,
+            minUI5Version
+        };
+
+        generateCustomHeaderSection(testDir, { ...testCustomSubSection }, fs);
+
+        const updatedManifest = fs.readJSON(join(testDir, 'webapp/manifest.json')) as Manifest;
+        const settings = (
+            updatedManifest['sap.ui5']?.['routing']?.['targets']?.['sample']?.['options'] as Record<string, any>
+        )['settings'];
+        expect(settings.content).toMatchSnapshot();
+
+        expect(fs.read(expectedFragmentPath)).toMatchSnapshot();
+        expect(fs.read(expectedFragmentPath.replace('.fragment.xml', '.js'))).toMatchSnapshot();
+    });
+
+    const positionTests = [
+        {
+            name: 'Create with anchor - latest ui5',
+            position: {
+                placement: Placement.Before,
+                anchor: 'Dummy'
+            }
+        },
+        {
+            name: 'Create without anchor - latest ui5',
+            position: {
+                placement: Placement.Before
+            }
+        },
+        {
+            name: 'Create without position - latest ui5',
+            position: undefined
+        },
+        {
+            name: 'Create with anchor - 1.85 ui5',
+            position: {
+                placement: Placement.Before,
+                anchor: 'Dummy'
+            },
+            minUI5Version: '1.85.1'
+        },
+        {
+            name: 'Create without anchor - 1.85 ui5',
+            position: {
+                placement: Placement.Before
+            },
+            minUI5Version: '1.85.1'
+        },
+        {
+            name: 'Create without position - 1.85 ui5',
+            position: undefined,
+            minUI5Version: '1.85.1'
+        }
+    ];
+    positionTests.forEach((testCase) => {
+        test(`Test 'position' property. ${testCase.name}`, () => {
+            generateCustomHeaderSection(
+                testDir,
+                {
+                    ...customHeaderSection,
+                    position: testCase.position,
+                    minUI5Version: testCase.minUI5Version
+                },
+                fs
+            );
+            const manifest = fs.readJSON(join(testDir, 'webapp/manifest.json')) as Manifest;
+            const headerSection = (
+                manifest?.['sap.ui5']?.['routing']?.['targets']?.['sample']?.['options'] as Record<string, any>
+            )?.['settings']?.['content']?.['header']?.['facets']?.['NewCustomHeaderSection'];
+            expect(headerSection).toMatchSnapshot();
+        });
+    });
+});
