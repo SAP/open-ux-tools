819de8d86868ae58a26f95903b041a6f75032f5a
C5285809 <ainis.broks@sap.com>
2023-08-08 18:40:23 +0300

    fix: failing tests

--- packages/fe-fpm-writer/test/unit/headersection.test.ts
+++ packages/fe-fpm-writer/test/unit/headersection.test.ts
@@ -14,112 +14,124 @@ describe('CustomHeaderSection generateCustomHeaderSection', () => {
     let fs: Editor;
     const testVersions = ['1.85', '1.86', '1.98'];
 
-    beforeEach(() => {
-        const manifest = JSON.parse(JSON.stringify(manifestSections));
-        manifest['sap.ui5'].routing.targets.sample.options.settings.content.body.sections = {};
-        fs = create(createStorage());
-        fs.delete(testDir);
-        fs.write(join(testDir, 'webapp/manifest.json'), JSON.stringify(manifest));
-    });
+    /*  for custom header sections 2 fragment definitions are supported:
+        1. template - standard fragment
+        2. templateEdit - fragment in edit mode
+        we test 2 scenarios (fragments in same folder and differents folders) for 3 different versions - 1.85, 1.86 and 1.98
+    */
 
-    describe('template and templateEdit fragments in same folder', () => {
-        const customHeaderSection: CustomHeaderSection = {
-            target: 'sample',
-            name: 'NewCustomHeaderSection',
-            folder: 'extensions/custom',
-            title: 'New Custom Header Section',
-            subTitle: 'Custom Header section subtitle',
-            edit: {
-                name: 'NewCustomHeaderSectionEdit',
-                folder: 'extensions/custom',
-                eventHandler: true
-            },
-            requestGroupId: RequestGroupId.Decoration,
-            flexSettings: {
-                designtime: DesignTime.Default
-            }
-        };
+    describe('read only and edit fragments in same extension folder', () => {
         let expectedEditFragmentPath: string;
-        const expectedFragmentPath = join(
-            testDir,
-            `webapp/${customHeaderSection.folder}/${customHeaderSection.name}.fragment.xml`
-        );
-        if (customHeaderSection.edit?.folder) {
-            expectedEditFragmentPath = join(
-                testDir,
-                `webapp/${customHeaderSection.edit.folder}/${customHeaderSection.edit.name}.fragment.xml`
-            );
-        }
-        test.each(testVersions)('Versions %s', (minUI5Version) => {
-            const testCustomHeaderSection: CustomHeaderSection = {
-                ...customHeaderSection,
-                eventHandler: true,
-                minUI5Version
-            };
 
-            generateCustomHeaderSection(testDir, { ...testCustomHeaderSection }, fs);
+        beforeEach(() => {
+            const manifest = JSON.parse(JSON.stringify(manifestSections));
+            manifest['sap.ui5'].routing.targets.sample.options.settings.content.body.sections = {};
+            fs = create(createStorage());
+            fs.delete(testDir);
+            fs.write(join(testDir, 'webapp/manifest.json'), JSON.stringify(manifest));
+        });
 
-            const updatedManifest = fs.readJSON(join(testDir, 'webapp/manifest.json')) as Manifest;
-            const settings = (
-                updatedManifest['sap.ui5']?.['routing']?.['targets']?.['sample']?.['options'] as Record<string, any>
-            )['settings'];
-            expect(settings.content).toMatchSnapshot();
-            expect(fs.read(expectedFragmentPath)).toMatchSnapshot();
-            expect(fs.read(expectedFragmentPath.replace('.fragment.xml', '.js'))).toMatchSnapshot();
-            expect(expectedEditFragmentPath).toBeDefined();
-            expect(fs.read(expectedEditFragmentPath)).toMatchSnapshot();
-            expect(fs.read(expectedEditFragmentPath.replace('.fragment.xml', '.js'))).toMatchSnapshot();
+        testVersions.forEach((minUI5Version: string) => {
+            test(`for version ${minUI5Version}`, () => {
+                const customHeaderSection: CustomHeaderSection = {
+                    target: 'sample',
+                    name: 'NewCustomHeaderSection',
+                    folder: 'extensions/custom',
+                    title: 'New Custom Header Section',
+                    subTitle: 'Custom Header section subtitle',
+                    edit: {
+                        name: 'NewCustomHeaderSectionEdit',
+                        folder: 'extensions/custom',
+                        eventHandler: true
+                    },
+                    requestGroupId: RequestGroupId.Decoration,
+                    flexSettings: {
+                        designtime: DesignTime.Default
+                    },
+                    minUI5Version,
+                    eventHandler: true
+                };
+                const expectedFragmentPath = join(
+                    testDir,
+                    `webapp/${customHeaderSection.folder}/${customHeaderSection.name}.fragment.xml`
+                );
+                if (customHeaderSection.edit?.folder) {
+                    expectedEditFragmentPath = join(
+                        testDir,
+                        `webapp/${customHeaderSection.edit.folder}/${customHeaderSection.edit.name}.fragment.xml`
+                    );
+                }
+                generateCustomHeaderSection(testDir, { ...customHeaderSection }, fs);
+
+                const updatedManifest = fs.readJSON(join(testDir, 'webapp/manifest.json')) as Manifest;
+                const settings = (
+                    updatedManifest['sap.ui5']?.['routing']?.['targets']?.['sample']?.['options'] as Record<string, any>
+                )['settings'];
+                expect(settings.content).toMatchSnapshot();
+                expect(fs.read(expectedFragmentPath)).toMatchSnapshot();
+                expect(fs.read(expectedFragmentPath.replace('.fragment.xml', '.js'))).toMatchSnapshot();
+                expect(expectedEditFragmentPath).toBeDefined();
+                expect(fs.read(expectedEditFragmentPath)).toMatchSnapshot();
+                expect(fs.read(expectedEditFragmentPath.replace('.fragment.xml', '.js'))).toMatchSnapshot();
+            });
         });
     });
 
-    describe('template and templateEdit fragments in different folders', () => {
-        // folder in edit property differs
-        const customHeaderSection: CustomHeaderSection = {
-            target: 'sample',
-            name: 'NewCustomHeaderSection',
-            folder: 'extensions/custom',
-            title: 'New Custom Header Section',
-            subTitle: 'Custom Header section subtitle',
-            edit: {
-                name: 'NewCustomHeaderSectionEdit',
-                folder: 'extensions/custom/different',
-                eventHandler: true
-            },
-            requestGroupId: RequestGroupId.Decoration,
-            flexSettings: {
-                designtime: DesignTime.Default
-            }
-        };
+    describe('read only and edit fragments in different extension folders', () => {
         let expectedEditFragmentPath: string;
-        const expectedFragmentPath = join(
-            testDir,
-            `webapp/${customHeaderSection.folder}/${customHeaderSection.name}.fragment.xml`
-        );
-        if (customHeaderSection.edit?.folder) {
-            expectedEditFragmentPath = join(
-                testDir,
-                `webapp/${customHeaderSection.edit.folder}/${customHeaderSection.edit.name}.fragment.xml`
-            );
-        }
-        test.each(testVersions)('Versions %s', (minUI5Version) => {
-            const testCustomHeaderSection: CustomHeaderSection = {
-                ...customHeaderSection,
-                eventHandler: true,
-                minUI5Version
-            };
 
-            generateCustomHeaderSection(testDir, { ...testCustomHeaderSection }, fs);
+        beforeEach(() => {
+            const manifest = JSON.parse(JSON.stringify(manifestSections));
+            manifest['sap.ui5'].routing.targets.sample.options.settings.content.body.sections = {};
+            fs = create(createStorage());
+            fs.delete(testDir);
+            fs.write(join(testDir, 'webapp/manifest.json'), JSON.stringify(manifest));
+        });
 
-            const updatedManifest = fs.readJSON(join(testDir, 'webapp/manifest.json')) as Manifest;
-            const settings = (
-                updatedManifest['sap.ui5']?.['routing']?.['targets']?.['sample']?.['options'] as Record<string, any>
-            )['settings'];
-            expect(settings.content).toMatchSnapshot();
-            expect(fs.read(expectedFragmentPath)).toMatchSnapshot();
-            expect(fs.read(expectedFragmentPath.replace('.fragment.xml', '.js'))).toMatchSnapshot();
-            expect(expectedEditFragmentPath).toBeDefined();
-            expect(fs.read(expectedEditFragmentPath)).toMatchSnapshot();
-            expect(fs.read(expectedEditFragmentPath.replace('.fragment.xml', '.js'))).toMatchSnapshot();
+        // folder in edit property differs
+        testVersions.forEach((minUI5Version: string) => {
+            test(`for version ${minUI5Version}`, () => {
+                const customHeaderSection: CustomHeaderSection = {
+                    target: 'sample',
+                    name: 'NewCustomHeaderSection',
+                    folder: 'extensions/custom',
+                    title: 'New Custom Header Section',
+                    subTitle: 'Custom Header section subtitle',
+                    edit: {
+                        name: 'NewCustomHeaderSectionEdit',
+                        folder: 'extensions/custom/different',
+                        eventHandler: true
+                    },
+                    requestGroupId: RequestGroupId.Decoration,
+                    flexSettings: {
+                        designtime: DesignTime.Default
+                    },
+                    minUI5Version,
+                    eventHandler: true
+                };
+                const expectedFragmentPath = join(
+                    testDir,
+                    `webapp/${customHeaderSection.folder}/${customHeaderSection.name}.fragment.xml`
+                );
+                if (customHeaderSection.edit?.folder) {
+                    expectedEditFragmentPath = join(
+                        testDir,
+                        `webapp/${customHeaderSection.edit.folder}/${customHeaderSection.edit.name}.fragment.xml`
+                    );
+                }
+                generateCustomHeaderSection(testDir, { ...customHeaderSection }, fs);
+
+                const updatedManifest = fs.readJSON(join(testDir, 'webapp/manifest.json')) as Manifest;
+                const settings = (
+                    updatedManifest['sap.ui5']?.['routing']?.['targets']?.['sample']?.['options'] as Record<string, any>
+                )['settings'];
+                expect(settings.content).toMatchSnapshot();
+                expect(fs.read(expectedFragmentPath)).toMatchSnapshot();
+                expect(fs.read(expectedFragmentPath.replace('.fragment.xml', '.js'))).toMatchSnapshot();
+                expect(expectedEditFragmentPath).toBeDefined();
+                expect(fs.read(expectedEditFragmentPath)).toMatchSnapshot();
+                expect(fs.read(expectedEditFragmentPath.replace('.fragment.xml', '.js'))).toMatchSnapshot();
+            });
         });
     });
 
