7e5ab7605768b0624544bd5d4b5102ca67f63448
Nikita Baranov <nikita.baranov@sap.com>
2023-08-14 13:30:24 +0300

    chore: added --glob flag to rimraf dist

--- packages/adp-tooling/src-client/index.ts
+++ packages/adp-tooling/src-client/index.ts
@@ -3,7 +3,7 @@ import FragmentDialog from './fragment';
 export default (rta: sap.ui.rta.RuntimeAuthoring) => {
     const fragmentDialog = new FragmentDialog(rta);
     const menu = rta.getDefaultPlugins().contextMenu;
-
+    
     menu.addMenuItem({
         id: 'ADD_FRAGMENT',
         text: 'Add: Fragment',
