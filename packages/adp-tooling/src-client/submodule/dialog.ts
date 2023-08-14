66fe43beabfa2de3ff00f0c2d2676ee367c4e449
Tobias Queck <tobias.queck@sap.com>
2023-08-14 18:05:44 +0200

    fix

--- packages/adp-tooling/src-client/submodule/dialog.ts
+++ packages/adp-tooling/src-client/submodule/dialog.ts
@@ -1,5 +1,4 @@
 import MesssageBox from 'sap/m/MessageBox';
-import { ButtonType } from 'sap/m/library';
 
 export function showDialog() {
     MesssageBox.show('Hello World', { icon: MesssageBox.Icon.WARNING });
