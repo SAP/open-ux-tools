de1e458d02bc205b0310b86dd2fa0add71adaa56
Tobias Queck <tobias.queck@sap.com>
2023-08-14 08:25:26 +0200

    enhanced example to also use a submodule

--- packages/adp-tooling/src-rta/loadPlugin.ts
+++ packages/adp-tooling/src-rta/loadPlugin.ts
@@ -1,9 +1,11 @@
+import { showDialog } from "./submodule/dialog.js";
+
 export default (rta: sap.ui.rta.RuntimeAuthoring) => {
     const menu = rta.getDefaultPlugins().contextMenu;
     menu.addMenuItem({
         id: 'ADD_FRAGMENT',
         text: 'Add: Fragment',
-        handler: () => alert(),
+        handler: () => showDialog(),
         icon: 'sap-icon://attachment-html'
     });
 }
