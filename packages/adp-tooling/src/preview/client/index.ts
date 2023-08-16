52a5b2e67b623389360a47cd545989df15f52936
Tobias Queck <tobias.queck@sap.com>
2023-08-16 12:20:38 +0200

    fix the build

--- packages/adp-tooling/src/preview/client/index.ts
+++ packages/adp-tooling/src/preview/client/index.ts
@@ -1,5 +1,5 @@
 import type RuntimeAuthoring from 'sap/ui/rta/RuntimeAuthoring';
 
 export default (_rta: RuntimeAuthoring) => {
-    // add custom plugins to the RuntimeAuthoring instance here
+    // add code to load custom plugins here
 };
