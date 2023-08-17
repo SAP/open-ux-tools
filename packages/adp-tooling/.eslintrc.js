b4ac629a18d8bfb00c732f52f31aa748709c2088
Tobias Queck <tobias.queck@sap.com>
2023-08-17 15:05:29 +0200

    removal of unnessary settings after cherry pick

--- packages/adp-tooling/.eslintrc.js
+++ packages/adp-tooling/.eslintrc.js
@@ -1,16 +1,7 @@
 module.exports = {
-    'root': true,
-    'extends': ['../../.eslintrc'],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': { 'project': './tsconfig.eslint.json', 'sourceType': 'module', 'tsconfigRootDir': __dirname },
-    'rules': {
-        'import/no-unresolved': 'off', // the rules has issues with UI5 imports
-        '@typescript-eslint/no-use-before-define': 'warn',
-        '@typescript-eslint/ban-ts-comment': 'warn',
-        '@typescript-eslint/no-this-alias': 'warn',
-        'consistent-this': 'off', // that - this
-        'space-before-function-paren': 'warn',
-        'jsdoc/match-description': 'off',
-        'radix': 'off'
+    extends: ['../../.eslintrc'],
+    parserOptions: { 
+        project: './tsconfig.eslint.json', 
+        tsconfigRootDir: __dirname 
     }
 };
