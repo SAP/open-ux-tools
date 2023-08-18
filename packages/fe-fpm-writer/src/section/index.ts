41b430ad1a30b0145cdc516fc252e76f626b3434
Andis Redmans <andis.redmans@sap.com>
2023-08-18 09:30:33 +0300

    fix: bug fix with template edit
    bug fix with template edit

--- packages/fe-fpm-writer/src/section/index.ts
+++ packages/fe-fpm-writer/src/section/index.ts
@@ -132,16 +132,25 @@ export function generateCustomHeaderSection(
     customHeaderSection: CustomHeaderSection,
     fs?: Editor
 ): Editor {
+    if (!fs) {
+        fs = create(createStorage());
+    }
     const manifestRoot = getManifestRoot('header-section', customHeaderSection.minUI5Version);
-    const { editor, section } = generate(basePath, customHeaderSection, manifestRoot, fs);
-    // Get edit fragment details
     const minVersion = coerce(customHeaderSection.minUI5Version);
-    if (section.edit && (!minVersion || gte(minVersion, '1.86.0'))) {
-        const editSection: CustomElement & Partial<InternalCustomSection> = section.edit;
+    const applyTemplateEdit = customHeaderSection.edit && (!minVersion || gte(minVersion, '1.86.0'));
+    let editSection: (CustomElement & Partial<InternalCustomSection>) | undefined = undefined;
+    // Prepare 'templateEdit' - apply namespace, folder paths resolution
+    if (customHeaderSection.edit && applyTemplateEdit) {
+        editSection = customHeaderSection.edit;
         const manifestPath = join(basePath, 'webapp/manifest.json');
-        const manifest = editor.readJSON(manifestPath) as Manifest;
+        const manifest = fs.readJSON(manifestPath) as Manifest;
         // Set folder, ns and path for edit fragment
         setCommonDefaults(editSection, manifestPath, manifest);
+    }
+    // Call standard custom section generation
+    const { editor, section } = generate(basePath, customHeaderSection, manifestRoot, fs);
+    // Handle 'templateEdit' - edit fragment details
+    if (editSection && applyTemplateEdit) {
         // Apply event handler for edit fragment
         if (editSection.eventHandler) {
             editSection.eventHandler = applyEventHandlerConfiguration(editor, editSection, editSection.eventHandler, {
