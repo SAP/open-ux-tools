e082cd21c29ce1b575ca6191bc1af44126222dfa
Jimmy Joseph <jimmy.joseph@sap.com>
2023-08-21 20:38:31 +0200

    review comments

--- packages/control-property-editor/jest.config.js
+++ packages/control-property-editor/jest.config.js
@@ -1,40 +1,20 @@
-module.exports = {
-    displayName: 'control-property-editor',
-    automock: false,
-    clearMocks: true,
-    collectCoverage: true,
-    snapshotFormat: {
-        escapeString: true,
-        printBasicPrototype: true
-    },
-    collectCoverageFrom: ['src/**/*.{ts,tsx}'],
-    coverageDirectory: 'reports/test/unit/coverage',
-    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
-    errorOnDeprecated: true,
-    globals: {
-        'ts-jest': {
-            jsx: 'react',
-            diagnostics: {
-                warnOnly: true,
-                exclude: /\.(spec|test)\.ts$/
-            }
-        }
-    },
-    notify: false,
-    notifyMode: 'failure',
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    testMatch: ['**/test/unit/**/*.(test).ts(x)?'],
-    // testResultsProcessor: 'jest-sonar-reporter',
-    transform: {
-        '^.+\\.test.tsx?$': 'ts-jest',
-        '.+\\.(css|sass|scss)$': 'jest-scss-transform'
-    },
-    transformIgnorePatterns: ['<rootDir>/node_modules/'],
-    verbose: false,
-    reporters: [
-        'default',
-        'summary',
-        ['jest-junit', { outputDirectory: 'reports/test/unit', outputName: 'junit-report.xml' }]
-    ]
+const config = require('../../jest.base');
+config.testEnvironment = 'jsdom';
+config.collectCoverageFrom = ['src/**/*.{ts,tsx}'];
+config.transform = {
+    '^.+\\.test.tsx?$': 'ts-jest',
+    '.+\\.(css|sass|scss)$': 'jest-scss-transform'
 };
+config.globals = {
+    'ts-jest': {
+        jsx: 'react',
+        diagnostics: {
+            warnOnly: true,
+            exclude: /\.(spec|test)\.ts$/
+        }
+    }
+};
+config.preset = 'ts-jest';
+config.transformIgnorePatterns = ['<rootDir>/node_modules/'];
+config.testMatch = ['**/test/unit/**/*.(test).ts(x)?'];
+module.exports = config;
