d139095e1dd881a22b15b20a78e642beb44254f0
Nikita Baranov <nikita.baranov@sap.com>
2023-08-21 14:43:04 +0300

    feat: add folders for fragments and controllers

---
+++ packages/adp-tooling/src/preview/client/controllers/AddFragment.controller.ts
@@ -0,0 +1,11 @@
+import Controller from 'sap/ui/core/mvc/Controller';
+
+/**
+ * @namespace AddFragment.controller
+ */
+export default class AddFragment extends Controller {
+    /*eslint-disable @typescript-eslint/no-empty-function*/
+    public onInit(): void {
+        alert('Alert from Controller');
+    }
+}
