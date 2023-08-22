717af2751afcf5329126733ee44c9af41f09d06c
J Long <john.long@sap.com>
2023-08-22 11:47:39 +0100

    fix(deploy-tooling): Apply exclude parameter when generating zip file

--- packages/deploy-tooling/src/ui5/archive.ts
+++ packages/deploy-tooling/src/ui5/archive.ts
@@ -14,17 +14,20 @@ import { createBuffer } from '../base';
 export async function createUi5Archive(
     logger: ToolsLogger,
     workspace: DuplexCollection,
-    projectName: string
+    projectName: string,
+    exclude: string[] = []
 ): Promise<Buffer> {
     logger.info('Creating archive with UI5 build result.');
     const prefix = `/resources/${projectName}/`;
     const zip = new ZipFile();
     const resources = await workspace.byGlob(`${prefix}**/*`);
     for (const resource of resources) {
-        const path = resource.getPath().replace(prefix, '');
-        logger.debug(`Adding ${path}`);
-        const buffer = await resource.getBuffer();
-        zip.addBuffer(buffer, path);
+        if (!exclude.some((regex) => resource.getPath().match(regex))) {
+            const path = resource.getPath().replace(prefix, '');
+            logger.debug(`Adding ${path}`);
+            const buffer = await resource.getBuffer();
+            zip.addBuffer(buffer, path);
+        }
     }
     logger.info('Archive created.');
     return createBuffer(zip);
