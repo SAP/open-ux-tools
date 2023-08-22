af84909dfb76b388c3c3d0f9d265fa6ab5abf32c
Nikita Baranov <nikita.baranov@sap.com>
2023-08-22 16:42:10 +0300

    refactor: change initializing of a dialog

--- packages/adp-tooling/src/preview/client/index.ts
+++ packages/adp-tooling/src/preview/client/index.ts
@@ -1,10 +1,15 @@
 import FragmentDialog from './dialogs/fragment';
 import type RuntimeAuthoring from 'sap/ui/rta/RuntimeAuthoring';
+import type { BaseDialog } from './dialogs/base';
 
 export default (rta: RuntimeAuthoring) => {
     const contextMenu = rta.getDefaultPlugins().contextMenu;
 
     const fragmentDialog = new FragmentDialog(rta);
 
-    fragmentDialog.init(contextMenu);
+    const dialogs: BaseDialog[] = [fragmentDialog];
+
+    for (const menuItem of dialogs) {
+        menuItem.init(contextMenu);
+    }
 };
