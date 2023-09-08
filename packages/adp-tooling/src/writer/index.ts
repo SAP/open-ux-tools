24bd8dbaa8124889d01dd84ba53557dd7ff94384
Tobias Queck <tobias.queck@sap.com>
2023-09-08 16:49:41 +0200

    updated the adp template for the new middleware config

--- packages/adp-tooling/src/writer/index.ts
+++ packages/adp-tooling/src/writer/index.ts
@@ -58,7 +58,15 @@ export async function generate(basePath: string, config: AdpWriterConfig, fs?: E
                 adp: {
                     target: fullConfig.target,
                     ignoreCertErrors: false
-                } as AdpPreviewConfig
+                } as AdpPreviewConfig,
+                rta: {
+                    editors: [
+                        {
+                            path: 'local/editor.html',
+                            developerMode: true
+                        }
+                    ]
+                }
             }
         },
         {
