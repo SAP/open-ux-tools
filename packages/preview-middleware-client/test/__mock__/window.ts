cd2205868f0f8f536c92dd279133a2c55e39c7c6
Tobias Queck <tobias.queck@sap.com>
2023-09-08 08:01:06 +0200

    global sap mocking

--- packages/preview-middleware-client/test/__mock__/window.ts
+++ packages/preview-middleware-client/test/__mock__/window.ts
@@ -1,3 +1,12 @@
 export const fetchMock = jest.fn();
+export const sapCoreMock = {
+    byId: jest.fn()
+};
+export const sapMock = {
+    ui: {
+        getCore: jest.fn().mockReturnValue(sapCoreMock)
+    }
+};
 
 window.fetch = fetchMock;
+window.sap = sapMock as unknown as typeof sap;
