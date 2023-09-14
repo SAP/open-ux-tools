3d381300c24e524124e625154029f2a28cf389e8
Tobias Queck <tobias.queck@sap.com>
2023-09-14 18:15:32 +0200

    fix test

--- packages/preview-middleware-client/test/__mock__/sap/ui/rta/RuntimeAuthoring.ts
+++ packages/preview-middleware-client/test/__mock__/sap/ui/rta/RuntimeAuthoring.ts
@@ -3,7 +3,7 @@ import type RuntimeAuthoring from 'sap/ui/rta/RuntimeAuthoring';
 const RuntimeAuthoringMock = {
     getService: jest.fn(),
     getCommandStack: jest.fn(),
-    getFlexSettings: jest.fn()
+    getFlexSettings: jest.fn().mockReturnValue({})
 };
 
 export default RuntimeAuthoringMock as unknown as RuntimeAuthoring & typeof RuntimeAuthoringMock;
