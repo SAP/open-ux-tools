e8ffa1a160da19fde3df787bf28f805199b3a472
Tobias Queck <tobias.queck@sap.com>
2023-09-14 11:07:42 +0200

    fix: use flexible approach for telemetry

--- packages/control-property-editor-common/src/telemetry.ts
+++ packages/control-property-editor-common/src/telemetry.ts
@@ -4,25 +4,39 @@ interface TelemetryData {
     controlName?: string;
 }
 
+const path = '/preview/api/telemetry';
+let enabled: boolean;
+
+async function isEnabled() {
+    if (enabled === undefined) {
+        const response = await fetch(path, {method: 'HEAD'});
+        enabled = response.status === 200;
+    }
+    return enabled;
+}
+
 /**
  * Reports telemetry data from Control Property Editor.
  *
  * @param data The TelemetryData object, that needs to be reported
  */
-export function reportTelemetry(data: TelemetryData) {
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    };
-    fetch('./control-property-editor/report-telemetry', requestOptions)
-        .then(async (response) => {
-            console.log(response);
-        })
-        .catch((error) => {
-            console.error('There was an error!', error);
-        });
+export async function reportTelemetry(data: TelemetryData) {
+    try {
+        if (await isEnabled()) {
+            const requestOptions = {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            };
+
+            await fetch(path, requestOptions);
+
+        }
+    } catch (_error) {
+        // something is wrong with the telemetry service
+        enabled = false;
+    }
 }
