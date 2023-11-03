71666b5dc007a169e90e3d03939083b6f784199c
C5226337 <davis.voicescuks@sap.com>
2023-11-03 11:55:39 +0200

    refactor: inefficient regex

--- packages/control-property-editor-common/test/unit/utils.test.ts
+++ packages/control-property-editor-common/test/unit/utils.test.ts
@@ -3,18 +3,30 @@ import { convertCamelCaseToPascalCase } from '../../src/utils';
 describe('utils', () => {
     describe('convertCamelCaseToPascalCase', () => {
         test('cases', () => {
-            expect(convertCamelCaseToPascalCase('busy')).toMatchInlineSnapshot(`"Busy"`);
+            expect(convertCamelCaseToPascalCase('busy')).toStrictEqual('Busy');
         });
         test('two words', () => {
-            expect(convertCamelCaseToPascalCase('hAlign')).toMatchInlineSnapshot(`"H Align"`);
+            expect(convertCamelCaseToPascalCase('hAlign')).toStrictEqual('H Align');
+        });
+        test('setFlexExtensionPointEnabled', () => {
+            expect(convertCamelCaseToPascalCase('setFlexExtensionPointEnabled')).toStrictEqual(
+                'Set Flex Extension Point Enabled'
+            );
+        });
+        test('addXMLAtExtensionPoint', () => {
+            expect(convertCamelCaseToPascalCase('addXMLAtExtensionPoint')).toStrictEqual('Add XML At Extension Point');
         });
         test('three words', () => {
-            expect(convertCamelCaseToPascalCase('ariaHasPopup')).toMatchInlineSnapshot(`"Aria Has Popup"`);
+            expect(convertCamelCaseToPascalCase('ariaHasPopup')).toStrictEqual('Aria Has Popup');
         });
-        test('abreviated words', () => {
-            expect(convertCamelCaseToPascalCase('addXMLhasChangeFilesWITHspaceNOw')).toMatchInlineSnapshot(
-                `"Add XMLhas Change Files WITHspace NOw"`
-            );
+        test('test-hyphen', () => {
+            expect(convertCamelCaseToPascalCase('test-hyphen')).toStrictEqual('Test Hyphen');
+        });
+        test('test_underscore', () => {
+            expect(convertCamelCaseToPascalCase('test_underscore')).toStrictEqual('Test Underscore');
+        });
+        test('test space', () => {
+            expect(convertCamelCaseToPascalCase('test space')).toStrictEqual('Test Space');
         });
     });
 });
