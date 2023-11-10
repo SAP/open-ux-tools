bee7c2984d5e60d165c0dd997973c4f960ad6b95
i501270 <nafees.ahmed01@sap.com>
2023-11-10 22:18:55 +0100

    fix: improve test coverage results

--- packages/odata-vocabularies/jest.config.js
+++ packages/odata-vocabularies/jest.config.js
@@ -9,5 +9,5 @@ config.modulePathIgnorePatterns.push('<rootDir>/test/test-output');
 config.modulePathIgnorePatterns.push('<rootDir>/templates');
 config.testPathIgnorePatterns = ['<rootDir>/node_modules/', '<rootDir>/dist/'];
 config.reporters.push('summary');
-config.collectCoverageFrom = ['**/*.ts', '!**/index.ts', '!**/tools/update.ts']; // ignoring index and update vocabulary file, index has only export statements and update is used to update the vocabularies (utility for updating not a deliverable code)
+config.collectCoverageFrom.push('!**/index.ts', '!**/tools/update.ts'); // ignoring index and update vocabulary file, index has only export statements and update is used to update the vocabularies (utility for updating not a deliverable code)
 module.exports = config;
