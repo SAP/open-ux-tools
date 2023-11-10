994cb97db0fc9a42058f1a067865b5ed827519cd
i501270 <nafees.ahmed01@sap.com>
2023-11-10 18:52:09 +0100

    fix: improve test coverage results

--- packages/odata-vocabularies/jest.config.js
+++ packages/odata-vocabularies/jest.config.js
@@ -4,10 +4,10 @@ config.globals = {
         tsconfig: 'tsconfig-test.json'
     }
 };
-// config.testResultsProcessor =  'jest-sonar-reporter';
-//config.coverageDirectory.push('reports/test/unit/coverage');
+config.testRegex = 'test/.*\\.test\\.ts$';
 config.modulePathIgnorePatterns.push('<rootDir>/test/test-output');
 config.modulePathIgnorePatterns.push('<rootDir>/templates');
 config.testPathIgnorePatterns = ['<rootDir>/node_modules/', '<rootDir>/dist/'];
 config.reporters.push('summary');
+config.collectCoverageFrom = ['**/*.ts', '!**/index.ts', '!**/tools/update.ts']; // ignoring index and update vocabulary file, index has only export statements and update is used to update the vocabularies (utility for updating not a deliverable code)
 module.exports = config;
