f3ca9f1d6fb1d4428504a34eea5728638c974289
i501270 <nafees.ahmed01@sap.com>
2023-11-13 13:50:46 +0100

    fix: add ignored file for test coverage

--- packages/odata-vocabularies/jest.config.js
+++ packages/odata-vocabularies/jest.config.js
@@ -8,6 +8,6 @@ config.testRegex = 'test/.*\\.test\\.ts$';
 config.modulePathIgnorePatterns.push('<rootDir>/test/test-output');
 config.modulePathIgnorePatterns.push('<rootDir>/templates');
 config.testPathIgnorePatterns = ['<rootDir>/node_modules/', '<rootDir>/dist/'];
-config.collectCoverageFrom.push('!src/**/index.ts', 'tools/update.ts'); // ignoring index and update vocabulary file, index has only export statements and update is used to update the vocabularies (utility for updating not a deliverable code)
+config.collectCoverageFrom.push('!src/**/index.ts', 'tools/update.ts', '!tools/run-update.ts'); // ignoring index and update vocabulary file, index has only export statements and update is used to update the vocabularies (utility for updating not a deliverable code)
 config.reporters.push('summary');
 module.exports = config;
