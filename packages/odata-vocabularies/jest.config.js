3907f41a18e6ba9d86c15f65c35854bceed680bd
i501270 <nafees.ahmed01@sap.com>
2023-11-13 11:16:42 +0100

    fix: include ignored files

--- packages/odata-vocabularies/jest.config.js
+++ packages/odata-vocabularies/jest.config.js
@@ -7,12 +7,7 @@ config.globals = {
 config.testRegex = 'test/.*\\.test\\.ts$';
 config.modulePathIgnorePatterns.push('<rootDir>/test/test-output');
 config.modulePathIgnorePatterns.push('<rootDir>/templates');
-config.testPathIgnorePatterns = [
-    '<rootDir>/node_modules/',
-    '<rootDir>/dist/',
-    '<rootDir>/src/tools/update.ts',
-    '<rootDir>/src/index.ts'
-];
+config.testPathIgnorePatterns = ['<rootDir>/node_modules/', '<rootDir>/dist/'];
 //config.collectCoverageFrom.push('!src/**/index.ts', '!src/tools/update.ts'); // ignoring index and update vocabulary file, index has only export statements and update is used to update the vocabularies (utility for updating not a deliverable code)
 config.reporters.push('summary');
 module.exports = config;
