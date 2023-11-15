998a43edf57cd8247b8ab33e27974def6bf0c0b2
Tobias Queck <tobias.queck@sap.com>
2023-11-15 13:58:21 +0100

    support fiori tools middlewares

--- packages/adp-tooling/test/unit/writer/index.test.ts
+++ packages/adp-tooling/test/unit/writer/index.test.ts
@@ -41,5 +41,42 @@ describe('ADP writer', () => {
             await generate(projectDir, config, fs);
             expect(fs.dump(projectDir)).toMatchSnapshot();
         });
+
+        test('add deploy config', async () => {
+            const projectDir = join(outputDir, 'deploy');
+            await generate(
+                projectDir,
+                {
+                    ...config,
+                    deploy: {
+                        package: '$TMP'
+                    }
+                },
+                fs
+            );
+            expect(
+                fs.dump(projectDir, (file) => ['package.json', 'ui5-deploy.yaml'].includes(file.basename))
+            ).toMatchSnapshot();
+        });
+
+        test('enable Fiori tools', async () => {
+            const projectDir = join(outputDir, 'fiori-tools');
+            await generate(
+                projectDir,
+                {
+                    ...config,
+                    deploy: {
+                        package: '$TMP'
+                    },
+                    options: {
+                        fioriTools: true
+                    }
+                },
+                fs
+            );
+            expect(
+                fs.dump(projectDir, (file) => ['package.json', 'ui5.yaml', 'ui5-deploy.yaml'].includes(file.basename))
+            ).toMatchSnapshot();
+        });
     });
 });
