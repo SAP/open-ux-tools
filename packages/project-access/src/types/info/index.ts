f9a60d3cda0d44677d5a1d08825a9fe71cfd288a
Klaus Keller <klaus.keller@sap.com>
2023-11-16 14:37:20 -0800

    feat: project structure proposal

--- packages/project-access/src/types/info/index.ts
+++ packages/project-access/src/types/info/index.ts
@@ -1,1 +1,32 @@
+import type { ManifestNamespace } from '../webapp';
+
 export type AppProgrammingLanguage = 'JavaScript' | 'TypeScript' | '';
+
+export interface Project {
+    root: string;
+    apps: { [index: string]: ApplicationStructure };
+    type: 'Cap' | 'Edmx'; // backward compatibility, please use detailedType
+    detailedType: DetailedProjectType;
+}
+
+export interface ApplicationStructure {
+    appRoot: string;
+    projectRelative: string;
+    manifest: string;
+    changes: string;
+    mainService: string;
+    services: { [index: string]: ServiceSpecification };
+    annotations?: { [serviceIndex: string]: string[] };
+}
+
+export interface ServiceSpecification {
+    uri?: string;
+    local?: string;
+    odataVersion?: ManifestNamespace.Setting['odataVersion'];
+    annotations?: {
+        uri?: string;
+        local?: string;
+    };
+}
+
+export type DetailedProjectType = 'EDMX Backend' | 'CAP Node.js' | 'CAP Java';
