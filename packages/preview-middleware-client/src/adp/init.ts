9d6bc026cd4478be91a361574d4292ce7216f473
Georgi Damyanov <georgi.damyanov@sap.com>
2023-11-28 20:45:10 +0200

    send action after cpe is initialized

--- packages/preview-middleware-client/src/adp/init.ts
+++ packages/preview-middleware-client/src/adp/init.ts
@@ -3,7 +3,11 @@ import type RuntimeAuthoring from 'sap/ui/rta/RuntimeAuthoring';
 
 import init from '../cpe/init';
 import { initDialogs } from './init-dialogs';
-import { ExternalAction, showMessage, startPostMessageCommunication } from '@sap-ux-private/control-property-editor-common';
+import {
+    ExternalAction,
+    showMessage,
+    startPostMessageCommunication
+} from '@sap-ux-private/control-property-editor-common';
 import { ActionHandler } from '../cpe/types';
 import ExtensionPointService from './extension-point';
 import VersionInfo from 'sap/ui/VersionInfo';
@@ -20,31 +24,31 @@ export default async function (rta: RuntimeAuthoring) {
         actionHandlers.push(handler);
     }
 
-    const { sendAction } = startPostMessageCommunication<ExternalAction>(window.parent, async function onAction(action) {
-        for (const handler of actionHandlers) {
-            try {
-                await handler(action);
-            } catch (error) {
-                log.error('Handler Failed: ', error);
+    const { sendAction } = startPostMessageCommunication<ExternalAction>(
+        window.parent,
+        async function onAction(action) {
+            for (const handler of actionHandlers) {
+                try {
+                    await handler(action);
+                } catch (error) {
+                    log.error('Handler Failed: ', error);
+                }
             }
         }
-    });
-    
-    const ui5Version = (await VersionInfo.load()) as { version: string };
-    const ui5VersionValidationMsg = getUI5VersionValidationMessage(ui5Version.version);
+    );
 
-    if(ui5VersionValidationMsg) {
-        sendAction(showMessage(ui5VersionValidationMsg));
-    }
-
-    
     // initialize fragment content menu entry
     initDialogs(rta);
     // initialize extension point service
     extPointService.init(subscribe);
     // also initialize the editor
     init(rta);
+
+    const ui5Version = (await VersionInfo.load()) as { version: string };
+    const ui5VersionValidationMsg = getUI5VersionValidationMessage(ui5Version.version);
+
+    if (ui5VersionValidationMsg) {
+        sendAction(showMessage(ui5VersionValidationMsg));
+    }
     log.debug('ADP init executed.');
 }
-
-
