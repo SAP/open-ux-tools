eae7efd106d88175babcf5e5cff2583ded497467
Klaus Keller <klaus.keller@sap.com>
2023-12-19 17:24:15 +0100

    fix: multi  version of modules

--- docs/tools-offline-instructions/script/deps-to-command.mjs
+++ docs/tools-offline-instructions/script/deps-to-command.mjs
@@ -3,7 +3,8 @@ import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const pathToDependenciesJson = join(__dirname, '..', 'dependencies.json');
+const pathToGlobalModules = join(__dirname, '../info/globalModules.json');
+const pathToProjectModules = join(__dirname, '../info/projectModules.json');
 const pathToReadmeMd = join(__dirname, '..', 'README.md');
 
 const regexp = new RegExp(/(?<=```shell\nnpm install ).*?(?= --registry=http:\/\/localhost:4873\/\n```)/);
@@ -12,7 +13,17 @@ const readFile = async (path) => promises.readFile(path, options);
 const writeFile = async (path, data) => promises.writeFile(path, data, options);
 const depsToString = (d) =>
     Object.keys(d)
-        .map((name) => `${name}@${d[name]}`)
+        .map((name) => {
+            if (Array.isArray(d[name])) {
+                if (d[name].length === 1) {
+                    return `${name}@${d[name][0]}`;
+                } else {
+                    return d[name].map((v) => `${(name + v).replace(/[@/^~]/g, '-')}@npm:${name}@${v}`).join(' ');
+                }
+            } else {
+                return `${name}@${d[name]}`;
+            }
+        })
         .join(' ');
 
 /**
@@ -20,8 +31,11 @@ const depsToString = (d) =>
  */
 async function updateReadMe() {
     try {
-        const dependencies = JSON.parse(await readFile(pathToDependenciesJson));
-        const dependenciesString = depsToString(dependencies.node_modules);
+        const dependencies = {
+            ...JSON.parse(await readFile(pathToGlobalModules)),
+            ...JSON.parse(await readFile(pathToProjectModules))
+        };
+        const dependenciesString = depsToString(dependencies);
         console.log(`\nUpdating dependencies in '${pathToReadmeMd}' with \n\n${dependenciesString}\n\n`);
         const markdown = await readFile(pathToReadmeMd);
         const newMarkdown = markdown.replace(regexp, dependenciesString);
