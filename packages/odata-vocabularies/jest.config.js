121d9ed52c537f500c36357938ade8b75793b55f
Davis <davis.voicescuks@sap.com>
2024-01-10 12:04:34 +0200

    chore: update annotation core

--- packages/odata-vocabularies/jest.config.js
+++ packages/odata-vocabularies/jest.config.js
@@ -1,5 +1,3 @@
 const config = require('../../jest.base');
-config.modulePathIgnorePatterns.push('<rootDir>/test/test-output');
-config.modulePathIgnorePatterns.push('<rootDir>/templates');
 config.collectCoverageFrom.push('!src/**/index.ts', 'tools/update.ts', '!tools/run-update.ts'); // ignoring index and update vocabulary file, index has only export statements and update is used to update the vocabularies (utility for updating not a deliverable code)
 module.exports = config;
