26bc1173b72feec2f499a54ed16fd0f886827bc6
Nikita Baranov <nikmacentosh@gmail.com>
2024-01-24 13:26:11 +0200

    feat: add fake connector implementation

--- packages/preview-middleware-client/src/flp/FakeLrepConnector.ts
+++ packages/preview-middleware-client/src/flp/FakeLrepConnector.ts
@@ -1,30 +1,99 @@
 import FakeLrepConnector from 'sap/ui/fl/FakeLrepConnector';
+import LrepConnector from 'sap/ui/fl/LrepConnector';
+
+interface FlexChange {
+    [key: string]: string | object | undefined;
+    support: {
+        generator?: string;
+    };
+}
+
+interface FetchedChanges {
+    [key: string]: FlexChange;
+}
+
+interface LoadChangesResult {
+    changes: {
+        loadModules: boolean;
+        changes: FlexChange[];
+        settings: {
+            [key: string]: string | boolean | undefined;
+        };
+    };
+    componentClassName: string;
+    etag: string;
+    loadModules: boolean;
+    messagebundle: string | undefined;
+}
 
 const path = '/preview/api/changes';
 
-// @ts-ignore
-jQuery.extend(FakeLrepConnector.prototype, {
-    loadChanges: async function () {
-        const response = await fetch(path, {
-            method: 'GET',
-            headers: {
-                'content-type': 'application/json'
-            }
-        });
-        const changes = await response.json();
-        return changes;
-    },
-    loadSettings: function () {
-        return Promise.resolve();
+function getFlexSettings() {
+    let result;
+    const bootstrapConfig = document.getElementById('sap-ui-bootstrap');
+    const flexSetting = bootstrapConfig?.getAttribute('data-open-ux-preview-flex-settings');
+    if (flexSetting) {
+        result = JSON.parse(flexSetting);
     }
-});
+    return result;
+}
 
-FakeLrepConnector.enableFakeConnector();
-// @ts-ignore
-FakeLrepConnector.loadFlexData = async function () {
-    return { variants: [] };
-};
+export async function create(changeArr: FlexChange[]) {
+    const settings = getFlexSettings();
+    await Promise.all(
+        changeArr.map(async (change) => {
+            if (settings) {
+                change.support ??= {};
+                change.support.generator = settings.generator;
+            }
 
-export default FakeLrepConnector;
-// // @ts-ignore
-// sap.ui.fl.LrepConnector.loadFlexData = () => {};
+            await fetch(path, {
+                method: 'POST',
+                body: JSON.stringify(change, null, 2),
+                headers: {
+                    'content-type': 'application/json'
+                }
+            });
+        })
+    );
+}
+
+export async function loadChanges() {
+    const lrep = new LrepConnector();
+
+    const response = await fetch(path, {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json'
+        }
+    });
+    const changes = (await response.json()) as FetchedChanges;
+
+    return LrepConnector.prototype.loadChanges
+        .apply(lrep, arguments as unknown as any)
+        .then((res: LoadChangesResult) => {
+            const arr: FlexChange[] = [];
+            Object.entries(changes).forEach(([_, val]) => {
+                arr.push(val);
+            });
+            res.changes.changes = arr;
+            return res;
+        });
+}
+
+export default function () {
+    // @ts-ignore
+    const version = sap.ui.version;
+    const minor = version.split(/[.-]/)[1];
+
+    if (minor < 72) {
+        // @ts-ignore
+        jQuery.extend(FakeLrepConnector.prototype, {
+            create,
+            loadChanges,
+            loadSettings: () => Promise.resolve()
+        });
+
+        FakeLrepConnector.enableFakeConnector();
+    }
+}
