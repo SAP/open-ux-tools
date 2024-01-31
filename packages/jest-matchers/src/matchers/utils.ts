c35b8f37dd172a2a9db0c2155eb4e5f008831f60
Cian Morrin <cian.morrin@sap.com>
2024-01-31 17:30:42 +0000

    feat(jest-matchers): remove old module

--- packages/jest-matchers/src/matchers/utils.ts
+++
@@ -1,6 +0,0 @@
-export function extractMessage({ getMessage, messages }: { getMessage?: () => string; messages: string[] }): void {
-    const message = getMessage && getMessage();
-    if (message) {
-        messages.push(message);
-    }
-}
--- packages/jest-matchers/src/setup/index.ts
+++
@@ -1,7 +0,0 @@
-import * as matchers from '../matchers';
-
-if (expect !== undefined) {
-    expect.extend(matchers);
-} else {
-    throw new Error("Cannot find Jest's global expect. Check if you have correctly installed the matchers");
-}
