6c5b2723ef0145ec5a532d4407b039d57f5267a3
Andis Redmans <andis.redmans@sap.com>
2024-02-01 20:46:38 +0200

    changelog
    changelog

---
+++ .changeset/early-moles-jam.md
@@ -0,0 +1,5 @@
+---
+'@sap-ux/ui-components': patch
+---
+
+fix: Allow to pass custom `data-*` attributes for `UISections`, `UISection`, `UIToolbar` and `UIToolbarColumn` components.
--- packages/ui-components/test/unit/components/UIToolbar.test.tsx
+++ packages/ui-components/test/unit/components/UIToolbar.test.tsx
@@ -1,14 +1,17 @@
 import { render } from '@testing-library/react';
 import * as React from 'react';
 import { UIToolbar, UIToolbarColumn } from '../../../src/components';
+import { mockResizeObserver } from '../../utils/utils';
+
+mockResizeObserver();
 
 describe('<UIToolbar />', () => {
     const selectors = {
-        toolbar: '.toolbar',
+        toolbar: '.ui-toolbar',
         column: '.ui-toolbar__column'
     };
     it('Render', () => {
-        const { container } = render(
+        render(
             <UIToolbar>
                 <UIToolbarColumn>
                     <div>Left</div>
@@ -19,13 +22,13 @@ describe('<UIToolbar />', () => {
             </UIToolbar>
         );
 
-        expect(container.querySelectorAll(selectors.toolbar).length).toEqual(1);
-        expect(container.querySelectorAll(selectors.column).length).toEqual(2);
+        expect(document.querySelectorAll(selectors.toolbar).length).toEqual(1);
+        expect(document.querySelectorAll(selectors.column).length).toEqual(2);
     });
 
     it('Render with custom attributes', () => {
         const testValue = 'test value';
-        const { container } = render(
+        render(
             <UIToolbar data-test={testValue}>
                 <UIToolbarColumn data-test2={testValue}>
                     <div>Left</div>
@@ -36,7 +39,7 @@ describe('<UIToolbar />', () => {
             </UIToolbar>
         );
 
-        expect(container.querySelector(`${selectors.toolbar} [data-test="test value"]`)).not.toEqual(null);
-        expect(container.querySelector(`${selectors.column} [data-test2="test value"]`)).not.toEqual(null);
+        expect(document.querySelector(`${selectors.toolbar}[data-test="test value"]`)).not.toEqual(null);
+        expect(document.querySelector(`${selectors.column}[data-test2="test value"]`)).not.toEqual(null);
     });
 });
