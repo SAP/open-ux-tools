93dd2c199c04a0f5b1e31e68eaa09548213d7878
Maruf Rasully <100434800+marufrasully@users.noreply.github.com>
2024-02-01 16:08:14 +0100

    fix: consume i18n in project acceess

--- packages/project-access/src/project/i18n/index.ts
+++ packages/project-access/src/project/i18n/index.ts
@@ -1,1 +1,29 @@
 export { getI18nPropertiesPaths } from './i18n';
+
+export { getI18nBundles, getCapI18nFolderNames } from './read';
+export {
+    createCapI18nEntries,
+    createManifestI18nEntries,
+    createUI5I18nEntries,
+    createAnnotationI18nEntries
+} from './write';
+
+export {
+    getI18nFolderNames,
+    getI18nDocumentation,
+    getI18nMaxLength,
+    getI18nTextType,
+    extractI18nKey,
+    getI18nUniqueKey,
+    convertToCamelCase,
+    convertToPascalCase,
+    printPropertiesI18nEntry,
+    printPropertiesI18nAnnotation,
+    csvToI18nBundle,
+    jsonToI18nBundle,
+    propertiesToI18nEntry,
+    createCapI18nEntries as createCdsI18nEntries,
+    createPropertiesI18nEntries,
+    getCapI18nBundle,
+    getPropertiesI18nBundle
+} from '@sap-ux/i18n';
---
+++ packages/project-access/src/types/i18n/index.ts
@@ -0,0 +1,13 @@
+export type {
+    // CdsEnvironment,
+    CdsI18nConfiguration,
+    CdsI18nEnv,
+    I18nAnnotation,
+    I18nBundle,
+    I18nEntry,
+    NewI18nEntry,
+    SapTextType,
+    ValueNode
+} from '@sap-ux/i18n';
+
+export { SapShortTextType, SapLongTextType } from '@sap-ux/i18n';
