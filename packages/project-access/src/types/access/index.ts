dbd827bc41447a3e02d372a23eb6085c4f6fc8d8
Klaus Keller <klaus.keller@sap.com>
2024-02-05 12:29:02 +0100

    feat: app and project access with unit tests

--- packages/project-access/src/types/access/index.ts
+++ packages/project-access/src/types/access/index.ts
@@ -1,17 +1,26 @@
 import type { I18nBundles, NewI18nEntry } from '../i18n';
-import type { I18nPropertiesPaths } from '../info';
+import type { ApplicationStructure, I18nPropertiesPaths, Project, ProjectType } from '../info';
 
-export interface ApplicationAccess {
-    createAnnotationI18nEntries: (newEntries: NewI18nEntry[]) => Promise<boolean>;
-    createUI5I18nEntries(newEntries: NewI18nEntry[], modelKey?: string): Promise<boolean>;
-    createManifestI18nEntries: (newEntries: NewI18nEntry[]) => Promise<boolean>;
-    createCapI18nEntries: (filePath: string, newI18nEntries: NewI18nEntry[]) => Promise<boolean>;
-    getCapI18nFolderNames: () => Promise<string[]>;
-    getI18nBundles: () => Promise<I18nBundles>;
-    getI18nPropertiesPaths: () => Promise<I18nPropertiesPaths>;
+interface BaseAccess {
+    readonly project: Project;
+    readonly root: string;
+    readonly projectType: ProjectType;
 }
 
-export interface ProjectAccess {
+export interface ApplicationAccess extends BaseAccess {
+    readonly app: ApplicationStructure;
+    createAnnotationI18nEntries(newEntries: NewI18nEntry[]): Promise<boolean>;
+    createUI5I18nEntries(newEntries: NewI18nEntry[], modelKey?: string): Promise<boolean>;
+    createManifestI18nEntries(newEntries: NewI18nEntry[]): Promise<boolean>;
+    createCapI18nEntries(filePath: string, newI18nEntries: NewI18nEntry[]): Promise<boolean>;
+    getAppId(): string;
+    getAppRoot(): string;
+    getCapI18nFolderNames(): Promise<string[]>;
+    getI18nBundles(): Promise<I18nBundles>;
+    getI18nPropertiesPaths(): Promise<I18nPropertiesPaths>;
+}
+
+export interface ProjectAccess extends BaseAccess {
     getApplicationIds: () => string[];
     getApplication: (appId: string) => ApplicationAccess;
 }
