6f5ad4e1332bd84a06e63376cca67d200538ac2d
Maruf Rasully <100434800+marufrasully@users.noreply.github.com>
2024-02-13 08:13:57 +0100

    chore: remove reexport

--- packages/project-access/src/project/i18n/index.ts
+++ packages/project-access/src/project/i18n/index.ts
@@ -7,23 +7,3 @@ export {
     createAnnotationI18nEntries,
     createCapI18nEntries
 } from './write';
-
-export {
-    getI18nFolderNames,
-    getI18nDocumentation,
-    getI18nMaxLength,
-    getI18nTextType,
-    extractI18nKey,
-    getI18nUniqueKey,
-    convertToCamelCase,
-    convertToPascalCase,
-    printPropertiesI18nEntry,
-    printPropertiesI18nAnnotation,
-    csvToI18nBundle,
-    jsonToI18nBundle,
-    propertiesToI18nEntry,
-    createCapI18nEntries as createCdsI18nEntries,
-    createPropertiesI18nEntries,
-    getCapI18nBundle,
-    getPropertiesI18nBundle
-} from '@sap-ux/i18n';
--- packages/project-access/src/project/index.ts
+++ packages/project-access/src/project/index.ts
@@ -12,30 +12,13 @@ export {
 } from './cap';
 export { getNodeModulesPath } from './dependencies';
 export {
-    convertToCamelCase,
-    convertToPascalCase,
     createAnnotationI18nEntries,
-    createCdsI18nEntries,
     createManifestI18nEntries,
     createUI5I18nEntries,
     createCapI18nEntries,
-    createPropertiesI18nEntries,
-    csvToI18nBundle,
-    extractI18nKey,
     getI18nBundles,
     getCapI18nFolderNames,
-    getI18nPropertiesPaths,
-    getI18nFolderNames,
-    getI18nDocumentation,
-    getI18nMaxLength,
-    getI18nTextType,
-    getI18nUniqueKey,
-    getCapI18nBundle,
-    getPropertiesI18nBundle,
-    printPropertiesI18nEntry,
-    jsonToI18nBundle,
-    printPropertiesI18nAnnotation,
-    propertiesToI18nEntry
+    getI18nPropertiesPaths
 } from './i18n';
 export { getAppProgrammingLanguage, getAppType, getProject, getProjectType } from './info';
 export { loadModuleFromProject } from './module-loader';
