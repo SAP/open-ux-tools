120f686bd8b654f810188cf577c52bb36e6e121a
broksy <ainis.broks@sap.com>
2024-02-14 11:17:42 +0200

    fix: code smell

--- packages/fe-fpm-writer/src/page/common.ts
+++ packages/fe-fpm-writer/src/page/common.ts
@@ -113,11 +113,11 @@ export function getManifestJsonExtensionHelper(
             case 'routes':
                 const routes = value as ManifestNamespace.Route[];
                 routes.push({
-                    name: config.id || `${config.entity}${config.name}`,
+                    name: config.id ?? `${config.entity}${config.name}`,
                     pattern: generateRoutePattern(routes, config.entity, config.navigation),
                     target: generateRouteTarget(
                         routes,
-                        config.id || `${config.entity}${config.name}`,
+                        config.id ?? `${config.entity}${config.name}`,
                         config.fcl,
                         config.navigation
                     )
