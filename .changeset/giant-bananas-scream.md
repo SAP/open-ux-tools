9b62f5a1dd8558afb919e0f35b4a7093dfcbae48
broksy <122096996+broksy@users.noreply.github.com>
2024-02-16 12:03:58 +0200

    feat: Optional ID parameter in page templates (#1663)
    * feat: use of optional id in object page template
    
    * Linting auto fix commit
    
    * feat: changeset
    
    * feat: id for list
    
    * feat: id for custom page
    
    * feat: routes
    
    * Linting auto fix commit
    
    * fix: code smell
    
    * feat: route id
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ .changeset/giant-bananas-scream.md
@@ -0,0 +1,5 @@
+---
+'@sap-ux/fe-fpm-writer': patch
+---
+
+Added optional `id` property to use as page key in manifest templates
--- packages/fe-fpm-writer/test/unit/page/list.test.ts
+++ packages/fe-fpm-writer/test/unit/page/list.test.ts
@@ -62,6 +62,20 @@ describe('ListReport', () => {
             expect(fs.readJSON(join(target, 'webapp/manifest.json'))).toMatchSnapshot();
         });
 
+        test('minimal input, plus optional page id', () => {
+            const target = join(testDir, 'minimal-input');
+            fs.write(join(target, 'webapp/manifest.json'), testAppManifest);
+            const minInput = {
+                ...minimalInput,
+                id: 'DummyPage'
+            };
+            const testApiData = JSON.parse(JSON.stringify(minInput));
+            //act
+            generate(target, testApiData, fs);
+            //check
+            expect(fs.readJSON(join(target, 'webapp/manifest.json'))).toMatchSnapshot();
+        });
+
         test('all optional settings used', () => {
             const target = join(testDir, 'all-settings');
             fs.write(join(target, 'webapp/manifest.json'), testAppManifest);
