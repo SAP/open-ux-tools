035fffba17b45c0753eb00e8fdf3cef3e81a1fff
i057546 <iain.o-farrell@sap.com>
2024-02-27 14:48:46 +0000

    Adds tests to cover

--- packages/cap-config-writer/src/cap-config/index.ts
+++ packages/cap-config-writer/src/cap-config/index.ts
@@ -42,6 +42,16 @@ export async function enableCdsUi5Plugin(basePath: string, fs?: Editor): Promise
  *
  * @param basePath - root path of the CAP project, where package.json is located
  * @param [fs] - optional: the memfs editor instance
+ * @returns true: cds-plugin-ui5 and all prerequisites are fulfilled; false: cds-plugin-ui5 is not enabled or not all prerequisites are fulfilled
+ */
+export async function checkCdsUi5PluginEnabled(basePath: string, fs?: Editor): Promise<boolean>;
+
+/**
+ * Check if cds-plugin-ui5 is enabled on a CAP project. Checks also all prerequisites, like minimum @sap/cds version.
+ * Overloaded function that returns detailed CAP plugin info.
+ *
+ * @param basePath - root path of the CAP project, where package.json is located
+ * @param [fs] - optional: the memfs editor instance
  * @param [moreInfo] if true return an object specifying detailed info about the cds and workspace state
  * @returns true: cds-plugin-ui5 and all prerequisites are fulfilled; false: cds-plugin-ui5 is not enabled or not all prerequisites are fulfilled or {@link: CdsUi5PluginInfo} with additional info
  */
@@ -49,7 +59,7 @@ export async function checkCdsUi5PluginEnabled(
     basePath: string,
     fs?: Editor,
     moreInfo?: boolean
-): Promise<CdsUi5PluginInfo>;
+): Promise<boolean | CdsUi5PluginInfo>;
 
 /**
  * Check if cds-plugin-ui5 is enabled on a CAP project. Checks also all prerequisites, like minimum @sap/cds version.
