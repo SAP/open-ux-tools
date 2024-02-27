e03720ca90f79e599eb8dec57f5173fdf18b5d26
J Long <john.long@sap.com>
2024-02-27 20:51:37 +0000

    Fix sonar issue

--- packages/btp-utils/src/destination.ts
+++ packages/btp-utils/src/destination.ts
@@ -186,7 +186,7 @@ function escapeRegExp(str: string): string {
  */
 function addSuffix(destinationName: string, suffix: Suffix): string {
     const suffixStr = ` (${suffix})`;
-    return destinationName.trim().match(`${escapeRegExp(suffixStr)}$`)
+    return RegExp(`${escapeRegExp(suffixStr)}$`).exec(destinationName.trim())
         ? destinationName
         : `${destinationName} (${suffix})`;
 }
