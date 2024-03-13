23a5f0972d4eea992f5eb91034b46a2924b85de5
D048415 <dominik.heim@sap.com>
2024-03-13 17:46:12 +0100

    redefine jest config to exclude file test.ts

--- packages/preview-middleware/jest.config.js
+++ packages/preview-middleware/jest.config.js
@@ -1,4 +1,5 @@
 const config = require('../../jest.base');
 config.modulePathIgnorePatterns.push('<rootDir>/test/test-output');
 config.modulePathIgnorePatterns.push('<rootDir>/.tmp');
+config.testMatch = ['test/**/*.test.ts'];
 module.exports = config;
