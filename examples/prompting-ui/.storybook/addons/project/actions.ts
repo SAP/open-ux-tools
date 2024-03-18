d774675795262da0bd428168021f6475cbdf5bd5
Andis Redmans <andis.redmans@sap.com>
2024-03-18 14:57:28 +0200

    feat: rename folder
    rename folder

--- examples/prompting-ui/.storybook/addons/project/actions.ts
+++ examples/prompting-ui/.storybook/addons/project/actions.ts
@@ -3,7 +3,7 @@ import { existsSync } from 'fs';
 import { GET_PROJECT_PATH, UPDATE_PROJECT_PATH, UPDATE_PROJECT_PATH_RESULT, SET_PROJECT_PATH } from './types';
 import type { ProjectActions } from './types';
 import { getProjectPath, setProjectPath, testAppPath } from './project';
-import { validateProject } from '../../building-blocks/connection';
+import { validateProject } from '../../backend';
 
 export async function handleAction(action: ProjectActions): Promise<ProjectActions | undefined> {
     let responseAction: ProjectActions | undefined = undefined;
--- examples/prompting-ui/.storybook/main.ts
+++ examples/prompting-ui/.storybook/main.ts
@@ -1,5 +1,5 @@
 import { resolve } from 'path';
-import { createWebSocketConnection } from './building-blocks';
+import { createWebSocketConnection } from './backend';
 
 module.exports = {
     stories: ['../stories/*.story.tsx'],
