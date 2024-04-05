e8d3cda06f05fd0483cb4657b6672f4a6d3b050d
D048415 <dominik.heim@sap.com>
2024-04-05 11:43:37 +0200

    set qunit autostart to false

--- packages/preview-middleware/templates/test/qunit.js
+++ packages/preview-middleware/templates/test/qunit.js
@@ -1,6 +1,8 @@
 sap.ui.define([
     'sap/ui/thirdparty/qunit-2'
 ], function() {
-        sap.ui.require(<%- JSON.stringify(tests) %>);
-    }
-);
+        QUnit.config.autostart = false;
+        sap.ui.require(<%- JSON.stringify(tests) %>, function() {
+            QUnit.start();
+    });
+});
