924b33d16aae7c1e882803eb17efb58941ac2b02
D068279 <vinay.hospete.krishnappa@sap.com>
2024-04-16 15:59:32 +0530

    fix: add application insight instrumentation key

--- packages/preview-middleware/src/base/telemetry-reporter.ts
+++ packages/preview-middleware/src/base/telemetry-reporter.ts
@@ -1,9 +1,11 @@
 import { isInternalFeaturesSettingEnabled } from '@sap-ux/feature-toggle';
 import { ClientFactory, SampleRate, initTelemetrySettings } from '@sap-ux/telemetry';
 import modulePackageJson from '../../package.json';
-import { join } from 'path';
-import dotenv from 'dotenv';
+// import { join } from 'path';
+// import dotenv from 'dotenv';
 import { Logger } from '@sap-ux/logger';
+
+const key = 'ApplicationInsightsInstrumentationKeyPLACEH0LDER';
 
 /**
  * Class handling telemetry initialization and reporting.
@@ -13,14 +15,15 @@ export class TelemetryReporter {
     constructor(private eventName: string, private readonly logger: Logger, private layer?: string) {}
     public async initializeTelemetry() {
         try {
-            const envFilePath = { path: join(__dirname, '..', '..', '.env') };
-            dotenv.config(envFilePath);
+            // const envFilePath = { path: join(__dirname, '..', '..', '.env') };
+            // dotenv.config(envFilePath);
             const internalFeature = isInternalFeaturesSettingEnabled();
             await initTelemetrySettings({
                 consumerModule: modulePackageJson,
                 internalFeature,
                 watchTelemetrySettingStore: true,
-                resourceId: process.env['OpenUxTools_ResourceId']
+                //resourceId: process.env['OpenUxTools_ResourceId']
+                resourceId: key
             });
             this.initialized = true;
         } catch (e) {
