81eb714e89dae268861df4c50c90593a0c43eed7
Klaus Keller <klaus.keller@sap.com>
2024-05-03 15:08:00 -0700

    feat: add getSapSystemUI5Version

--- packages/ui5-info/src/index.ts
+++ packages/ui5-info/src/index.ts
@@ -3,3 +3,4 @@ export type { UI5Theme, UI5Version, UI5VersionFilterOptions } from './types';
 export { getUI5Versions } from './ui5-version-info';
 export { supportedUi5VersionFallbacks } from './ui5-version-fallback';
 export { ui5ThemeIds, getDefaultUI5Theme, getUi5Themes } from './ui5-theme-info';
+export { getSapSystemUI5Version } from './ui5-version-backend';
