3de091b4f6b4650c471b0a9acd950434353ff70c
Tobias Queck <tobias.queck@sap.com>
2024-05-16 17:04:10 +0200

    experimenting with inquirer modules and simple prompts

---
+++ packages/create/src/cli/generate/fiori-app.ts
@@ -0,0 +1,64 @@
+import type { Command } from 'commander';
+import { getLogger, traceChanges } from '../../tracing';
+import { getInquirerAdapter, runNpmInstallCommand } from '../../common';
+import type { Ui5App } from '@sap-ux/ui5-application-writer';
+import { generate as generateUi5App } from '@sap-ux/ui5-application-writer';
+import { prompt as promptUi5App  } from '@sap-ux/ui5-application-inquirer';
+
+/**
+ * Add a new sub-command to generate SAP UI5 adaptation projects the given command.
+ *
+ * @param cmd main command that is to be enhanced
+ */
+export function addGenerateFioriApp(cmd: Command): void {
+    cmd.command('fiori-app [path]')
+        .option('-n, --skip-install', 'skip npm install step')
+        .option('-s, --simulate', 'simulate only do not write or install')
+        .action(async (path, options) => {
+            await generateFioriApp(path, { ...options }, !!options.simulate, !!options.skipInstall);
+        });
+}
+
+/**
+ * Generate a Fiori project based on the given parameters.
+ *
+ * @param basePath target folder of the new project
+ * @param _defaults optional defaults
+ * @param simulate if set to true, then no files will be written to the filesystem
+ * @param skipInstall if set to true then `npm i` is not executed in the new project
+ */
+async function generateFioriApp(
+    basePath: string,
+    _defaults: any,
+    simulate: boolean,
+    skipInstall: boolean
+): Promise<void> {
+    const logger = getLogger();
+    try {
+        logger.debug(`Called generate Fiori app for path '${basePath}', skip install is '${skipInstall}'`);
+        const ui5AppAnswers = await promptUi5App(getInquirerAdapter(), {});
+
+        const config: Ui5App = {
+            app: {
+                id: ui5AppAnswers.name!
+            },
+            package: {
+                name: ui5AppAnswers.name!
+            }
+        };
+
+        const fs = await generateUi5App(basePath, config);
+
+        if (!simulate) {
+            await new Promise((resolve) => fs.commit(resolve));
+            if (!skipInstall) {
+                runNpmInstallCommand(basePath);
+                logger.info('Executed npm install');
+            }
+        } else {
+            await traceChanges(fs);
+        }
+    } catch (error) {
+        logger.error(error.message);
+    }
+}
--- packages/create/src/common/index.ts
+++ packages/create/src/common/index.ts
@@ -1,4 +1,6 @@
+import type { InquirerAdapter } from '@sap-ux/ui5-application-inquirer';
 import { spawnSync } from 'child_process';
+import prompts from 'prompts';
 
 /**
  * Run npm install command.
@@ -13,4 +15,31 @@ export function runNpmInstallCommand(basePath: string, installArgs: string[] = [
         cwd: basePath,
         stdio: [0, 1, 2]
     });
+}
+
+function convertQuestion(question: any, answers: any): any {
+    return {
+        type: question.type === 'input' ? 'text' : question.type,
+        name: question.name,
+        message: question.message,
+        validate:
+            typeof question.validate === 'function'
+                ? (value: any) => question.validate(value, answers)
+                : question.validate,
+        initial: typeof question.default === 'function' ? () => question.default(answers) : question.default
+    };
+}
+
+/**
+ * Get the inquirer adapter.
+ *
+ * @returns the inquirer adapter
+ */
+export function getInquirerAdapter(): InquirerAdapter {
+    return {
+        promptModule: undefined,
+        async prompt(questions: any, answers = {}): Promise<any> {
+            return prompts(questions.map((q: any) => convertQuestion(q, answers)));
+        }
+    };
 }
