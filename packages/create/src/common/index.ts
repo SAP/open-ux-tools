01718784641abe6d1992dc1b6f970a2e60924046
Tobias Queck <tobias.queck@sap.com>
2024-05-22 11:36:16 +0200

    latest version of the common prompt conversion

--- packages/create/src/common/index.ts
+++ packages/create/src/common/index.ts
@@ -1,7 +1,5 @@
 import { spawnSync } from 'child_process';
-import prompts from 'prompts';
-import type { PromptType, PromptObject } from 'prompts';
-import type { ListQuestion, YUIQuestion } from '@sap-ux/inquirer-common';
+export { promptYUIQuestions } from './prompts';
 
 /**
  * Run npm install command.
@@ -16,100 +14,4 @@ export function runNpmInstallCommand(basePath: string, installArgs: string[] = [
         cwd: basePath,
         stdio: [0, 1, 2]
     });
-}
-
-// Temporary here until PR is merged - https://github.com/SAP/open-ux-tools/pull/1940/files
-
-/**
- * Checks if a property is a function.
- *
- * @param property property to be checked
- * @returns true if the property is a function
- */
-function isFunction(property: unknown | Function): property is Function {
-    return typeof property === 'function';
-}
-
-const QUESTION_TYPE_MAP: Record<string, PromptType> = {
-    input: 'text',
-    list: 'autocomplete',
-    checkbox: 'multiselect'
-};
-
-/**
- *
- * @param question
- * @param listQuestion
- * @param prompt
- */
-async function enhanceListQuestion(
-    listQuestion: ListQuestion,
-    prompt: PromptObject,
-    answers: { [key: string]: unknown }
-) {
-    const choices: Array<{ name: string; value: unknown } | string | number> = (
-        isFunction(listQuestion.choices) ? await listQuestion.choices(answers) : listQuestion.choices
-    ) as Array<{ name: string; value: unknown } | string | number>;
-    const mapppedChoices = choices.map((choice) => ({
-        title: typeof choice === 'object' ? choice.name : `${choice}`,
-        value: typeof choice === 'object' ? choice.value : choice
-    }));
-    const initialValue = (prompt.initial as Function)();
-    prompt.choices = mapppedChoices;
-    prompt.initial = () =>
-        mapppedChoices[initialValue]
-            ? initialValue
-            : mapppedChoices.findIndex((choice) => choice.value === initialValue);
-}
-
-/**
- * Converts a YUI question to a simple prompts question.
- *
- * @param question YUI question to be converted
- * @param answers previously given answers
- * @returns question converted to prompts question
- */
-export async function convertQuestion(
-    question: YUIQuestion & { choices?: unknown },
-    answers: { [key: string]: unknown }
-): Promise<PromptObject> {
-    const prompt: PromptObject = {
-        type: QUESTION_TYPE_MAP[question.type ?? 'input'] ?? question.type,
-        name: question.name,
-        message: isFunction(question.message) ? await question.message(answers) : await question.message,
-        validate: (value: unknown) =>
-            isFunction(question.validate) ? question.validate(value, answers) : question.validate ?? true,
-        initial: () => (isFunction(question.default) ? question.default(answers) : question.default)
-    };
-    if (question.choices) {
-        await enhanceListQuestion(question as ListQuestion, prompt, answers);
-    }
-    return prompt;
-}
-
-/**
- * Prompt a list of YeomanUI questions with the simple prompts module.
- *
- * @param questions list of questions
- * @param useDefaults - if true, the default values are used for all prompts
- * @returns the answers to the questions
- */
-export async function promptYUIQuestions<T>(questions: YUIQuestion[], useDefaults: boolean): Promise<T> {
-    const answers: { [key: string]: unknown } = {};
-    for (const question of questions) {
-        if (isFunction(question.when) ? question.when(answers) : question.when !== false) {
-            if (useDefaults) {
-                answers[question.name] = isFunction(question.default) ? question.default(answers) : question.default;
-            } else {
-                const q = await convertQuestion(question, answers);
-                const answer = await prompts(q, {
-                    onCancel: () => {
-                        throw new Error('User canceled the prompt');
-                    }
-                });
-                answers[question.name] = answer[question.name];
-            }
-        }
-    }
-    return answers as T;
 }
