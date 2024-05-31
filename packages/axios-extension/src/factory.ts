a200a859f17201f30c950134fa0cb7b552471921
i057546 <iain.o-farrell@sap.com>
2024-05-31 14:01:54 +0100

    Revert bad merge

--- packages/axios-extension/src/factory.ts
+++ packages/axios-extension/src/factory.ts
@@ -158,6 +158,7 @@ export function createForAbapOnCloud(options: AbapCloudOptions & Partial<Provide
             const { url, cookies, ...config } = options;
             provider = createInstance<AbapServiceProvider>(AbapServiceProvider, {
                 baseURL: url,
+                cookies,
                 ...config
             });
             if (!cookies) {
