53225b6cd917b2c4914815f116f2d8fffeb3bc15
Austin Devine <austin.devine@sap.com>
2024-06-10 20:01:15 +0100

    remove duplicate log and fix test

--- packages/axios-extension/src/abap/adt-catalog/generators/ui-service-generator.ts
+++ packages/axios-extension/src/abap/adt-catalog/generators/ui-service-generator.ts
@@ -1,4 +1,3 @@
-import type { Logger } from '@sap-ux/logger';
 import type { GeneratorEntry } from './types';
 import type { BusinessObject } from '../../types';
 import { AdtService } from '../services';
@@ -7,8 +6,6 @@ import { AdtService } from '../services';
  *
  */
 export class UiServiceGenerator extends AdtService {
-    public log: Logger;
-
     protected bo!: BusinessObject;
 
     /**
