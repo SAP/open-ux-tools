1c037bc26af2f5b88ee97d4a7563060b8fe654a6
Georgi Damyanov <georgi.damyanov@sap.com>
2024-06-11 13:30:52 +0300

    refactor: remove ununsed definition

--- packages/preview-middleware-client/types/sap.ui.d.ts
+++ packages/preview-middleware-client/types/sap.ui.d.ts
@@ -1,5 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 declare namespace sap.ui {
     export const version: string;
-    export function requireSync(module: string): any;
 }
