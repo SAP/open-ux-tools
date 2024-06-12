915e7d0cc319af0146a0a2157fc41dd969a4d6f4
Andis Redmans <90789422+815are@users.noreply.github.com>
2024-06-12 08:53:30 +0300

    fix: UIContextualMenu. Wrong visualisation of menu item without text when menu contains at least one menu item with icon (#2008)
    * fix: mixture menu items
    
    mixture menu items
    
    * fix: different approach
    
    different approach
    
    * fix: changelog and test
    
    changelog and test
    
    * fix: storybook
    
    storybook
    
    ---------
    
    Co-authored-by: Klaus Keller <66327622+Klaus-Keller@users.noreply.github.com>

--- packages/ui-components/test/unit/components/UIContextualMenu.test.tsx
+++ packages/ui-components/test/unit/components/UIContextualMenu.test.tsx
@@ -78,4 +78,27 @@ describe('<UIDropdown />', () => {
         expect(containerElement.childNodes[0]).toBe(textElement);
         expect(containerElement.childNodes[1]).toBe(iconElement);
     });
+
+    it('Test mexture menu - item with icon and item without icon', () => {
+        wrapper.setProps({
+            items: [
+                {
+                    key: 'item1',
+                    text: 'menu item 1'
+                },
+                {
+                    key: 'item2',
+                    iconProps: {
+                        iconName: UiIcons.GuidedDevelopment
+                    },
+                    text: 'menu item 2'
+                }
+            ]
+        });
+        wrapper.update();
+        //Check if only one icon is rendered
+        expect(wrapper.find(`i[data-icon-name="${UiIcons.GuidedDevelopment}"]`).length).toEqual(1);
+        // Check if two menu items are rendered
+        expect(wrapper.find('.ms-ContextualMenu-linkContent').length).toEqual(2);
+    });
 });
