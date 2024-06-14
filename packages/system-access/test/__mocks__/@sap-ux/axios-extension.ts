74125f3500a0ebf190980173f5b0bd7299f81815
Tobias Queck <tobias.queck@sap.com>
2024-06-14 09:21:36 +0200

    Revert "remove unnessary mocking"
    This reverts commit 64be09fc84fc97d75ffd363c12263a4a8eb3d044.

---
+++ packages/system-access/test/__mocks__/@sap-ux/axios-extension.ts
@@ -0,0 +1,13 @@
+const axiosExt = jest.requireActual('@sap-ux/axios-extension');
+
+const mockedProvider = {};
+
+module.exports = {
+    ...axiosExt,
+    mockedProvider,
+    createForAbap: jest.fn().mockReturnValue(mockedProvider),
+    createForAbapOnCloud: jest.fn((options: unknown) => {
+        const provider = axiosExt.createForAbapOnCloud(options);
+        return provider;
+    })
+};
