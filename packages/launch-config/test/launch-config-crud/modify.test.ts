6a73bd3b1c27b0d8fbd886983486960e8adda100
broksy <ainis.broks@sap.com>
2024-06-20 13:19:21 +0300

    fix: add scenario test

--- packages/launch-config/test/launch-config-crud/modify.test.ts
+++ packages/launch-config/test/launch-config-crud/modify.test.ts
@@ -1,194 +1,196 @@
 import type { LaunchConfig } from '../../src';
 import { convertOldLaunchConfigToFioriRun, createFioriLaunchConfig } from '../../src';
 import { TestPaths } from '../test-data/utils';
-const runnableId = JSON.stringify({
-    runnableId: TestPaths.v2lrop
-});
 
-test('Modify old config that uses run-scripts without args defined', async () => {
-    const launchConfig: LaunchConfig = {
-        name: '',
-        cwd: '',
-        runtimeArgs: ['run-script', 'start'],
-        type: 'node',
-        request: 'launch',
-        runtimeExecutable: 'npm',
-        args: [],
-        windows: {
-            runtimeExecutable: 'npm.cmd'
-        },
-        console: 'internalConsole',
-        internalConsoleOptions: 'openOnSessionStart',
-        outputCapture: 'std',
-        env: {
-            'run.config': runnableId
-        }
-    };
-    const modified = await convertOldLaunchConfigToFioriRun(launchConfig);
-    expect(modified.args).toEqual([
-        '--open',
-        'test/flpSandbox.html?sap-client=100#masterDetail-display',
-        '--config',
-        'ui5.yaml'
-    ]);
-    expect(modified.env).toEqual({
-        'run.config': runnableId
+describe('modify', () => {
+    const runnableId = JSON.stringify({
+        runnableId: TestPaths.v2lrop
     });
-    expect(modified.runtimeExecutable).toEqual('npx');
-    expect(modified.windows.runtimeExecutable).toEqual('npx.cmd');
-    expect(modified.runtimeArgs).toEqual(['fiori', 'run']);
-});
+    test('Modify old config that uses run-scripts without args defined', async () => {
+        const launchConfig: LaunchConfig = {
+            name: '',
+            cwd: '',
+            runtimeArgs: ['run-script', 'start'],
+            type: 'node',
+            request: 'launch',
+            runtimeExecutable: 'npm',
+            args: [],
+            windows: {
+                runtimeExecutable: 'npm.cmd'
+            },
+            console: 'internalConsole',
+            internalConsoleOptions: 'openOnSessionStart',
+            outputCapture: 'std',
+            env: {
+                'run.config': runnableId
+            }
+        };
+        const modified = await convertOldLaunchConfigToFioriRun(launchConfig);
+        expect(modified.args).toEqual([
+            '--open',
+            'test/flpSandbox.html?sap-client=100#masterDetail-display',
+            '--config',
+            'ui5.yaml'
+        ]);
+        expect(modified.env).toEqual({
+            'run.config': runnableId
+        });
+        expect(modified.runtimeExecutable).toEqual('npx');
+        expect(modified.windows.runtimeExecutable).toEqual('npx.cmd');
+        expect(modified.runtimeArgs).toEqual(['fiori', 'run']);
+    });
 
-test('Modify old config that uses run-scripts without args defined: start script contains short args keys', async () => {
-    const launchConfig: LaunchConfig = {
-        name: '',
-        cwd: '',
-        runtimeArgs: ['run-script', 'start'],
-        type: 'node',
-        request: 'launch',
-        runtimeExecutable: 'npm',
-        args: [],
-        windows: {
-            runtimeExecutable: 'npm.cmd'
-        },
-        console: 'internalConsole',
-        internalConsoleOptions: 'openOnSessionStart',
-        outputCapture: 'std',
-        env: {
+    test('Modify old config that uses run-scripts without args defined: start script contains short args keys', async () => {
+        const launchConfig: LaunchConfig = {
+            name: '',
+            cwd: '',
+            runtimeArgs: ['run-script', 'start'],
+            type: 'node',
+            request: 'launch',
+            runtimeExecutable: 'npm',
+            args: [],
+            windows: {
+                runtimeExecutable: 'npm.cmd'
+            },
+            console: 'internalConsole',
+            internalConsoleOptions: 'openOnSessionStart',
+            outputCapture: 'std',
+            env: {
+                'run.config': JSON.stringify({
+                    runnableId: TestPaths.freestyleProjects
+                })
+            }
+        };
+        const modified = await convertOldLaunchConfigToFioriRun(launchConfig);
+        expect(modified.args).toEqual([
+            '--open',
+            'test/flpSandbox.html?sap-client=100#masterDetail-display',
+            '--config',
+            'ui5.yaml'
+        ]);
+        expect(modified.env).toEqual({
             'run.config': JSON.stringify({
                 runnableId: TestPaths.freestyleProjects
             })
-        }
-    };
-    const modified = await convertOldLaunchConfigToFioriRun(launchConfig);
-    expect(modified.args).toEqual([
-        '--open',
-        'test/flpSandbox.html?sap-client=100#masterDetail-display',
-        '--config',
-        'ui5.yaml'
-    ]);
-    expect(modified.env).toEqual({
-        'run.config': JSON.stringify({
-            runnableId: TestPaths.freestyleProjects
-        })
+        });
+        expect(modified.runtimeExecutable).toEqual('npx');
+        expect(modified.windows.runtimeExecutable).toEqual('npx.cmd');
+        expect(modified.runtimeArgs).toEqual(['fiori', 'run']);
     });
-    expect(modified.runtimeExecutable).toEqual('npx');
-    expect(modified.windows.runtimeExecutable).toEqual('npx.cmd');
-    expect(modified.runtimeArgs).toEqual(['fiori', 'run']);
-});
 
-test('Modify old config that uses run-scripts with args defined', async () => {
-    const launchConfig: LaunchConfig = {
-        name: '',
-        cwd: '',
-        runtimeArgs: ['run-script', 'start'],
-        type: 'node',
-        request: 'launch',
-        runtimeExecutable: 'npm',
-        args: [
-            '--',
-            '--ui5Uri https://ui5.sap.com --ui5 1.90.7',
-            '--backendConfig',
-            '[{"path":"","url":"","client":""}]'
-        ],
-        windows: {
-            runtimeExecutable: 'npm.cmd'
-        },
-        console: 'internalConsole',
-        internalConsoleOptions: 'openOnSessionStart',
-        outputCapture: 'std',
-        env: {
+    test('Modify old config that uses run-scripts with args defined', async () => {
+        const launchConfig: LaunchConfig = {
+            name: '',
+            cwd: '',
+            runtimeArgs: ['run-script', 'start'],
+            type: 'node',
+            request: 'launch',
+            runtimeExecutable: 'npm',
+            args: [
+                '--',
+                '--ui5Uri https://ui5.sap.com --ui5 1.90.7',
+                '--backendConfig',
+                '[{"path":"","url":"","client":""}]'
+            ],
+            windows: {
+                runtimeExecutable: 'npm.cmd'
+            },
+            console: 'internalConsole',
+            internalConsoleOptions: 'openOnSessionStart',
+            outputCapture: 'std',
+            env: {
+                'run.config': runnableId
+            }
+        };
+        const modified = await convertOldLaunchConfigToFioriRun(launchConfig);
+        expect(modified.args).toEqual([
+            '--open',
+            'test/flpSandbox.html?sap-client=100#masterDetail-display',
+            '--config',
+            'ui5.yaml'
+        ]);
+        expect(modified.env).toEqual({
+            'run.config': runnableId,
+            FIORI_TOOLS_UI5_VERSION: '1.90.7',
+            FIORI_TOOLS_UI5_URI: 'https://ui5.sap.com',
+            FIORI_TOOLS_BACKEND_CONFIG: '[{"path":"","url":"","client":""}]'
+        });
+        expect(modified.runtimeExecutable).toEqual('npx');
+        expect(modified.windows.runtimeExecutable).toEqual('npx.cmd');
+        expect(modified.runtimeArgs).toEqual(['fiori', 'run']);
+    });
+
+    test('Modify old config that uses run-scripts with no start script and args defined', async () => {
+        const launchConfig: LaunchConfig = {
+            name: '',
+            cwd: '',
+            runtimeArgs: ['run-script', 'not-start'],
+            type: 'node',
+            request: 'launch',
+            runtimeExecutable: 'npm',
+            args: [],
+            windows: {
+                runtimeExecutable: 'npm.cmd'
+            },
+            console: 'internalConsole',
+            internalConsoleOptions: 'openOnSessionStart',
+            outputCapture: 'std',
+            env: {
+                'run.config': runnableId
+            }
+        };
+        const modified = await convertOldLaunchConfigToFioriRun(launchConfig);
+        expect(modified.args).toEqual([]);
+        expect(modified.env).toEqual({
             'run.config': runnableId
-        }
-    };
-    const modified = await convertOldLaunchConfigToFioriRun(launchConfig);
-    expect(modified.args).toEqual([
-        '--open',
-        'test/flpSandbox.html?sap-client=100#masterDetail-display',
-        '--config',
-        'ui5.yaml'
-    ]);
-    expect(modified.env).toEqual({
-        'run.config': runnableId,
-        FIORI_TOOLS_UI5_VERSION: '1.90.7',
-        FIORI_TOOLS_UI5_URI: 'https://ui5.sap.com',
-        FIORI_TOOLS_BACKEND_CONFIG: '[{"path":"","url":"","client":""}]'
+        });
+        expect(modified.runtimeExecutable).toEqual('npx');
+        expect(modified.windows.runtimeExecutable).toEqual('npx.cmd');
+        expect(modified.runtimeArgs).toEqual(['fiori', 'run']);
     });
-    expect(modified.runtimeExecutable).toEqual('npx');
-    expect(modified.windows.runtimeExecutable).toEqual('npx.cmd');
-    expect(modified.runtimeArgs).toEqual(['fiori', 'run']);
-});
 
-test('Modify old config that uses run-scripts with no start script and args defined', async () => {
-    const launchConfig: LaunchConfig = {
-        name: '',
-        cwd: '',
-        runtimeArgs: ['run-script', 'not-start'],
-        type: 'node',
-        request: 'launch',
-        runtimeExecutable: 'npm',
-        args: [],
-        windows: {
-            runtimeExecutable: 'npm.cmd'
-        },
-        console: 'internalConsole',
-        internalConsoleOptions: 'openOnSessionStart',
-        outputCapture: 'std',
-        env: {
-            'run.config': runnableId
-        }
-    };
-    const modified = await convertOldLaunchConfigToFioriRun(launchConfig);
-    expect(modified.args).toEqual([]);
-    expect(modified.env).toEqual({
-        'run.config': runnableId
+    test('Modify nothing, launch config is defined for fiori run', async () => {
+        const launchConfig = createFioriLaunchConfig('WORKSPACE_FOLDER', {
+            name: 'TEST_NAME',
+            projectRoot: TestPaths.v2lrop,
+            ui5Version: 'snapshot',
+            ui5VersionUri: 'https://sapui5preview-sapui5.dispatcher.int.sap.eu2.hana.ondemand.com'
+        });
+        // clone object to create a new reference
+        const expected: LaunchConfig = Object.assign({}, launchConfig);
+        const modified = await convertOldLaunchConfigToFioriRun(launchConfig);
+        expect(modified).toStrictEqual(expected);
     });
-    expect(modified.runtimeExecutable).toEqual('npx');
-    expect(modified.windows.runtimeExecutable).toEqual('npx.cmd');
-    expect(modified.runtimeArgs).toEqual(['fiori', 'run']);
-});
 
-test('Modify nothing, launch config is defined for fiori run', async () => {
-    const launchConfig = createFioriLaunchConfig('WORKSPACE_FOLDER', {
-        name: 'TEST_NAME',
-        projectRoot: TestPaths.v2lrop,
-        ui5Version: 'snapshot',
-        ui5VersionUri: 'https://sapui5preview-sapui5.dispatcher.int.sap.eu2.hana.ondemand.com'
+    test('Do not modify other types of run configurations', async () => {
+        const launchConfig = {
+            name: 'Some run config',
+            type: 'node',
+            request: 'launch',
+            program: '${workspaceFolder}/index.js',
+            cwd: '${workspaceFolder}/myproject',
+            args: [],
+            envFile: '${workspaceFolder}/myproject/.env'
+        };
+        // clone object to create a new reference
+        const expected = Object.assign({}, launchConfig);
+        const modified = await convertOldLaunchConfigToFioriRun(launchConfig as any);
+        expect(modified).toStrictEqual(expected);
     });
-    // clone object to create a new reference
-    const expected: LaunchConfig = Object.assign({}, launchConfig);
-    const modified = await convertOldLaunchConfigToFioriRun(launchConfig);
-    expect(modified).toStrictEqual(expected);
-});
 
-test('Do not modify other types of run configurations', async () => {
-    const launchConfig = {
-        name: 'Some run config',
-        type: 'node',
-        request: 'launch',
-        program: '${workspaceFolder}/index.js',
-        cwd: '${workspaceFolder}/myproject',
-        args: [],
-        envFile: '${workspaceFolder}/myproject/.env'
-    };
-    // clone object to create a new reference
-    const expected = Object.assign({}, launchConfig);
-    const modified = await convertOldLaunchConfigToFioriRun(launchConfig as any);
-    expect(modified).toStrictEqual(expected);
-});
-
-test('Do not modify only if no project root', async () => {
-    const launchConfig = {
-        name: 'Some run config',
-        runtimeArgs: ['run-script', 'start'],
-        type: 'node',
-        request: 'launch',
-        program: '${workspaceFolder}/index.js',
-        cwd: '${workspaceFolder}/myproject',
-        envFile: '${workspaceFolder}/myproject/.env'
-    };
-    // clone object to create a new reference
-    const expected = Object.assign({}, launchConfig);
-    const modified = await convertOldLaunchConfigToFioriRun(launchConfig as any);
-    expect(modified).toStrictEqual(expected);
+    test('Do not modify only if no project root', async () => {
+        const launchConfig = {
+            name: 'Some run config',
+            runtimeArgs: ['run-script', 'start'],
+            type: 'node',
+            request: 'launch',
+            program: '${workspaceFolder}/index.js',
+            cwd: '${workspaceFolder}/myproject',
+            envFile: '${workspaceFolder}/myproject/.env'
+        };
+        // clone object to create a new reference
+        const expected = Object.assign({}, launchConfig);
+        const modified = await convertOldLaunchConfigToFioriRun(launchConfig as any);
+        expect(modified).toStrictEqual(expected);
+    });
 });
--- packages/launch-config/test/scenario-add-config.test.ts
+++ packages/launch-config/test/scenario-add-config.test.ts
@@ -1,109 +1,121 @@
-import { DirName, createDirectory, deleteDirectory, findAllProjectRoots } from '@sap-ux/project-access';
-import { copyFileSync } from 'fs';
-import { dirname, join } from 'path';
+import { DirName, findFioriArtifacts } from '@sap-ux/project-access';
+import { create as createStorage } from 'mem-fs';
+import { create } from 'mem-fs-editor';
+import { join } from 'path';
 import {
-    getDefaultLaunchConfigOptionsForProject,
     getLaunchConfigFiles,
     addFioriElementsLaunchConfig,
     getLaunchConfigByName,
     getUI5VersionUri,
-    launchConfigFile
+    launchConfigFile,
+    getDefaultLaunchConfigOptionsForProject
 } from '../src';
 import { TestPaths } from './test-data/utils';
-const feLaunchJsonCopy = join(TestPaths.tmpDir, DirName.VSCode, launchConfigFile);
 
-afterAll(async () => {
-    await deleteDirectory(TestPaths.tmpDir);
-});
-
-test('Add new launch config to existing launch.json file', async () => {
-    // Prepare temp launch.json
-    await deleteDirectory(TestPaths.tmpDir);
-    await createDirectory(TestPaths.tmpDir);
-    await createDirectory(dirname(feLaunchJsonCopy));
-    copyFileSync(TestPaths.feProjectsLaunchConfig, feLaunchJsonCopy);
-
+const getApplicationRoot = async (appName = 'v2lrop'): Promise<string> => {
     // Get all projects
-    const projectRoots = await findAllProjectRoots(TestPaths.workspaceRoots);
-    expect(projectRoots.length).toBe(2);
-    const projectRoot = projectRoots.find((p) => p.endsWith('v2lrop')) || '';
-    expect(projectRoot).not.toBe('');
+    const applications = await findFioriArtifacts({
+        wsFolders: TestPaths.workspaceRoots,
+        artifacts: ['applications']
+    });
+    const allApplications = applications.applications;
+    expect(allApplications?.length).toBe(2);
+    // get specific application from workspace
+    const application = allApplications?.find((p) => p.appRoot.endsWith(appName));
+    expect(application).toBeDefined();
+    return application?.appRoot || '';
+};
 
-    // Select name
-    const defaultOptions = await getDefaultLaunchConfigOptionsForProject(projectRoot);
+describe('add config scenario', () => {
+    const memFs = create(createStorage());
+    const feLaunchJsonCopy = join(TestPaths.tmpDir, DirName.VSCode, launchConfigFile);
 
-    const name = defaultOptions.name;
+    afterAll(async () => {
+        memFs.delete(TestPaths.tmpDir);
+    });
+    test('Add new launch config to existing launch.json file', async () => {
+        // Prepare temp launch.json
+        memFs.copy(TestPaths.feProjectsLaunchConfig, feLaunchJsonCopy);
 
-    // Select UI5 version
-    const ui5Version = 'myLatest';
+        const projectRoot = await getApplicationRoot('v2lrop');
 
-    // Set ui5VersionUri
-    const ui5VersionUri = getUI5VersionUri(ui5Version);
+        // Select name
+        const defaultOptions = await getDefaultLaunchConfigOptionsForProject(projectRoot);
 
-    // Select destination
-    const backendConfigs = [{ path: 'TEST_PATH', name: 'TEST_DESTINAME' }];
+        const name = defaultOptions.name;
 
-    // Add the launch config
-    await addFioriElementsLaunchConfig(TestPaths.tmpDir, {
-        name,
-        projectRoot,
-        ui5Version,
-        ui5VersionUri,
-        backendConfigs
+        // Select UI5 version
+        const ui5Version = 'myLatest';
+
+        // // Set ui5VersionUri
+        const ui5VersionUri = getUI5VersionUri(ui5Version);
+
+        // // Select destination
+        const backendConfigs = [{ path: 'TEST_PATH', name: 'TEST_DESTINAME', url: 'dummy' }];
+
+        // // Add the launch config
+        await addFioriElementsLaunchConfig(
+            TestPaths.tmpDir,
+            {
+                name,
+                projectRoot,
+                ui5Version,
+                ui5VersionUri,
+                backendConfigs
+            },
+            memFs
+        );
+
+        // // Get launch config files in workspace
+        const launchConfigs = await getLaunchConfigFiles(TestPaths.tmpDir, memFs);
+        expect(launchConfigs.length).toBe(1);
+        const launchConfigPath = launchConfigs.pop() as string;
+
+        const resultConfig = await getLaunchConfigByName(launchConfigPath, 'Launch Fiori app: v2lrop', memFs);
+        expect(resultConfig.env.FIORI_TOOLS_BACKEND_CONFIG).toEqual(
+            '[{"path":"TEST_PATH","name":"TEST_DESTINAME","url":"dummy"}]'
+        );
+        expect(resultConfig.env.FIORI_TOOLS_UI5_VERSION).toEqual('myLatest');
+        expect(resultConfig.env.FIORI_TOOLS_UI5_URI).toEqual('https://ui5.sap.com');
     });
 
-    // Get launch config files in workspace
-    const launchConfigs = getLaunchConfigFiles(TestPaths.tmpDir);
-    expect(launchConfigs.length).toBe(1);
-    const launchConfigPath = launchConfigs.pop() as string;
+    test('Create new launch config launch.json file', async () => {
+        const projectRoot = await getApplicationRoot('v2lrop');
 
-    const resultConfig = await getLaunchConfigByName(launchConfigPath, 'Launch Fiori app: v2lrop');
-    expect(resultConfig.env.FIORI_TOOLS_BACKEND_CONFIG).toEqual('[{"path":"TEST_PATH","name":"TEST_DESTINAME"}]');
-    expect(resultConfig.env.FIORI_TOOLS_UI5_VERSION).toEqual('myLatest');
-    expect(resultConfig.env.FIORI_TOOLS_UI5_URI).toEqual('https://ui5.sap.com');
-});
+        const name = `TEST_LAUNCH_CONFIG`;
 
-test('Create new launch config launch.json file', async () => {
-    // Prepare directory for launch config
-    await deleteDirectory(TestPaths.tmpDir);
-    await createDirectory(TestPaths.tmpDir);
-    await createDirectory(dirname(feLaunchJsonCopy));
+        // Select UI5 version
+        const ui5Version = '1.1.1';
 
-    // Get all projects
-    const projectRoots = await findAllProjectRoots(TestPaths.workspaceRoots);
-    expect(projectRoots.length).toBe(2);
-    const projectRoot = projectRoots.find((p) => p.endsWith('v2lrop')) || '';
-    expect(projectRoot).not.toBe('');
+        // Set ui5VersionUri
+        const ui5VersionUri = getUI5VersionUri(ui5Version);
 
-    const name = `TEST_LAUNCH_CONFIG`;
+        // Select destination
+        const backendConfigs = [{ path: 'TEST_PATH', url: 'TEST_URL', client: 'TEST_CLIENT' }];
 
-    // Select UI5 version
-    const ui5Version = '1.1.1';
+        // Add the launch config
+        await addFioriElementsLaunchConfig(
+            TestPaths.tmpDir,
+            {
+                name,
+                projectRoot,
+                ui5Version,
+                ui5VersionUri,
+                backendConfigs
+            },
+            memFs
+        );
 
-    // Set ui5VersionUri
-    const ui5VersionUri = getUI5VersionUri(ui5Version);
+        // Get launch config files in workspace
+        const launchConfigs = await getLaunchConfigFiles(TestPaths.tmpDir, memFs);
+        expect(launchConfigs.length).toBe(1);
+        const launchConfigPath = launchConfigs.pop() as string;
 
-    // Select destination
-    const backendConfigs = [{ path: 'TEST_PATH', url: 'TEST_URL', client: 'TEST_CLIENT' }];
-
-    // Add the launch config
-    await addFioriElementsLaunchConfig(TestPaths.tmpDir, {
-        name,
-        projectRoot,
-        ui5Version,
-        ui5VersionUri,
-        backendConfigs
+        const resultConfig = await getLaunchConfigByName(launchConfigPath, 'TEST_LAUNCH_CONFIG', memFs);
+        expect(resultConfig.env.FIORI_TOOLS_BACKEND_CONFIG).toEqual(
+            '[{"path":"TEST_PATH","url":"TEST_URL","client":"TEST_CLIENT"}]'
+        );
+        expect(resultConfig.env.FIORI_TOOLS_UI5_VERSION).toEqual('1.1.1');
+        expect(resultConfig.env.FIORI_TOOLS_UI5_URI).toEqual('https://ui5.sap.com');
     });
-
-    // Get launch config files in workspace
-    const launchConfigs = getLaunchConfigFiles(TestPaths.tmpDir);
-    expect(launchConfigs.length).toBe(1);
-    const launchConfigPath = launchConfigs.pop() as string;
-
-    const resultConfig = await getLaunchConfigByName(launchConfigPath, 'TEST_LAUNCH_CONFIG');
-    expect(resultConfig.env.FIORI_TOOLS_BACKEND_CONFIG).toEqual(
-        '[{"path":"TEST_PATH","url":"TEST_URL","client":"TEST_CLIENT"}]'
-    );
-    expect(resultConfig.env.FIORI_TOOLS_UI5_VERSION).toEqual('1.1.1');
-    expect(resultConfig.env.FIORI_TOOLS_UI5_URI).toEqual('https://ui5.sap.com');
 });
