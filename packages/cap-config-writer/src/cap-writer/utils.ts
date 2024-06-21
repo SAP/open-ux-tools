e72176d0ea77489f52ca8b281c98d9cd4911fbc9
I743583 <kitty.jose@sap.com>
2024-06-21 12:12:39 +0100

    reimplement launch text ftn

--- packages/cap-config-writer/src/cap-writer/utils.ts
+++ packages/cap-config-writer/src/cap-writer/utils.ts
@@ -2,6 +2,25 @@ import { t } from '../i18n';
 import type { CapRuntime } from '@sap-ux/odata-service-inquirer';
 
 /**
+ * Determines the URL based on the project type.
+ *
+ * @param capType The type of CAP runtime used in the application (e.g., 'Java' or 'Node.js').
+ * @param projectName The project's name, which is the module name.
+ * @param appId If appId is provided, it will be used to open the application instead of the project name. This option is available for use with npm workspaces.
+ * @returns The URL for launching the project.
+ */
+function getCapUrl(capType: CapRuntime, projectName: string, appId?: string): string {
+    const projectPath = appId ?? projectName + '/webapp';
+    if (capType === 'Java') {
+        // For Java projects
+        return `http://localhost:8080/${projectName}/webapp/index.html`;
+    } else {
+        // For Node.js projects or when capType is undefined
+        return `http://localhost:4004/${projectPath}/index.html`;
+    }
+}
+
+/**
  * Generates a launch text for the launching of applications.
  *
  * @param capType The type of CAP runtime used in the application will be provided for CAP applications.
@@ -10,26 +29,10 @@ import type { CapRuntime } from '@sap-ux/odata-service-inquirer';
  * @returns The launch text for the application.
  */
 export function getLaunchText(capType: CapRuntime, projectName: string, appId?: string): string {
-    /**
-     * Determines the URL based on the project type.
-     * @returns The URL for launching the project.
-     */
-    function getCapUrl(): string {
-        const project = appId ?? projectName + '/webapp';
-        if (capType === 'Java') {
-            // For Java projects
-            return `http://localhost:8080/${projectName}/webapp/index.html`;
-        } else {
-            // For Node.js projects or when capType is undefined
-            return `http://localhost:4004/${project}/index.html`;
-        }
-    }
-
-    let mvnCommand = '';
-    if (capType === 'Java') {
-        // If capType is 'Java', set the Maven command for running Spring Boot
-        mvnCommand = ' (```mvn spring-boot:run```)';
-    }
-    const capUrl = getCapUrl();
+    // Determine the Maven command if the project is a Java project
+    const mvnCommand = capType === 'Java' ? ' (```mvn spring-boot:run```)' : '';
+    const capUrl = getCapUrl(capType, projectName, appId);
+    
+    // Return launch text
     return `${t('launchCapText', { mvnCommand, capUrl })}`;
 }
