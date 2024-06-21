cf41abcdd507c11fdd2f81020ed3bbb1d0f3f64c
broksy <ainis.broks@sap.com>
2024-06-21 18:56:18 +0300

    fix: refactoring

--- packages/launch-config/src/types/constants.ts
+++ packages/launch-config/src/types/constants.ts
@@ -2,3 +2,4 @@ export const FIORI_TOOLS_LAUNCH_CONFIG_HANDLER_ID = 'fiori_tools';
 export const LAUNCH_JSON_FILE = 'launch.json';
 export const INTERNAL_UI5_PATH = 'https://sapui5preview-sapui5.dispatcher.int.sap.eu2.hana.ondemand.com';
 export const FRAMEWORK_VERSION_ARGUMENT = '--framework-version';
+export const RUN_SCRIPT = 'run-script';
