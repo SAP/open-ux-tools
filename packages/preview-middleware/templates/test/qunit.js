1cf3e0605dd65a1d59b71e26deea91ffddac80ba
Tobias Queck <tobias.queck@sap.com>
2024-06-25 14:18:00 +0200

    adding shim configs to ensure proper order of loading

--- packages/preview-middleware/templates/test/qunit.js
+++ packages/preview-middleware/templates/test/qunit.js
@@ -1,11 +1,27 @@
-window.QUnit ??= {};
-QUnit.config ??= {};
-QUnit.config.autostart = false;
-QUnit.config.testTimeout = 180000;
+sap.ui.loader.config({
+    shim: {
+        "sap/ui/qunit/qunit-junit": {
+            deps: ["sap/ui/thirdparty/qunit-2"]
+        },
+        "sap/ui/qunit/qunit-coverage": {
+            deps: ["sap/ui/thirdparty/qunit-2"]
+        },
+        "sap/ui/thirdparty/sinon-qunit": {
+            deps: ["sap/ui/thirdparty/qunit-2", "sap/ui/thirdparty/sinon"]
+        },
+        "sap/ui/qunit/sinon-qunit-bridge": {
+            deps: ["sap/ui/thirdparty/qunit", "sap/ui/thirdparty/sinon-4"]
+        }        
+    }
+});
 
-sap.ui.require(['sap/ui/thirdparty/qunit-2'
-], function() {
+sap.ui.require([
+    "sap/ui/thirdparty/qunit-2",
+    "sap/ui/qunit/qunit-junit",
+    "sap/ui/qunit/qunit-coverage"
+], function (QUnit) {
     'use strict';
+    QUnit.config.autostart = false;
     sap.ui.require(<%- JSON.stringify(tests) %>, function() {
         QUnit.start();
     }); 
