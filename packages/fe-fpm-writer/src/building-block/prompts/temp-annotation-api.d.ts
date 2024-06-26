236284ffc7289c92aa6c690a146c50d91e4c907f
C5310768 <alina.govoruhina@sap.com>
2024-06-26 13:02:44 +0300

    feat: use annotation api

--- packages/fe-fpm-writer/src/building-block/prompts/temp-annotation-api.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@sap-ux/fiori-annotation-api' {
-    export class FioriAnnotationService {
-        public static createService(project: Project, serviceName: string, appName: string): FioriAnnotationService {}
-        public sync(): void;
-        public getSchema(): RawMetadata;
-    }
-}
