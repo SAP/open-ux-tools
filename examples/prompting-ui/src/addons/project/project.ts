85386b94ace10e1b63facaae2b5f74154711ddc8
Andis Redmans <andis.redmans@sap.com>
2024-06-26 16:16:07 +0300

    fix: do not create fe-fpm-cli folder when storybook is launched
    do not create fe-fpm-cli folder when storybook is launched

--- examples/prompting-ui/src/addons/project/project.ts
+++ examples/prompting-ui/src/addons/project/project.ts
@@ -1,6 +1,6 @@
 import { join } from 'path';
 
-export const testAppPath = join(__dirname, '../../../fe-fpm-cli/test-output/fe-app', `${Date.now()}`);
+export const testAppPath = join(__dirname, `../../../test-output/fe-app-${Date.now()}`);
 let currentAppPath = testAppPath;
 
 export const setProjectPath = (path: string): void => {
