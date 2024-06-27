87960c4ad82d518fc3eb8b8be6b774a605288070
Andis Redmans <andis.redmans@sap.com>
2024-06-27 20:06:38 +0300

    feat: storybook enhancement
    storybook enhancement

--- packages/ui-prompting/stories/Questions.story.tsx
+++ packages/ui-prompting/stories/Questions.story.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Questions } from '../src/components';
 import type { PromptQuestion } from '../src';
 import { initIcons } from '@sap-ux/ui-components';
-import { saveValues } from './utils';
+import { useStorage } from './utils';
 
 export default { title: 'Basic/Questions' };
 
@@ -37,9 +37,55 @@ const questions: PromptQuestion[] = [
 ];
 
 export const defaultUsage = (): JSX.Element => {
+    const [saveValues] = useStorage();
     return (
         <Questions
             questions={questions}
+            answers={{}}
+            choices={{}}
+            onChange={(answers) => {
+                saveValues(answers);
+            }}
+            onChoiceRequest={() => {}}
+            validation={{}}
+        />
+    );
+};
+
+const objectBasedQuestions: PromptQuestion[] = [
+    {
+        name: 'test.dummy',
+        message: 'test -> dummy',
+        type: 'input'
+    },
+    {
+        name: 'test.dummy2',
+        message: 'test -> dummy2',
+        type: 'checkbox',
+        choices
+    },
+    {
+        name: 'test2.dummy',
+        message: 'test2 -> dummy',
+        type: 'list',
+        choices
+    },
+    {
+        name: 'test3',
+        message: 'test3',
+        type: 'input'
+    },
+    {
+        name: 'test4.child.dummy',
+        message: 'test4 -> child -> dummy',
+        type: 'input'
+    },
+];
+export const ObjectBasedQuestions = (): JSX.Element => {
+    const [saveValues] = useStorage();
+    return (
+        <Questions
+            questions={objectBasedQuestions}
             answers={{}}
             choices={{}}
             onChange={(answers) => {
--- packages/ui-prompting/stories/utils.ts
+++ packages/ui-prompting/stories/utils.ts
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
 import { Answers } from 'inquirer';
 
 const storageKey = 'storybook-answers';
 
 export function saveValues(answers: Answers) {
     window.localStorage.setItem(storageKey, JSON.stringify(answers, undefined, 4));
+}
+
+export function useStorage(): [(answers: Answers) => void] {
+    useEffect(() => {
+        saveValues({});
+    }, []);
+    return [saveValues];
 }
