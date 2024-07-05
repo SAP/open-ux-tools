098cf4406429026dad3eb2b220e9f39fcc8ac60f
Klaus Keller <klaus.keller@sap.com>
2024-07-05 10:45:52 +0200

    chore: unit tests

--- packages/project-access/test/project/specification.test.ts
+++ packages/project-access/test/project/specification.test.ts
@@ -1,19 +1,28 @@
 import { join } from 'path';
-const moduleCacheRoot = join(__dirname, '../test-data/module-loader');
-const fioriToolsDirectory = join(__dirname, '../test-data/specification');
+// Needs to be done before importing getSpecification() to mock the module cache path
 jest.doMock('../../src/constants', () => ({
     ...(jest.requireActual('../../src/constants') as {}),
-    moduleCacheRoot,
-    fioriToolsDirectory
+    moduleCacheRoot: join(__dirname, '../test-data/module-loader'),
+    fioriToolsDirectory: join(__dirname, '../test-data/specification')
 }));
+import * as fsMock from 'fs';
 import type { Logger } from '@sap-ux/logger';
 import { getSpecification, refreshSpecificationDistTags } from '../../src';
 import * as commandMock from '../../src/command';
 import * as moduleMock from '../../src/project/module-loader';
 import * as fileMock from '../../src/file';
 
+jest.mock('fs', () => {
+    const actual = jest.requireActual('fs');
+    return { ...actual, existsSync: jest.fn().mockImplementation(actual.existsSync) };
+});
+
 describe('Test getSpecification', () => {
     type Specification = { exec: () => string };
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
 
     test('Get specification from project', async () => {
         const logger = getMockLogger();
@@ -31,6 +40,20 @@ describe('Test getSpecification', () => {
         const specification = await getSpecification<Specification>(root, { logger });
         expect(specification.exec()).toBe('specification-mock');
         expect(logger.debug).toHaveBeenCalledWith("Specification loaded from cache using dist-tag 'UI5-1.2'");
+    });
+
+    test('Get specification from cache with dist tag refresh', async () => {
+        jest.spyOn(fsMock, 'existsSync').mockReturnValueOnce(false);
+        const npmCommandSpy = jest.spyOn(commandMock, 'execNpmCommand').mockResolvedValueOnce('{"UI5-1.2": "0.1.2"}');
+        jest.spyOn(fileMock, 'writeFile').mockResolvedValueOnce();
+        const logger = getMockLogger();
+        const root = join(__dirname, '../test-data/specification/app');
+        const specification = await getSpecification<Specification>(root, { logger });
+        expect(specification.exec()).toBe('specification-mock');
+        expect(logger.debug).toBeCalledWith(expect.stringContaining('Specification dist-tags not found'));
+        expect(npmCommandSpy).toBeCalledWith(['view', '@sap/ux-specification', 'dist-tags', '--json'], {
+            logger
+        });
     });
 
     test('Get specification with invalid app root, should throw error, with logger', async () => {
