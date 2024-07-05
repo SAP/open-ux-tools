bce637d93ae73da4863f22c3bd2518c8cec2011d
I743583 <kitty.jose@sap.com>
2024-07-05 08:40:57 +0100

    merge latest updates from odata-service-writer

--- packages/odata-service-writer/src/index.ts
+++ packages/odata-service-writer/src/index.ts
@@ -144,8 +144,8 @@ async function generate(basePath: string, service: OdataService, fs?: Editor): P
         }
     }
 
-    // update package.json if required and for non-cap applications
-    if (!serviceIsCds(service) && paths.packageJson && paths.ui5Yaml) {
+    // update package.json for non-cap applications
+    if (isServiceTypeEdmx && paths.packageJson && paths.ui5Yaml) {
         updatePackageJson(paths.packageJson, fs, !!service.metadata);
     }
 
