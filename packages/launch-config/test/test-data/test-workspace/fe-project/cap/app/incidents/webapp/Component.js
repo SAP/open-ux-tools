d9120a33041c3dea01a03c072c9dea06fc71c890
broksy <ainis.broks@sap.com>
2024-07-05 15:41:50 +0300

    chore: cap test data

--- packages/launch-config/test/test-data/test-workspace/fe-project/cap/app/incidents/webapp/Component.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define(['sap/fe/core/AppComponent'], function(AppComponent) {
-    'use strict';
-
-    return AppComponent.extend('sap.fe.demo.incidents.Component', {
-        metadata: {
-            manifest: 'json'
-        }
-    });
-});
