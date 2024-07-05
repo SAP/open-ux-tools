51da72074852027e93f4f842f580880669ed3fac
broksy <ainis.broks@sap.com>
2024-07-05 14:38:42 +0300

    chore: test data

--- packages/launch-config/test/test-data/test-workspace/fe-project/v2lrop/webapp/Component.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
-    return AppComponent.extend('test.v2.lrop.v2lrop.Component', {
-        metadata: {
-            manifest: 'json'
-        }
-    });
-});
