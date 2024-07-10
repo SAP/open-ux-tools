6f716b27e5fe2f71c6494de4586f26c8ef5fbb86
broksy <ainis.broks@sap.com>
2024-07-10 14:49:52 +0300

    chore: combined test files

--- packages/launch-config/test/launch-config-crud/create.test.ts
+++ packages/launch-config/test/launch-config-crud/create.test.ts
@@ -2,8 +2,9 @@ import { join } from 'path';
 import { create as createStorage } from 'mem-fs';
 import { create } from 'mem-fs-editor';
 import { createLaunchConfig } from '../../src/launch-config-crud/create';
-import { FileName } from '@sap-ux/project-access';
+import { DirName, FileName } from '@sap-ux/project-access';
 import { TestPaths } from '../test-data/utils';
+import { getUI5VersionUri } from '../../src';
 
 describe('create', () => {
     const memFs = create(createStorage());
@@ -31,6 +32,59 @@ describe('create', () => {
             })
         };
         const launchJSONPath = join(TestPaths.tmpDir, '.vscode', 'launch.json');
+        expect(result.exists(launchJSONPath)).toBe(true);
+        expect(result.readJSON(launchJSONPath)).toStrictEqual({
+            version: '0.2.0',
+            configurations: [
+                {
+                    console: 'internalConsole',
+                    cwd: join('${workspaceFolder}', 'fe-project'),
+                    env: expectedEnv,
+                    internalConsoleOptions: 'openOnSessionStart',
+                    name: 'LaunchConfig_One',
+                    outputCapture: 'std',
+                    request: 'launch',
+                    runtimeArgs: ['fiori', 'run'],
+                    runtimeExecutable: 'npx',
+                    type: 'node',
+                    windows: {
+                        runtimeExecutable: 'npx.cmd'
+                    }
+                }
+            ]
+        });
+    });
+
+    test('launch.json file is missing, create new file with new config (include UI5 version and backend config)', async () => {
+        const launchJSONPath = join(TestPaths.tmpDir, DirName.VSCode, 'launch.json');
+        // delete existing file
+        memFs.delete(launchJSONPath);
+        // Select UI5 version
+        const ui5Version = 'myLatest';
+        // Set ui5VersionUri
+        const ui5VersionUri = getUI5VersionUri(ui5Version);
+        // Select destination
+        const backendConfigs = [{ path: 'TEST_PATH', name: 'TEST_DESTINAME', url: 'dummy' }];
+        const result = await createLaunchConfig(
+            TestPaths.tmpDir,
+            {
+                name: 'LaunchConfig_One',
+                projectRoot: join(TestPaths.tmpDir, 'fe-project'),
+                ui5Version,
+                ui5VersionUri,
+                backendConfigs
+            },
+            memFs
+        );
+        const expectedEnv = {
+            FIORI_TOOLS_BACKEND_CONFIG: JSON.stringify(backendConfigs),
+            FIORI_TOOLS_UI5_URI: 'https://ui5.sap.com',
+            FIORI_TOOLS_UI5_VERSION: 'myLatest',
+            'run.config': JSON.stringify({
+                handlerId: 'fiori_tools',
+                runnableId: join(TestPaths.tmpDir, 'fe-project')
+            })
+        };
         expect(result.exists(launchJSONPath)).toBe(true);
         expect(result.readJSON(launchJSONPath)).toStrictEqual({
             version: '0.2.0',
