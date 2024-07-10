9638586e56840d733c2a71df510f3b5f9c263820
Oksana Korotkova <oksana.korotkova@sap.com>
2024-07-10 11:43:50 +0100

    fix(ui5-application-writer): code review

--- packages/ui5-application-writer/src/types.ts
+++ packages/ui5-application-writer/src/types.ts
@@ -51,7 +51,7 @@ export interface UI5 {
     descriptorVersion: string;
     ui5Libs: string | string[];
     ui5Theme: string;
-    customUi5Libs?: string | string[];
+    customUi5Libs?: string[];
 }
 
 // Additional configurable features
