facbdbc8abe4754758cf0656fd1ea738feb6203a
Nikita Baranov <nikmacentosh@gmail.com>
2024-07-15 12:15:51 +0300

    refactor: update jsdoc text

--- packages/adp-tooling/src/base/helper.ts
+++ packages/adp-tooling/src/base/helper.ts
@@ -5,10 +5,10 @@ import type { UI5FlexLayer } from '@sap-ux/project-access';
 import type { DescriptorVariant } from '../types';
 
 /**
- * Check environment is running in an internal scenario.
+ * Check environment is running in an customer scenario.
  *
  * @param layer - UI5 Flex layer
- * @returns true if running in an internal scenario, false otherwise
+ * @returns true if running in an customer scenario, false otherwise
  */
 export function isCustomerBase(layer: UI5FlexLayer): boolean {
     return layer === 'CUSTOMER_BASE';
