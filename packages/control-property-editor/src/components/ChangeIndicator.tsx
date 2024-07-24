904ce6ed4349fe052f440b3f493206c5fb39d374
Emre Cevik <emre.cevik@sap.com>
2024-07-24 10:51:04 +0200

    The tooltip text of the green circles for the saving state when hovering has been changed.

--- packages/control-property-editor/src/components/ChangeIndicator.tsx
+++ packages/control-property-editor/src/components/ChangeIndicator.tsx
@@ -18,6 +18,7 @@ export function ChangeIndicator(props: ChangeIndicatorProps): ReactElement {
     const { saved, pending, id } = props;
     const rest = { id };
     const color = 'var(--vscode-terminal-ansiGreen)';
+    const type = id == 'sapUshellDashboardPage--ChangeIndicator' ? 'control' : 'property';
 
     if (saved > 0 && pending === 0) {
         return (
@@ -29,7 +30,7 @@ export function ChangeIndicator(props: ChangeIndicatorProps): ReactElement {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
                 {...rest}>
-                <ChangeIndicatorTooltip {...props} />
+                <ChangeIndicatorTooltip changeIndicatorProps={props} type={type} />
                 <circle cx="4" cy="4" r="4" fill={color} />
             </svg>
         );
@@ -45,7 +46,7 @@ export function ChangeIndicator(props: ChangeIndicatorProps): ReactElement {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
                 {...rest}>
-                <ChangeIndicatorTooltip {...props} />
+                <ChangeIndicatorTooltip changeIndicatorProps={props} type={type} />
                 <circle cx="4" cy="4" r="3.5" stroke={color} />
             </svg>
         );
@@ -53,7 +54,7 @@ export function ChangeIndicator(props: ChangeIndicatorProps): ReactElement {
 
     return (
         <svg role="img" width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg" {...rest}>
-            <ChangeIndicatorTooltip {...props} />
+            <ChangeIndicatorTooltip changeIndicatorProps={props} type={type} />
             <circle cx="4" cy="4" r="3.5" stroke={color} />
             <path d="M4 8a4 4 0 1 0 0-8v8Z" fill={color} />
         </svg>
@@ -63,18 +64,20 @@ export function ChangeIndicator(props: ChangeIndicatorProps): ReactElement {
 /**
  * React element for change indicator tooltip.
  *
- * @param changeIndicatorProps - ChangeIndicatorProps
+ * @param props - props
+ * @param props.changeIndicatorProps - ChangeIndicatorProps
+ * @param props.type  - either control or property
  * @returns ReactElement
  */
-function ChangeIndicatorTooltip(changeIndicatorProps: ChangeIndicatorProps): ReactElement {
-    const { saved, pending } = changeIndicatorProps;
+function ChangeIndicatorTooltip(props: { changeIndicatorProps: ChangeIndicatorProps; type: string }): ReactElement {
+    const { saved, pending } = props.changeIndicatorProps;
     const { t } = useTranslation();
     if (saved > 0 && pending === 0) {
-        return <title>{t('SAVED_CHANGES')}</title>;
+        return <title>{t('SAVED_CHANGES', { type: props.type })}</title>;
     }
 
     if (pending > 0 && saved === 0) {
-        return <title>{t('UNSAVED_CHANGES')}</title>;
+        return <title>{t('UNSAVED_CHANGES', { type: props.type })}</title>;
     }
-    return <title>{t('SAVED_AND_UNSAVED_CHANGES')}</title>;
+    return <title>{t('SAVED_AND_UNSAVED_CHANGES', { type: props.type })}</title>;
 }
