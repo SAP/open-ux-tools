4f98bac8d2d902489a9047a332f407e1d3c721ce
emrecevik06 <emreali9906@hotmail.com>
2024-07-25 08:42:59 +0200

    Removed unnecessary lines

--- packages/control-property-editor/src/components/ChangeIndicator.tsx
+++ packages/control-property-editor/src/components/ChangeIndicator.tsx
@@ -16,7 +16,7 @@ interface ChangeIndicatorProps {
  * @returns ReactElement
  */
 export function ChangeIndicator(props: ChangeIndicatorProps): ReactElement {
-    const { saved, pending, id, type } = props;
+    const { saved, pending, id } = props;
     const rest = { id };
     const color = 'var(--vscode-terminal-ansiGreen)';
 
@@ -30,7 +30,7 @@ export function ChangeIndicator(props: ChangeIndicatorProps): ReactElement {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
                 {...rest}>
-                <ChangeIndicatorTooltip changeIndicatorProps={props} type={type} />
+                <ChangeIndicatorTooltip {...props} />
                 <circle cx="4" cy="4" r="4" fill={color} />
             </svg>
         );
@@ -46,7 +46,7 @@ export function ChangeIndicator(props: ChangeIndicatorProps): ReactElement {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
                 {...rest}>
-                <ChangeIndicatorTooltip changeIndicatorProps={props} type={type} />
+                <ChangeIndicatorTooltip {...props} />
                 <circle cx="4" cy="4" r="3.5" stroke={color} />
             </svg>
         );
@@ -54,7 +54,7 @@ export function ChangeIndicator(props: ChangeIndicatorProps): ReactElement {
 
     return (
         <svg role="img" width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg" {...rest}>
-            <ChangeIndicatorTooltip changeIndicatorProps={props} type={type} />
+            <ChangeIndicatorTooltip {...props} />
             <circle cx="4" cy="4" r="3.5" stroke={color} />
             <path d="M4 8a4 4 0 1 0 0-8v8Z" fill={color} />
         </svg>
@@ -64,22 +64,18 @@ export function ChangeIndicator(props: ChangeIndicatorProps): ReactElement {
 /**
  * React element for change indicator tooltip.
  *
- * @param props - props
- * @param props.changeIndicatorProps - ChangeIndicatorProps
- * @param props.type  - either control or property
+ * @param changeIndicatorProps - ChangeIndicatorProps
  * @returns ReactElement
  */
-function ChangeIndicatorTooltip(
-    props: Readonly<{ changeIndicatorProps: ChangeIndicatorProps; type: string }>
-): ReactElement {
-    const { saved, pending } = props.changeIndicatorProps;
+function ChangeIndicatorTooltip(changeIndicatorProps: ChangeIndicatorProps): ReactElement {
+    const { saved, pending, type } = changeIndicatorProps;
     const { t } = useTranslation();
     if (saved > 0 && pending === 0) {
-        return <title>{t('SAVED_CHANGES', { type: props.type })}</title>;
+        return <title>{t('SAVED_CHANGES', { type: type })}</title>;
     }
 
     if (pending > 0 && saved === 0) {
-        return <title>{t('UNSAVED_CHANGES', { type: props.type })}</title>;
+        return <title>{t('UNSAVED_CHANGES', { type: type })}</title>;
     }
-    return <title>{t('SAVED_AND_UNSAVED_CHANGES', { type: props.type })}</title>;
+    return <title>{t('SAVED_AND_UNSAVED_CHANGES', { type: type })}</title>;
 }
