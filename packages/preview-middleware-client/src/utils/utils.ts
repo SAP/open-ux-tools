5a559dc6d3df40b992549a159ebff39b782e46dd
D048415 <dominik.heim@sap.com>
2024-08-01 12:10:54 +0200

    use fallback ui5 version instead of throwing error

--- packages/preview-middleware-client/src/utils/utils.ts
+++ packages/preview-middleware-client/src/utils/utils.ts
@@ -1,13 +1,15 @@
 import type {SingleVersionInfo} from '../../types/global';
 import VersionInfo from 'sap/ui/VersionInfo';
+import Log from 'sap/base/Log';
 
 /**
  * Retrieve the UI5 version from sap.ui.core library
  */
 export async function getUi5Version() {
-    const version = (await VersionInfo.load({library:'sap.ui.core'}) as SingleVersionInfo)?.version;
+    let version = (await VersionInfo.load({library:'sap.ui.core'}) as SingleVersionInfo)?.version;
     if (!version) {
-        throw new Error('Could not get UI5 version');
+        Log.error('Could not get UI5 version of application. Using \'1.121.0\' as fallback.');
+        version = '1.121.0';
     }
     return version;
 }
