a6a00bb1b90800321a74268480f898dc3faa6e8c
D048415 <dominik.heim@sap.com>
2024-08-02 14:59:50 +0200

    improve test

--- packages/preview-middleware-client/test/unit/utils/version.test.ts
+++ packages/preview-middleware-client/test/unit/utils/version.test.ts
@@ -4,11 +4,12 @@ import {SingleVersionInfo} from '../../../types/global';
 
 describe('utils/version', () => {
     test('getUi5Version', async () => {
-        jest.spyOn(VersionInfo, 'load').mockResolvedValueOnce({
+        const versionInfoLoadMock = jest.spyOn(VersionInfo, 'load').mockResolvedValueOnce({
             name: 'sap.ui.core',
             version: '1.124.0'
         } as SingleVersionInfo);
         const version = await getUi5Version();
+        expect(versionInfoLoadMock).toBeCalledWith({library: 'sap.ui.core'});
         expect(version).toEqual('1.124.0');
     });
     test('getUi5Version fallback to 1.121.0', async () => {
