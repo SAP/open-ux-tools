1bf0ea9b3d43e3f1dc341254fd9672ed508b93c1
D048415 <dominik.heim@sap.com>
2024-08-02 15:12:53 +0200

    prettier

--- packages/preview-middleware-client/src/utils/version.ts
+++ packages/preview-middleware-client/src/utils/version.ts
@@ -1,4 +1,4 @@
-import type {SingleVersionInfo} from '../../types/global';
+import type { SingleVersionInfo } from '../../types/global';
 import VersionInfo from 'sap/ui/VersionInfo';
 import Log from 'sap/base/Log';
 
@@ -6,9 +6,9 @@ import Log from 'sap/base/Log';
  * Retrieve the UI5 version from sap.ui.core library
  */
 export async function getUi5Version() {
-    let version = (await VersionInfo.load({library:'sap.ui.core'}) as SingleVersionInfo)?.version;
+    let version = ((await VersionInfo.load({ library: 'sap.ui.core' })) as SingleVersionInfo)?.version;
     if (!version) {
-        Log.error('Could not get UI5 version of application. Using \'1.121.0\' as fallback.');
+        Log.error('Could not get UI5 version of application. Using 1.121.0 as fallback.');
         version = '1.121.0';
     }
     return version;
--- packages/preview-middleware-client/test/unit/utils/version.test.ts
+++ packages/preview-middleware-client/test/unit/utils/version.test.ts
@@ -1,6 +1,6 @@
-import {getUi5Version} from 'open/ux/preview/client/utils/version';
+import { getUi5Version } from 'open/ux/preview/client/utils/version';
 import VersionInfo from 'mock/sap/ui/VersionInfo';
-import {SingleVersionInfo} from '../../../types/global';
+import { SingleVersionInfo } from '../../../types/global';
 
 describe('utils/version', () => {
     test('getUi5Version', async () => {
@@ -9,11 +9,11 @@ describe('utils/version', () => {
             version: '1.124.0'
         } as SingleVersionInfo);
         const version = await getUi5Version();
-        expect(versionInfoLoadMock).toBeCalledWith({library: 'sap.ui.core'});
+        expect(versionInfoLoadMock).toBeCalledWith({ library: 'sap.ui.core' });
         expect(version).toEqual('1.124.0');
     });
     test('getUi5Version fallback to 1.121.0', async () => {
         const version = await getUi5Version();
         expect(version).toEqual('1.121.0');
     });
-})
+});
