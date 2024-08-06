02ea9440be2f94591910863b76678b93b38ffc02
i501270 <120391284+nahmed22@users.noreply.github.com>
2024-08-06 14:57:39 +0200

    fix: code smells

--- packages/text-document-utils/src/line-offsets.ts
+++ packages/text-document-utils/src/line-offsets.ts
@@ -1,14 +1,14 @@
 /**
  * Computes line offsets for the given string.
  *
- * @param text
- * @returns
+ * @param text - The input text
+ * @returns - An array of line offsets
  */
 export function getLineOffsets(text: string): number[] {
     const lineOffsets: number[] = [0];
     let index = 0;
     while (index < text.length) {
-        const match = text.substr(index).match(/[\r\n]/);
+        const match = /[\r\n]/.exec(text.slice(index));
         if (match?.index !== undefined) {
             index += match.index + 1;
             if (text[index - 1] === '\r' && text[index] === '\n') {
