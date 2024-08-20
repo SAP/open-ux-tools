92606e0b12ab1f93dbbfedd69276fdbf2f9e02ec
Nikita Baranov <nikmacentosh@gmail.com>
2024-08-20 16:24:44 +0300

    test: add test suites for helpers

---
+++ packages/adp-tooling/test/unit/prompts/creation/questions/helper/additional-messages.test.ts
@@ -0,0 +1,193 @@
+import { Severity } from '@sap-devx/yeoman-ui-types';
+import { AdaptationProjectType } from '@sap-ux/axios-extension';
+
+import { t } from '../../../../../../src/i18n';
+import type { AppIdentifier } from '../../../../../../src/prompts/creation/identifier';
+import type { Application, ConfigInfoPrompter, ConfigurationInfoAnswers } from '../../../../../../src';
+import {
+    appAdditionalMessages,
+    projectTypeAdditionalMessages,
+    systemAdditionalMessages,
+    versionAdditionalMessages
+} from '../../../../../../src';
+
+describe('additional messages', () => {
+    describe('systemAdditionalMessages', () => {
+        it('should return undefined if no project types are available', () => {
+            const result = systemAdditionalMessages(undefined, {
+                adaptationProjectTypes: [],
+                activeLanguages: []
+            });
+            expect(result).toBeUndefined();
+        });
+
+        it('should return undefined if it is a cloud project', () => {
+            const flexUISystem = { isOnPremise: false, isUIFlex: false };
+            const result = systemAdditionalMessages(flexUISystem, {
+                adaptationProjectTypes: [AdaptationProjectType.CLOUD_READY],
+                activeLanguages: []
+            });
+            expect(result).toBeUndefined();
+        });
+
+        it('should return undefined if all conditions pass', () => {
+            const flexUISystem = { isOnPremise: true, isUIFlex: true };
+            const result = systemAdditionalMessages(flexUISystem, {
+                adaptationProjectTypes: [AdaptationProjectType.ON_PREMISE, AdaptationProjectType.CLOUD_READY],
+                activeLanguages: []
+            });
+            expect(result).toBeUndefined();
+        });
+
+        it('should return error message when system is not on premise and UI Flex is disabled', () => {
+            const flexUISystem = { isOnPremise: false, isUIFlex: false };
+            const systemInfo = { adaptationProjectTypes: [AdaptationProjectType.ON_PREMISE], activeLanguages: [] };
+            const expected = {
+                message: t('validators.notDeployableNotFlexEnabledSystemError'),
+                severity: Severity.error
+            };
+            const result = systemAdditionalMessages(flexUISystem, systemInfo);
+            expect(result).toEqual(expected);
+        });
+
+        it('should return error message when system is not on premise and UI Flex is enabled', () => {
+            const flexUISystem = { isOnPremise: false, isUIFlex: true };
+            const systemInfo = { adaptationProjectTypes: [AdaptationProjectType.ON_PREMISE], activeLanguages: [] };
+            const expected = {
+                message: t('validators.notDeployableSystemError'),
+                severity: Severity.error
+            };
+            const result = systemAdditionalMessages(flexUISystem, systemInfo);
+            expect(result).toEqual(expected);
+        });
+
+        it('should return warning when system is on premise but UI Flex is disabled', () => {
+            const flexUISystem = { isOnPremise: true, isUIFlex: false };
+            const systemInfo = { adaptationProjectTypes: [AdaptationProjectType.ON_PREMISE], activeLanguages: [] };
+            const expected = {
+                message: t('validators.notFlexEnabledError'),
+                severity: Severity.warning
+            };
+            const result = systemAdditionalMessages(flexUISystem, systemInfo);
+            expect(result).toEqual(expected);
+        });
+    });
+
+    describe('appAdditionalMessages', () => {
+        const app = { id: '1', title: 'Test App' };
+
+        it('should return undefined if no application is provided', () => {
+            const result = appAdditionalMessages(undefined as unknown as Application, {} as ConfigInfoPrompter);
+            expect(result).toBeUndefined();
+        });
+
+        it('should provide informational message if appSync is true and application is supported', () => {
+            const expected = {
+                message: t('prompts.appInfoLabel'),
+                severity: Severity.information
+            };
+            const prompter = {
+                isApplicationSupported: true,
+                appIdentifier: {
+                    appSync: true
+                } as unknown as AppIdentifier
+            } as ConfigInfoPrompter;
+
+            const result = appAdditionalMessages(app as Application, prompter);
+            expect(result).toEqual(expected);
+        });
+
+        it('should provide warning message if adp over adp is not supported', () => {
+            const expected = {
+                message: t('prompts.notSupportedAdpOverAdpLabel'),
+                severity: Severity.warning
+            };
+            const prompter = {
+                isApplicationSupported: true,
+                appIdentifier: {
+                    appSync: false,
+                    getIsSupportedAdpOverAdp: jest.fn().mockReturnValue(false),
+                    getIsPartiallySupportedAdpOverAdp: jest.fn().mockReturnValue(false)
+                } as unknown as AppIdentifier
+            } as ConfigInfoPrompter;
+
+            const result = appAdditionalMessages(app as Application, prompter);
+            expect(result).toEqual(expected);
+        });
+
+        it('should provide warning when adp over adp is partially supported', () => {
+            const expected = {
+                message: t('prompts.isPartiallySupportedAdpOverAdpLabel'),
+                severity: Severity.warning
+            };
+            const prompter = {
+                isApplicationSupported: true,
+                appIdentifier: {
+                    appSync: false,
+                    isV4AppInternalMode: false,
+                    getIsSupportedAdpOverAdp: jest.fn().mockReturnValue(true),
+                    getIsPartiallySupportedAdpOverAdp: jest.fn().mockReturnValue(true)
+                } as unknown as AppIdentifier
+            } as ConfigInfoPrompter;
+            const result = appAdditionalMessages(app as Application, prompter);
+            expect(result).toEqual(expected);
+        });
+
+        it('should provide warning for V4 apps in internal mode', () => {
+            const expected = {
+                message: t('prompts.v4AppNotOfficialLabel'),
+                severity: Severity.warning
+            };
+            const prompter = {
+                appIdentifier: {
+                    appSync: false,
+                    isV4AppInternalMode: true,
+                    getIsSupportedAdpOverAdp: jest.fn().mockReturnValue(true),
+                    getIsPartiallySupportedAdpOverAdp: jest.fn().mockReturnValue(false)
+                } as unknown as AppIdentifier
+            } as ConfigInfoPrompter;
+            const result = appAdditionalMessages(app as Application, prompter);
+            expect(result).toEqual(expected);
+        });
+    });
+
+    describe('versionAdditionalMessages', () => {
+        const prompter = {
+            ui5VersionDetected: false,
+            isCloudProject: false,
+            hasSystemAuthentication: true,
+            isLoginSuccessfull: true,
+            shouldAuthenticate: jest.fn().mockReturnValue(false)
+        } as unknown as ConfigInfoPrompter;
+
+        it('should provide a warning when UI5 version is not detected', () => {
+            const answers = { system: 'U1Y_010' } as ConfigurationInfoAnswers;
+            const expected = {
+                message: t('validators.ui5VersionNotDetectedError'),
+                severity: Severity.warning
+            };
+            const result = versionAdditionalMessages(answers, prompter);
+            expect(result).toEqual(expected);
+        });
+    });
+
+    describe('projectTypeAdditionalMessages', () => {
+        const prompter = {
+            shouldAuthenticate: jest.fn().mockReturnValue(false),
+            isCloudProject: true,
+            ui5Manager: {
+                latestVersion: '1.127.0'
+            }
+        } as unknown as ConfigInfoPrompter;
+
+        it('should return information message when the project is cloud based', () => {
+            const answers = { system: 'CAC_080' } as ConfigurationInfoAnswers;
+            const expected = {
+                message: t('prompts.currentUI5VersionLabel', { version: '1.127.0' }),
+                severity: Severity.information
+            };
+            const result = projectTypeAdditionalMessages(answers, prompter);
+            expect(result).toEqual(expected);
+        });
+    });
+});
---
+++ packages/adp-tooling/test/unit/prompts/creation/questions/helper/conditions.test.ts
@@ -0,0 +1,154 @@
+import { AdaptationProjectType } from '@sap-ux/axios-extension';
+import {
+    showInternalQuestions,
+    showUI5VersionQuestion,
+    showProjectTypeQuestion,
+    showApplicationErrorQuestion,
+    showApplicationQuestion,
+    showCredentialQuestion,
+    showExtensionProjectQuestion
+} from '../../../../../../src/prompts/creation/questions/helper/conditions';
+import type { AppIdentifier } from '../../../../../../src/prompts/creation/identifier';
+import type { Application, ConfigInfoPrompter, ConfigurationInfoAnswers } from '../../../../../../src';
+
+jest.mock('@sap-ux/btp-utils', () => ({
+    isAppStudio: jest.fn().mockReturnValue(true)
+}));
+
+describe('Configuration Visibility Tests', () => {
+    let prompter = {} as ConfigInfoPrompter;
+    let answers: ConfigurationInfoAnswers;
+
+    beforeEach(() => {
+        prompter.isCustomerBase = false;
+        prompter.shouldAuthenticate = jest.fn().mockReturnValue(false);
+        prompter.isApplicationSupported = true;
+        prompter.isCloudProject = false;
+        prompter.hasSystemAuthentication = true;
+        prompter.isLoginSuccessfull = true;
+        prompter.appIdentifier = {
+            appSync: false
+        } as AppIdentifier;
+        prompter.systemInfo = {
+            adaptationProjectTypes: [AdaptationProjectType.ON_PREMISE, AdaptationProjectType.CLOUD_READY],
+            activeLanguages: []
+        };
+        prompter.flexUISystem = {
+            isOnPremise: true,
+            isUIFlex: false
+        };
+
+        answers = {
+            system: 'U1Y_010',
+            application: {
+                id: '1'
+            }
+        } as ConfigurationInfoAnswers;
+    });
+
+    describe('showInternalQuestions', () => {
+        it('should return true when all conditions are met', () => {
+            const result = showInternalQuestions(answers, prompter);
+            expect(result).toBeTruthy();
+        });
+
+        it('should return false if system is not defined', () => {
+            answers.system = undefined as unknown as string;
+            const result = showInternalQuestions(answers, prompter);
+            expect(result).toBeFalsy();
+        });
+
+        it('should return false if isCustomerBase is true', () => {
+            prompter.isCustomerBase = true;
+            const result = showInternalQuestions(answers, prompter);
+            expect(result).toBeFalsy();
+        });
+    });
+
+    describe('showUI5VersionQuestion', () => {
+        it('should return true when all conditions are met and system is onPremise', () => {
+            const result = showUI5VersionQuestion(answers, prompter);
+            expect(result).toBeTruthy();
+        });
+
+        it('should return false if system info is missing adaptation project types', () => {
+            prompter.systemInfo.adaptationProjectTypes = [];
+            const result = showUI5VersionQuestion(answers, prompter);
+            expect(result).toBeFalsy();
+        });
+
+        it('should return false if the project is cloud ready', () => {
+            prompter.isCloudProject = true;
+            const result = showUI5VersionQuestion(answers, prompter);
+            expect(result).toBeFalsy();
+        });
+    });
+
+    describe('showProjectTypeQuestion', () => {
+        it('should return true if system is present and authentication is not required', () => {
+            const result = showProjectTypeQuestion(answers, prompter);
+            expect(result).toBeTruthy();
+        });
+
+        it('should return false if system is undefined', () => {
+            answers.system = undefined as unknown as string;
+            const result = showProjectTypeQuestion(answers, prompter);
+            expect(result).toBeFalsy();
+        });
+    });
+
+    describe('showApplicationQuestion', () => {
+        it('should return true when all conditions are met', () => {
+            const result = showApplicationQuestion(answers, prompter);
+            expect(result).toBeTruthy();
+        });
+
+        it('should return false if system info lacks adaptation project types', () => {
+            prompter.systemInfo.adaptationProjectTypes = [];
+            const result = showApplicationQuestion(answers, prompter);
+            expect(result).toBeFalsy();
+        });
+    });
+
+    describe('showCredentialQuestion', () => {
+        it('should return true if system is defined and has authentication', () => {
+            const result = showCredentialQuestion(answers, prompter);
+            expect(result).toBeTruthy();
+        });
+
+        it('should return false if system is undefined', () => {
+            answers.system = undefined as unknown as string;
+            const result = showCredentialQuestion(answers, prompter);
+            expect(result).toBeFalsy();
+        });
+    });
+
+    describe('showApplicationErrorQuestion', () => {
+        it('should return true when conditions trigger an application error', () => {
+            prompter.isApplicationSupported = false;
+            prompter.flexUISystem = { isOnPremise: true, isUIFlex: true };
+            const result = showApplicationErrorQuestion(answers, prompter);
+            expect(result).toBeTruthy();
+        });
+
+        it('should return false when application is not defined', () => {
+            answers.application = undefined as unknown as Application;
+            const result = showApplicationErrorQuestion(answers, prompter);
+            expect(result).toBeFalsy();
+        });
+    });
+
+    describe('showExtensionProjectQuestion', () => {
+        it('should return true if conditions allow for an extension project', () => {
+            prompter.isCloudProject = false;
+            const result = showExtensionProjectQuestion(answers, prompter);
+            expect(result).toBeTruthy();
+        });
+
+        it('should return false if cloud project is true', () => {
+            prompter.isCloudProject = true;
+            const result = showExtensionProjectQuestion(answers, prompter);
+            expect(result).toBeFalsy();
+        });
+    });
+});
