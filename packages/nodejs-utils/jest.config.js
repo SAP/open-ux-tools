d3d10c7eeeb00fce774a205ddbd8bd11719b8603
Austin Devine <austin.devine@sap.com>
2024-08-21 16:23:47 +0100

    WIP nodejs-utils and btp-utils updates

--- packages/nodejs-utils/jest.config.js
+++ packages/nodejs-utils/jest.config.js
@@ -1,11 +1,2 @@
 const config = require('../../jest.base');
-config.setupFilesAfterEnv = ['jest-extended/all'];
-config.transform = {
-    '^.+\\.tsx?$': [
-        'ts-jest',
-        {
-            tsconfig: 'test/tsconfig.json'
-        }
-    ]
-};
 module.exports = config;
