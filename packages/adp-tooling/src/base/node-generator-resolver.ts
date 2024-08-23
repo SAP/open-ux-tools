6d395b63c1e16105d873c3bce51cc8646da5c0f1
Nikita Baranov <nikmacentosh@gmail.com>
2024-08-23 13:01:33 +0300

    refactor: improve jsdoc

--- packages/adp-tooling/src/base/node-generator-resolver.ts
+++ packages/adp-tooling/src/base/node-generator-resolver.ts
@@ -2,6 +2,11 @@ import { resolve } from 'path';
 
 /**
  * Attempts to resolve the path to a specific node module generator from the NODE_PATH environment variable.
+ * This is particularly used in the prompt for extension projects within SAP Business Application Studio (BAS)
+ * when an application is not supported by the adaptation project. It functions by resolving the path to the
+ * generator which is then utilized with `this.composeWith()` of the Yeoman generator. If the path to the generator
+ * is found, it returns the path, allowing the extension project to continue. If no path is found, it indicates that
+ * the Extensibility Generator is not installed in the development space, preventing the user from proceeding.
  *
  * @returns {string | undefined} The resolved path to the generator module if found, or undefined if not found.
  */
