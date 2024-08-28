1a99abc0545944a31a963687cda4f40685970dd9
Martin Januska <140699298+martinjanus-sap@users.noreply.github.com>
2024-08-28 12:06:13 +0100

    fix(fiori-elements-writer): manifest separation ui5 libs (#2280)
    * fix(fiori-elements-writer): manifest seperation ui5 libs
    
    * fix(fiori-elements-writer): ff snapshot
    
    * fix(fiori-elements-writer): snapshots
    
    * fix(fiori-elements-writer): docs ui5 interface
    
    * fix(fiori-elements-writer): unit test manifestLibs
    
    * fix(fiori-elements-writer): ui5 interface descriptions
    
    * fix(fiori-elements-writer): changeset
    
    * fix(fiori-elements-writer): ff writer to load sap.ushell

--- packages/fiori-elements-writer/src/fpmConfig.ts
+++ packages/fiori-elements-writer/src/fpmConfig.ts
@@ -1,8 +1,6 @@
-import { join } from 'path';
 import type { Editor } from 'mem-fs-editor';
 import { generateCustomPage } from '@sap-ux/fe-fpm-writer';
 import type { FioriElementsApp, FPMSettings } from './types';
-import { UI5Config } from '@sap-ux/ui5-config';
 /**
  * Processes the template for the Flexible Programming Model (FPM).
  * Generates a custom page and updates the UI5 local yaml.
@@ -28,8 +26,4 @@ export async function generateFpmConfig<T extends {}>(
         },
         fs
     );
-    const ui5LocalConfigPath = join(basePath, 'ui5-local.yaml');
-    const ui5LocalConfig = await UI5Config.newInstance(fs.read(ui5LocalConfigPath));
-    ui5LocalConfig.addUI5Libs(['sap.fe.templates']);
-    fs.write(ui5LocalConfigPath, ui5LocalConfig.toString());
 }
