87ea59dd9c8c7d64f00d32b9519b8b6acb3a4710
Austin Devine <austin.devine@sap.com>
2024-09-05 13:58:56 +0100

    bundle templates inline

--- packages/fe-fpm-writer/esbuild.js
+++ packages/fe-fpm-writer/esbuild.js
@@ -1,12 +1,18 @@
-const pkg = require("./package.json")
+const pkg = require('./package.json');
 
 require('esbuild').buildSync({
     bundle: true,
     loader: { '.data': 'binary' },
-    outdir: './dist/templates',
-    entryPoints: ['src/templates/index.ts'],
+    outdir: './dist',
+    entryPoints: ['src/index.ts'],
+    external: [
+        ...Object.keys(pkg.dependencies || {}),
+        ...Object.keys(pkg.peerDependencies || {}),
+        ...Object.keys(pkg.devDependencies || {})
+    ],
     mainFields: ['module', 'main'], // https://stackoverflow.com/a/69352281
     target: 'node18',
     platform: 'node',
-    format: 'cjs'
+    format: 'cjs',
+    sourcemap: true
 });
