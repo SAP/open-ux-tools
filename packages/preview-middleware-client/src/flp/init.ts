6a5286ea3d6633109ef9a3444d4108fd34707a52
D048415 <dominik.heim@sap.com>
2024-10-01 21:15:43 +0200

    refactoring

--- packages/preview-middleware-client/src/flp/init.ts
+++ packages/preview-middleware-client/src/flp/init.ts
@@ -267,7 +267,8 @@ export async function init({
     customInit?: string | null;
 }): Promise<void> {
     const urlParams = new URLSearchParams(window.location.search);
-    const container = sap?.ushell?.Container ?? await import('sap/ushell/Container');
+    let container = sap?.ushell?.Container ??
+        (await import('sap/ushell/Container')).default as unknown as typeof sap.ushell.Container;
     let scenario: string = '';
     const ui5VersionInfo = await getUi5Version();
     // Register RTA if configured
