0ffbf18313b3f70366d1d9fbc7b7a4e73280666c
Jimmy Joseph <jimmy.joseph@sap.com>
2024-10-02 22:14:16 +0200

    fix: retrieve metadata

--- packages/adp-tooling/src/base/abap.ts
+++ packages/adp-tooling/src/base/abap.ts
@@ -29,7 +29,16 @@ export async function getManifest(appId: string, adpConfig: AdpPreviewConfig, lo
     }
     try {
         const response = await provider.get(manifestUrl);
-        return JSON.parse(response.data);
+        const manifest = JSON.parse(response.data);
+
+        // retrieve localmetadata from base project
+        const dataSourceName = Object.keys(manifest?.['sap.app']?.dataSources)?.[0];
+        const metadataUrl = `${appInfo.url}/${manifest?.['sap.app']?.dataSources?.[dataSourceName].settings.localUri}`;
+        const metadata = await provider.get(metadataUrl);
+
+        //otherwise using serviceurl from manifest/$metadata eg: in this format https://services.odata.org/V2/Northwind/Northwind.svc/$metadata
+        console.log(metadata.data);
+        return manifest;
     } catch (error) {
         if (isAxiosError(error)) {
             logger.error('Manifest fetching failed');
