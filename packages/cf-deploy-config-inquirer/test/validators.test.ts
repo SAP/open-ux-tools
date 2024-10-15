67aed0018f13b871d7729030f323b12499f2f303
I743583 <kitty.jose@sap.com>
2024-10-15 13:26:45 +0100

    add validate tests

--- packages/cf-deploy-config-inquirer/test/validators.test.ts
+++ packages/cf-deploy-config-inquirer/test/validators.test.ts
@@ -1,43 +1,36 @@
 import { t } from '../src/i18n';
 import { validateDestinationQuestion } from '../src/prompts/validators';
 
-describe('validateInput', () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
-
-    it('returns true for valid alphanumeric input within length limit', () => {
-        expect(validateDestinationQuestion('valid_input-123')).toBe(true);
-    });
-
-    it('returns empty error message for empty input', () => {
-        expect(validateDestinationQuestion('')).toBe(t('errors.emptyDestinationNameError'));
-    });
-
-    it('returns error for input with invalid characters', () => {
-        expect(validateDestinationQuestion('invalid!@#')).toBe(t('errors.destinationNameError'));
-    });
-
-    it('returns error for input exceeding 200 characters', () => {
-        const longInput = 'a'.repeat(201);
-        expect(validateDestinationQuestion(longInput)).toBe(t('errors.destinationNameLengthError'));
-    });
-});
-
 describe('validateDestinationQuestion', () => {
-    beforeEach(() => {
+    beforeEach(async () => {
         jest.clearAllMocks();
+    });
+
+    const cfServiceInput: [any, any][] = [
+        [' ', t('errors.emptyDestinationNameError')],
+        ['', t('errors.emptyDestinationNameError')],
+        ['ABC ', t('errors.destinationNameError')],
+        ['ABC&', t('errors.destinationNameError')],
+        ['ABC$', t('errors.destinationNameError')],
+        ['ABC abc', t('errors.destinationNameError')],
+        ['a'.repeat(201), t('errors.destinationNameLengthError')],
+        ['ABCabc', true],
+        ['123ABCabc', true],
+        ['123ABCabc123', true],
+        ['_ABCabc123', true],
+        ['-ABCabc123', true],
+        ['ABC', true],
+        ['ABC-abc', true],
+        ['ABC_abc', true],
+        [{}, true]
+    ];
+
+    test.each(cfServiceInput)('Validate destination field %p', (input, toEqual) => {
+        const output = validateDestinationQuestion(input, false);
+        expect(output).toEqual(toEqual);
     });
 
     it('returns true if allowEmptyChoice is true', () => {
         expect(validateDestinationQuestion('', true)).toBe(true);
-    });
-
-    it('validates non-empty input when allowEmptyChoice is false', () => {
-        expect(validateDestinationQuestion('valid_input-123', false)).toBe(true);
-    });
-
-    it('returns error if input is empty and allowEmptyChoice is false', () => {
-        expect(validateDestinationQuestion('', false)).toBe(t('errors.emptyDestinationNameError'));
     });
 });
