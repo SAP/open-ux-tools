2fa3fdac7562ca10baf564c4d6c0f04d93e06b32
kitzkan <kitty.jose@sap.com>
2024-10-16 13:39:18 +0100

    adding `@sap-ux/cf-deploy-config-inquirer` (#2436)
    * cf prompts app gen flow
    
    * changing name to addManagedApprouter
    
    * add overwrite logic
    
    * add test
    
    * update tests and lint
    
    * refactor prompt types
    
    * adding additional choice lists
    
    * cf choice list
    
    * fix choices
    
    * fix test
    
    * fix dest name
    
    * remove reference to sap system
    
    * added propmpts
    
    * added propmpts
    
    * change names of answers for writers
    
    * lint
    
    * read me
    
    * cf comments
    
    * adding changeset
    
    * add js docs
    
    * change name to projectRoot
    
    * Linting auto fix commit
    
    * remove cap reference
    
    * fix readme
    
    * add tests
    
    * fix sonar issues
    
    * search autocomplete feature
    
    * fix auto complete
    
    * change name to destination
    
    * Linting auto fix commit
    
    * adding comments
    
    * Linting auto fix commit
    
    * rename prompt names to cf
    
    * fix dest name message
    
    * rename overwrite prompt
    
    * altering types
    
    * Update cf-deploy-config-inquirer.i18n.json
    
    * adding addBTPdestList Flag
    
    * add log and test
    
    * add validate tests
    
    * add log info
    
    * add warning.btpDestinationListWarning for vscode when no list returned from btpDest
    
    * logging
    
    * update logging
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: J Long <john.long@sap.com>

---
+++ packages/cf-deploy-config-inquirer/src/prompts/validators.ts
@@ -0,0 +1,53 @@
+import { t } from '../i18n';
+import type { CfSystemChoice } from '../types';
+
+/**
+ *
+ * @param input The input string to check for emptiness.
+ * @returns returns true if the input string is not empty, otherwise false.
+ */
+function isNotEmpty(input: string): boolean {
+    return !!input?.trim();
+}
+
+/**
+ * Validates the input string for the following:
+ * - It must not be empty after trimming whitespace.
+ * - It must contain only alphanumeric characters, underscores, or dashes.
+ * - It must not exceed 200 characters.
+ *
+ * @param {string} input - The input string to validate.
+ * @returns {boolean|string} `true` if the input is valid, otherwise an error message.
+ */
+function validateInput(input: string): boolean | string {
+    if (!isNotEmpty(input)) {
+        return t('errors.emptyDestinationNameError');
+    }
+    const result = /^[a-z0-9_-]+$/i.test(input);
+    if (!result) {
+        return t('errors.destinationNameError');
+    }
+    if (input.length > 200) {
+        return t('errors.destinationNameLengthError');
+    }
+    return true;
+}
+
+/**
+ * Validates the destination name or input string. If `allowEmptyChoice` is true,
+ * the validation will pass immediately. Otherwise, the input will be validated
+ * against rules (non-empty, valid characters, length check).
+ *
+ * @param {string} input - The destination name or input string to validate.
+ * @param {boolean} allowEmptyChoice - Whether to allow an empty input as a valid choice.
+ * @returns {boolean|string} `true` if the input is valid or empty choices are allowed, otherwise an error message.
+ */
+export function validateDestinationQuestion(
+    input: string | CfSystemChoice,
+    allowEmptyChoice: boolean = false
+): boolean | string {
+    if (allowEmptyChoice) {
+        return true;
+    }
+    return typeof input === 'string' ? validateInput(input) : true;
+}
