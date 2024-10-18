d852caac1d4ab5803a82b5840e4a83af2972ebc8
Andis Redmans <andis.redmans@sap.com>
2024-10-18 16:08:39 +0300

    comment
    comment

--- packages/ui-components/src/theme/index.ts
+++ packages/ui-components/src/theme/index.ts
@@ -1,6 +1,9 @@
 import type { Theme } from '@fluentui/react';
 import { createTheme, loadTheme } from '@fluentui/react';
 
+/**
+ * Method intializes default styles for 'ui-components' theme.
+ */
 export function initTheme(): void {
     const appTheme: Theme = createTheme({
         defaultFontStyle: {
