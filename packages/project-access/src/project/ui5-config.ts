ab5ab30ad7fd41e5b083d1bb68b1f5fd5cad7ce8
D048415 <dominik.heim@sap.com>
2024-10-23 18:27:17 +0200

    project-access: get list of *.y*ml files from project root

--- packages/project-access/src/project/ui5-config.ts
+++ packages/project-access/src/project/ui5-config.ts
@@ -3,6 +3,7 @@ import type { Editor } from 'mem-fs-editor';
 import { UI5Config } from '@sap-ux/ui5-config';
 import { FileName } from '../constants';
 import { fileExists, readFile } from '../file';
+import { readdir } from 'fs';
 
 /**
  * Get path to webapp.
@@ -40,4 +41,40 @@ export async function readUi5Yaml(projectRoot: string, fileName: string, memFs?:
         return await UI5Config.newInstance(yamlString);
     }
     throw Error(`File '${fileName}' not found in project '${projectRoot}'`);
+}
+
+/**
+ * Scans the project directory for ui5 configuration yaml files.
+ *
+ * @param memFs - mem-fs editor instance
+ * @param projectRoot - path to project root, where ui5 configuration y*ml files are located
+ * @returns {Promise<string[]>} list of UI5 configuration yaml file names
+ * @throws {Error} if an error occurs while reading files from projectRoot
+ */
+export async function getAllUi5YamlFileNames(memFs: Editor, projectRoot: string): Promise<string[]> {
+    return new Promise((resolve) => {
+        //use 'fs' here directly because we only create a list of file names without any i/o operations
+        readdir(projectRoot, (error, files) => {
+            if (error) {
+                throw new Error(`Error reading files from directory '${projectRoot}': ${error}`);
+            }
+            const yamlFileNames = new Set<string>();
+            files
+                .filter((file) => file.endsWith('.yaml') || file.endsWith('.yml'))
+                .forEach((file) => {
+                    yamlFileNames.add(file);
+                });
+            //add not yet saved .y*ml files from mem-fs as well
+            const memYamlFiles = memFs.dump(projectRoot, '*.yaml|*.yml');
+            for (const file in memYamlFiles) {
+                if (memYamlFiles[file].state === 'deleted') {
+                    yamlFileNames.delete(file);
+                    continue;
+                }
+                yamlFileNames.add(file);
+            }
+            //todo: schema validation to verify if a yaml file is a valid ui5 configuration yaml
+            resolve(Array.from(yamlFileNames));
+        });
+    });
 }
