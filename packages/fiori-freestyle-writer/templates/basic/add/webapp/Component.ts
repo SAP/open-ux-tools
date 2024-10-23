a11c4fb28f5642a67ff270c207861be4a4f06f4d
Florian Vogt <florian.vogt@sap.com>
2024-10-23 18:29:59 +0200

    refactor: Update templates regarding UI5 best practices

--- packages/fiori-freestyle-writer/templates/basic/add/webapp/Component.ts
+++ packages/fiori-freestyle-writer/templates/basic/add/webapp/Component.ts
@@ -7,22 +7,20 @@ import { createDeviceModel } from "./model/models";
 export default class Component extends BaseComponent {
 
 	public static metadata = {
-		manifest: "json"
+		manifest: "json",
+        interfaces: [
+            "sap.ui.core.IAsyncContentCreation"
+        ]
 	};
 
-    /**
-     * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-     * @public
-     * @override
-     */
 	public init() : void {
 		// call the base component's init function
 		super.init();
 
-        // enable routing
-        this.getRouter().initialize();
-
         // set the device model
         this.setModel(createDeviceModel(), "device");
+
+        // enable routing
+        this.getRouter().initialize();
 	}
 }
