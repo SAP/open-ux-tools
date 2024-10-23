94e65cda03dfc76c0f1fe1d5bbc8b8604dd8cda3
SRKPendyala <158585087+SRKPendyala@users.noreply.github.com>
2024-10-23 11:43:29 +0530

    We've fixed now create command for Windows

--- packages/create/src/common/index.ts
+++ packages/create/src/common/index.ts
@@ -10,8 +10,16 @@ export { promptYUIQuestions } from './prompts';
 export function runNpmInstallCommand(basePath: string, installArgs: string[] = []): void {
     const npmCommand = process.platform.startsWith('win') ? 'npm.cmd' : 'npm';
     const args = ['install', ...installArgs];
-    spawnSync(npmCommand, args, {
+    const result = spawnSync(npmCommand, args, {
         cwd: basePath,
-        stdio: [0, 1, 2]
+        stdio: [0,1,2],
+        shell: true
     });
+    if (result.error) {
+        console.error(`Error running npm install: ${result.error.message}, BasePath: ${basePath}, Args: ${args}`);
+    } else if (result.status !== 0) {
+        console.log(`npm process exited with code ${result.status}`);
+    } else {
+        console.log(`Package installed successfully in ${basePath}`);
+    }
 }
