9cc0dda9a1afd52aadaad9557d4c2c738f0f0aa5
SRKPendyala <siva.rama.krishna.pendyala@sap.com>
2024-10-25 09:21:56 +0530

    fix: Reused execNpmCommand fn from project-access and fixed tests

--- packages/create/src/common/index.ts
+++ packages/create/src/common/index.ts
@@ -1,4 +1,5 @@
-import { spawnSync } from 'child_process';
+import { execNpmCommand } from '@sap-ux/project-access';
+import type { Logger } from '@sap-ux/logger';
 export { promptYUIQuestions } from './prompts';
 
 /**
@@ -6,20 +7,14 @@ export { promptYUIQuestions } from './prompts';
  *
  * @param basePath - path to application root
  * @param [installArgs] - optional string array of arguments
+ * @param [options] - optional options
+ * @param [options.logger] - optional logger instance
  */
-export function runNpmInstallCommand(basePath: string, installArgs: string[] = []): void {
-    const npmCommand = process.platform.startsWith('win') ? 'npm.cmd' : 'npm';
-    const args = ['install', ...installArgs];
-    const result = spawnSync(npmCommand, args, {
-        cwd: basePath,
-        stdio: [0,1,2],
-        shell: true
+export function runNpmInstallCommand(basePath: string, installArgs: string[] = [], options?:{ logger?: Logger }): void {
+    const logger = options?.logger;
+    execNpmCommand(['install', ...installArgs], { cwd: basePath, logger: logger}).then(() => {
+        logger?.info('npm install completed successfully.');
+    }).catch((error) => {
+        logger?.error(`npm install failed. '${(error as Error).message}'`);
     });
-    if (result.error) {
-        console.error(`Error running npm install: ${result.error.message}, BasePath: ${basePath}, Args: ${args}`);
-    } else if (result.status !== 0) {
-        console.log(`npm process exited with code ${result.status}`);
-    } else {
-        console.log(`Package installed successfully in ${basePath}`);
-    }
 }
