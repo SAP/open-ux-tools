b2de757ea31780f6a4c8dc6e0292a7405e6db621
Austin Devine <devinea@users.noreply.github.com>
2024-11-05 15:21:30 +0000

    Update locate-reuse-lib.js ui5 linter issues (#2334)
    * Update locate-reuse-lib.js
    
    * update snapshots
    
    * Add linter to other modules
    
    * further updates to support GTE and LT UI5 1.120.x
    
    * Update FF with locate reuse libs
    
    * fixes
    
    * lint fixes
    
    * Update logic on FF project checks
    
    * fix
    
    * fixes
    
    * reformat projectChecks
    
    * fix ui5 linter errors in locate-reuse-libs.js
    
    * Create sixty-dolls-allow.md

---
+++ packages/ui5-application-writer/src/utils.ts
@@ -0,0 +1,49 @@
+import { gte } from 'semver';
+import { type UI5 } from './types';
+
+export const ui5LtsVersion_1_71 = '1.71.0';
+export const ui5LtsVersion_1_120 = '1.120.0';
+
+/**
+ * Compares two UI5 versions to determine if the first is greater than or equal to the second.
+ *
+ * @param {string} ui5VersionA - The first UI5 version to compare.
+ * @param {string} ui5VersionB - The second UI5 version to compare.
+ * @returns {boolean} - True if the first version is greater than or equal to the second, false otherwise.
+ */
+export function compareUI5VersionGte(ui5VersionA: string, ui5VersionB: string): boolean {
+    if (ui5VersionA === '') {
+        // latest version
+        return true;
+    } else {
+        return gte(ui5VersionA, ui5VersionB, { loose: true });
+    }
+}
+
+/**
+ * Gets the template version path based on the UI5 version and template type.
+ *
+ * @param {UI5} ui5 - The UI5 configuration.
+ * @returns {string} - The template version path.
+ */
+export function getTemplateVersionPath(ui5: UI5): string {
+    let templateVersionPath = '';
+    const ui5Version = ui5?.minUI5Version ?? ui5?.version ?? '';
+    if (ui5Version && compareUI5VersionGte(ui5Version, ui5LtsVersion_1_120)) {
+        templateVersionPath = ui5LtsVersion_1_120;
+    } else {
+        templateVersionPath = ui5LtsVersion_1_71;
+    }
+
+    return templateVersionPath;
+}
+
+/**
+ * Processes the destination path by removing specific version segments.
+ *
+ * @param {string} filePath - The file path to process.
+ * @returns {string} - The processed file path.
+ */
+export function processDestinationPath(filePath: string): string {
+    return filePath.replace('/1.120.0', '').replace('/1.71.0', '');
+}
