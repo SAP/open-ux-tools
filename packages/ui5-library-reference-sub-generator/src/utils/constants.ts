5a68903c48b74beabe90e026b43fb2f274c37f0e
Cian Morrin <99677697+cianmSAP@users.noreply.github.com>
2024-11-05 13:38:04 +0000

    feat(ref-lib-gen): adds new module @sap-ux/ui5-library-reference-sub-generator (#2512)
    * feat(ref-lib-gen): adds new module @sap-ux/ui5-library-reference-sub-generator
    
    * feat(ref-lib): update package.json for jest naming collision
    
    * feat(ref-lib): fix mocking
    
    * feat(ref-lib): remove dynamic loading of vscode
    
    * feat(ref-lib): ui fix
    
    * feat(ref-lib): fix tests

---
+++ packages/ui5-library-reference-sub-generator/src/utils/constants.ts
@@ -0,0 +1,7 @@
+export const generatorTitle = 'SAP Fiori Reusable Library';
+export const prompts = [
+    {
+        name: 'Create a Reference',
+        description: 'Add library reference to an SAP Fiori application'
+    }
+];
