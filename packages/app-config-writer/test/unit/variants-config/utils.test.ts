dc4205293f9c34a0afc320fd36f5cd2674b6e3fd
D048415 <dominik.heim@sap.com>
2024-11-15 11:54:14 +0100

    fix tests

--- packages/app-config-writer/test/unit/variants-config/utils.test.ts
+++ packages/app-config-writer/test/unit/variants-config/utils.test.ts
@@ -25,14 +25,14 @@ describe('utils', () => {
         test('defaults', () => {
             const packageJson = { devDependencies: { '@sap/ux-ui5-tooling': '1.15.3' } };
             expect(utils.enhanceUrlParametersWithRta(packageJson)).toStrictEqual(
-                'fiori-tools-rta-mode=true&sap-ui-rta-skip-flex-validation=true&sap-ui-xx-condense-changes=true'
+                'fiori-tools-rta-mode=true&sap-ui-rta-skip-flex-validation=true&sap-ui-xx-condense-changes=true&sap-ui-xx-viewCache=false'
             );
         });
 
         test('parameters overwrite with sap-client', () => {
             const packageJson = { devDependencies: { '@sap-ux/preview-middleware': '0.16.88' } };
             expect(utils.enhanceUrlParametersWithRta(packageJson, { 'sap-client': '500' })).toStrictEqual(
-                'fiori-tools-rta-mode=true&sap-ui-rta-skip-flex-validation=true&sap-ui-xx-condense-changes=true&sap-client=500'
+                'fiori-tools-rta-mode=true&sap-ui-rta-skip-flex-validation=true&sap-ui-xx-condense-changes=true&sap-ui-xx-viewCache=false&sap-client=500'
             );
         });
     });
