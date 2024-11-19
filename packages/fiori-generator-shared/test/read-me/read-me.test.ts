575ff896356905a54fd64e13b17f32c70f3da7f0
Cian Morrin <99677697+cianmSAP@users.noreply.github.com>
2024-11-19 13:03:31 +0000

    fix(fiori-gen-shared): fix reading README template in fiori gen shared (#2595)
    * fix(fiori-gen): move function calling template to src dir
    
    * fix(fiori-gen): changeset

--- packages/fiori-generator-shared/test/read-me/read-me.test.ts
+++ packages/fiori-generator-shared/test/read-me/read-me.test.ts
@@ -1,8 +1,8 @@
 import path from 'path';
 import memFs from 'mem-fs';
 import memFsEditor from 'mem-fs-editor';
-import { generateReadMe } from '../../src/read-me/read-me';
-import type { ReadMe } from '../../src/read-me/types';
+import { generateReadMe } from '../../src/read-me';
+import type { ReadMe } from '../../src/types';
 
 function getLaunchText(): string {
     return (
