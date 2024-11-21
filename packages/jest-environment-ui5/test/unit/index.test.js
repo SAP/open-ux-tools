0610c3cd42c0332fd7da98b0bc98d3b335a1d052
Nicolas Lunet <nicolas.lunet@sap.com>
2024-11-21 12:29:37 +0100

    chore: lint

--- packages/jest-environment-ui5/test/unit/index.test.js
+++ packages/jest-environment-ui5/test/unit/index.test.js
@@ -1,17 +1,14 @@
 const jestCLI = require('jest');
 describe('Custom environment', () => {
     it('Can be created', async () => {
-
-        let failed = false
+        let failed = false;
         try {
-            await jestCLI.run("--detectOpenHandles --config jest-ui5.config.js", process.cwd());
+            await jestCLI.run('--detectOpenHandles --config jest-ui5.config.js', process.cwd());
         } catch {
             failed = true;
         }
         expect(failed).toBe(false);
 
-
         // This is done centrally in the CustomEnvironment constructor but we need to call it here for the test purpose
-
-    },60000);
+    }, 60000);
 });
