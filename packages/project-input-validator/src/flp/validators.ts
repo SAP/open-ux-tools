105668d3b97fedaa23cfbf0560548e1ec111d9aa
Nikita Baranov <nikmacentosh@gmail.com>
2024-11-22 13:37:10 +0200

    refactor: prevent empty input validation in ui without user input

--- packages/project-input-validator/src/flp/validators.ts
+++ packages/project-input-validator/src/flp/validators.ts
@@ -1,5 +1,6 @@
+import { t } from '../i18n';
 import type { AllowedCharacters } from '../general/validators';
-import { validateAllowedCharacters, validateEmptyString, validateMaxLength } from '../general/validators';
+import { validateAllowedCharacters, validateMaxLength } from '../general/validators';
 
 /**
  * Validates that text input does not have zero length and optionally is less than the specified maximum length.
@@ -11,9 +12,9 @@ import { validateAllowedCharacters, validateEmptyString, validateMaxLength } fro
  * @returns true, if all validation checks pass or a message explaining the validation failure
  */
 export function validateText(input: string, maxLength = 0, allowedCharacters?: AllowedCharacters[]): boolean | string {
-    const emptyValidation = validateEmptyString(input);
-    if (typeof emptyValidation === 'string') {
-        return emptyValidation;
+    const length = input ? input.trim().length : 0;
+    if (!length) {
+        return t('general.inputCannotBeEmpty');
     }
 
     const maxLengthValidation = validateMaxLength(input, maxLength);
