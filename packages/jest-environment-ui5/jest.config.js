a0ad808ae9681a6e13a51ec1903502bf6b723278
Nicolas Lunet <nicolas.lunet@sap.com>
2024-11-22 14:00:07 +0100

    chore: sonar

--- packages/jest-environment-ui5/jest.config.js
+++ packages/jest-environment-ui5/jest.config.js
@@ -1,5 +1,5 @@
 const config = require('../../jest.base');
 config.testMatch = ['**/test/unit/?(*.)+(spec|test).[jt]s?(x)'];
-config.collectCoverageFrom = ['src/*.js'];
+config.collectCoverageFrom = ['src/**/*.js'];
 config.collectCoverage = true;
 module.exports = config;
