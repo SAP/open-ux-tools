998777bb545a199ef09645e077d890df3d170132
John Long <john.long@sap.com>
2024-11-27 16:07:51 +0000

    fix(cf-deploy-config-writer): update destination policu and remove excess module

--- packages/cf-deploy-config-writer/src/mta-config/mta.ts
+++ packages/cf-deploy-config-writer/src/mta-config/mta.ts
@@ -335,6 +335,7 @@ export class MtaConfig {
     }
 
     /**
+     * Cleanup missing content for the respective approuter types.
      *
      * @private
      * @returns {Promise<void>} A promise that resolves when the change request has been processed.
@@ -515,14 +516,17 @@ export class MtaConfig {
      * Append and/or cleanup the destination resource if missing in mta.yaml.
      *
      * @param {boolean} isManagedApp - if true, append managed approuter configuration
+     * @param {boolean} doCleanUp - if true, will ensure any missing modules | resources are appended
      * @returns {Promise<void>} - A promise that resolves when the change request has been processed.
      */
-    public async addRoutingModules(isManagedApp = false): Promise<void> {
+    public async addRoutingModules(isManagedApp = false, doCleanUp = true): Promise<void> {
         if (isManagedApp && !this.modules.has('com.sap.application.content:destination')) {
             await this.addManagedAppRouter();
         }
 
-        await this.cleanupMissingResources();
+        if (doCleanUp) {
+            await this.cleanupMissingResources();
+        }
 
         // Handle standalone | managed
         for (const module of [
@@ -868,7 +872,7 @@ export class MtaConfig {
                                     'sap.cloud.service': `${this.prefix.slice(0, 100)}`
                                 }
                             ],
-                            'existing_destinations_policy': 'ignore'
+                            'existing_destinations_policy': 'update'
                         }
                     }
                 },
