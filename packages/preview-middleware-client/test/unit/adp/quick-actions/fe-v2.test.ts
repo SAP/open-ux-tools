8720347e3469c98fb00457bce08bab0972c5d14f
Nafees Ahmed <nafees.ahmed01@sap.com>
2024-11-27 19:28:51 +0100

    fix: unit test

--- packages/preview-middleware-client/test/unit/adp/quick-actions/fe-v2.test.ts
+++ packages/preview-middleware-client/test/unit/adp/quick-actions/fe-v2.test.ts
@@ -3,7 +3,6 @@ import RuntimeAuthoring, { RTAOptions } from 'sap/ui/rta/RuntimeAuthoring';
 import RuntimeAuthoringMock from 'mock/sap/ui/rta/RuntimeAuthoring';
 
 import { quickActionListChanged, executeQuickAction } from '@sap-ux-private/control-property-editor-common';
-import * as VersionUtils from '../../../../src/utils/version';
 
 jest.mock('../../../../src/adp/init-dialogs', () => {
     return {
@@ -39,7 +38,7 @@ import {
 import { DialogNames } from 'open/ux/preview/client/adp/init-dialogs';
 import * as adpUtils from 'open/ux/preview/client/adp/utils';
 import type { ChangeService } from '../../../../src/cpe/changes/service';
-import { Ui5VersionInfo } from '../../../../src/utils/version';
+import VersionInfo from 'mock/sap/ui/VersionInfo';
 
 describe('FE V2 quick actions', () => {
     let sendActionMock: jest.Mock;
@@ -865,40 +864,32 @@ describe('FE V2 quick actions', () => {
         describe('enable table filtering for different valid UI5 versions', () => {
             const testCases: {
                 validVersion: boolean,
-                major: int;
-                minor: int;
-                patch?: int;
+                versionInfo: string
             }[] = [
                     {
-                        validVersion: true, major: 1, minor: 96, patch: 37
+                        validVersion: true, versionInfo: '1.96.37'
                     },
                     {
-                        validVersion: true, major: 1, minor: 108, patch: 38
+                        validVersion: true, versionInfo: '1.108.38'
                     },
                     {
-                        validVersion: true, major: 1, minor: 96, patch: 38
+                        validVersion: true, versionInfo: '1.96.38'
                     },
                     {
-                        validVersion: true, major: 1, minor: 120, patch: 23
+                        validVersion: true, versionInfo: '1.120.23'
                     },
                     {
-                        validVersion: true, major: 1, minor: 128
+                        validVersion: true, versionInfo: '1.128'
                     },
                     {
-                        validVersion: true, major: 1, minor: 130
+                        validVersion: true, versionInfo: '1.130'
                     },
                     {
-                        validVersion: false, major: 1, minor: 96, patch: 36
+                        validVersion: false, versionInfo: '1.96.36'
                     }
                 ];
             test.each(testCases)('initialize and execute action (%s)', async (testCase) => {
-                jest.spyOn(VersionUtils, 'getUi5Version').mockReturnValue(
-                    Promise.resolve({
-                        major: testCase.major,
-                        minor: testCase.minor,
-                        patch: testCase.patch
-                    } as Ui5VersionInfo)
-                );
+                VersionInfo.load.mockResolvedValue({ name: 'sap.ui.core', version: testCase.versionInfo });
                 sapCoreMock.byId.mockImplementation((id) => {
                     if (id == 'SmartFilterBar') {
                         return {
