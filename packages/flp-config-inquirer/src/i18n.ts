cfdd44241d8e762fedcce994e0f6cb204c1386e3
Nikita B. <39880364+nikmace@users.noreply.github.com>
2024-11-27 14:31:20 +0200

    feat: FLP Configuration inquirer (#2580)
    * feat: add base files for the new inquirer
    
    * test: add some of the tests
    
    * fix: lint errors
    
    * refactor: remove unused method
    
    * chore: add flp inquirer package to create moduke
    
    * feat: enhance navigation config command
    
    * test: fix and add new tests
    
    * refactor: extract prompt validation from writer module
    
    * fix: lint errors
    
    * chore: add new package to sonar and update readme
    
    * chore: add cset
    
    * test: add basic prompt tests
    
    * test: add index tests
    
    * test: add get questions test
    
    * test: add test for uncovered condition
    
    * fix: incorrect prompt message
    
    * fix: test
    
    * chore: remove unused deps
    
    * refactor: reorder imports
    
    * chore: update lock and tsconfig
    
    * refactor: move method to project input validators
    
    * Linting auto fix commit
    
    * refactor: use getHostEnvironment from gen shared
    
    * Linting auto fix commit
    
    * fix: i18n translations
    
    * chore: update cset
    
    * refactor: reuse general validation methods
    
    * test: fix and and new tests
    
    * refactor: prevent empty input validation in ui without user input
    
    * refactor: change validation to be platform specific
    
    * chore: update readme for new command
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- packages/flp-config-inquirer/src/i18n.ts
+++ packages/flp-config-inquirer/src/i18n.ts
@@ -1,24 +1,27 @@
 import i18next from 'i18next';
 import type { TOptions } from 'i18next';
+import { addi18nResourceBundle as addProjectInputI18nResourceBundle } from '@sap-ux/project-input-validator';
 
-import translations from './translations/project-input-validator.i18n.json';
+import translations from './translations/flp-config-inquirer.i18n.json';
 
-export const PROJECT_INPUT_VALIDATOR_NS = 'project-input-validator';
+export const FLP_CONFIG_NAMESPACE = 'flp-config-inquirer';
 
 /**
- * Adds the `project-input-validator` resource bundle to i18next.
+ * Adds the `flp-config-inquirer` resource bundle to i18next.
  * May be required to load i18n translations after initialising in the consumer module.
  */
 export function addi18nResourceBundle(): void {
-    i18next.addResourceBundle('en', PROJECT_INPUT_VALIDATOR_NS, translations);
+    i18next.addResourceBundle('en', FLP_CONFIG_NAMESPACE, translations);
 }
 
 /**
  * Initialize i18next with the translations for this module.
  */
-export async function initI18nProjectValidators(): Promise<void> {
+export async function initI18n(): Promise<void> {
     await i18next.init({ lng: 'en', fallbackLng: 'en' });
     addi18nResourceBundle();
+    // add the project-input-validator i18n resource bundle to ensure all translations are available
+    addProjectInputI18nResourceBundle();
 }
 
 /**
@@ -30,11 +33,11 @@ export async function initI18nProjectValidators(): Promise<void> {
  */
 export function t(key: string, options?: TOptions): string {
     if (!options?.ns) {
-        options = Object.assign(options ?? {}, { ns: PROJECT_INPUT_VALIDATOR_NS });
+        options = Object.assign(options ?? {}, { ns: FLP_CONFIG_NAMESPACE });
     }
     return i18next.t(key, options);
 }
 
-initI18nProjectValidators().catch(() => {
-    // needed by lint
+initI18n().catch(() => {
+    // Needed for lint
 });
