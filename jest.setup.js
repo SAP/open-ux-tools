c32a91eb1691359cbb5f5586e39073c74310e508
I743583 <kitty.jose@sap.com>
2024-12-02 15:01:16 +0530

    fix pnpm

--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,11 @@
 // jest.setup.js
 
-// Mocking the '@zowe/secrets-for-zowe-sdk' module to simulate keyring functionality
-// This ensures that tests for the KeyStoreManager class which utilizes '@zowe/secrets-for-zowe-sdk'
-// does not have any side effects while testing.
+// Mocking the '@zowe/secrets-for-zowe-sdk' module to simulate keyring functionality.
+// This mock ensures that tests for the KeyStoreManager class in @sap-ux/store or any other components
+// using '@zowe/secrets-for-zowe-sdk' are being mocked.
+// This approach prevents side effects and allows controlled testing environments.
+
+// Using jest.mock to replace the implementation of '@zowe/secrets-for-zowe-sdk'.
 jest.mock('@zowe/secrets-for-zowe-sdk', () => ({
     keyring: {
         setPassword: jest.fn(),
