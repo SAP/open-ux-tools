3c1f65c97e3556a9e6b5b9dc686d54e6ff2be2db
John Long <john.long@sap.com>
2024-12-03 10:16:51 +0000

    fix(cf-deploy-config-writer): allow underscore as a supported character

--- packages/cf-deploy-config-writer/test/unit/utils.test.ts
+++ packages/cf-deploy-config-writer/test/unit/utils.test.ts
@@ -1,4 +1,4 @@
-import { validateVersion } from '../../src/utils';
+import { validateVersion, toMtaModuleName } from '../../src/utils';
 import { MTAVersion } from '../../src/constants';
 
 describe('CF utils', () => {
@@ -17,6 +17,14 @@ describe('CF utils', () => {
             expect(() => validateVersion()).not.toThrowError();
             expect(validateVersion(MTAVersion)).toBeTruthy();
             expect(validateVersion('1')).toBeTruthy();
+        });
+
+        test('Validate - toMtaModuleName', () => {
+            expect(toMtaModuleName('0.0.0')).toEqual('000');
+            expect(toMtaModuleName('cf_mta_id')).toEqual('cf_mta_id');
+            expect(toMtaModuleName('cf.mta.00')).toEqual('cfmta00');
+            expect(toMtaModuleName('cf_mta.!£$%^&*,()')).toEqual('cf_mta');
+            expect(toMtaModuleName('c'.repeat(130))).toEqual('c'.repeat(128));
         });
     });
 });
