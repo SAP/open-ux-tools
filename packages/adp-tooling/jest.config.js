ef7dcf5f47bc97582a33c909811e92c355f633a1
I743583 <kitty.jose@sap.com>
2024-12-04 09:29:15 +0530

    using FIORI_TOOLS_DISABLE_SECURE_STORE

--- packages/adp-tooling/jest.config.js
+++ packages/adp-tooling/jest.config.js
@@ -1,3 +1,2 @@
 const config = require('../../jest.base');
-config.setupFilesAfterEnv = ['../../jest.setup.js'];
 module.exports = config;
--- packages/environment-check/jest.config.js
+++ packages/environment-check/jest.config.js
@@ -1,4 +1,3 @@
 const config = require('../../jest.base');
 config.modulePathIgnorePatterns.push('<rootDir>/coverage');
-config.setupFilesAfterEnv = ['../../jest.setup.js'];
 module.exports = config;
