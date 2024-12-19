beb38a9e9680bd34b088cf8ba5db0628e3e70c0d
mmilko01 <mihail.milkov@sap.com>
2024-12-19 14:00:25 +0200

    chore: typo

--- packages/odata-service-inquirer/src/types.ts
+++ packages/odata-service-inquirer/src/types.ts
@@ -265,7 +265,7 @@ export type SystemSelectionPromptOptions = {
     /**
      * Only show the default choice in the system selection prompt, this is used to skip the system selection prompt if the default choice is already known.
      * If the `defaultChoice` value is not found in the systems choices, or the `defaultChoice` option is not specified,
-     * this option will not be applied and the full list of choices will be presented o the user.
+     * this option will not be applied and the full list of choices will be presented to the user.
      */
     onlyShowDefaultChoice?: boolean;
 };
