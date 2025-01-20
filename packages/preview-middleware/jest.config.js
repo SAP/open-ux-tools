524f0367c6c18b2048d69c7b144aa5861c30f862
Maruf Rasully <100434800+marufrasully@users.noreply.github.com>
2025-01-20 11:03:08 +0100

    chore: pw reporting checking

--- packages/preview-middleware/jest.config.js
+++ packages/preview-middleware/jest.config.js
@@ -2,4 +2,5 @@ const config = require('../../jest.base');
 config.modulePathIgnorePatterns.push('<rootDir>/test/test-output');
 config.modulePathIgnorePatterns.push('<rootDir>/.tmp');
 config.testMatch = ['<rootDir>/test/**/*.test.ts'];
+// adding dummy comment
 module.exports = config;
