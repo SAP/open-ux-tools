ca59e475ccdd1962654b566a56c44605c05b918d
I743583 <kitty.jose@sap.com>
2025-01-22 17:38:16 +0000

    enable npm workspace comment

--- packages/fiori-elements-writer/src/types.ts
+++ packages/fiori-elements-writer/src/types.ts
@@ -121,7 +121,7 @@ export interface FioriElementsApp<T> extends Ui5App {
          */
         addTests?: boolean;
         /**
-         * Enables NPM workspaces for the application.
+         * Enables NPM workspaces for single top-level, root CAP package.
          * This is applicable when the service is of type `capService`. For more details, refer to the
          * `applyCAPUpdates` function at {@link ../../cap-config-writer/src/cap-writer/updates.ts}.
          * When set to true and the CDS UI5 plugin is enabled, the application will be served using the `appId`.
