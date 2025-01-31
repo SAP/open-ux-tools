01e1b3c6181c58f823972d0704ffb89afc9eecd2
Georgi Damyanov <georgi.damyanov@sap.com>
2025-01-31 11:22:08 +0200

    refactor: remove redundant await

--- packages/preview-middleware-client/test/unit/cpe/changes/validator.spec.ts
+++ packages/preview-middleware-client/test/unit/cpe/changes/validator.spec.ts
@@ -32,17 +32,17 @@ describe('vaildateBindingModel', () => {
 
     test('should throw when invalid binding model string is provided', async () => {
         getTextMock.mockReturnValue('Invalid binding string.');
-        await expect(async () => await validateBindingModel(mockModifiedcontrol as unknown as UI5Element, '{}')).rejects.toThrow('Invalid binding string.');
+        await expect(() => validateBindingModel(mockModifiedcontrol as unknown as UI5Element, '{}')).rejects.toThrow('Invalid binding string.');
     });
 
     test('should throw when invalid binding string for i18n model is provided', async () => {
         getTextMock.mockReturnValue('Invalid binding string. Supported value pattern is {i18n>YOUR_KEY}');
-        await expect(async () => await validateBindingModel(mockModifiedcontrol as unknown as UI5Element, '{ i18n }')).rejects.toThrow('Invalid binding string. Supported value pattern is {i18n>YOUR_KEY}');
+        await expect(() => validateBindingModel(mockModifiedcontrol as unknown as UI5Element, '{ i18n }')).rejects.toThrow('Invalid binding string. Supported value pattern is {i18n>YOUR_KEY}');
     });
 
     test('should throw when the provided key does not exist in i18n.properties', async () => {
         getTextMock.mockReturnValue('Invalid key in the binding string. Supported value pattern is {i18n>YOUR_KEY}. Check if the key already exists in i18n.properties.If not, add the key in the i18n.properties file and reload the editor for the new key to take effect.');
-        await expect(async () => await validateBindingModel(mockModifiedcontrol as unknown as UI5Element, '{ i18n>test }')).rejects.toThrow('Invalid key in the binding string. Supported value pattern is {i18n>YOUR_KEY}. Check if the key already exists in i18n.properties.If not, add the key in the i18n.properties file and reload the editor for the new key to take effect.');
+        await expect(() => validateBindingModel(mockModifiedcontrol as unknown as UI5Element, '{ i18n>test }')).rejects.toThrow('Invalid key in the binding string. Supported value pattern is {i18n>YOUR_KEY}. Check if the key already exists in i18n.properties.If not, add the key in the i18n.properties file and reload the editor for the new key to take effect.');
     });
 
     test('should throw error when invalid binding model is provided', async () => {
@@ -50,6 +50,6 @@ describe('vaildateBindingModel', () => {
         const control = {
             getModel: jest.fn().mockReturnValue(undefined)
         };
-        await expect(async () => await validateBindingModel(control as unknown as UI5Element, '{ i18n>test }')).rejects.toThrow('Invalid binding model.');
+        await expect(() => validateBindingModel(control as unknown as UI5Element, '{ i18n>test }')).rejects.toThrow('Invalid binding model.');
     });
 });
