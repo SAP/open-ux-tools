5a9e40ce4e86dc03085498a616793ed688885268
Georgi Damyanov <georgi.damyanov@sap.com>
2025-01-31 11:02:31 +0200

    test: enhance tests

--- packages/preview-middleware-client/test/unit/cpe/changes/validator.spec.ts
+++ packages/preview-middleware-client/test/unit/cpe/changes/validator.spec.ts
@@ -1,5 +1,8 @@
 import type UI5Element from 'sap/ui/core/Element';
 import { validateBindingModel } from '../../../../src/cpe/changes/validator';
+import { getTextBundle } from '../../../../src/i18n';
+
+jest.mock('../../../../src/i18n');
 
 describe('vaildateBindingModel', () => {
     const mockModifiedcontrol = {
@@ -11,46 +14,42 @@ describe('vaildateBindingModel', () => {
             )
         })
     };
+
+    let getTextMock: jest.Mock;
+
+    beforeEach(() => {
+        getTextMock = jest.fn();
+
+        (getTextBundle as jest.Mock).mockResolvedValue({
+            hasText: jest.fn().mockReturnValue(true),
+            getText: getTextMock
+        });
+    });
+
+    afterEach(() => {
+        getTextMock.mockClear();
+    })
+
     test('should throw when invalid binding model string is provided', async () => {
-        try {
-            await validateBindingModel(mockModifiedcontrol as unknown as UI5Element, '{}');
-        } catch (error) {
-            expect(error).toBeInstanceOf(SyntaxError);
-            expect(error.message).toBe('Invalid binding string.');
-        }
+        getTextMock.mockReturnValue('Invalid binding string.');
+        await expect(async () => await validateBindingModel(mockModifiedcontrol as unknown as UI5Element, '{}')).rejects.toThrow('Invalid binding string.');
     });
 
     test('should throw when invalid binding string for i18n model is provided', async () => {
-        try {
-            await validateBindingModel(mockModifiedcontrol as unknown as UI5Element, '{ i18n }');
-        } catch (error) {
-            expect(error).toBeInstanceOf(SyntaxError);
-            expect(error.message).toBe('Invalid binding string. Supported value pattern is {i18n>YOUR_KEY}');
-        }
+        getTextMock.mockReturnValue('Invalid binding string. Supported value pattern is {i18n>YOUR_KEY}');
+        await expect(async () => await validateBindingModel(mockModifiedcontrol as unknown as UI5Element, '{ i18n }')).rejects.toThrow('Invalid binding string. Supported value pattern is {i18n>YOUR_KEY}');
     });
 
     test('should throw when the provided key does not exist in i18n.properties', async () => {
-        try {
-            await validateBindingModel(mockModifiedcontrol as unknown as UI5Element, '{ i18n>test }');
-        } catch (error) {
-            expect(error).toBeInstanceOf(SyntaxError);
-            expect(error.message).toBe(
-                'Invalid key in the binding string. Supported value pattern is {i18n>YOUR_KEY}. Check if the key already exists in i18n.properties.If not, add the key in the i18n.properties file and reload the editor for the new key to take effect.'
-            );
-        }
+        getTextMock.mockReturnValue('Invalid key in the binding string. Supported value pattern is {i18n>YOUR_KEY}. Check if the key already exists in i18n.properties.If not, add the key in the i18n.properties file and reload the editor for the new key to take effect.');
+        await expect(async () => await validateBindingModel(mockModifiedcontrol as unknown as UI5Element, '{ i18n>test }')).rejects.toThrow('Invalid key in the binding string. Supported value pattern is {i18n>YOUR_KEY}. Check if the key already exists in i18n.properties.If not, add the key in the i18n.properties file and reload the editor for the new key to take effect.');
     });
 
     test('should throw error when invalid binding model is provided', async () => {
+        getTextMock.mockReturnValue('Invalid binding model.');
         const control = {
             getModel: jest.fn().mockReturnValue(undefined)
         };
-        try {
-            await validateBindingModel(control as unknown as UI5Element, '{ i18n|namespace>test }');
-        } catch (error) {
-            expect(error).toBeInstanceOf(SyntaxError);
-            expect(error.message).toBe(
-                'Invalid binding model.'
-            );
-        }
+        await expect(async () => await validateBindingModel(control as unknown as UI5Element, '{ i18n>test }')).rejects.toThrow('Invalid binding model.');
     });
 });
