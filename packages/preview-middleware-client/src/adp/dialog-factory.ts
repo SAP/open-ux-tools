1e2543e8e2ace893f1e6914890fc648f5dff1c46
Jimmy Joseph <jimmy.joseph@sap.com>
2025-02-18 13:24:04 +0100

    fix: add default index for v4 table action

--- packages/preview-middleware-client/src/adp/dialog-factory.ts
+++ packages/preview-middleware-client/src/adp/dialog-factory.ts
@@ -61,7 +61,10 @@ export class DialogFactory {
         switch (dialogName) {
             case DialogNames.ADD_FRAGMENT:
                 controller = new AddFragment(`open.ux.preview.client.adp.controllers.${dialogName}`, overlay, rta, {
-                    ...('aggregation' in options && { aggregation: options.aggregation, defaultAggregationArrayIndex: options.defaultAggregationArrayIndex }),
+                    ...('aggregation' in options && { aggregation: options.aggregation }),
+                    ...('defaultAggregationArrayIndex' in options && {
+                        defaultAggregationArrayIndex: options.defaultAggregationArrayIndex
+                    }),
                     title: resources.getText(options.title ?? 'ADP_ADD_FRAGMENT_DIALOG_TITLE')
                 });
                 break;
--- packages/preview-middleware-client/src/adp/quick-actions/common/create-page-action.ts
+++ packages/preview-middleware-client/src/adp/quick-actions/common/create-page-action.ts
@@ -4,7 +4,7 @@ import FlexCommand from 'sap/ui/rta/command/FlexCommand';
 import { DialogFactory, DialogNames } from '../../dialog-factory';
 import { QuickActionContext, SimpleQuickActionDefinition } from '../../../cpe/quick-actions/quick-action-definition';
 import { SimpleQuickActionDefinitionBase } from '../simple-quick-action-base';
-import { getApplicationType } from '../../../utils/application';
+import { ApplicationType, getApplicationType } from '../../../utils/application';
 import { getUi5Version, isLowerThanMinimalUi5Version } from '../../../utils/version';
 import { DIALOG_ENABLEMENT_VALIDATOR } from '../dialog-enablement-validator';
 
@@ -15,29 +15,29 @@ const CONTROL_TYPES = ['sap.f.DynamicPageTitle', 'sap.uxap.ObjectPageHeader', 's
  * Quick Action for adding a custom page action.
  */
 export class AddPageActionQuickAction extends SimpleQuickActionDefinitionBase implements SimpleQuickActionDefinition {
+    private appType: ApplicationType;
     constructor(context: QuickActionContext) {
         super(ADD_PAGE_ACTION, CONTROL_TYPES, 'QUICK_ACTION_ADD_CUSTOM_PAGE_ACTION', context, [
             DIALOG_ENABLEMENT_VALIDATOR
         ]);
+        this.appType = getApplicationType(this.context.rta.getRootControlInstance().getManifest());
     }
 
     async initialize(): Promise<void> {
-        const appType = getApplicationType(this.context.rta.getRootControlInstance().getManifest());
         const version = await getUi5Version();
-        if (appType === 'fe-v4' && isLowerThanMinimalUi5Version(version, { major: 1, minor: 130 })) {
+        if (this.appType === 'fe-v4' && isLowerThanMinimalUi5Version(version, { major: 1, minor: 130 })) {
             return;
         }
         super.initialize();
     }
 
     async execute(): Promise<FlexCommand[]> {
-        const appType = getApplicationType(this.context.rta.getRootControlInstance().getManifest());
         if (this.control) {
             const overlay = OverlayRegistry.getOverlay(this.control) || [];
             await DialogFactory.createDialog(overlay, this.context.rta, DialogNames.ADD_FRAGMENT, undefined, {
                 aggregation: 'actions',
                 title: 'QUICK_ACTION_ADD_CUSTOM_PAGE_ACTION',
-                ...(appType === 'fe-v2' && { defaultAggregationArrayIndex: 1 }) // only for
+                ...(this.appType === 'fe-v2' && { defaultAggregationArrayIndex: 1 })
             });
         }
         return [];
