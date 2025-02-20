b317a0ded25b82df003ca120dad78de67599026a
mmilko01 <mihail.milkov@sap.com>
2025-02-20 13:16:46 +0200

    fix: remove unneeded provider instance

--- packages/preview-middleware/src/base/flp.ts
+++ packages/preview-middleware/src/base/flp.ts
@@ -847,26 +847,6 @@ function serializeUi5Configuration(config: Map<string, string>): string {
 }
 
 /**
- * Determines if the given ADP project is an ABAP Cloud project.
- *
- * @param adpConfig - The configuration object for the ADP preview.
- * @param logger - The logger instance to use for logging.
- * @returns A promise that resolves to a boolean indicating whether the project is an ABAP Cloud project.
- */
-async function isCloudAdpProject(adpConfig: AdpPreviewConfig, logger: ToolsLogger): Promise<boolean> {
-    const { target, ignoreCertErrors } = adpConfig;
-    const provider = await createAbapServiceProvider(
-        target,
-        {
-            ignoreCertErrors
-        },
-        true,
-        logger
-    );
-    return await provider.isAbapCloud();
-}
-
-/**
  * Initialize the preview for an adaptation project.
  *
  * @param rootProject reference to the project
@@ -887,7 +867,6 @@ export async function initAdp(
     if (appVariant) {
         const adp = new AdpPreview(config, rootProject, util, logger);
         const variant = JSON.parse(await appVariant.getString()) as DescriptorVariant;
-        const isCloud = await isCloudAdpProject(config, logger);
         const layer = await adp.init(variant);
         if (flp.rta) {
             flp.rta.layer = layer;
@@ -895,7 +874,7 @@ export async function initAdp(
                 ...flp.rta.options,
                 projectId: variant.id,
                 scenario: 'ADAPTATION_PROJECT',
-                isCloud
+                isCloud: adp.isCloudProject
             };
             for (const editor of flp.rta.endpoints) {
                 editor.pluginScript ??= 'open/ux/preview/client/adp/init';
