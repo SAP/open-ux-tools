d6118c95c3feaf902a56cc070b0507f66a563a9d
J Long <john.long@sap.com>
2025-02-24 10:44:40 +0000

    2821/add cap mta prompting (#2889)
    * fix(cf-deploy-config-writer): support new prompt to support user adding MTA to CAP project during deployment
    
    * fix(cf-deploy-config-writer): fix linting
    
    * fix(cf-deploy-config-writer): fix sonar issues
    
    * fix(cf-deploy-config-writer): support new CAP MTA continue option
    
    * fix(cf-deploy-config-writer): add changeset
    
    * fix(cf-deploy-config-writer): remove duplicate changeset
    
    * fix(cf-deploy-config-writer): fix spelling mistake
    
    * fix(cf-deploy-config-writer): remove spelling fix
    
    * fix(flp): fix spelling
    
    * fix(cf-deploy-config-writer): fix merge conflict
    
    * fix(cf-deploy-config-writer): cleanup code
    
    * fix(cf-deploy-config-writer): remove unused code and error handling
    
    * fix(cf-deploy-config-writer): fix text formatting

---
+++ packages/cf-deploy-config-sub-generator/test/fixtures/cap/app/testui5app/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("project1.controller.App", {
+      onInit() {
+      }
+  });
+});
