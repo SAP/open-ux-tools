b2f7ce79d8ca6945121fd4cdde2a4a6d893c5f57
D048415 <dominik.heim@sap.com>
2025-02-24 09:47:14 +0100

    keep checkCdsUi5PluginEnabled as export in cap-config-writer

--- packages/cap-config-writer/src/index.ts
+++ packages/cap-config-writer/src/index.ts
@@ -1,3 +1,5 @@
+import { checkCdsUi5PluginEnabled } from '@sap-ux/project-access';
+export { checkCdsUi5PluginEnabled };
 export { enableCdsUi5Plugin } from './cap-config';
 export type { CapService, CapRuntime } from './cap-config/types';
 export type { CapServiceCdsInfo, CapProjectSettings } from './cap-config/types';
