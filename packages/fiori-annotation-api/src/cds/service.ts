dba6f02773daaf13ce51e076f872687526f5800e
Davis <davis.voicescuks@sap.com>
2025-02-28 11:10:08 +0200

    fix: compile error when new .cds files are added to memfs

--- packages/fiori-annotation-api/src/cds/service.ts
+++ packages/fiori-annotation-api/src/cds/service.ts
@@ -9,11 +9,17 @@ import type { CDSService, TextFile } from '../types';
  *
  * @param projectRoot - Absolute path of the project.
  * @param serviceName - Name of the CDS service.
+ * @param fileCache - Files from memfs.
  * @param clearCache - Flag indicating if the CDS file resolution cache should be cleared.
  * @returns CDS service structure.
  */
-export async function getCDSService(projectRoot: string, serviceName: string, clearCache = false): Promise<CDSService> {
-    const files = await getCdsFiles(projectRoot, clearCache);
+export async function getCDSService(
+    projectRoot: string,
+    serviceName: string,
+    fileCache: Map<string, string>,
+    clearCache = false
+): Promise<CDSService> {
+    const files = await getCdsFiles(projectRoot, fileCache, clearCache);
     const serviceFiles = files.map((uri): TextFile => {
         return { uri: pathToFileURL(uri).toString(), isReadOnly: uri.indexOf('node_modules') !== -1 };
     });
