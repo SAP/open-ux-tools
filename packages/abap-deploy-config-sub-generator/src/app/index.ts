07736fb78d433232b65436836e32b0a473003eb6
Georgi Damyanov <georgi.damyanov@sap.com>
2025-03-07 10:38:10 +0200

    fix: build

--- packages/abap-deploy-config-sub-generator/src/app/index.ts
+++ packages/abap-deploy-config-sub-generator/src/app/index.ts
@@ -27,7 +27,7 @@ import { isInternalFeaturesSettingEnabled } from '@sap-ux/feature-toggle';
 import { isAppStudio } from '@sap-ux/btp-utils';
 import { DEFAULT_PACKAGE_ABAP } from '@sap-ux/abap-deploy-config-inquirer/dist/constants';
 import type { AbapDeployConfig, FioriToolsProxyConfigBackend } from '@sap-ux/ui5-config';
-import type { YeomanEnvironment } from '@sap-ux/fiori-generator-shared;
+import type { YeomanEnvironment } from '@sap-ux/fiori-generator-shared';
 import type { AbapDeployConfigOptions, AbapDeployConfigPromptOptions } from './types';
 import type { AbapDeployConfigAnswersInternal, AbapDeployConfigQuestion } from '@sap-ux/abap-deploy-config-inquirer';
 
