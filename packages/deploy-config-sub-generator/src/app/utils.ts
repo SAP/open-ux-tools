ffbcf28e9125dfc48168bda9cc82a4240a9acfee
Cian Morrin <99677697+cianmSAP@users.noreply.github.com>
2025-03-13 15:40:34 +0000

    fix(deploy-gen): support running deploy generator standalone (#3023)
    * fix(deploy-gen): support running deploy generator standalone
    
    * fix(deploy-gen): remove condition
    
    * fix(deploy-gen): move condition
    
    * fix(deploy-gen): sonar
    
    * fix(deploy-gen): fix gen name

--- packages/deploy-config-sub-generator/src/app/utils.ts
+++ packages/deploy-config-sub-generator/src/app/utils.ts
@@ -1,4 +1,6 @@
+import { basename } from 'path';
 import type { DeployConfigOptions } from '../types';
+import type { GeneratorOptions } from 'yeoman-generator';
 
 /**
  * Parses the target from the CLI args or the options.
@@ -18,4 +20,38 @@ export function parseTarget(args: string | string[], opts: DeployConfigOptions):
         result = opts.target;
     }
     return result;
+}
+
+/**
+ * Returns the details for the YUI prompt.
+ *
+ * @param appRootPath - path to the application to be displayed in YUI step description
+ * @returns step details
+ */
+export function getYUIDetails(appRootPath: string): { name: string; description: string }[] {
+    return [
+        {
+            name: 'Deployment Configuration',
+            description: `Configure Deployment settings - ${basename(appRootPath)}`
+        }
+    ];
+}
+
+/**
+ * Registers all the root generator's namespaces i.e the subgenerators.
+ *
+ * @param rootGenerator - the root generator name
+ * @param generatorNamespace - the namespace of the generator
+ * @param isPackageRegistered - function to check if a package is registered
+ * @param lookup - function to lookup and register the package's namespaces
+ */
+export function registerNamespaces(
+    rootGenerator: string,
+    generatorNamespace: string,
+    isPackageRegistered: GeneratorOptions['env.isPackageRegistered'],
+    lookup: GeneratorOptions['env.lookup']
+): void {
+    if (rootGenerator && !isPackageRegistered(generatorNamespace)) {
+        lookup({ packagePatterns: [rootGenerator] });
+    }
 }
