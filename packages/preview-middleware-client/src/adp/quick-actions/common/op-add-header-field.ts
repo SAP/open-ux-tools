11430891b2c3eabd63802aa9cc04f3c486cc141e
Nafees Ahmed <nafees.ahmed01@sap.com>
2025-03-13 13:34:09 +0100

    fix: review comments

--- packages/preview-middleware-client/src/adp/quick-actions/common/op-add-header-field.ts
+++ packages/preview-middleware-client/src/adp/quick-actions/common/op-add-header-field.ts
@@ -4,7 +4,6 @@ import ObjectPageLayout from 'sap/uxap/ObjectPageLayout';
 import FlexBox from 'sap/m/FlexBox';
 
 import { DialogFactory, DialogNames } from '../../dialog-factory';
-import { getRelevantControlFromActivePage } from '../../../cpe/quick-actions/utils';
 import { QuickActionContext, SimpleQuickActionDefinition } from '../../../cpe/quick-actions/quick-action-definition';
 import { isA } from '../../../utils/core';
 import { SimpleQuickActionDefinitionBase } from '../simple-quick-action-base';
@@ -18,14 +17,17 @@ const CONTROL_TYPES = ['sap.uxap.ObjectPageLayout'];
 /**
  * Quick Action for adding a Header Field to an Object Page.
  */
-export class AddHeaderFieldQuickAction extends SimpleQuickActionDefinitionBase implements SimpleQuickActionDefinition {
+export class AddHeaderFieldQuickAction
+    extends SimpleQuickActionDefinitionBase<ObjectPageLayout>
+    implements SimpleQuickActionDefinition
+{
     constructor(context: QuickActionContext) {
         super(OP_ADD_HEADER_FIELD_TYPE, CONTROL_TYPES, 'QUICK_ACTION_OP_ADD_HEADER_FIELD', context, [
             DIALOG_ENABLEMENT_VALIDATOR,
             {
                 run: async (): Promise<EnablementValidatorResult> => {
                     const i18n = await getTextBundle();
-                    if (!(this.control as ObjectPageLayout).getShowHeaderContent()) {
+                    if (!this.control?.getShowHeaderContent()) {
                         return {
                             type: 'error',
                             message: i18n.getText('DISABLE_SHOW_HEADER_CONTENT')
@@ -38,13 +40,10 @@ export class AddHeaderFieldQuickAction extends SimpleQuickActionDefinitionBase i
     }
 
     async execute(): Promise<FlexCommand[]> {
-        const objectPageLayout = getRelevantControlFromActivePage(
-            this.context.controlIndex,
-            this.context.view,
-            CONTROL_TYPES
-        )[0] as ObjectPageLayout;
-
-        const headerContent = objectPageLayout.getHeaderContent();
+        if (!this.control) {
+            return [];
+        }
+        const headerContent = this.control.getHeaderContent();
 
         // check if only flex box exist in the headerContent.
         if (headerContent.length === 1 && isA<FlexBox>('sap.m.FlexBox', headerContent[0])) {
