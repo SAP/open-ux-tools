508c9cb5b81c912d3e65ba18ceed33f77ac31ae4
I743583 <kitty.jose@sap.com>
2025-03-27 16:11:57 +0000

    export updateWorkspaceFoldersIfNeeded from launch config

--- packages/launch-config/src/launch-config-crud/create.ts
+++ packages/launch-config/src/launch-config-crud/create.ts
@@ -106,7 +106,7 @@ async function handleExistingLaunchJson(
  *
  * @param {UpdateWorkspaceFolderOptions} updateWorkspaceFolders - The options for updating workspace folders.
  */
-function updateWorkspaceFoldersIfNeeded(updateWorkspaceFolders?: UpdateWorkspaceFolderOptions): void {
+export function updateWorkspaceFoldersIfNeeded(updateWorkspaceFolders?: UpdateWorkspaceFolderOptions): void {
     if (updateWorkspaceFolders) {
         const { uri, vscode, projectName } = updateWorkspaceFolders;
         if (uri && vscode) {
@@ -128,6 +128,7 @@ function updateWorkspaceFoldersIfNeeded(updateWorkspaceFolders?: UpdateWorkspace
  * @param {Editor} fs - The file system editor to read and write the `launch.json` file.
  * @param {DebugOptions} debugOptions - Debug configuration options that dictate how the `launch.json`
  *     should be generated and what commands should be logged.
+ * @param {boolean} enableVSCodeReload - A flag indicating whether the workspace should be reloaded in VS Code.
  * @param {Logger} logger - Logger instance for logging information or warnings.
  * @returns {Promise<Editor>} - Returns the file system editor after potentially modifying the workspace
  *     and updating or creating the `launch.json` file.
@@ -136,6 +137,7 @@ async function handleDebugOptions(
     rootFolder: string,
     fs: Editor,
     debugOptions: DebugOptions,
+    enableVSCodeReload: boolean = true,
     logger?: Logger
 ): Promise<Editor> {
     const { launchJsonPath, workspaceFolderUri, cwd, appNotInWorkspace } = handleWorkspaceConfig(
@@ -159,18 +161,20 @@ async function handleDebugOptions(
         writeLaunchJsonFile(fs, launchJsonWritePath, configurations);
     }
 
-    // The `workspaceFolderUri` is a URI obtained from VS Code that specifies the path to the workspace folder.
-    // This URI is populated when a reload of the workspace is required. It allows us to identify and update
-    // the workspace folder correctly within VS Code.
-    const updateWorkspaceFolders = workspaceFolderUri
+    // Conditionally update workspace folders based on the enableVSCodeReload flag
+    if (enableVSCodeReload) {
+        // The `workspaceFolderUri` is a URI obtained from VS Code that specifies the path to the workspace folder.
+        // This URI is populated when a reload of the workspace is required. It allows us to identify and update
+        // the workspace folder correctly within VS Code.
+        const updateWorkspaceFolders = workspaceFolderUri
         ? ({
-              uri: workspaceFolderUri,
-              projectName: basename(rootFolder),
-              vscode: debugOptions.vscode
-          } as UpdateWorkspaceFolderOptions)
+            uri: workspaceFolderUri,
+            projectName: basename(rootFolder),
+            vscode: debugOptions.vscode
+        } as UpdateWorkspaceFolderOptions)
         : undefined;
-
-    updateWorkspaceFoldersIfNeeded(updateWorkspaceFolders);
+        updateWorkspaceFoldersIfNeeded(updateWorkspaceFolders);
+    }
     return fs;
 }
 
@@ -197,5 +201,5 @@ export async function createLaunchConfig(
     if (!debugOptions.vscode) {
         return fs;
     }
-    return await handleDebugOptions(rootFolder, fs, debugOptions, logger);
+    return await handleDebugOptions(rootFolder, fs, debugOptions, fioriOptions.enableVSCodeReload, logger);
 }
