3b639296514d8b52b6e816d25c4b7e967584bede
Jimmy Joseph <jimmy.joseph@sap.com>
2025-04-25 09:32:04 +0200

    resuse existing actions

--- packages/preview-middleware-client/src/adp/quick-actions/free-style/registry.ts
+++ packages/preview-middleware-client/src/adp/quick-actions/free-style/registry.ts
@@ -5,20 +5,23 @@ import type {
 import { QuickActionDefinitionRegistry } from '../../../cpe/quick-actions/registry';
 
 import { AddControllerToPageQuickAction } from '../common/add-controller-to-page';
-import { ToggleClearFilterBarQuickAction } from './lr-toggle-clear-filter-bar';
-import { ChangeTableColumnsQuickAction } from './change-table-columns';
+import { ToggleClearFilterBarQuickAction } from '../fe-v4/lr-toggle-clear-filter-bar';
+import { ToggleSemanticDateRangeFilterBar } from '../fe-v4/lr-enable-semantic-date-range-filter-bar';
+import { ChangeTableColumnsQuickAction } from '../fe-v4/change-table-columns';
 import { AddHeaderFieldQuickAction } from '../common/op-add-header-field';
 import { AddCustomSectionQuickAction } from '../common/op-add-custom-section';
-import { AddTableCustomColumnQuickAction } from './create-table-custom-column';
+import { AddTableCustomColumnQuickAction } from '../fe-v4/create-table-custom-column';
 import { AddPageActionQuickAction } from '../common/create-page-action';
-import { AddTableActionQuickAction } from './create-table-action';
-import { EnableTableFilteringQuickAction } from './lr-enable-table-filtering';
-import { ToggleSemanticDateRangeFilterBar } from './lr-enable-semantic-date-range-filter-bar';
-import { EnableTableEmptyRowModeQuickAction } from './op-enable-empty-row-mode';
+import { AddTableActionQuickAction } from '../fe-v4/create-table-action';
 import { AddNewAnnotationFile } from '../common/add-new-annotation-file';
-import { EnableVariantManagementQuickAction } from './enable-variant-management';
-import { AddNewSubpage } from '../fe-v4/add-new-subpage';
 import { ChangeTableActionsQuickAction } from '../fe-v4/change-table-actions';
+import { ChangeTableActionsQuickAction as ChangeTableActionsQuickActionV2 } from '../fe-v2/change-table-actions';
+import { ChangeTableColumnsQuickAction as ChangeTableColumnsQuickActionV2 } from '../fe-v2/change-table-columns';
+import { AddTableActionQuickAction as AddTableActionQuickActionV2 } from '../fe-v2/create-table-action';
+import { AddTableCustomColumnQuickAction as AddTableCustomColumnQuickActionV2 } from '../fe-v2/create-table-custom-column';
+import { ToggleSemanticDateRangeFilterBar as ToggleSemanticDateRangeFilterBarV2 } from '../fe-v2/lr-enable-semantic-date-range-filter-bar';
+import { EnableListReportVariantManagementQuickAction as EnableListReportVariantManagementQuickActionV2 } from '../fe-v2/lr-enable-variant-management';
+import { ToggleClearFilterBarQuickAction as ToggleClearFilterBarQuickActionV2 } from '../fe-v2/lr-toggle-clear-filter-bar';
 
 type PageName = 'listReport' | 'objectPage';
 
@@ -39,47 +42,30 @@ export default class FSQuickActionRegistry extends QuickActionDefinitionRegistry
         const definitionGroups: QuickActionDefinitionGroup[] = [];
         for (let index = 0; index < activePages.length; index++) {
             const { name, view } = activePages[index];
-            if (name === 'listReport') {
-                definitionGroups.push({
-                    title: 'LIST REPORT',
-                    definitions: [
-                        AddControllerToPageQuickAction,
-                        AddPageActionQuickAction,
-                        ToggleClearFilterBarQuickAction,
-                        ToggleSemanticDateRangeFilterBar,
-                        EnableVariantManagementQuickAction,
-                        ChangeTableActionsQuickAction,
-                        ChangeTableColumnsQuickAction,
-                        AddTableActionQuickAction,
-                        AddTableCustomColumnQuickAction,
-                        EnableTableFilteringQuickAction,
-                        AddNewAnnotationFile,
-                        AddNewSubpage
-                    ],
-                    view,
-                    key: name + index
-                });
-            } else if (name === 'objectPage') {
-                definitionGroups.push({
-                    title: 'OBJECT PAGE',
-                    definitions: [
-                        AddControllerToPageQuickAction,
-                        AddPageActionQuickAction,
-                        AddHeaderFieldQuickAction,
-                        AddCustomSectionQuickAction,
-                        EnableVariantManagementQuickAction,
-                        ChangeTableActionsQuickAction,
-                        ChangeTableColumnsQuickAction,
-                        AddTableActionQuickAction,
-                        AddTableCustomColumnQuickAction,
-                        EnableTableEmptyRowModeQuickAction,
-                        AddNewAnnotationFile,
-                        AddNewSubpage
-                    ],
-                    view,
-                    key: name + index
-                });
-            }
+            definitionGroups.push({
+                title: 'LIST REPORT', // ToDO: find view name from routing targets in manifest json
+                definitions: [
+                    AddControllerToPageQuickAction,
+                    AddPageActionQuickAction,
+                    AddHeaderFieldQuickAction,
+                    AddCustomSectionQuickAction,
+                    //ToggleClearFilterBarQuickAction, // TODO: for v4 controls see how to toggle clear filter bar 
+                    // ToggleSemanticDateRangeFilterBar, // TODO: for v4 controls see how to toggle semantic date range
+                    ChangeTableActionsQuickAction,
+                    ChangeTableColumnsQuickAction,
+                    AddTableActionQuickAction,
+                    AddTableCustomColumnQuickAction,
+                    ToggleClearFilterBarQuickActionV2,
+                    // ToggleSemanticDateRangeFilterBarV2,  // TODO: for v2 controls see how to toggle semantic date range
+                    ChangeTableActionsQuickActionV2,
+                    ChangeTableColumnsQuickActionV2,
+                    AddTableActionQuickActionV2,
+                    AddTableCustomColumnQuickActionV2,
+                    AddNewAnnotationFile,
+                ],
+                view,
+                key: name + index
+            });
         }
         return definitionGroups;
     }
