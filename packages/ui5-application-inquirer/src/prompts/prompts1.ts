24902729f1d2c27a41302222666f1eb03bbdac90
Cian Morrin <cian.morrin@sap.com>
2025-04-30 13:49:52 +0100

    fix(ui5-app-inq): enhance checks when virtual endpoints prompt is shown

--- packages/ui5-application-inquirer/src/prompts/prompts1.ts
+++ packages/ui5-application-inquirer/src/prompts/prompts1.ts
@@ -281,20 +281,16 @@ export function getAddFlpConfigPrompt(
 /**
  * Get the `enableVirtualEndpoints` prompt.
  *
- * @param isCapProject is this a CAP project - additional checks performed if true
  * @param capCdsInfo CDS UI5 plugin information
  * @returns the `enableVirtualEndpoints` prompt
  */
-export function getEnableVirtualEndpoints(
-    isCapProject: boolean,
-    capCdsInfo?: CdsUi5PluginInfo
-): UI5ApplicationQuestion {
+export function getEnableVirtualEndpoints(capCdsInfo?: CdsUi5PluginInfo): UI5ApplicationQuestion {
     return {
         when: (answers: UI5ApplicationAnswers): boolean => {
-            if (isCapProject) {
+            if (capCdsInfo) {
                 return (
-                    capCdsInfo?.isCdsUi5PluginEnabled ||
-                    !!(capCdsInfo?.hasMinCdsVersion && !capCdsInfo?.hasCdsUi5Plugin && !!answers.enableTypeScript)
+                    capCdsInfo.isCdsUi5PluginEnabled ||
+                    !!(capCdsInfo.hasMinCdsVersion && !capCdsInfo.hasCdsUi5Plugin && !!answers.enableTypeScript)
                 );
             }
             return true;
