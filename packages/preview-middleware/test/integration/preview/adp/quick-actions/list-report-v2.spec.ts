848cb6455f35269bec3b00a56fe6b58232981c98
Davis Voicescuks <davis.voicescuks@sap.com>
2025-05-01 11:50:42 +0300

    test: add table action

--- packages/preview-middleware/test/integration/preview/adp/quick-actions/list-report-v2.spec.ts
+++ packages/preview-middleware/test/integration/preview/adp/quick-actions/list-report-v2.spec.ts
@@ -25,6 +25,9 @@ class QuickActionPanel {
     get changeTableColumns() {
         return this.page.getByRole('button', { name: 'Change Table Columns' });
     }
+    get addCustomTableAction() {
+        return this.page.getByRole('button', { name: 'Add Custom Table Action' });
+    }
     constructor(page: Page) {
         this.page = page;
     }
@@ -34,6 +37,9 @@ class Toolbar {
     private readonly page: Page;
     get saveButton() {
         return this.page.getByRole('button', { name: 'Save' });
+    }
+    get saveAndReloadButton() {
+        return this.page.getByRole('button', { name: 'Save and Reload' });
     }
     get uiAdaptationButton() {
         return this.page.getByRole('button', { name: 'UI Adaptation' });
@@ -94,6 +100,51 @@ class TableSettings {
     constructor(frame: FrameLocator) {
         this.frame = frame;
     }
+}
+
+interface Changes {
+    annotations: Record<string, string>;
+    coding: Record<string, string>;
+    fragments: Record<string, string>;
+    changes: object[];
+}
+
+async function readChanges(root: string): Promise<Changes> {
+    const changesDirectory = join(root, 'webapp', 'changes');
+    const result: Changes = {
+        annotations: {},
+        coding: {},
+        fragments: {},
+        changes: []
+    };
+    try {
+        const files = await readdir(changesDirectory, { withFileTypes: true });
+        for (const file of files) {
+            try {
+                if (file.isFile()) {
+                    const text = await readFile(join(changesDirectory, file.name), { encoding: 'utf-8' });
+                    result.changes.push(JSON.parse(text) as object);
+                } else if (file.name === 'annotations' || file.name === 'coding' || file.name === 'fragments') {
+                    const children = await readdir(join(changesDirectory, file.name), { withFileTypes: true });
+                    for (const child of children) {
+                        if (child.isFile()) {
+                            result[file.name][child.name] = await readFile(
+                                join(changesDirectory, file.name, child.name),
+                                {
+                                    encoding: 'utf-8'
+                                }
+                            );
+                        }
+                    }
+                }
+            } catch (e) {
+                // ignore error
+            }
+        }
+    } catch (e) {
+        // ignore error
+    }
+    return result;
 }
 
 test.describe(`@quick-actions @fe-v2`, () => {
@@ -220,13 +271,12 @@ test.describe(`@quick-actions @fe-v2`, () => {
             await expect(previewFrame.getByRole('button', { name: 'Open in VS Code' })).toBeVisible();
         });
 
-        test.only('Change table columns', async ({ page, previewFrame, projectCopy, ui5Version }) => {
+        test('Change table columns', async ({ page, previewFrame, projectCopy, ui5Version }) => {
             test.skip(
                 gte(ui5Version, '1.96.0', { loose: true }) && !satisfies(ui5Version, '^1.108.0'),
                 'Change table columns is not supported in this version'
             );
 
-            const lr = new ListReport(previewFrame);
             const tableSettings = new TableSettings(previewFrame);
             const editor = new AdaptationEditorShell(page);
 
@@ -235,6 +285,55 @@ test.describe(`@quick-actions @fe-v2`, () => {
             await expect(tableSettings.tableSettingsDialog.getByText('String Property')).toBeVisible();
             await expect(tableSettings.tableSettingsDialog.getByText('Boolean Property')).toBeVisible();
             await expect(tableSettings.tableSettingsDialog.getByText('Currency')).toBeVisible();
+        });
+
+        test('Add Custom Table Action', async ({ page, previewFrame, projectCopy, ui5Version }) => {
+            test.skip(
+                lt(ui5Version, '1.96.0', { loose: true }),
+                'Change table columns is not supported in this version'
+            );
+
+            const lr = new ListReport(previewFrame);
+            const tableSettings = new TableSettings(previewFrame);
+            const editor = new AdaptationEditorShell(page);
+
+            await editor.quickActions.addCustomTableAction.click();
+
+            await previewFrame.getByRole('textbox', { name: 'Fragment Name' }).fill('table-action');
+            await previewFrame.getByLabel('Footer actions').getByRole('button', { name: 'Create' }).click();
+
+            await editor.toolbar.saveAndReloadButton.click();
+
+            await expect(editor.toolbar.saveButton).toBeDisabled();
+
+            await expect
+                .poll(async () => readChanges(projectCopy), {
+                    message: 'make sure change file is created'
+                })
+                .toEqual(
+                    expect.objectContaining({
+                        fragments: expect.objectContaining({
+                            'table-action.fragment.xml': expect.stringMatching(
+                                new RegExp(`<!-- Use stable and unique IDs!-->
+<core:FragmentDefinition xmlns:core='sap.ui.core' xmlns='sap.m'>
+    <!--  add your xml here -->
+    <Button text="New Button"  id="btn-[a-z0-9]+"></Button>
+</core:FragmentDefinition>
+`)
+                            )
+                        }),
+                        changes: expect.arrayContaining([
+                            expect.objectContaining({
+                                fileType: 'change',
+                                changeType: 'addXML',
+                                content: expect.objectContaining({
+                                    targetAggregation: 'content',
+                                    fragmentPath: 'fragments/table-action.fragment.xml'
+                                })
+                            })
+                        ])
+                    })
+                );
         });
     });
 });
