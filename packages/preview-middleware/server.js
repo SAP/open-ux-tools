6ffdbdae638b56c034101e35698b2e3a3240dfac
Davis Voicescuks <davis.voicescuks@sap.com>
2025-05-06 18:59:01 +0300

    test: fix for UI5 1.96

--- packages/preview-middleware/server.js
+++ packages/preview-middleware/server.js
@@ -1,71 +1,83 @@
-import { readFile, rm, stat, symlink, writeFile } from 'fs/promises';
-import { join } from 'path';
-import path from 'path';
-import fs from 'fs';
-
-import express from 'express';
-import ZipFile from 'adm-zip';
-
-import { install } from '@sap-ux-private/playwright';
-
+'use strict';
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { 'default': mod };
+    };
+Object.defineProperty(exports, '__esModule', { value: true });
+const promises_1 = require('fs/promises');
+const path_1 = require('path');
+const path_2 = __importDefault(require('path'));
+const fs_1 = __importDefault(require('fs'));
+const express_1 = __importDefault(require('express'));
+const adm_zip_1 = __importDefault(require('adm-zip'));
+const playwright_1 = require('@sap-ux-private/playwright');
 /**
  * Global setup.
  *
  * It fetches maintained UI5 versions and add them to `process.env` variable.
  */
-async function globalSetup(): Promise<void> {
-    await install(join(__dirname, '..', 'fixtures', 'mock'));
-    const app = express();
+async function globalSetup() {
+    await (0, playwright_1.install)((0, path_1.join)(__dirname, 'test', 'fixtures', 'mock'));
+    const app = (0, express_1.default)();
     const port = 3050;
-    const mapping: Record<string, Set<string>> = {};
-
+    const mapping = {};
     // Define the path to the static content
-    const staticPath = path.join(__dirname, '..', 'fixtures-copy');
+    const staticPath = path_2.default.join(__dirname, 'test', 'fixtures-copy');
+    app.use((req, res, next) => {
+        console.log('Request URL:', req.url);
+        next();
+    });
 
+    app.get('/status', (req, res) => {
+        
+        res.sendStatus(200);
+    });
     // Serve static files
-
-    app.use('/sap/bc/ui5_ui5/ui5/', express.static(staticPath));
+    app.use('/sap/bc/ui5_ui5/ui5/', express_1.default.static(staticPath));
     app.get('/sap/bc/lrep/actions/getcsrftoken', (req, res) => {
         res.sendStatus(200);
     });
     app.get('/sap/bc/adt/discovery', (req, res) => {
-        const filePath = path.join(__dirname, 'responses', 'discovery.xml');
-        const readStream = fs.createReadStream(filePath);
-
+        const filePath = path_2.default.join(__dirname, 'test', 'integration', 'responses', 'discovery.xml');
+        const readStream = fs_1.default.createReadStream(filePath);
         readStream.on('open', () => {
             res.setHeader('Content-Type', 'application/xml');
             readStream.pipe(res);
         });
-
         readStream.on('error', (err) => {
             console.error('Error reading file:', err);
             res.status(500).send('Internal Server Error');
         });
     });
-
     app.put('/sap/bc/lrep/appdescr_variant_preview', (req, res) => {
-        const bufferChunks: any[] = [];
+        const bufferChunks = [];
         req.on('data', (chunk) => {
             bufferChunks.push(chunk);
         });
-
         req.on('end', async () => {
+            var _a;
+            var _b;
             const buffer = Buffer.concat(bufferChunks);
             try {
-                const directory = new ZipFile(buffer);
-
-                const variant = JSON.parse(directory.readFile('manifest.appdescr_variant')!.toString('utf-8'));
+                const directory = new adm_zip_1.default(buffer);
+                const variant = JSON.parse(directory.readFile('manifest.appdescr_variant').toString('utf-8'));
                 const baseAppDirectory = `${variant.reference}`;
-                const manifestText = await readFile(
-                    join(__dirname, '..', 'fixtures-copy', `${variant.reference}`, 'webapp', 'manifest.json'),
+                const manifestText = await (0, promises_1.readFile)(
+                    (0, path_1.join)(
+                        __dirname,
+                        'test',
+                        'fixtures-copy',
+                        `${variant.reference}`,
+                        'webapp',
+                        'manifest.json'
+                    ),
                     'utf-8'
                 );
-
                 const manifest = JSON.parse(manifestText);
                 manifest['sap.app'].id = variant.id;
-                mapping[variant.reference] ??= new Set();
+                (_a = mapping[(_b = variant.reference)]) !== null && _a !== void 0 ? _a : (mapping[_b] = new Set());
                 mapping[variant.reference].add(variant.id);
-
                 res.json({
                     [variant.id]: {
                         name: variant.reference,
@@ -134,7 +146,6 @@ async function globalSetup(): Promise<void> {
             }
         });
     });
-
     app.get('/sap/bc/ui2/app_index/ui5_app_info_json', (req, res) => {
         const baseAppDirectory = `${req.query.id}`;
         const variants = mapping[req.query.id];
@@ -171,20 +182,23 @@ async function globalSetup(): Promise<void> {
                     ]
                 };
                 return acc;
-            }, {} as Record<string, any>)
+            }, {})
         );
     });
-
     // Start the server
     const server = app.listen(port, () => {
         console.log(`Mock ABAP backend is running on http://localhost:${port}`);
     });
-
     // await use(`http://localhost:${port}`);
-
     // server.close(() => {
     //     console.log(`Mock ABAP backend server closed: http://localhost:${port}`);
     // });
 }
 
-export default globalSetup;
+globalSetup()
+    .then(() => {
+        console.log('Global setup completed successfully.');
+    })
+    .catch((error) => {
+        console.error('Error during global setup:', error);
+    });
