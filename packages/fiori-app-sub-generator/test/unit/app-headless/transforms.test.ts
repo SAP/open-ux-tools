02a09591096dce80034916359090642845edb31e
i057546 <iain.o-farrell@sap.com>
2025-05-28 12:54:34 +0100

    Fixes setting destination in headless app config throws
    https://github.com/SAP/open-ux-tools/issues/3274

--- packages/fiori-app-sub-generator/test/unit/app-headless/transforms.test.ts
+++ packages/fiori-app-sub-generator/test/unit/app-headless/transforms.test.ts
@@ -4,7 +4,8 @@ import type { FFAppConfig } from '../../../src/types';
 import {
     appConfigInvalidCapServiceName,
     appConfigInvalidEdmx,
-    appConfigNotSupportedVersion
+    appConfigNotSupportedVersion,
+    appConfigDest
 } from './test-data/testHeadlessAppConfigs';
 
 /**
@@ -24,5 +25,39 @@ describe('Test headless', () => {
         expect(() => {
             transformExtState(appConfigInvalidCapServiceName as unknown as FFAppConfig);
         }).toThrow(t('error.appConfigMissingRequiredProperty', { propertyName: 'capService.serviceName' }));
+
+        // Should include destination name if specified
+        expect(transformExtState(appConfigDest as unknown as FFAppConfig)).toEqual({
+            floorplan: 'basic',
+            project: {
+                description: 'An SAP Fiori application.',
+                enableCodeAssist: false,
+                enableEslint: false,
+                enableTypeScript: false,
+                flpAppId: '',
+                localUI5Version: '1.88.1',
+                name: 'simple',
+                namespace: '',
+                sapux: false,
+                skipAnnotations: false,
+                targetFolder:
+                    '/Users/Projects/tools-suite/packages/app-generator/fiori-freestyle/test/test-output/app-headless-test',
+                title: 'App Title',
+                ui5Theme: 'sap_fiori_3',
+                ui5Version: '1.88.1'
+            },
+            service: {
+                client: undefined,
+                destinationName: 'SomeDestinationName',
+                edmx: expect.stringContaining(
+                    '<?xml version="1.0" encoding="utf-8" ?><edmx:DataServices m:DataServiceVersion="2.0"></edmx:DataServices><edmx:Edmx Version="1.0"'
+                ),
+                host: undefined,
+                servicePath: undefined,
+                source: 'sapSystem',
+                version: '2'
+            },
+            viewName: 'view1'
+        });
     });
 });
