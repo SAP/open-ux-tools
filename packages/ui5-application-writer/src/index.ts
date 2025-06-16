7d02ad797e1759c96bb0c894aa1df469eb4ed272
Cian Morrin <cian.morrin@sap.com>
2025-06-16 12:56:38 +0100

    fix(fiori-gen): correct flp default path

--- packages/ui5-application-writer/src/index.ts
+++ packages/ui5-application-writer/src/index.ts
@@ -113,7 +113,7 @@ function getPreviewMiddlewareOpts(app: App, ui5Theme: string, useVirtualPreviewE
         }),
         // only add flp intent if not using virtual endpoints (default `app-preview` will be used by middleware)
         ...(!useVirtualPreviewEndpoints && {
-            localStartFile: app.localStartFile ?? 'flpSandbox.html',
+            localStartFile: app.localStartFile ?? 'test/flpSandbox.html',
             flpAction: app.flpAction,
             appId: app.id
         })
