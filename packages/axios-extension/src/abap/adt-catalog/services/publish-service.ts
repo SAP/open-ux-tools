e2df474e2109776f0d90bac953ee6a87fb50e44c
vinayhospete <vinay.hospete.krishnappa@sap.com>
2025-06-17 10:20:45 +0530

    fix: remove publish method

--- packages/axios-extension/src/abap/adt-catalog/services/publish-service.ts
+++ packages/axios-extension/src/abap/adt-catalog/services/publish-service.ts
@@ -1,5 +1,5 @@
 import { AdtService } from './adt-service';
-import type { AdtCategory, ODataServiceTechnicalDetails, PublishResponse } from '../../types';
+import type { AdtCategory, ODataServiceTechnicalDetails } from '../../types';
 
 /**
  *
@@ -20,37 +20,6 @@ export class PublishService extends AdtService {
      */
     public static getAdtCatagory(): AdtCategory {
         return PublishService.adtCategory;
-    }
-
-    /**
-     * Build the service binding content, used in the publish request.
-     *
-     * @param serviceType - The service type.
-     * @param serviceBindingName - The service binding name.
-     * @returns The service binding content.
-     */
-    private buildServiceBindingContent(serviceType: string, serviceBindingName: string): string {
-        return `<?xml version="1.0" encoding="UTF-8"?><adtcore:objectReferences xmlns:adtcore="http://www.sap.com/adt/core"><adtcore:objectReference adtcore:type="${serviceType}" adtcore:name="${serviceBindingName}"/></adtcore:objectReferences>`;
-    }
-
-    /**
-     * Publish the service binding.
-     *
-     * @param type - The type of the service binding.
-     * @param bindingName - The name of the service binding.
-     * @returns The response status message.
-     */
-    public async publish(type: string, bindingName: string): Promise<PublishResponse> {
-        const content = this.buildServiceBindingContent(type, bindingName);
-        const response = await this.post(`/publishjobs`, content, {
-            headers: {
-                'Content-Type': 'application/xml',
-                Accept: 'application/xml, application/vnd.sap.as+xml;charset=UTF-8;dataname=com.sap.adt.StatusMessage'
-            }
-        });
-
-        const data = this.parseResponse(response.data);
-        return data['abap']['values']['DATA'];
     }
 
     /**
