099e6bf3124cdd40d2864b8063fcbf43274a89b8
Andis Redmans <andis.redmans@sap.com>
2025-06-19 17:42:54 +0300

    fix: overwrite fluentui overlay background
    overwrite fluentui overlay background

--- packages/ui-components/test/unit/components/UIDialog.test.tsx
+++ packages/ui-components/test/unit/components/UIDialog.test.tsx
@@ -205,6 +205,14 @@ describe('<UIDialog />', () => {
     });
 
     describe('Styles', () => {
+        it('Basic style', () => {
+            const dialog = wrapper.find(Dialog);
+            const props = dialog.props();
+            expect(props.modalProps?.overlay?.styles).toEqual({
+                background: 'var(--vscode-editor-background)',
+                opacity: 0.8
+            });
+        });
         it('Title - single line', () => {
             const dialog = wrapper.find(Dialog);
             const props = dialog.props();
