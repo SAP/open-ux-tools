f9ea9e33a085b1d2c3aa4d698ba9434aad60d417
mmilko01 <162288787+mmilko01@users.noreply.github.com>
2025-06-24 09:59:25 +0300

    feat: Enhance ADP FLP configuration generator (#3263)
    * feat: enhance adp FLP config generator
    
    * fix: inbound ids prompt
    
    * fix: prompting
    
    * fix: add prompt pages
    
    * fix: add validation for duplicate inbound and cloud project
    
    * feat: adopt writing
    
    * fix: rename function
    
    * feat: enhance CLI command
    
    * fix: small fixes
    
    * fix: validation for duplicate inbound
    
    * fix: cache provider for back navigation
    
    * refactor: move util functions
    
    * chore: create changeset
    
    * fix: add warning translaction
    
    * fix: address review comments
    
    * fix: address review comments
    
    * fix: default values for semantic object and action
    
    * fix: return charecter limit for action to 60
    
    * test: enhance adp-tooling tests
    
    * fix: consumption in adp generator
    
    * fix: keep FLP config pages after back navigation
    
    * fix: prevent moving of FLP pages
    
    * fix: steps sequence
    
    * fix: prevent deploy step to be removed after navigation
    
    * fix: change sequence of update of wizard steps
    
    * test: enhance axios-extension tests
    
    * test: enhance create tests
    
    * test: enhance i18n tests
    
    * Linting auto fix commit
    
    * fix: add tooltips for sem obj and action in case of ADP
    
    * fix: texts
    
    * fix: additional new lines in i18n.properties
    
    * test: enhance flp-config-inquirer tests
    
    * test: enhance adp-flp-config-sub-generator tests
    
    * Linting auto fix commit
    
    * test: enhance generator-adp tests
    
    * chore: fix typo
    
    * test: enhance adp-flp-config-sub-generator tests
    
    * fix: sonar issues
    
    * test: enhance flp-config-inquirer tests
    
    * test: enhance tests
    
    * fix: remove unneeded code
    
    * fix: ui texts
    
    * fix: keep default axiox params after paramsSerializer
    
    * fix: create default logger when running in CLI
    
    * fix: store icon field in change
    
    * fix: wrong step name
    
    * fix: remove tooltip in add new scenario
    
    * fix: address review comments
    
    * test: adapt  adp-tooling tests
    
    * fix: remove additional empty line when removing keys
    
    * docs: JSDOC
    
    * fix: address review comments
    
    * fix: address review comments regarding texts
    
    * chore: fix formatting
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Davis Voicescuks <davis.voicescuks@sap.com>

--- packages/axios-extension/src/abap/index.ts
+++ packages/axios-extension/src/abap/index.ts
@@ -10,7 +10,9 @@ export {
     AdaptationConfig,
     MergedAppDescriptor,
     SystemInfo,
-    AdaptationProjectType
+    AdaptationProjectType,
+    Inbound,
+    InboundContent
 } from './lrep-service';
 export { AbapServiceProvider } from './abap-service-provider';
 export { AppIndex, AppIndexService, Ui5AppInfo, Ui5AppInfoContent, App } from './app-index-service';
