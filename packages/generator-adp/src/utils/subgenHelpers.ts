248f11f75ac4327ee446b38588952d2e4f85e4c9
mmilko01 <mihail.milkov@sap.com>
2025-06-26 17:42:40 +0300

    fix: lint issue

--- packages/generator-adp/src/utils/subgenHelpers.ts
+++ packages/generator-adp/src/utils/subgenHelpers.ts
@@ -3,7 +3,6 @@ import type { AppWizard } from '@sap-devx/yeoman-ui-types';
 
 import type { ToolsLogger } from '@sap-ux/logger';
 import type { ManifestNamespace } from '@sap-ux/project-access';
-import type { ILogWrapper } from '@sap-ux/fiori-generator-shared';
 import type { ConfigAnswers, AttributesAnswers, SystemLookup, FlexLayer } from '@sap-ux/adp-tooling';
 
 import { t } from './i18n';
