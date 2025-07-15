d27b2e64f9219c065f6abe9f6b34d1b6f849c379
Peter Murphy <petermurphy5672@gmail.com>
2025-07-15 14:40:44 +0100

    Update abap-deploy-config-writer

---
+++ packages/abap-deploy-config-writer/eslint.config.mjs
@@ -0,0 +1,19 @@
+import baseConfig from '../../eslint.config.mjs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+export default [
+    {
+        ignores: ['test/test-output', 'test/sample', 'dist']
+    },
+    ...baseConfig,
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+        languageOptions: {
+            parserOptions: {
+                project: './tsconfig.eslint.json',
+                tsconfigRootDir: path.dirname(fileURLToPath(import.meta.url))
+            }
+        }
+    }
+];
