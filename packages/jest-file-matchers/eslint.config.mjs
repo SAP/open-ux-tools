d11c6ff4c884eff5b081623e1d1ca9983996f94f
Lukas Wenner <21086980+lwenner@users.noreply.github.com>
2025-07-16 15:55:31 +0100

    Update jest-file-matchers

---
+++ packages/jest-file-matchers/eslint.config.mjs
@@ -0,0 +1,22 @@
+import baseConfig from '../../eslint.config.mjs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+export default [
+    {
+        ignores: ['test', 'dist']
+    },
+    ...baseConfig,
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+        languageOptions: {
+            parserOptions: {
+                project: './tsconfig.eslint.json',
+                tsconfigRootDir: path.dirname(fileURLToPath(import.meta.url))
+            }
+        },
+        rules: {
+            '@typescript-eslint/no-namespace': 'off'
+        }
+    }
+];
