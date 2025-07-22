101ab4e79cc03889ea9d103867c85a8322e1997b
mmilko01 <mihail.milkov@sap.com>
2025-07-22 18:57:05 +0300

    test: fix tests

--- packages/adp-tooling/test/unit/writer/editors.test.ts
+++ packages/adp-tooling/test/unit/writer/editors.test.ts
@@ -1,9 +1,16 @@
 import type { AnnotationsData } from '../../../src';
 import { ChangeType, generateChange } from '../../../src';
 import { WriterFactory } from '../../../src/writer/changes/writer-factory';
+import * as memFsEditor from 'mem-fs-editor';
+import * as memFs from 'mem-fs';
+
+jest.mock('mem-fs-editor');
+jest.mock('mem-fs');
 
 describe('generateChange', () => {
     const writeSpy = jest.fn();
+    const createStorageSpy = jest.spyOn(memFs, 'create').mockReturnValue({} as memFs.Store);
+    const createEditorSpy = jest.spyOn(memFsEditor, 'create').mockReturnValue({} as memFsEditor.Editor);
 
     beforeEach(() => {
         jest.clearAllMocks();
@@ -30,6 +37,32 @@ describe('generateChange', () => {
         );
 
         expect(writeSpy).toHaveBeenCalledWith({ variant: {}, annotation: {} });
+        expect(createEditorSpy).toHaveBeenCalled();
+        expect(createStorageSpy).toHaveBeenCalled();
+        expect(fs).toBeDefined();
+    });
+
+    it('should successfully invoke the writer for a given generator type with passed editor', async () => {
+        const fs = await generateChange(
+            '/path/to/project',
+            ChangeType.ADD_ANNOTATIONS_TO_ODATA,
+            {
+                variant: {},
+                annotation: {}
+            } as AnnotationsData,
+            {} as memFsEditor.Editor
+        );
+
+        expect(WriterFactory.createWriter).toHaveBeenCalledWith(
+            ChangeType.ADD_ANNOTATIONS_TO_ODATA,
+            {},
+            '/path/to/project',
+            undefined
+        );
+
+        expect(writeSpy).toHaveBeenCalledWith({ variant: {}, annotation: {} });
+        expect(createEditorSpy).not.toHaveBeenCalled();
+        expect(createStorageSpy).not.toHaveBeenCalled();
         expect(fs).toBeDefined();
     });
 });
