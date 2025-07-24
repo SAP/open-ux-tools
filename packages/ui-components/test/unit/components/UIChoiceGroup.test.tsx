4c8f4704fbfd38347d58c932676290dbc493dcd4
Andis Redmans <andis.redmans@sap.com>
2025-07-24 14:09:00 +0300

    fix: lint
    lint

--- packages/ui-components/test/unit/components/UIChoiceGroup.test.tsx
+++ packages/ui-components/test/unit/components/UIChoiceGroup.test.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { render } from '@testing-library/react';
 import { UIChoiceGroup } from '../../../src/components/UIChoiceGroup/UIChoiceGroup';
 import { compareStylesBySelector, findStyleFromStyleSheets } from '../../utils/styles';
