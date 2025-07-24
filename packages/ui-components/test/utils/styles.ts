b0ee040818c006c73a7a2cdf32f4b8480d7ced70
Andis Redmans <andis.redmans@sap.com>
2025-07-24 14:09:17 +0300

    fix: lint
    lint

--- packages/ui-components/test/utils/styles.ts
+++ packages/ui-components/test/utils/styles.ts
@@ -2,14 +2,40 @@ export const getStylesByElement = (element?: Element): CSSStyleDeclaration => {
     return element ? window.getComputedStyle(element) : ({} as CSSStyleDeclaration);
 };
 
-export const getStylesBySelector = (selector: string): CSSStyleDeclaration => {
-    const element = document.body.querySelector(selector);
-    return getStylesByElement(element);
+const toKebabCase = (prop: string): string => {
+    return prop.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
 };
 
-export const compareStyles = (styles: CSSStyleDeclaration, expectedStyles: Partial<CSSStyleDeclaration>): void => {
+export const findStyleFromStyleSheets = (styleProperty: string, element?: Element, selector?: string) => {
+    styleProperty = toKebabCase(styleProperty);
+    const classNames = element ? Array.from(element.classList) : [];
+    for (let className of classNames) {
+        if (selector) {
+            className += selector;
+        }
+        for (let i = 0; i < document.styleSheets.length; i++) {
+            const sheet = document.styleSheets[i];
+            for (let j = 0; j < sheet.cssRules.length; j++) {
+                const rule = sheet.cssRules[j];
+                if (rule instanceof CSSStyleRule && rule.selectorText?.includes(className)) {
+                    return rule.style[styleProperty];
+                }
+            }
+        }
+    }
+};
+
+export const compareStyles = (
+    styles: CSSStyleDeclaration,
+    expectedStyles: Partial<CSSStyleDeclaration>,
+    element?: Element
+): void => {
     for (const name in expectedStyles) {
-        expect(styles[name]).toEqual(expectedStyles[name]);
+        if (typeof expectedStyles[name] === 'string' && expectedStyles[name].startsWith('var(')) {
+            expect(findStyleFromStyleSheets(name, element)).toEqual(expectedStyles[name]);
+        } else {
+            expect(styles[name]).toEqual(expectedStyles[name]);
+        }
     }
 };
 
@@ -18,10 +44,12 @@ export const compareStylesByElement = (
     expectedStyles: Partial<CSSStyleDeclaration>
 ): void => {
     const styles = getStylesByElement(element);
-    compareStyles(styles, expectedStyles);
+    compareStyles(styles, expectedStyles, element);
 };
 
 export const compareStylesBySelector = (selector: string, expectedStyles: Partial<CSSStyleDeclaration>): void => {
-    const styles = getStylesBySelector(selector);
-    compareStyles(styles, expectedStyles);
+    const element = document.body.querySelector(selector);
+    expect(element).toBeInTheDocument();
+    const styles = getStylesByElement(element);
+    compareStyles(styles, expectedStyles, element);
 };
