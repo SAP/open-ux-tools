ad49c30195c176bc44bcf4bb36ffe6b35f3f7ed0
Davis Voicescuks <davis.voicescuks@sap.com>
2025-08-06 16:24:56 +0300

    fix: add custom table column quick action (#3517)
    * fix: add custom table column quick action
    
    * ci: force cache invalidation
    
    * ci: revert previous commit
    
    * test: remove sync views dialog check

--- packages/preview-middleware-client/src/adp/controllers/AddSubpage.controller.ts
+++ packages/preview-middleware-client/src/adp/controllers/AddSubpage.controller.ts
@@ -119,9 +119,10 @@ export default class AddSubpage extends BaseDialog<AddSubpageModel> {
      * @param event Event
      */
     async onCreateBtnPress(event: Event) {
-        await super.onCreateBtnPressHandler();
         const source = event.getSource<Button>();
         source.setEnabled(false);
+        await super.onCreateBtnPressHandler();
+
 
         const flexSettings = this.rta.getFlexSettings();
         const navProperty = this.model.getProperty('/selectedNavigation/key');
