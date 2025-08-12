449cb97603f53923d8bc4f249a4166b23107b58c
Jimmy Joseph <jimmy.joseph@sap.com>
2025-08-12 09:07:49 +0200

    fix: lint issue

--- tests/integration/adaptation-editor/src/scenarios/quick-actions/test-utils.ts
+++ tests/integration/adaptation-editor/src/scenarios/quick-actions/test-utils.ts
@@ -1,6 +1,7 @@
 import { readdir, readFile } from 'fs/promises';
 import { join } from 'path';
-import { expect, type Page, type FrameLocator, type Locator } from '@sap-ux-private/playwright';
+import { expect, type FrameLocator, type Page, type Locator } from '@sap-ux-private/playwright';
+import { gte } from 'semver';
 
 interface Changes {
     annotations: Record<string, string>;
@@ -45,7 +46,7 @@ export class ListReport {
 }
 
 /**
- * Class representing Table Settings in the Adaptation Editor.
+ * Class representing the table settings dialog in the Adaptation Editor.
  */
 export class TableSettings {
     private readonly frame: FrameLocator;
@@ -119,87 +120,171 @@ export class TableSettings {
 /**
  * Class representing the quick action panel in the Adaptation Editor.
  */
-export class QuickActionPanel {
+class QuickActionPanel {
     private readonly page: Page;
+
+    /**
+     * @returns Locator for the button to add a controller to the page.
+     */
     get addControllerToPage(): Locator {
         return this.page.getByRole('button', { name: 'Add Controller to Page' });
     }
+
+    /**
+     * @returns Locator for the button to show the page controller.
+     */
     get showPageController(): Locator {
         return this.page.getByRole('button', { name: 'Show Page Controller' });
     }
+
+    /**
+     * @returns Locator for the button to enable the "Clear" button in the filter bar.
+     */
     get enableClearButton(): Locator {
         return this.page.getByRole('button', { name: 'Enable "Clear" Button in Filter Bar' });
     }
+
+    /**
+     * @returns Locator for the button to disable the "Clear" button in the filter bar.
+     */
     get disableClearButton(): Locator {
         return this.page.getByRole('button', { name: 'Disable "Clear" Button in Filter Bar' });
     }
+
+    /**
+     * @returns Locator for the button to enable the semantic date range in the filter bar.
+     */
     get enableSemanticDateRange(): Locator {
         return this.page.getByRole('button', { name: 'Enable Semantic Date Range in Filter Bar' });
     }
+
+    /**
+     * @returns Locator for the button to disable the semantic date range in the filter bar.
+     */
     get disableSemanticDateRange(): Locator {
         return this.page.getByRole('button', { name: 'Disable Semantic Date Range in Filter Bar' });
     }
+
+    /**
+     * @returns Locator for the button to change table columns.
+     */
     get changeTableColumns(): Locator {
         return this.page.getByRole('button', { name: 'Change Table Columns' });
     }
+    /**
+     * @returns Locator for the button to change Table Actions.
+     */
     get changeTableActions(): Locator {
         return this.page.getByRole('button', { name: 'Change Table Actions' });
     }
+
+    /**
+     * @returns Locator for the button to add a custom table action.
+     */
     get addCustomTableAction(): Locator {
         return this.page.getByRole('button', { name: 'Add Custom Table Action' });
     }
+
+    /**
+     * @returns Locator for the button to add a custom table column.
+     */
     get addCustomTableColumn(): Locator {
         return this.page.getByRole('button', { name: 'Add Custom Table Column' });
     }
+
+    /**
+     * @returns Locator for the button to enable variant management in tables and charts.
+     */
     get enableVariantManagementInTablesAndCharts(): Locator {
         return this.page.getByRole('button', { name: 'Enable Variant Management in Tables and Charts' });
     }
+    /**
+     * @returns Locator for the button to enable empty row mode for tables.
+     */
     get enableEmptyRowMode(): Locator {
         return this.page.getByRole('button', { name: 'Enable Empty Row Mode for Tables' });
     }
+    /**
+     *
+     * @param page
+     */
     constructor(page: Page) {
         this.page = page;
     }
 }
 
-export class Toolbar {
+/**
+ * Class representing the ADP Toolbar.
+ */
+class Toolbar {
     private readonly page: Page;
+    /**
+     * @returns Locator for the "Save" button.
+     */
     get saveButton(): Locator {
         return this.page.getByRole('button', { name: 'Save' });
     }
+    /**
+     * @returns Locator for the "Save and Reload" button.
+     */
     get saveAndReloadButton(): Locator {
         return this.page.getByRole('button', { name: 'Save and Reload' });
     }
+    /**
+     * @returns Locator for the "UI Adaptation" mode button.
+     */
     get uiAdaptationModeButton(): Locator {
         return this.page.getByRole('button', { name: 'UI Adaptation' });
     }
+    /**
+     * @returns Locator for the "Navigation" mode button.
+     */
     get navigationModeButton(): Locator {
         return this.page.getByRole('button', { name: 'Navigation' });
     }
+    /**
+     * @param page - Page object for the toolbar.
+     */
     constructor(page: Page) {
         this.page = page;
     }
 }
-
-export class ChangesPanel {
+/**
+ * Class representing the Changes Panel in the Adaptation Editor.
+ */
+class ChangesPanel {
     private readonly page: Page;
+    /**
+     * @returns Locator for the "Reload" button.
+     */
     get reloadButton(): Locator {
         return this.page.getByRole('link', { name: 'Reload' });
     }
+    /**
+     * @param page - Page object for the Changes Panel.
+     */
     constructor(page: Page) {
         this.page = page;
     }
 }
 
+/**
+ * Class representing the Adaptation Editor shell.
+ */
 export class AdaptationEditorShell {
     private readonly page: Page;
     private readonly ui5Version: string;
     readonly quickActions: QuickActionPanel;
     readonly toolbar: Toolbar;
     readonly changesPanel: ChangesPanel;
+
     async reloadCompleted(): Promise<void> {
         await expect(this.toolbar.uiAdaptationModeButton).toBeEnabled({ timeout: 15_000 });
     }
+    /**
+     * @param page - Page object for the Adaptation Editor.
+     * @param ui5Version - UI5 version.
+     */
     constructor(page: Page, ui5Version: string) {
         this.ui5Version = ui5Version;
         this.page = page;
@@ -209,16 +294,27 @@ export class AdaptationEditorShell {
     }
 }
 
+/**
+ * Class representing a dialog in the Adaptation Editor.
+ */
 export class AdpDialog {
     private readonly frame: FrameLocator;
     private readonly ui5Version: string;
+    /**
+     * @returns Locator for the "Create" button in the dialog.
+     */
     get createButton(): Locator {
-        if (this.ui5Version && this.ui5Version >= '1.120.0') {
+        if (gte(this.ui5Version, '1.120.0')) {
             return this.frame.getByLabel('Footer actions').getByRole('button', { name: 'Create' });
         } else {
             return this.frame.locator('#createDialogBtn');
         }
     }
+
+    /**
+     * @param frame - FrameLocator for the dialog.
+     * @param ui5Version - UI5 version.
+     */
     constructor(frame: FrameLocator, ui5Version: string) {
         this.frame = frame;
         this.ui5Version = ui5Version;
@@ -228,10 +324,10 @@ export class AdpDialog {
 /**
  * Read changes from the changes folder in the project.
  *
- * @param root - The root directory of the project.
- * @returns An object containing the parsed changes.
+ * @param root - Root directory of the project.
+ * @returns Promise resolving to an object containing annotations, coding, fragments, and changes.
  */
-export async function readChanges(root: string): Promise<any> {
+export async function readChanges(root: string): Promise<Changes> {
     const changesDirectory = join(root, 'webapp', 'changes');
     const result: Changes = {
         annotations: {},
@@ -245,14 +341,26 @@ export async function readChanges(root: string): Promise<any> {
             try {
                 if (file.isFile()) {
                     const text = await readFile(join(changesDirectory, file.name), { encoding: 'utf-8' });
-                    result.changes.push(JSON.parse(text));
+                    result.changes.push(JSON.parse(text) as object);
+                } else if (file.name === 'annotations' || file.name === 'coding' || file.name === 'fragments') {
+                    const children = await readdir(join(changesDirectory, file.name), { withFileTypes: true });
+                    for (const child of children) {
+                        if (child.isFile()) {
+                            result[file.name][child.name] = await readFile(
+                                join(changesDirectory, file.name, child.name),
+                                {
+                                    encoding: 'utf-8'
+                                }
+                            );
+                        }
+                    }
                 }
             } catch (e) {
                 // ignore error
             }
         }
     } catch (e) {
-        // ignore err
+        // ignore error
     }
     return result;
 }
