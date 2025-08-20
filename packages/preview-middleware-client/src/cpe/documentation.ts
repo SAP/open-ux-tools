04e9922c94216e2f8c53281ec4d7dd3437cc4570
Atanas Vasilev <a.vasilev@sap.com>
2025-08-20 13:24:37 +0300

    refactor: Remove unused import

--- packages/preview-middleware-client/src/cpe/documentation.ts
+++ packages/preview-middleware-client/src/cpe/documentation.ts
@@ -4,7 +4,7 @@ import type { Properties } from './utils';
 import Log from 'sap/base/Log';
 import { MessageBarType, PropertiesInfo } from '@sap-ux-private/control-property-editor-common';
 import { sendInfoCenterMessage } from '../utils/info-center-message';
-import { FetchError, getError } from '../utils/error';
+import { FetchError } from '../utils/error';
 
 export interface ControlMetadata {
     baseType: string | undefined;
