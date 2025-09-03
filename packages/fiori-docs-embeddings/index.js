b668a672a07a413d0a36ce9e46e9242febc7d747
Austin Devine <austin.devine@sap.com>
2025-09-03 11:18:25 +0100

    fiori-docs-embeddings - convert esm to commonjs for better backward compatibility

--- packages/fiori-docs-embeddings/index.js
+++ packages/fiori-docs-embeddings/index.js
@@ -3,51 +3,47 @@
  * Provides access to SAP Fiori documentation embeddings and related data
  */
 
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const path = require('path');
 
 /**
  * Get the path to the data directory
  * @returns {string} Absolute path to data directory
  */
-export function getDataPath() {
-    return join(__dirname, 'data');
+function getDataPath() {
+    return path.join(__dirname, 'data');
 }
 
 /**
  * Get the path to the embeddings directory
  * @returns {string} Absolute path to embeddings directory
  */
-export function getEmbeddingsPath() {
-    return join(__dirname, 'data', 'embeddings');
+function getEmbeddingsPath() {
+    return path.join(__dirname, 'data', 'embeddings');
 }
 
 /**
  * Get the path to the search data directory
  * @returns {string} Absolute path to search directory
  */
-export function getSearchPath() {
-    return join(__dirname, 'data', 'search');
+function getSearchPath() {
+    return path.join(__dirname, 'data', 'search');
 }
 
 /**
  * Get the path to the documentation data directory
  * @returns {string} Absolute path to docs directory
  */
-export function getDocsPath() {
-    return join(__dirname, 'data', 'docs');
+function getDocsPath() {
+    return path.join(__dirname, 'data', 'docs');
 }
 
 /**
  * Check if embeddings data is available
  * @returns {Promise<boolean>} True if embeddings are available
  */
-export async function hasEmbeddings() {
+async function hasEmbeddings() {
     try {
-        const fs = await import('fs/promises');
+        const fs = require('fs/promises');
         const embeddingsPath = getEmbeddingsPath();
         const stat = await fs.stat(embeddingsPath);
         return stat.isDirectory();
@@ -56,4 +52,12 @@ export async function hasEmbeddings() {
     }
 }
 
-export { getDataPath as default };
+module.exports = {
+    getDataPath,
+    getEmbeddingsPath,
+    getSearchPath,
+    getDocsPath,
+    hasEmbeddings
+};
+
+module.exports.default = getDataPath;
