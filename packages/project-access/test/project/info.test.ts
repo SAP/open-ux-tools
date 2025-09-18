c385a76fc374b4f653ff5bb906c8143eccc15701
Vladislavs-Silins-SAP <vladislavs.silins@sap.com>
2025-09-18 10:23:36 +0300

    feat: enhance Project type with custom paths support (#3632)
    * feat(project-access): add support for CAP custom paths
    
    Enhanced the project type with optional `capCustomPaths` to incorporate additional CAP options. Introduced `getCapCustomPaths` functionality to extract these paths during project detail retrieval. This update aligns with CAP project enhancements.
    
    * feat(project-access): add support for CAP custom paths
    
    Enhanced the project type with optional `capCustomPaths` to incorporate additional CAP options. Introduced `getCapCustomPaths` functionality to extract these paths during project detail retrieval. This update aligns with CAP project enhancements.
    
    * feat(project-access): enhance project type with capCustomPaths
    
    Added `capCustomPaths` property to the Project type for managing custom paths in CAP projects. This update ensures these paths are considered during performance optimization checks.
    
    * feat(project-access): update tests to include capCustomPaths
    
    Added assertions for the `capCustomPaths` property in the project info test to ensure its correctness. This change aligns the tests with the recent enhancements to support CAP custom paths.
    
    * fix(project-access): refine capCustomPaths logic for EDMXBackend
    
    Adjusted the logic to ensure `capCustomPaths` is undefined for projects of type `EDMXBackend`. Updated test cases to validate this behavior. This fix improves consistency and correctness in handling project types.
    
    ---------
    
    Co-authored-by: Klaus Keller <66327622+Klaus-Keller@users.noreply.github.com>

--- packages/project-access/test/project/info.test.ts
+++ packages/project-access/test/project/info.test.ts
@@ -103,6 +103,11 @@ describe('Test getProject()', () => {
 
         expect(project.root).toBe(projectRoot);
         expect(project.projectType).toBe('CAPNodejs');
+        expect(project.capCustomPaths).toStrictEqual({
+            'app': 'app/',
+            'db': 'db/',
+            'srv': 'srv/'
+        });
         expect(Object.keys(project.apps).length).toBe(3);
 
         const appOne = project.apps[join('apps/one')];
@@ -213,7 +218,8 @@ describe('Test getProject()', () => {
         expect(project).toEqual({
             'root': projectRoot,
             'projectType': 'EDMXBackend',
-            'apps': {}
+            'apps': {},
+            'capCustomPaths': undefined
         });
     });
 });
