570966da27ab98e8789afddf54fcd7d1c1afe213
D048415 <dominik.heim@sap.com>
2025-10-02 14:02:18 +0200

    refactoring

--- packages/ui5-proxy-middleware/src/base/utils.ts
+++ packages/ui5-proxy-middleware/src/base/utils.ts
@@ -17,9 +17,6 @@ import type { Url } from 'url';
 import { t } from '../i18n';
 import type { ReaderCollection } from '@ui5/fs';
 import type { Socket } from 'node:net';
-import type connect from 'connect';
-
-export type EnhancedIncomingMessage = (IncomingMessage & Pick<Request, 'originalUrl'>) | connect.IncomingMessage;
 
 /**
  * Handler for the proxy response event.
@@ -362,12 +359,7 @@ export function getPathRewrite(config: ProxyConfig, ui5Ver: string): Options['pa
     if (config.pathReplace) {
         // Remove trailing slash from pathReplace if present
         const sanitizedPathReplace = config.pathReplace?.replace(/\/$/, '');
-        return (path: string, req: EnhancedIncomingMessage) => {
-            // ensure that the path starts with the configured path
-            // it might get lost when you nest router instances
-            const originalUrl = req.originalUrl?.includes(path) ? req.originalUrl : path;
-            return originalUrl.replace(config.path, sanitizedPathReplace);
-        }
+        return (path: string) => (path.startsWith(config.path) ? path.replace(config.path, sanitizedPathReplace) : sanitizedPathReplace + path);
     }
     return (path: string) => (path.startsWith(config.path) ? ui5Ver + path : ui5Ver + config.path + path);
 }
