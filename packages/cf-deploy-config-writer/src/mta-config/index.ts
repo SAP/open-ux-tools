7108f7d73589c76d44bce47d9c6c04a6b1ff71c1
John Long <john.long@sap.com>
2025-10-03 11:40:08 +0100

    fix: update node:fs

--- packages/cf-deploy-config-writer/src/mta-config/index.ts
+++ packages/cf-deploy-config-writer/src/mta-config/index.ts
@@ -1,4 +1,4 @@
-import { readFileSync, writeFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'node:fs';
 import { join } from 'path';
 import { render } from 'ejs';
 import { MtaConfig } from './mta';
--- packages/cf-deploy-config-writer/src/utils.ts
+++ packages/cf-deploy-config-writer/src/utils.ts
@@ -1,5 +1,5 @@
 import { join, normalize, posix } from 'path';
-import { promises as fs, readFileSync } from 'fs';
+import { promises as fs, readFileSync } from 'node:fs';
 import { coerce, satisfies } from 'semver';
 import type { Editor } from 'mem-fs-editor';
 import { CommandRunner } from '@sap-ux/nodejs-utils';
