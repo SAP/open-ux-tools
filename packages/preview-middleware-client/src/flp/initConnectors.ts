b4d0ba8010f25944d59f0f4be46b2b245aeb63be
D048415 <dominik.heim@sap.com>
2025-10-09 09:56:27 +0200

    fix jsdoc

--- packages/preview-middleware-client/src/flp/initConnectors.ts
+++ packages/preview-middleware-client/src/flp/initConnectors.ts
@@ -3,8 +3,8 @@ import { getUi5Version, isLowerThanMinimalUi5Version } from '../utils/version';
 /**
  * Initializes UI5 connectors based on the current UI5 version.
  *
- * For UI5 versions below 1.78, this function dynamically requires and executes a FakeLrepConnector.
- * For UI5 versions 1.78 and above, a local connector that reuses the WorkspaceConnector is being defined in preview-middleware/src/base/config.ts.
+ * For UI5 versions below 1.84, this function dynamically requires and executes a FakeLrepConnector.
+ * For UI5 versions 1.84 and above, a local connector that reuses the WorkspaceConnector is being defined in preview-middleware/src/base/config.ts.
  * This setup allows for flexibility in using different connectors based on the UI5 version.
  *
  * @example
