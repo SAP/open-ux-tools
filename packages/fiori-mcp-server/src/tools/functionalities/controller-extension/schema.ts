9210c600833a4b5b63fc4a277072156d023c948c
Andis Redmans <andis.redmans@sap.com>
2025-10-16 19:18:59 +0300

    test: adjustments
    adjustments

--- packages/fiori-mcp-server/src/tools/functionalities/controller-extension/schema.ts
+++ packages/fiori-mcp-server/src/tools/functionalities/controller-extension/schema.ts
@@ -10,10 +10,7 @@ export const ControllerExtensionCreationSchema: zod.ZodObject<{
     controllerName: zod.ZodString;
     pageId?: zod.ZodOptional<zod.ZodEnum>;
 }> = zod.object({
-    pageType: zod
-        .enum(['ListReport', 'ObjectPage'])
-        .describe('Type of page. Use this when defining a global controller extension (not tied to a specific page).')
-        .default('ListReport'),
+    pageType: zod.enum(['ListReport', 'ObjectPage']).describe('Type of page').default('ListReport'),
     controllerName: zod.string().regex(EXTENSION_FILE_NAME_PATTERN).describe('Name of new controller extension file')
 });
 
@@ -35,7 +32,7 @@ export async function buildControllerExtensionSchema(ftfsFileIo?: SapuxFtfsFileI
                 .enum(pageIds)
                 .optional()
                 .describe(
-                    'Use this when defining a controller extension for a specific page. Required if the extension should be assigned to a particular page.'
+                    'If controller extension should be assigned for specific page, then pageId should be provided'
                 )
         });
     }
