3aa8a1c304c86f155a6f0ad80512d4ff1a5fa705
Oksana Korotkova <oksana.korotkova@sap.com>
2025-10-30 13:01:02 +0000

    fix(inquirer-common): move convert edmx function

--- packages/inquirer-common/test/unit/metadata/utils.test.ts
+++ packages/inquirer-common/test/unit/metadata/utils.test.ts
@@ -1,4 +1,4 @@
-import { convertEdmxWithVersion } from '../../../src/metadata/utils';
+import { convertEdmxWithVersion, convertEdmxToConvertedMetadata } from '../../../src/metadata/utils';
 import { OdataVersion } from '@sap-ux/odata-service-writer';
 import fs from 'fs';
 import path from 'path';
@@ -79,6 +79,116 @@ describe('metadata utils', () => {
 
             const result = convertEdmxWithVersion(v2Edmx);
             expect(result.odataVersion).toBe(OdataVersion.v2);
+        });
+    });
+
+    describe('convertEdmxToConvertedMetadata', () => {
+        test('should convert valid EDMX to ConvertedMetadata', () => {
+            const result = convertEdmxToConvertedMetadata(validEdmxV4);
+
+            expect(result).toBeDefined();
+            expect(result.version).toBe('4.0');
+            expect(result.entitySets).toBeDefined();
+        });
+
+        test('should throw on invalid EDMX', () => {
+            const invalidEdmx = 'This is not valid XML';
+
+            expect(() => convertEdmxToConvertedMetadata(invalidEdmx)).toThrow('errors.unparseableMetadata');
+        });
+
+        test('should throw on unparseable OData version', () => {
+            const invalidVersionEdmx = `<?xml version="1.0" encoding="utf-8"?>
+                <edmx:Edmx Version="invalid" xmlns:edmx="http://docs.oasis-open.org/odata/ns/edmx">
+                    <edmx:DataServices>
+                        <Schema Namespace="TestService" xmlns="http://docs.oasis-open.org/odata/ns/edmx">
+                            <EntityContainer Name="Container">
+                                <EntitySet Name="TestEntity" EntityType="TestService.TestEntity" />
+                            </EntityContainer>
+                            <EntityType Name="TestEntity">
+                                <Key>
+                                    <PropertyRef Name="ID" />
+                                </Key>
+                                <Property Name="ID" Type="Edm.String" />
+                            </EntityType>
+                        </Schema>
+                    </edmx:DataServices>
+                </edmx:Edmx>`;
+
+            expect(() => convertEdmxToConvertedMetadata(invalidVersionEdmx)).toThrow('errors.unparseableMetadata');
+        });
+    });
+
+    describe('Error handling and generator crash prevention', () => {
+        test('should handle null/undefined version gracefully in both functions', () => {
+            const nullVersionEdmx = `<?xml version="1.0" encoding="utf-8"?>
+                <edmx:Edmx xmlns:edmx="http://docs.oasis-open.org/odata/ns/edmx">
+                    <edmx:DataServices>
+                        <Schema Namespace="TestService" xmlns="http://docs.oasis-open.org/odata/ns/edm">
+                            <EntityContainer Name="Container">
+                                <EntitySet Name="TestEntity" EntityType="TestService.TestEntity" />
+                            </EntityContainer>
+                            <EntityType Name="TestEntity">
+                                <Key>
+                                    <PropertyRef Name="ID" />
+                                </Key>
+                                <Property Name="ID" Type="Edm.String" />
+                            </EntityType>
+                        </Schema>
+                    </edmx:DataServices>
+                </edmx:Edmx>`;
+
+            // Both functions should throw controlled errors, not crash
+            expect(() => convertEdmxToConvertedMetadata(nullVersionEdmx)).toThrow();
+            expect(() => convertEdmxWithVersion(nullVersionEdmx)).toThrow();
+        });
+
+        test('should handle empty string EDMX gracefully', () => {
+            expect(() => convertEdmxToConvertedMetadata('')).toThrow('errors.unparseableMetadata');
+            expect(() => convertEdmxWithVersion('')).toThrow('errors.unparseableMetadata');
+        });
+
+        test('should handle malformed XML gracefully', () => {
+            const malformedXml = '<?xml version="1.0"?><invalid><unclosed>';
+
+            expect(() => convertEdmxToConvertedMetadata(malformedXml)).toThrow('errors.unparseableMetadata');
+            expect(() => convertEdmxWithVersion(malformedXml)).toThrow('errors.unparseableMetadata');
+        });
+
+        test('should demonstrate error is catchable by generators', () => {
+            // This test simulates how a generator would handle the errors
+            let caughtError1: Error | null = null;
+            let caughtError2: Error | null = null;
+
+            try {
+                convertEdmxToConvertedMetadata('invalid xml');
+            } catch (error) {
+                caughtError1 = error as Error;
+            }
+
+            try {
+                convertEdmxWithVersion('invalid xml');
+            } catch (error) {
+                caughtError2 = error as Error;
+            }
+
+            // Both errors should be caught, not cause crashes
+            expect(caughtError1).toBeInstanceOf(Error);
+            expect(caughtError2).toBeInstanceOf(Error);
+            expect(caughtError1?.message).toContain('errors.unparseableMetadata');
+            expect(caughtError2?.message).toContain('errors.unparseableMetadata');
+        });
+
+        test('should provide meaningful error messages for debugging', () => {
+            try {
+                convertEdmxWithVersion('not xml at all');
+            } catch (error) {
+                // Error message should contain information about what went wrong
+                expect((error as Error).message).toContain('errors.unparseableMetadata');
+                // Should not be a generic crash or unhelpful error
+                expect((error as Error).message).not.toContain('TypeError');
+                expect((error as Error).message).not.toContain('undefined is not a function');
+            }
         });
     });
 });
