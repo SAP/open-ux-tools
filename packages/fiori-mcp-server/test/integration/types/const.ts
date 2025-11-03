5ad6523e281817075860bb60c7f0c3d4259d68d9
Andis Redmans <andis.redmans@sap.com>
2025-11-03 15:08:18 +0200

    feat: move test project to existing test data folder
    move test project to existing test data folder

--- packages/fiori-mcp-server/test/integration/types/const.ts
+++ packages/fiori-mcp-server/test/integration/types/const.ts
@@ -2,6 +2,6 @@ import { join } from 'path';
 
 export const FOLDER_PATHS = {
     snapshots: join(__dirname, '../snapshots'),
-    originalProjects: join(__dirname, `../test-data/original`),
-    copiedProjects: join(__dirname, `../test-data/copy`)
+    originalProjects: join(__dirname, `../../test-data/original`),
+    copiedProjects: join(__dirname, `../../test-data/copy`)
 };
