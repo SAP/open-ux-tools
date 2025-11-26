f441c4f9de1e1eba701dbbe30d66155fef80832d
Tomasz Borychowski <tomasz.borychowski@sap.com>
2025-11-26 11:43:43 +0000

    chore(fiori-mcp): cleanup

--- packages/fiori-mcp-server/test/integration/types/const.ts
+++ packages/fiori-mcp-server/test/integration/types/const.ts
@@ -3,6 +3,5 @@ import { join } from 'path';
 export const FOLDER_PATHS = {
     snapshots: join(__dirname, '../snapshots'),
     originalProjects: join(__dirname, `../../test-data/original`),
-    copiedProjects: join(__dirname, `../../test-data/copy`),
-    tempFolder: join(__dirname, `../../test-data/temp`)
+    copiedProjects: join(__dirname, `../../test-data/copy`)
 };
