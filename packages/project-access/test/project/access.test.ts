49080991f9fe91d520e915c5188fd1f20ae5ba28
D048415 <dominik.heim@sap.com>
2025-12-09 14:35:56 +0100

    add unit test

--- packages/project-access/test/project/access.test.ts
+++ packages/project-access/test/project/access.test.ts
@@ -549,6 +549,7 @@ describe('Test function createProjectAccess()', () => {
             [join('apps/one'), join('apps/two'), join('apps/freestyle')].sort()
         );
         expect(projectAccess.getApplication(join('apps/one')).getAppId()).toBe(join('apps/one'));
+        expect(await projectAccess.getApplicationIdFromManifestId('two')).toBe(join('apps/two'));
     });
 
     test('Standalone app', async () => {
