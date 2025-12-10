005cf80154e6ef3563ce3b93b9ebd8ab9f7316d9
Austin Devine <austin.devine@sap.com>
2025-12-10 13:21:25 +0000

    fix: disable JSDoc rules in ESLint configuration for logger package

--- packages/logger/eslint.config.js
+++ packages/logger/eslint.config.js
@@ -11,5 +11,16 @@ module.exports = [
                 project: './tsconfig.eslint.json',
             },
         },
+        rules: {
+        'jsdoc/require-returns': 'off',
+        'jsdoc/require-param': 'off',
+        'jsdoc/require-jsdoc': 'off',
+        'jsdoc/match-description': 'off',
+        'jsdoc/multiline-blocks': 'off',
+        'jsdoc/tag-lines': 'off',
+        'jsdoc/require-param-description': 'off',
+        'jsdoc/no-multi-asterisks': 'off',
+        'jsdoc/check-tag-names': 0
+    }
     },
 ];
