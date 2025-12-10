aa89b6f5e3e36b3ecc78efb14c12d02ce97ff426
Austin Devine <devinea@users.noreply.github.com>
2025-12-10 15:20:57 +0000

    Update packages/ui5-library-reference-inquirer/test/unit/choices.test.ts
    Co-authored-by: Dominik Heim <73878785+heimwege@users.noreply.github.com>

--- packages/ui5-library-reference-inquirer/test/unit/choices.test.ts
+++ packages/ui5-library-reference-inquirer/test/unit/choices.test.ts
@@ -1,7 +1,6 @@
 import { join } from 'node:path';
 import { getLibraryChoices, getProjectChoices } from '../../src/choices';
-import type { Manifest } from '@sap-ux/project-access';
-import { ReuseLibType } from '@sap-ux/project-access';
+import { type Manifest, ReuseLibType } from '@sap-ux/project-access';
 
 describe('choices utils', () => {
     test('should return project choices', async () => {
