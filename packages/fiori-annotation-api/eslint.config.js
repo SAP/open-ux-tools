a7f69984479aed3d5336119511994a851f3244df
Austin Devine <austin.devine@sap.com>
2025-12-10 00:39:22 +0000

    re-add eslint overrides

--- packages/fiori-annotation-api/eslint.config.js
+++ packages/fiori-annotation-api/eslint.config.js
@@ -4,12 +4,44 @@ const tsParser = require('@typescript-eslint/parser');
 module.exports = [
     ...base,
     {
+        files: ['src/**/*.ts'],
+        ignores: ['dist', 'test/fixtures/**', 'coverage', 'node_modules/**', 'eslint.config.js'],
         languageOptions: {
+            parser: tsParser,
             parserOptions: {
-                parser: tsParser,
                 tsconfigRootDir: __dirname,
-                project: './tsconfig.eslint.json',
-            },
+                project: './tsconfig.eslint.json'
+            }
         },
+        rules: {
+            'jsdoc/require-jsdoc': [
+                'error',
+                {
+                    'publicOnly': true,
+                    'require': {
+                        'FunctionDeclaration': true,
+                        'MethodDefinition': true,
+                        'ClassDeclaration': true,
+                        'ArrowFunctionExpression': true,
+                        'FunctionExpression': true
+                    },
+                    'exemptEmptyFunctions': true
+                }
+            ]
+        }
     },
+    {
+        files: ['test/**/*.ts'],
+        ignores: ['dist', 'test/fixtures/**', 'coverage', 'node_modules/**', 'eslint.config.js'],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                tsconfigRootDir: __dirname,
+                project: './tsconfig.eslint.json'
+            }
+        },
+        rules: {
+            'jsdoc/require-jsdoc': 'off'
+        }
+    }
 ];
