8fc8f6acb4bef27d4090ba2b76371567997089dd
Austin Devine <austin.devine@sap.com>
2025-12-10 16:28:12 +0000

    Revert rules to how it was in .eslintrc.js

--- packages/preview-middleware-client/eslint.config.js
+++ packages/preview-middleware-client/eslint.config.js
@@ -2,13 +2,13 @@ const { defineConfig } = require('eslint/config');
 const js = require('@eslint/js');
 
 const jsdoc = require('eslint-plugin-jsdoc');
-const config  = require('@sap-ux/eslint-plugin-fiori-tools');
+// const config = require('@sap-ux/eslint-plugin-fiori-tools');
 
 module.exports = defineConfig([
     {
         ignores: ['dist', 'test/fixtures/**', 'coverage', 'node_modules/**', 'eslint.config.js']
     },
-    ...config.defaultTS,
+    // ...config.defaultTS,
     {
         languageOptions: {
             ecmaVersion: 5,
@@ -23,32 +23,15 @@ module.exports = defineConfig([
             jsdoc
         },
         rules: {
-            quotes: [
+            'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
+            'valid-jsdoc': [
                 'error',
-                'single',
                 {
-                    allowTemplateLiterals: true
+                    requireParamType: false,
+                    requireReturn: false,
+                    requireReturnType: false
                 }
             ],
-
-            // Replace valid-jsdoc with eslint-plugin-jsdoc rules
-            'jsdoc/check-alignment': 'error',
-            'jsdoc/check-param-names': 'error',
-            'jsdoc/check-tag-names': 'error',
-            'jsdoc/check-types': 'error',
-            'jsdoc/implements-on-classes': 'error',
-            // "jsdoc/newline-after-description": 'error',
-            'jsdoc/no-types': 'error',
-            'jsdoc/require-description': 'error',
-            'jsdoc/require-param': 'error',
-            'jsdoc/require-param-description': 'error',
-            'jsdoc/require-param-name': 'error',
-            'jsdoc/require-param-type': 'off',
-            'jsdoc/require-returns': 'off',
-            'jsdoc/require-returns-check': 'error',
-            'jsdoc/require-returns-description': 'error',
-            'jsdoc/require-returns-type': 'off',
-
             '@typescript-eslint/no-unused-vars': [
                 'error',
                 {
@@ -56,7 +39,6 @@ module.exports = defineConfig([
                     argsIgnorePattern: '^_'
                 }
             ],
-
             '@typescript-eslint/no-unsafe-argument': 'warn',
             '@typescript-eslint/no-unsafe-member-access': 'warn',
             '@typescript-eslint/no-unsafe-assignment': 'warn'
