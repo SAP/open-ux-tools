8547e89bac8f437fb875db422bed1e42317150f6
Austin Devine <austin.devine@sap.com>
2025-12-11 23:56:25 +0000

    Update ESLint configurations to use recommended settings for JavaScript and TypeScript in  templates and update snapshots

--- packages/ui5-application-writer/templates/optional/eslint/eslint.config.js
+++ packages/ui5-application-writer/templates/optional/eslint/eslint.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('eslint/config');
 const fioriTools  = require('@sap-ux/eslint-plugin-fiori-tools');
 
 module.exports = defineConfig([
-    ...fioriTools.config.defaultJS,
+    ...fioriTools.configs.recommended,
     {
         plugins: {
             'fiori-custom': fioriTools // backward compatibility
