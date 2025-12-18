066549080e866a653cf7a2759950e2210288378d
David O'Connor <david.o-connor@sap.com>
2025-12-18 10:44:34 +0000

    add timeout

--- packages/repo-app-import-sub-generator/test/app.test.ts
+++ packages/repo-app-import-sub-generator/test/app.test.ts
@@ -27,7 +27,7 @@ import { handleWorkspaceConfig } from '@sap-ux/launch-config';
 import { EventName } from '../src/telemetryEvents';
 import { getUI5Versions } from '@sap-ux/ui5-info';
 
-jest.setTimeout(30000);
+jest.setTimeout(60000);
 
 jest.mock('@sap/ux-specification', () => ({}));
 
