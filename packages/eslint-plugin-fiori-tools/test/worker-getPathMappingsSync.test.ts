50c2147b6447b9c57b9b857e3255f9614bf3726b
Austin Devine <austin.devine@sap.com>
2026-01-16 15:08:44 +0000

    simplify - revert

--- packages/eslint-plugin-fiori-tools/test/worker-getPathMappingsSync.test.ts
+++ packages/eslint-plugin-fiori-tools/test/worker-getPathMappingsSync.test.ts
@@ -6,13 +6,9 @@ describe('Project Access Tests', () => {
     const testInputPath = join(__dirname, 'test-input');
 
     describe('Synchronized worker version', () => {
-        /**
-         * Enable v8 coverage collection in worker threads by passing execArgv.
-         * This propagates NODE_V8_COVERAGE and other Node.js flags to the worker.
-         */
-        const getPathMappingsWorker = createSyncFn(join(__dirname, '../src/worker-getPathMappingsSync.ts'), {
-            execArgv: process.execArgv
-        }) as (projectRoot: string) => PathMappings;
+        const getPathMappingsWorker = createSyncFn(join(__dirname, '../src/worker-getPathMappingsSync.ts')) as (
+            projectRoot: string
+        ) => PathMappings;
 
         test('synchronized worker returns webapp path for standard project', () => {
             const projectPath = join(testInputPath, 'standardProject');
