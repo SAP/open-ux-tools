13ddfa0a46c96630cda7ba3e717ed9c0b4036920
i057546 <iain.o-farrell@sap.com>
2026-01-19 18:41:46 +0000

    Fix for odata version restriction not applied when connecting
    internal ref: 36758

--- packages/odata-service-inquirer/src/prompts/datasources/sap-system/abap-on-prem/questions.ts
+++ packages/odata-service-inquirer/src/prompts/datasources/sap-system/abap-on-prem/questions.ts
@@ -127,7 +127,7 @@ export function getAbapOnPremSystemQuestions(
                 return valRes;
             }
         } as InputQuestion<AbapOnPremAnswers>,
-        ...getCredentialsPrompts<AbapOnPremAnswers>(connectValidator, abapOnPremPromptNamespace, sapClientRef)
+        ...getCredentialsPrompts<AbapOnPremAnswers>(connectValidator, abapOnPremPromptNamespace, sapClientRef, requiredOdataVersion)
     ];
 
     if (systemNamePromptOptions?.hide !== true) {
