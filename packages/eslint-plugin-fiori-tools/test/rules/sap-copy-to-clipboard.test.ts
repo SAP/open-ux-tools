43bec7536759b6bf4300c2d09b8cac49ba4dceb0
Alina Govoruhina <alina.govoruhina@sap.com>
2026-01-26 11:38:08 +0200

    test: add unit tests

--- packages/eslint-plugin-fiori-tools/test/rules/sap-copy-to-clipboard.test.ts
+++ packages/eslint-plugin-fiori-tools/test/rules/sap-copy-to-clipboard.test.ts
@@ -1,5 +1,5 @@
 import { RuleTester } from 'eslint';
-import disableCopyToClipboardRule from '../../src/rules/sap-disable-copy-to-clipboard';
+import copyToClipboardRule from '../../src/rules/sap-copy-to-clipboard';
 import { meta, languages } from '../../src/index';
 import {
     getAnnotationsAsXmlCode,
@@ -8,7 +8,11 @@ import {
     V4_ANNOTATIONS,
     V4_ANNOTATIONS_PATH,
     V4_MANIFEST,
-    V4_MANIFEST_PATH
+    V4_MANIFEST_PATH,
+    V2_ANNOTATIONS,
+    V2_ANNOTATIONS_PATH,
+    V2_MANIFEST,
+    V2_MANIFEST_PATH
 } from '../test-helper';
 
 const ruleTester = new RuleTester({
@@ -16,7 +20,7 @@ const ruleTester = new RuleTester({
     language: '@sap-ux/eslint-plugin-fiori-tools/fiori'
 });
 
-const FACETS = {
+const FACETSV4 = {
     filename: V4_ANNOTATIONS_PATH,
     code: getAnnotationsAsXmlCode(
         V4_ANNOTATIONS,
@@ -50,14 +54,69 @@ const FACETS = {
     )
 };
 
-const TEST_NAME = 'sap-disable-copy-to-clipboard';
+const FACETSV2 = {
+    filename: V2_ANNOTATIONS_PATH,
+    code: getAnnotationsAsXmlCode(
+        V2_ANNOTATIONS,
+        `
+            <Annotations Target="TECHED_ALP_SOA_SRV.Z_SEPMRA_SO_SALESORDERANALYSISType">
+                 <Annotation Term="UI.Facets" >
+                 <Collection>
+                        <Record Type="UI.ReferenceFacet">
+                            <PropertyValue Property="ID" String="Products"/>
+                            <PropertyValue Property="Label" String="TableFacet"/>
+                            <PropertyValue Property="Target" AnnotationPath="@UI.LineItem"/>
+                        </Record>
+                    </Collection>
+                </Annotation>
+            </Annotations>
+            `
+    )
+};
+
+const TEST_NAME = 'sap-copy-to-clipboard';
 const { createValidTest, createInvalidTest } = setup(TEST_NAME);
 
-ruleTester.run(TEST_NAME, disableCopyToClipboardRule, {
+ruleTester.run(TEST_NAME, copyToClipboardRule, {
     valid: [
+        // V2
         createValidTest(
             {
-                name: 'disableCopyToClipboard missing',
+                name: 'V2 - copy missing',
+                filename: V2_MANIFEST_PATH,
+                code: JSON.stringify(V2_MANIFEST, undefined, 2)
+            },
+            []
+        ),
+        createValidTest(
+            {
+                name: 'V2 - object page table - redundant true value',
+                filename: V2_MANIFEST_PATH,
+                code: getManifestAsCode(V2_MANIFEST, [
+                    {
+                        path: [
+                            'sap.ui.generic.app',
+                            'pages',
+                            'AnalyticalListPage|Z_SEPMRA_SO_SALESORDERANALYSIS',
+                            'pages',
+                            'ObjectPage|Z_SEPMRA_SO_SALESORDERANALYSIS',
+                            'component',
+                            'settings',
+                            'sections',
+                            '',
+                            'tableSettings',
+                            'copy'
+                        ],
+                        value: true
+                    }
+                ])
+            },
+            [FACETSV2]
+        ),
+        // V4
+        createValidTest(
+            {
+                name: 'V4 - disableCopyToClipboard missing',
                 filename: V4_MANIFEST_PATH,
                 code: JSON.stringify(V4_MANIFEST, undefined, 2)
             },
@@ -65,7 +124,7 @@ ruleTester.run(TEST_NAME, disableCopyToClipboardRule, {
         ),
         createValidTest(
             {
-                name: 'object page table - redundant false value',
+                name: 'V4 - object page table - redundant false value',
                 filename: V4_MANIFEST_PATH,
                 code: getManifestAsCode(V4_MANIFEST, [
                     {
@@ -85,14 +144,46 @@ ruleTester.run(TEST_NAME, disableCopyToClipboardRule, {
                     }
                 ])
             },
-            [FACETS]
+            [FACETSV4]
         )
     ],
 
     invalid: [
         createInvalidTest(
             {
-                name: 'disableCopyToClipboard is true',
+                name: 'V2 - copy is false',
+                filename: V2_MANIFEST_PATH,
+                code: getManifestAsCode(V2_MANIFEST, [
+                    {
+                        path: [
+                            'sap.ui.generic.app',
+                            'pages',
+                            'AnalyticalListPage|Z_SEPMRA_SO_SALESORDERANALYSIS',
+                            'pages',
+                            'ObjectPage|Z_SEPMRA_SO_SALESORDERANALYSIS',
+                            'component',
+                            'settings',
+                            'sections',
+                            '',
+                            'tableSettings',
+                            'copy'
+                        ],
+                        value: false
+                    }
+                ]),
+                errors: [
+                    {
+                        messageId: 'sap-copy-to-clipboard',
+                        line: 154,
+                        column: 23
+                    }
+                ]
+            },
+            [FACETSV2]
+        ),
+        createInvalidTest(
+            {
+                name: 'V4 - disableCopyToClipboard is true',
                 filename: V4_MANIFEST_PATH,
                 code: getManifestAsCode(V4_MANIFEST, [
                     {
@@ -113,13 +204,13 @@ ruleTester.run(TEST_NAME, disableCopyToClipboardRule, {
                 ]),
                 errors: [
                     {
-                        messageId: 'sap-disable-copy-to-clipboard',
+                        messageId: 'sap-copy-to-clipboard',
                         line: 122,
                         column: 21
                     }
                 ]
             },
-            []
+            [FACETSV4]
         )
     ]
 });
