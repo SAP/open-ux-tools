b51202204fa59cad6214cbb3d99c31a033f04f53
Alina Govoruhina <alina.govoruhina@sap.com>
2026-02-03 14:03:09 +0200

    refactor: update tule unit tests

--- packages/eslint-plugin-fiori-tools/test/rules/sap-table-column-vertical-alignment.test.ts
+++ packages/eslint-plugin-fiori-tools/test/rules/sap-table-column-vertical-alignment.test.ts
@@ -11,30 +11,78 @@ const ruleTester = new RuleTester({
 const TEST_NAME = 'sap-table-column-vertical-alignment';
 const { createValidTest, createInvalidTest } = setup(TEST_NAME);
 
+/**
+ * Updates manifest for testing tableColumnVerticalAlignment rule.
+ *
+ * @param tableColumnVerticalAlignment Tested property value.
+ * @param page Page that has a Responsive type table.
+ * @param ui5Version Minimum UI5 version.
+ * @returns Manifest content as string
+ */
+const getManifest = (
+    tableColumnVerticalAlignment: 'Top' | 'Middle' | 'Bottom' | undefined = undefined,
+    page: 'ALP' | 'OP' | undefined = undefined,
+    ui5Version: string = '1.75.0'
+): string => {
+    const manifestChanges = [
+        {
+            path: ['sap.ui5', 'dependencies', 'minUI5Version'],
+            value: ui5Version
+        }
+    ];
+    if (page) {
+        manifestChanges.push({
+            path:
+                page === 'ALP'
+                    ? [
+                          'sap.ui.generic.app',
+                          'pages',
+                          'AnalyticalListPage|Z_SEPMRA_SO_SALESORDERANALYSIS',
+                          'component',
+                          'settings',
+                          'tableSettings',
+                          'type'
+                      ]
+                    : [
+                          'sap.ui.generic.app',
+                          'pages',
+                          'AnalyticalListPage|Z_SEPMRA_SO_SALESORDERANALYSIS',
+                          'pages',
+                          'ObjectPage|Z_SEPMRA_SO_SALESORDERANALYSIS',
+                          'component',
+                          'settings',
+                          'sections',
+                          'Products',
+                          'tableSettings',
+                          'type'
+                      ],
+            value: 'ResponsiveTable'
+        });
+    }
+    if (tableColumnVerticalAlignment) {
+        manifestChanges.push({
+            path: ['sap.ui.generic.app', 'settings', 'tableColumnVerticalAlignment'],
+            value: tableColumnVerticalAlignment
+        });
+    }
+    return getManifestAsCode(V2_MANIFEST, manifestChanges);
+};
+
 ruleTester.run(TEST_NAME, rule, {
     valid: [
         createValidTest(
             {
+                name: 'ui5 version lower than 1.75',
+                filename: V2_MANIFEST_PATH,
+                code: getManifest('Top', 'ALP', '1.60.5')
+            },
+            []
+        ),
+        createValidTest(
+            {
                 name: 'tableColumnVerticalAlignment value is "Middle", Responsive table exists on AnalyticalListPage',
                 filename: V2_MANIFEST_PATH,
-                code: getManifestAsCode(V2_MANIFEST, [
-                    {
-                        path: ['sap.ui.generic.app', 'settings', 'tableColumnVerticalAlignment'],
-                        value: 'Middle'
-                    },
-                    {
-                        path: [
-                            'sap.ui.generic.app',
-                            'pages',
-                            'AnalyticalListPage|Z_SEPMRA_SO_SALESORDERANALYSIS',
-                            'component',
-                            'settings',
-                            'tableSettings',
-                            'type'
-                        ],
-                        value: 'ResponsiveTable'
-                    }
-                ])
+                code: getManifest('Middle', 'ALP')
             },
             []
         ),
@@ -42,28 +90,7 @@ ruleTester.run(TEST_NAME, rule, {
             {
                 name: 'tableColumnVerticalAlignment value is "Middle", Responsive table exists on ObjectPage',
                 filename: V2_MANIFEST_PATH,
-                code: getManifestAsCode(V2_MANIFEST, [
-                    {
-                        path: ['sap.ui.generic.app', 'settings', 'tableColumnVerticalAlignment'],
-                        value: 'Middle'
-                    },
-                    {
-                        path: [
-                            'sap.ui.generic.app',
-                            'pages',
-                            'AnalyticalListPage|Z_SEPMRA_SO_SALESORDERANALYSIS',
-                            'pages',
-                            'ObjectPage|Z_SEPMRA_SO_SALESORDERANALYSIS',
-                            'component',
-                            'settings',
-                            'sections',
-                            'Products',
-                            'tableSettings',
-                            'type'
-                        ],
-                        value: 'ResponsiveTable'
-                    }
-                ])
+                code: getManifest('Middle', 'OP')
             },
             []
         ),
@@ -71,12 +98,7 @@ ruleTester.run(TEST_NAME, rule, {
             {
                 name: 'tableColumnVerticalAlignment value is "Top", no Responsive tables exist',
                 filename: V2_MANIFEST_PATH,
-                code: getManifestAsCode(V2_MANIFEST, [
-                    {
-                        path: ['sap.ui.generic.app', 'settings', 'tableColumnVerticalAlignment'],
-                        value: 'Top'
-                    }
-                ])
+                code: getManifest('Top')
             },
             []
         ),
@@ -84,24 +106,7 @@ ruleTester.run(TEST_NAME, rule, {
             {
                 name: 'tableColumnVerticalAlignment value is undefined, Responsive table exists on ObjectPage',
                 filename: V2_MANIFEST_PATH,
-                code: getManifestAsCode(V2_MANIFEST, [
-                    {
-                        path: [
-                            'sap.ui.generic.app',
-                            'pages',
-                            'AnalyticalListPage|Z_SEPMRA_SO_SALESORDERANALYSIS',
-                            'pages',
-                            'ObjectPage|Z_SEPMRA_SO_SALESORDERANALYSIS',
-                            'component',
-                            'settings',
-                            'sections',
-                            'Products',
-                            'tableSettings',
-                            'type'
-                        ],
-                        value: 'ResponsiveTable'
-                    }
-                ])
+                code: getManifest(undefined, 'OP')
             },
             []
         ),
@@ -109,7 +114,7 @@ ruleTester.run(TEST_NAME, rule, {
             {
                 name: 'tableColumnVerticalAlignment value is undefined, no Responsive tables exist',
                 filename: V2_MANIFEST_PATH,
-                code: getManifestAsCode(V2_MANIFEST, [])
+                code: getManifest()
             },
             []
         )
@@ -119,38 +124,8 @@ ruleTester.run(TEST_NAME, rule, {
             {
                 name: 'tableColumnVerticalAlignment value is "Top", Responsive table exists on AnalyticalListPage',
                 filename: V2_MANIFEST_PATH,
-                code: getManifestAsCode(V2_MANIFEST, [
-                    {
-                        path: ['sap.ui.generic.app', 'settings', 'tableColumnVerticalAlignment'],
-                        value: 'Top'
-                    },
-                    {
-                        path: [
-                            'sap.ui.generic.app',
-                            'pages',
-                            'AnalyticalListPage|Z_SEPMRA_SO_SALESORDERANALYSIS',
-                            'component',
-                            'settings',
-                            'tableSettings',
-                            'type'
-                        ],
-                        value: 'ResponsiveTable'
-                    }
-                ]),
-                output: getManifestAsCode(V2_MANIFEST, [
-                    {
-                        path: [
-                            'sap.ui.generic.app',
-                            'pages',
-                            'AnalyticalListPage|Z_SEPMRA_SO_SALESORDERANALYSIS',
-                            'component',
-                            'settings',
-                            'tableSettings',
-                            'type'
-                        ],
-                        value: 'ResponsiveTable'
-                    }
-                ]),
+                code: getManifest('Top', 'ALP'),
+                output: getManifest(undefined, 'ALP'),
                 errors: [
                     {
                         messageId: 'sap-table-column-vertical-alignment',
@@ -165,46 +140,8 @@ ruleTester.run(TEST_NAME, rule, {
             {
                 name: 'tableColumnVerticalAlignment value is "Bottom", Responsive table exists on ObjectPage',
                 filename: V2_MANIFEST_PATH,
-                code: getManifestAsCode(V2_MANIFEST, [
-                    {
-                        path: ['sap.ui.generic.app', 'settings', 'tableColumnVerticalAlignment'],
-                        value: 'Bottom'
-                    },
-                    {
-                        path: [
-                            'sap.ui.generic.app',
-                            'pages',
-                            'AnalyticalListPage|Z_SEPMRA_SO_SALESORDERANALYSIS',
-                            'pages',
-                            'ObjectPage|Z_SEPMRA_SO_SALESORDERANALYSIS',
-                            'component',
-                            'settings',
-                            'sections',
-                            'Products',
-                            'tableSettings',
-                            'type'
-                        ],
-                        value: 'ResponsiveTable'
-                    }
-                ]),
-                output: getManifestAsCode(V2_MANIFEST, [
-                    {
-                        path: [
-                            'sap.ui.generic.app',
-                            'pages',
-                            'AnalyticalListPage|Z_SEPMRA_SO_SALESORDERANALYSIS',
-                            'pages',
-                            'ObjectPage|Z_SEPMRA_SO_SALESORDERANALYSIS',
-                            'component',
-                            'settings',
-                            'sections',
-                            'Products',
-                            'tableSettings',
-                            'type'
-                        ],
-                        value: 'ResponsiveTable'
-                    }
-                ]),
+                code: getManifest('Bottom', 'OP'),
+                output: getManifest(undefined, 'OP'),
                 errors: [
                     {
                         messageId: 'sap-table-column-vertical-alignment',
