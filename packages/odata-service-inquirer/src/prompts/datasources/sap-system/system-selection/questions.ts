8e97043b72569ff99eea6c4dd9022888f89c31d4
Oksana Korotkova <oksana.korotkova@sap.com>
2026-02-04 12:59:07 +0000

    feat(odata-service-inquirer): unhide value helps in gen

--- packages/odata-service-inquirer/src/prompts/datasources/sap-system/system-selection/questions.ts
+++ packages/odata-service-inquirer/src/prompts/datasources/sap-system/system-selection/questions.ts
@@ -121,7 +121,7 @@ export async function getSystemSelectionQuestions(
                     connectValidator,
                     systemSelectionPromptNamespace,
                     promptOptions?.serviceSelection,
-                    false // Hide value help download prompt
+                    true // Show value help download prompt
                 ) as Question[],
                 (answers: Answers) => (answers as SystemSelectionAnswers).systemSelection?.type !== 'newSystemChoice'
             )
