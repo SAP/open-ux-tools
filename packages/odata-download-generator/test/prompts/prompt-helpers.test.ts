12df8852c23e195b8a118e5e6d28e1ab978fd3f9
i057546 <iain.o-farrell@sap.com>
2026-02-06 15:58:08 +0000

    Fix test by mocking selected spec functions

--- packages/odata-download-generator/test/prompts/prompt-helpers.test.ts
+++ packages/odata-download-generator/test/prompts/prompt-helpers.test.ts
@@ -1,13 +1,25 @@
-import type { EntityType } from '@sap-ux/vocabularies-types';
+import { createApplicationAccess, FileName } from '@sap-ux/project-access';
 import { readFile } from 'node:fs/promises';
 import { join } from 'node:path';
 import { createEntityChoices } from '../../src/data-download/prompts/prompt-helpers';
-import { type Entity } from '../../src/data-download/types';
 import { getEntityModel } from '../../src/data-download/utils';
-import { createApplicationAccess } from '@sap-ux/project-access';
+import * as commandMock from '@sap-ux/project-access/dist/command';
+import * as fileMock from '@sap-ux/project-access/dist/file';
 
-describe('Test propt-helpers', () => {
-    test('should create entity set choices', async () => {
+const readJSONOriginal = fileMock.readJSON;
+
+describe('Test prompt-helpers', () => {
+    test('should create entity set choices based on app model (from specification)', async () => {
+        // Prevent spec from fetching versions and writing on test jobs
+        jest.spyOn(commandMock, 'execNpmCommand').mockResolvedValueOnce('{"latest": "1.142.1"}');
+        jest.spyOn(fileMock, 'writeFile').mockResolvedValueOnce();
+        jest.spyOn(fileMock, 'readJSON').mockImplementation(async (path) =>
+            path.endsWith(FileName.SpecificationDistTags)
+                ? {
+                      latest: '1.142.1'
+                  }
+                : readJSONOriginal(path)
+        );
         // Load the test app
         const appPath = join(__dirname, '../test-data/test-apps/travel');
         const appAccess = await createApplicationAccess(appPath);
@@ -21,5 +33,5 @@ describe('Test propt-helpers', () => {
 
         const entityChoices = createEntityChoices(entityModel.listEntity, entityModel?.pageObjectEntities);
         expect(entityChoices).toMatchSnapshot();
-    }, 20000);
+    });
 });
