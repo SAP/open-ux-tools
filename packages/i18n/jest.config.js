7465596e53f2792d2d657f4d1e0531c9e3e2fe7a
Austin Devine <austin.devine@sap.com>
2026-02-06 19:09:01 +0000

    fix i18n tests

--- packages/i18n/jest.config.js
+++ packages/i18n/jest.config.js
@@ -1,2 +1,14 @@
 const config = require('../../jest.base');
-module.exports = config;
+
+module.exports = {
+    ...config,
+    transform: {
+        ...config.transform,
+        '^.+\\.m?js$': ['ts-jest', {
+            tsconfig: {
+                allowJs: true
+            }
+        }]
+    },
+    transformIgnorePatterns: ['<rootDir>/../../node_modules/(?!mem-fs|mem-fs-editor)'],
+};
