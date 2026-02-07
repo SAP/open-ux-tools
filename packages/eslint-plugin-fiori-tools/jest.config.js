f79d66938322f0814bed27aa785a5aaa8336db32
Austin Devine <austin.devine@sap.com>
2026-02-07 11:20:43 +0000

    WIP

--- packages/eslint-plugin-fiori-tools/jest.config.js
+++ packages/eslint-plugin-fiori-tools/jest.config.js
@@ -1,8 +1,3 @@
 const config = require('../../jest.base');
-module.exports = {
-    ...config,
-    // Coverage is handled by c8 wrapper for worker thread support
-    collectCoverage: false,
-    setupFiles: ['<rootDir>/test/global-setup.ts'],
-    transformIgnorePatterns: ['<rootDir>/../../node_modules/(?!(mem-fs|mem-fs-editor)/)']  
-}
+config.transformIgnorePatterns = ['<rootDir>/../../node_modules/(?!(mem-fs|mem-fs-editor)/)'];
+module.exports = config;
