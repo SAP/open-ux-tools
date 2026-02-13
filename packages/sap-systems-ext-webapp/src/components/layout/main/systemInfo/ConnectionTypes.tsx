9f949370da0bbbc574f4d17c115058b3647363f2
Cian Morrin <99677697+cianmSAP@users.noreply.github.com>
2026-02-13 16:04:06 +0000

    feat(sap-systems-ext): support adding full service urls as a new connection type (#4143)
    * feat(sap-systems-ext): support adding full service urls as a new connection type
    
    * feat(sap-systems-ext): ensure getAll returns odata_service types
    
    * feat(sap-systems-ext): correct name
    
    * feat(sap-systems-ext): pr comments and sonar
    
    * feat(sap-systems-ext): pr comments
    
    * feat(sap-systems-ext): update readme and tsdoc

---
+++ packages/sap-systems-ext-webapp/src/components/layout/main/systemInfo/ConnectionTypes.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import type { ReactElement } from 'react';
+import type { ConnectionType } from '@sap-ux/store';
+import { UIChoiceGroup } from '@sap-ux/ui-components';
+import { useTranslation } from 'react-i18next';
+
+interface ConnectionTypeProps {
+    readonly setConnectionType: (connType: ConnectionType) => void;
+}
+
+/**
+ * Renders the connection types radio buttons.
+ *
+ * @param props - system types props
+ * @param props.setConnectionType - function to set the connection type
+ * @returns - the system types JSX element
+ */
+export function ConnectionTypes({ setConnectionType }: Readonly<ConnectionTypeProps>): ReactElement {
+    const { t } = useTranslation();
+
+    const connectionTypeOptions: { key: ConnectionType; text: string }[] = [
+        { key: 'abap_catalog', text: t('options.abapCatalog') },
+        { key: 'odata_service', text: t('options.serviceUrlEndpoint') }
+    ];
+
+    return (
+        <div className="store-text-field">
+            <label className="store-detail-label">{t('labels.connectionType')}</label>
+            <UIChoiceGroup
+                name="connectionType"
+                id="connType"
+                options={connectionTypeOptions}
+                defaultSelectedKey={'abap_catalog'}
+                inline={true}
+                onChange={(e, option) => {
+                    setConnectionType(option?.key as ConnectionType);
+                }}
+            />
+        </div>
+    );
+}
