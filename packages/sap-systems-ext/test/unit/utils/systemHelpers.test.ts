0adc2dc0be6ee933c7b9b0dfd7f411dba29b4fb2
Cian Morrin <cian.morrin@sap.com>
2026-02-19 11:38:32 +0000

    sap-systems-ext: text update

--- packages/sap-systems-ext/test/unit/utils/systemHelpers.test.ts
+++ packages/sap-systems-ext/test/unit/utils/systemHelpers.test.ts
@@ -44,7 +44,7 @@ describe('Test the system helpers', () => {
 
         const result = await confirmPrompt(ConfirmationPromptType.Overwrite, 'Test System');
         expect(showWarningMessageSpy).toHaveBeenCalledWith(
-            'A connection already exists with name [Test System]. Do you want to overwrite?',
+            'Connection already exists with name [Test System]. Do you want to overwrite?',
             { modal: true },
             'Yes',
             'No'
