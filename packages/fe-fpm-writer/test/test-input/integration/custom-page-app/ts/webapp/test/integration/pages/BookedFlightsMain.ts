e64a592666291eb6b86f3a0f6f95f0d1e943904f
Oksana Korotkova <oksana.korotkova@sap.com>
2026-02-25 11:02:12 +0000

    chore(fe-fpm-writer): remove incorrectly added test-input files

--- packages/fe-fpm-writer/test/test-input/integration/custom-page-app/ts/webapp/test/integration/pages/BookedFlightsMain.ts
+++
@@ -1,9 +0,0 @@
-import {
-	createPageObjects
-} from "sap/fe/test/TemplatePage";
-
-export default createPageObjects({
-	onThePage: {
-		viewName: "sap.fe.demo.customPageAppTs::BookedFlightsMain"
-	}
-});
