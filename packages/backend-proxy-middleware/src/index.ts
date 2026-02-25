e8a652f9aa935c6159baa69b5ab99ccd6d1c875f
D048415 <dominik.heim@sap.com>
2026-02-25 17:19:16 +0100

    refactoring

--- packages/backend-proxy-middleware/src/index.ts
+++ packages/backend-proxy-middleware/src/index.ts
@@ -1,7 +1,7 @@
 import { generateProxyMiddlewareOptions, createProxy, initI18n } from './base/proxy';
 
 export { generateProxyMiddlewareOptions, createProxy };
-export type { BaseBackendConfig, LocalBackendConfig, DestinationBackendConfig, BackendConfig, BackendMiddlewareConfig } from './base/types';
+export type { LocalBackendConfig, DestinationBackendConfig, BackendConfig } from './base/types';
 
 initI18n().catch(() => {
     // Ignore any errors since the middleware will still work
