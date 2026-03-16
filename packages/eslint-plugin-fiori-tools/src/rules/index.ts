5055dde5e5139097c1cc9825ec3da1ca7f74c30c
Marco Vieth <marco.vieth@sap.com>
2026-03-16 11:16:01 +0100

    feat: Add sap-strict-uom-filtering ESLint rule to prevent disabling UOM filtering (#4395)
    * feat: Add sap-strict-uom-filtering ESLint rule to prevent disabling UOM filtering
    
    * add changeset
    
    * add UI5 version check for strict UOM filtering rule (>=1.143.0)

--- packages/eslint-plugin-fiori-tools/src/rules/index.ts
+++ packages/eslint-plugin-fiori-tools/src/rules/index.ts
@@ -9,6 +9,7 @@ import {
     ENABLE_EXPORT,
     ENABLE_PASTE,
     STATE_PRESERVATION_MODE,
+    STRICT_UOM_FILTERING,
     TABLE_PERSONALIZATION,
     TABLE_COLUMN_VERTICAL_ALIGNMENT
 } from '../language/diagnostics';
@@ -68,6 +69,7 @@ import flexEnabledRule from './sap-flex-enabled';
 import requireWidthIncludingColumnHeader from './sap-width-including-column-header';
 import creationModeForTable from './sap-creation-mode-for-table';
 import statePreservationMode from './sap-state-preservation-mode';
+import strictUomFilteringRule from './sap-strict-uom-filtering';
 import copyToClipboard from './sap-copy-to-clipboard';
 import enableExport from './sap-enable-export';
 import enablePaste from './sap-enable-paste';
@@ -133,6 +135,7 @@ export const rules: Record<string, Rule.RuleModule | FioriRuleDefinition | Fiori
     [ENABLE_PASTE]: enablePaste,
     [CREATION_MODE_FOR_TABLE]: creationModeForTable,
     [STATE_PRESERVATION_MODE]: statePreservationMode,
+    [STRICT_UOM_FILTERING]: strictUomFilteringRule,
     [TABLE_PERSONALIZATION]: tablePersonalization,
     [TABLE_COLUMN_VERTICAL_ALIGNMENT]: tableColumnVerticalAlignment
 };
