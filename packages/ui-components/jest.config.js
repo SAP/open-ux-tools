d415f65ef18d65007584ab3fa3b718b80a8e6d17
Maruf Rasully <maruf.rasully@sap.com>
2026-03-19 15:47:07 +0100

    fix Jest 30 Enzyme compatibility by resolving Cheerio exports and required web API polyfills

--- packages/ui-components/jest.config.js
+++ packages/ui-components/jest.config.js
@@ -1,8 +1,14 @@
 const config = require('../../jest.base');
 config.testEnvironment = 'jsdom';
+// Enzyme depends on cheerio via CommonJS. Force Node export conditions so Jest
+// does not resolve cheerio's browser ESM build under jsdom.
+config.testEnvironmentOptions = { customExportConditions: ['node', 'node-addons'] };
 config.collectCoverageFrom = ['src/**/*.{ts,tsx}'];
 config.setupFilesAfterEnv = ['<rootDir>/test/test-setup.js', '<rootDir>/test/test-shim.js'];
 config.snapshotResolver = '<rootDir>/test/utils/snapshotResolver.js';
+config.moduleNameMapper = {
+    '^cheerio/lib/utils$': 'cheerio/utils'
+};
 config.transform = {
     '^.+\\.tsx?$': [
         'ts-jest',
