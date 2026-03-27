067ae3acd70aac95987335360af53cb24d277e8c
D048415 <dominik.heim@sap.com>
2026-03-27 22:59:28 +0100

    fix sonar issues

--- packages/backend-proxy-middleware/src/base/proxy.ts
+++ packages/backend-proxy-middleware/src/base/proxy.ts
@@ -178,7 +178,7 @@ export const PathRewriters = {
             if (!queryString) {
                 return path;
             }
-            return path.indexOf('?') !== -1 ? `${path}&${queryString}` : `${path}?${queryString}`;
+            return path.includes('?') ? `${path}&${queryString}` : `${path}?${queryString}`;
         };
     },
 
