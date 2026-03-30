1e66896ba15fb8aefc9f1a94b2f2bd2910ce540b
mushegh.zakaryan <mushegh.zakaryan@sap.com>
2026-03-30 23:41:29 +0400

    fix: add border radius to error messages in dropdown and combobox components

--- packages/ui-components/test/unit/components/UICombobox.test.tsx
+++ packages/ui-components/test/unit/components/UICombobox.test.tsx
@@ -73,18 +73,13 @@ describe('<UIComboBox />', () => {
             {},
             `
             Object {
-              "__shadowConfig__": Object {
-                "__isShadowConfig__": true,
-                "inShadow": false,
-                "stylesheetKey": "ComboBox",
-                "window": [Window],
-              },
               "errorMessage": Array [
                 Object {
                   "backgroundColor": "var(--vscode-inputValidation-errorBackground)",
                   "borderBottom": "1px solid var(--vscode-inputValidation-errorBorder)",
                   "borderColor": "var(--vscode-inputValidation-errorBorder)",
                   "borderLeft": "1px solid var(--vscode-inputValidation-errorBorder)",
+                  "borderRadius": "var(--vscode-cornerRadius-small)",
                   "borderRight": "1px solid var(--vscode-inputValidation-errorBorder)",
                   "color": "var(--vscode-input-foreground)",
                   "margin": 0,
@@ -113,18 +108,13 @@ describe('<UIComboBox />', () => {
             {},
             `
             Object {
-              "__shadowConfig__": Object {
-                "__isShadowConfig__": true,
-                "inShadow": false,
-                "stylesheetKey": "ComboBox",
-                "window": [Window],
-              },
               "errorMessage": Array [
                 Object {
                   "backgroundColor": "var(--vscode-inputValidation-errorBackground)",
                   "borderBottom": "1px solid var(--vscode-inputValidation-errorBorder)",
                   "borderColor": "var(--vscode-inputValidation-errorBorder)",
                   "borderLeft": "1px solid var(--vscode-inputValidation-errorBorder)",
+                  "borderRadius": "var(--vscode-cornerRadius-small)",
                   "borderRight": "1px solid var(--vscode-inputValidation-errorBorder)",
                   "color": "var(--vscode-input-foreground)",
                   "margin": 0,
