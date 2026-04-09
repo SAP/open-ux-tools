a6f48d85911f3eccbd854b706c7374396e9e87e0
Cian Morrin <cian.morrin@sap.com>
2026-04-09 14:39:36 +0100

    sap-systems-ext-webapp tests

--- packages/sap-systems-ext-webapp/jest.config.mjs
+++ packages/sap-systems-ext-webapp/jest.config.mjs
@@ -1,13 +1,32 @@
 import baseConfig from '../../jest.base.mjs';
 const config = { ...baseConfig };
+config.extensionsToTreatAsEsm = ['.ts', '.tsx'];
 config.testEnvironment = 'jsdom';
 config.collectCoverageFrom = ['src/**/*.{ts,tsx}'];
 config.transform = {
-    '^.+\\.test.tsx?$': 'ts-jest',
+    '^.+\\.[jt]sx?$': [
+        'ts-jest',
+        {
+            useESM: true,
+            tsconfig: {
+                module: 'NodeNext',
+                moduleResolution: 'NodeNext',
+                isolatedModules: true,
+                allowJs: true,
+                jsx: 'react-jsx'
+            },
+            diagnostics: {
+                ignoreCodes: [151001]
+            }
+        }
+    ],
     '.+\\.(css|sass|scss)$': 'jest-scss-transform'
 };
-config.preset = 'ts-jest';
-config.transformIgnorePatterns = ['<rootDir>/node_modules/'];
+config.transformIgnorePatterns = ['<rootDir>/node_modules/(?!(@sap-ux|@sap-ux-private|@reduxjs|react-redux|redux|i18next|react-i18next)/)'];
+config.moduleNameMapper = {
+    ...config.moduleNameMapper,
+    '^@reduxjs/toolkit$': '<rootDir>/node_modules/@reduxjs/toolkit/dist/redux-toolkit.esm.js'
+};
 config.testMatch = ['**/test/unit/**/*.(test).ts(x)?'];
-config.setupFiles = ['./test/setup.ts'];
+config.setupFiles = ['<rootDir>/../../jest.setup.mjs', './test/setup.ts'];
 export default config;
