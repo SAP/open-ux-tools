98db3c14a6e0534010dbe567910adbfcade7a15c
Cian Morrin <cian.morrin@sap.com>
2026-04-10 00:27:47 +0100

    mockserver-config-writer: fix prompt directory import for ESM

--- packages/mockserver-config-writer/src/index.ts
+++ packages/mockserver-config-writer/src/index.ts
@@ -1,4 +1,4 @@
 export { generateMockserverConfig, removeMockserverConfig } from './mockserver-config/index.js';
-export { getMockserverConfigQuestions } from './prompt';
+export { getMockserverConfigQuestions } from './prompt/index.js';
 export { t } from './i18n';
 export * from './types';
