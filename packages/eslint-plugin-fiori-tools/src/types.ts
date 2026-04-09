1fd972898fa51905622fabde107f6b971daaa291
Alina Govoruhina <alina.govoruhina@sap.com>
2026-04-09 14:31:12 +0300

    fix: fiori language types

--- packages/eslint-plugin-fiori-tools/src/types.ts
+++ packages/eslint-plugin-fiori-tools/src/types.ts
@@ -1,10 +1,9 @@
 import type { CustomRuleDefinitionType, CustomRuleTypeDefinitions, RuleVisitor } from '@eslint/core';
 import type { AnyNode } from '@humanwhocodes/momoa';
 import type { JSONLanguageOptions, JSONSourceCode } from '@eslint/json';
-import type { FioriJSONSourceCode } from './language/json/source-code';
-import type { FioriXMLSourceCode } from './language/xml/source-code';
 import type { XMLToken, XMLAstNode } from '@xml-tools/ast';
 import type { AnyNode as AnyAnnotationNode } from '@sap-ux/odata-annotation-core';
+import type { FioriLanguageOptions, FioriSourceCode } from './language/fiori-language';
 
 /**
  * Type definition for manifest.json specific ESLint rules.
@@ -32,8 +31,8 @@ export type ManifestRuleDefinition<Options extends Partial<CustomRuleTypeDefinit
  */
 export type FioriRuleDefinition<Options extends Partial<CustomRuleTypeDefinitions> = object> = CustomRuleDefinitionType<
     {
-        LangOptions: JSONLanguageOptions;
-        Code: FioriJSONSourceCode | FioriXMLSourceCode;
+        LangOptions: FioriLanguageOptions;
+        Code: FioriSourceCode;
         Visitor: RuleVisitor;
         Node: AnyNode | XMLAstNode | XMLToken | AnyAnnotationNode;
     },
