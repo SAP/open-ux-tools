aadb8d292fec9956015ace786e83aeace1b77e4e
Cian Morrin <cian.morrin@sap.com>
2026-04-10 08:45:13 +0100

    fix: flp-config-sub-generator ESM test migration
    Add CJS-compatible mocks for chalk and prompts modules, and configure
    moduleNameMapper in jest.config.mjs to resolve named export issues from
    CJS modules in ESM mode.

--- packages/flp-config-sub-generator/jest.config.mjs
+++ packages/flp-config-sub-generator/jest.config.mjs
@@ -4,4 +4,12 @@ config.snapshotFormat = {
     escapeString: false,
     printBasicPrototype: false
 };
+config.moduleNameMapper = {
+    ...config.moduleNameMapper,
+    '^chalk$': '<rootDir>/test/__mocks__/chalk.cjs',
+    '^prompts$': '<rootDir>/test/__mocks__/prompts.cjs'
+};
+config.transformIgnorePatterns = [
+    'node_modules/(?!(@sap-ux|@sap-ux-private|@sap/ux-cds-compiler-facade|chalk)/)'
+];
 export default config;
