51e28620b560405cd401fe8ecf15c8cb870399a1
Cian Morrin <cian.morrin@sap.com>
2026-04-10 10:36:03 +0100

    jest-environment-ui5: fix ESM test configuration
    - Remove extensionsToTreatAsEsm override that prevented ESM setup file loading
    - Add NODE_OPTIONS='--experimental-vm-modules' to all UI5 test scripts

--- packages/jest-environment-ui5/jest.config.mjs
+++ packages/jest-environment-ui5/jest.config.mjs
@@ -3,9 +3,6 @@ const config = { ...baseConfig };
 config.testMatch = ['**/test/unit/?(*.)+(spec|test).[jt]s?(x)'];
 config.collectCoverageFrom = ['src/**/*.js'];
 config.collectCoverage = true;
-// This package has CJS source files (.js with require/module.exports)
-// Don't treat .js extensions as ESM, and exclude fixture test files
-config.extensionsToTreatAsEsm = [];
 config.testPathIgnorePatterns = [
     ...(config.testPathIgnorePatterns || []),
     '/test/fixtures/'
