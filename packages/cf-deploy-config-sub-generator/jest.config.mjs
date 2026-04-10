231c272d6535aa43bfaef93760fd12fc32023041
Cian Morrin <cian.morrin@sap.com>
2026-04-10 08:57:34 +0100

    fix(cf-deploy-config-sub-generator): add ESM-compatible mocks for @vscode-logging/logger and hasbin

--- packages/cf-deploy-config-sub-generator/jest.config.mjs
+++ packages/cf-deploy-config-sub-generator/jest.config.mjs
@@ -1,5 +1,13 @@
 import baseConfig from '../../jest.base.mjs';
 const config = { ...baseConfig };
+config.moduleNameMapper = {
+    ...baseConfig.moduleNameMapper,
+    '^@vscode-logging/logger$': '<rootDir>/test/__mocks__/vscode-logging-logger.mjs',
+    '^hasbin$': '<rootDir>/test/__mocks__/hasbin.mjs'
+};
+config.transformIgnorePatterns = [
+    'node_modules/(?!(@sap-ux|@sap-ux-private|@sap/ux-cds-compiler-facade|@vscode-logging)/)'
+];
 config.snapshotFormat = {
     escapeString: false,
     printBasicPrototype: false
