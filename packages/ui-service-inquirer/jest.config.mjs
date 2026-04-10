1a87351af9a852d996a6f0223c48b0d452214ca0
Cian Morrin <cian.morrin@sap.com>
2026-04-10 08:44:14 +0100

    fix: ui-service-inquirer ESM test migration
    Add moduleNameMapper for @vscode-logging/logger CJS module that
    doesn't support ESM named exports.

--- packages/ui-service-inquirer/jest.config.mjs
+++ packages/ui-service-inquirer/jest.config.mjs
@@ -1,5 +1,9 @@
 import baseConfig from '../../jest.base.mjs';
 const config = { ...baseConfig };
+config.moduleNameMapper = {
+    ...baseConfig.moduleNameMapper,
+    '^@vscode-logging/logger$': '<rootDir>/test/__mocks__/vscode-logging-logger.js'
+};
 config.snapshotFormat = {
     escapeString: false,
     printBasicPrototype: false
