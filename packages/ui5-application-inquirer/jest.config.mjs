de39d7ca53e0fa23cba85510514f2c89a8a671ac
Cian Morrin <cian.morrin@sap.com>
2026-04-10 08:46:25 +0100

    fix: ui5-application-inquirer ESM test migration
    Add moduleNameMapper for @vscode-logging/logger CJS module and
    transformIgnorePatterns for @vscode-logging.

--- packages/ui5-application-inquirer/jest.config.mjs
+++ packages/ui5-application-inquirer/jest.config.mjs
@@ -1,5 +1,12 @@
 import baseConfig from '../../jest.base.mjs';
 const config = { ...baseConfig };
+config.moduleNameMapper = {
+    ...baseConfig.moduleNameMapper,
+    '^@vscode-logging/logger$': '<rootDir>/test/__mocks__/vscode-logging-logger.mjs'
+};
+config.transformIgnorePatterns = [
+    'node_modules/(?!(@sap-ux|@sap-ux-private|@sap/ux-cds-compiler-facade|@vscode-logging)/)'
+];
 config.snapshotFormat = {
     escapeString: false,
     printBasicPrototype: false
