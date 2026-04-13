ab4219cc2eaba3de9e716b95d735da9a9c8296b7
Marco Vieth <marco.vieth@sap.com>
2026-04-13 10:51:00 +0200

    test: trigger CI for eslint-plugin-fiori-tools heap OOM issue

--- packages/eslint-plugin-fiori-tools/jest.config.js
+++ packages/eslint-plugin-fiori-tools/jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
     // Coverage is handled by c8 wrapper for worker thread support
     collectCoverage: false,
     setupFiles: ['<rootDir>/test/global-setup.ts']
+    // TODO: investigate OOM on Node 20 arm64 after test run
 }
