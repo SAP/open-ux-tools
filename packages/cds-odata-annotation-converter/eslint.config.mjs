224c7effcfe0e51e88cedf468d1f1293743ec377
Cian Morrin <cian.morrin@sap.com>
2026-04-13 09:49:19 +0100

    linting fixed for cds-odata-annotation-converter

--- packages/cds-odata-annotation-converter/eslint.config.mjs
+++ packages/cds-odata-annotation-converter/eslint.config.mjs
@@ -11,6 +11,9 @@ const __dirname = path.dirname(__filename);
 export default [
     ...base,
     {
+        ignores: ['**/*.cjs']
+    },
+    {
         languageOptions: {
             parserOptions: {
                 parser: tsParser,
