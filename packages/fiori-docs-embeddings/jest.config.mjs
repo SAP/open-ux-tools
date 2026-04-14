e96f99336488d18e2f9acfadded83835d9724c0e
Cian Morrin <cian.morrin@sap.com>
2026-04-14 12:41:31 +0100

    fix(fiori-docs-embeddings): override setupFiles to fix Windows Jest ESM error
    The root jest.setup.mjs file cannot be loaded on Windows because Jest doesn't
    treat .mjs files as ES modules by default, causing 'Cannot use import statement
    outside a module' errors.
    
    Since all tests in this package already import from @jest/globals, they don't
    need the global injection from the setup file. Override setupFiles to empty array
    to skip the problematic root setup file.
    
    Fixes Windows CI failure where Jest couldn't parse jest.setup.mjs

--- packages/fiori-docs-embeddings/jest.config.mjs
+++ packages/fiori-docs-embeddings/jest.config.mjs
@@ -2,5 +2,6 @@ import baseConfig from '../../jest.base.mjs';
 
 export default {
     ...baseConfig,
+    setupFiles: [], // Override base config - tests already import from @jest/globals
     modulePathIgnorePatterns: [...baseConfig.modulePathIgnorePatterns, '<rootDir>/test/data/', '<rootDir>/data/']
 };
