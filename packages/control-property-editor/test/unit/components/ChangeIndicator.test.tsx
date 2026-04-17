45029e6422562477190ae5134a68f7df3cca7799
Jimmy Joseph <jimmy.joseph@sap.com>
2026-04-17 13:28:03 +0200

    sonar svg role attribute issue

--- packages/control-property-editor/test/unit/components/ChangeIndicator.test.tsx
+++ packages/control-property-editor/test/unit/components/ChangeIndicator.test.tsx
@@ -5,103 +5,30 @@ import { render } from '../utils';
 import { ChangeIndicator } from '../../../src/components/ChangeIndicator';
 
 describe('ChangeIndicator', () => {
-    test('saved changes', () => {
+    test('saved changes - shows tooltip on hover', () => {
         const { container } = render(<ChangeIndicator id={'change-indicator'} saved={1} pending={0} type="property" />);
-        expect(container.querySelector('svg')).toMatchInlineSnapshot(`
-            <svg
-              fill="none"
-              height="8"
-              id="change-indicator"
-              viewBox="0 0 8 8"
-              width="8"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>
-                All changes on this property are saved
-              </title>
-              <circle
-                cx="4"
-                cy="4"
-                fill="var(--vscode-terminal-ansiGreen)"
-                r="4"
-              />
-            </svg>
-        `);
+        const icon = container.querySelector('i');
+        expect(icon).toHaveAttribute('title', 'All changes on this property are saved');
     });
 
-    test('pending changes', () => {
+    test('pending changes - shows tooltip on hover', () => {
         const { container } = render(<ChangeIndicator saved={0} pending={2} type="property" />);
-        expect(container.querySelector('svg')).toMatchInlineSnapshot(`
-            <svg
-              fill="none"
-              height="8"
-              viewBox="0 0 8 8"
-              width="8"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>
-                All changes on this property are not saved
-              </title>
-              <circle
-                cx="4"
-                cy="4"
-                r="3.5"
-                stroke="var(--vscode-terminal-ansiGreen)"
-              />
-            </svg>
-        `);
+        const icon = container.querySelector('i');
+        expect(icon).toHaveAttribute('title', 'All changes on this property are not saved');
     });
 
-    test('pending and saved changes', () => {
+    test('pending and saved changes - shows tooltip on hover', () => {
         const { container } = render(<ChangeIndicator saved={3} pending={2} type="property" />);
-        expect(container.querySelector('svg')).toMatchInlineSnapshot(`
-            <svg
-              fill="none"
-              height="8"
-              viewBox="0 0 8 8"
-              width="8"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>
-                This property has previously saved changes and currently unsaved changes
-              </title>
-              <circle
-                cx="4"
-                cy="4"
-                r="3.5"
-                stroke="var(--vscode-terminal-ansiGreen)"
-              />
-              <path
-                d="M4 8a4 4 0 1 0 0-8v8Z"
-                fill="var(--vscode-terminal-ansiGreen)"
-              />
-            </svg>
-        `);
+        const icon = container.querySelector('i');
+        expect(icon).toHaveAttribute(
+            'title',
+            'This property has previously saved changes and currently unsaved changes'
+        );
     });
 
-    test('do not add unknown properties', () => {
-        const { container } = render(
-            <ChangeIndicator id={'change-indicator'} saved={1} pending={0} {...{ xyz: 'abc ' }} type="property" />
-        );
-        expect(container.querySelector('svg')).toMatchInlineSnapshot(`
-            <svg
-              fill="none"
-              height="8"
-              id="change-indicator"
-              viewBox="0 0 8 8"
-              width="8"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>
-                All changes on this property are saved
-              </title>
-              <circle
-                cx="4"
-                cy="4"
-                fill="var(--vscode-terminal-ansiGreen)"
-                r="4"
-              />
-            </svg>
-        `);
+    test('id prop is applied to icon element', () => {
+        const { container } = render(<ChangeIndicator id={'change-indicator'} saved={1} pending={0} type="property" />);
+        const icon = container.querySelector('i');
+        expect(icon).toHaveAttribute('id', 'change-indicator');
     });
 });
