4de2bd62ac1cdbe3470ed18e8e666d0fd2a7bc8b
mmilko01 <mihail.milkov@sap.com>
2026-04-20 18:04:11 +0300

    fix: plural

--- packages/adp-tooling/test/unit/prompts/add-new-model/index.test.ts
+++ packages/adp-tooling/test/unit/prompts/add-new-model/index.test.ts
@@ -156,7 +156,7 @@ describe('getPrompts', () => {
 
         expect(typeof validation).toBe('function');
         expect(validation?.('customer.testName')).toBe(
-            'Data Source and SAPUI5 model with the same name was already added to the project. Rename and try again.'
+            'Data Source and SAPUI5 model with the same name were already added to the project. Rename and try again.'
         );
     });
 
