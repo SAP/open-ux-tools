389d7f37370898cef3b5d73a3b023eaddf9fc178
Andis Redmans <andis.redmans@sap.com>
2026-05-07 08:53:14 +0300

    test: additional test
    additional test

--- packages/preview-middleware-client/test/unit/flp/init.test.ts
+++ packages/preview-middleware-client/test/unit/flp/init.test.ts
@@ -179,6 +179,11 @@ describe('flp/init', () => {
                     name: 'Client more than 3 symbols',
                     value: '4444',
                     expected: '/sap/bc/ui2/app_index/ui5_app_info?id=test.lib,'
+                },
+                {
+                    name: 'Client less than 3 symbols',
+                    value: '44',
+                    expected: '/sap/bc/ui2/app_index/ui5_app_info?id=test.lib,'
                 }
             ];
             test.each(sapClientParamTests)('$name', async ({ value, expected }) => {
