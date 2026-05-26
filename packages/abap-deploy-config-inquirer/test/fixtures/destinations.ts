4bddc9d3e7960b010f27463526b7f3cbc2f024dd
J Long <john.long@sap.com>
2026-05-26 14:20:58 +0100

    feat(abap-deploy-config-inquirer): store destination Authentication type in PromptState

--- packages/abap-deploy-config-inquirer/test/fixtures/destinations.ts
+++ packages/abap-deploy-config-inquirer/test/fixtures/destinations.ts
@@ -14,5 +14,45 @@ export const mockDestinations = {
         Description: 'Mock destination 2',
         Host: 'https://mock.url.dest2.com',
         ProxyType: 'OnPremise'
+    },
+    DestSAML: {
+        Name: 'DestSAML',
+        Type: 'HTTP',
+        Authentication: 'SAMLAssertion',
+        Description: 'SAML destination',
+        Host: 'https://mock.url.saml.com',
+        ProxyType: 'Internet'
+    },
+    DestOAuth2CC: {
+        Name: 'DestOAuth2CC',
+        Type: 'HTTP',
+        Authentication: 'OAuth2ClientCredentials',
+        Description: 'OAuth2 client credentials destination',
+        Host: 'https://mock.url.oauth2cc.com',
+        ProxyType: 'Internet'
+    },
+    DestOAuth2JWT: {
+        Name: 'DestOAuth2JWT',
+        Type: 'HTTP',
+        Authentication: 'OAuth2JWTBearer',
+        Description: 'OAuth2 JWT bearer destination',
+        Host: 'https://mock.url.oauth2jwt.com',
+        ProxyType: 'Internet'
+    },
+    DestBasic: {
+        Name: 'DestBasic',
+        Type: 'HTTP',
+        Authentication: 'BasicAuthentication',
+        Description: 'Basic auth destination',
+        Host: 'https://mock.url.basic.com',
+        ProxyType: 'Internet'
+    },
+    DestClientCert: {
+        Name: 'DestClientCert',
+        Type: 'HTTP',
+        Authentication: 'ClientCertificateAuthentication',
+        Description: 'Client cert destination',
+        Host: 'https://mock.url.cert.com',
+        ProxyType: 'Internet'
     }
 };
