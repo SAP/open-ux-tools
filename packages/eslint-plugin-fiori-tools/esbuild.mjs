acc7d3d9aef5ade819a3a777b9437c92524d7dfa
Andis Redmans <andis.redmans@sap.com>
2026-06-09 17:16:18 +0300

    fix: remove unused require
    remove unused require

--- packages/eslint-plugin-fiori-tools/esbuild.mjs
+++ packages/eslint-plugin-fiori-tools/esbuild.mjs
@@ -6,7 +6,6 @@ import { dirname, join } from 'node:path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const require = createRequire(import.meta.url);
 const production = process.argv.includes('--production');
 const watch = process.argv.includes('--watch');
 
