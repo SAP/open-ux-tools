81308c2f59e2aa6bf6ee40053193d87d7ed8d46a
D048415 <dominik.heim@sap.com>
2026-06-15 13:53:29 +0200

    fix: re-introduce sonar issue with top level await because this is actually needed

--- packages/preview-middleware-client/src/flp/sandbox1Init.ts
+++ packages/preview-middleware-client/src/flp/sandbox1Init.ts
@@ -109,16 +109,14 @@ export async function init({
 // eslint-disable-next-line @sap-ux/fiori-tools/sap-no-dom-access,@sap-ux/fiori-tools/sap-browser-api-warning, @sap-ux/fiori-tools/sap-no-global-variable
 const bootstrapConfig = document.getElementById('sap-ui-bootstrap');
 if (bootstrapConfig) {
-    try {
-        await init({
-            appUrls: bootstrapConfig.dataset.openUxPreviewLibsManifests,
-            flex: bootstrapConfig.dataset.openUxPreviewFlexSettings,
-            customInit: bootstrapConfig.dataset.openUxPreviewCustomInit,
-            enhancedHomePage: !!bootstrapConfig.dataset.openUxPreviewEnhancedHomepage,
-            enableCardGenerator: !!bootstrapConfig.dataset.openUxPreviewEnableCardGenerator
-        });
-    } catch (e) {
+    init({
+        appUrls: bootstrapConfig.dataset.openUxPreviewLibsManifests,
+        flex: bootstrapConfig.dataset.openUxPreviewFlexSettings,
+        customInit: bootstrapConfig.dataset.openUxPreviewCustomInit,
+        enhancedHomePage: !!bootstrapConfig.dataset.openUxPreviewEnhancedHomepage,
+        enableCardGenerator: !!bootstrapConfig.dataset.openUxPreviewEnableCardGenerator
+    }).catch((e) => {
         const error = getError(e);
         Log.error('Sandbox initialization failed: ' + error.message);
-    }
+    });
 }
