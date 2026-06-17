90669f5a7cb88c1f74e6b5012338a47860ec882f
Alina Govoruhina <alina.govoruhina@sap.com>
2026-06-17 15:37:17 +0300

    test: add rule unit tests

---
+++ packages/eslint-plugin-fiori-tools/test/rules/sap-no-live-mode.test.ts
@@ -0,0 +1,103 @@
+import { RuleTester } from 'eslint';
+import noLiveModeRule from '../../src/rules/sap-no-live-mode.js';
+import { meta, languages } from '../../src/index.js';
+import { getManifestAsCode, setup, V4_MANIFEST, V4_MANIFEST_PATH } from '../test-helper.js';
+
+const ruleTester = new RuleTester({
+    plugins: { ['@sap-ux/eslint-plugin-fiori-tools']: { ...meta, languages } },
+    language: '@sap-ux/eslint-plugin-fiori-tools/fiori'
+});
+
+const TEST_NAME = 'sap-no-live-mode';
+const { createValidTest, createInvalidTest } = setup(TEST_NAME);
+
+ruleTester.run(TEST_NAME, noLiveModeRule, {
+    valid: [
+        createValidTest(
+            {
+                name: 'V4 - non-json file',
+                filename: 'file_name.xml',
+                code: '<>'
+            },
+            []
+        ),
+        createValidTest(
+            {
+                name: 'V4 - liveMode not defined',
+                filename: V4_MANIFEST_PATH,
+                code: JSON.stringify(V4_MANIFEST, undefined, 2)
+            },
+            []
+        ),
+        createValidTest(
+            {
+                name: 'V4 - object page table - liveMode is false, property is not checked',
+                filename: V4_MANIFEST_PATH,
+                code: getManifestAsCode(V4_MANIFEST, [
+                    {
+                        path: [
+                            'sap.ui5',
+                            'routing',
+                            'targets',
+                            'IncidentsObjectPage',
+                            'options',
+                            'settings',
+                            'liveMode'
+                        ],
+                        value: false
+                    }
+                ])
+            },
+            []
+        )
+    ],
+
+    invalid: [
+        createInvalidTest(
+            {
+                name: 'V4 - list report page - liveMode is true',
+                filename: V4_MANIFEST_PATH,
+                code: getManifestAsCode(V4_MANIFEST, [
+                    {
+                        path: ['sap.ui5', 'routing', 'targets', 'IncidentsList', 'options', 'settings', 'liveMode'],
+                        value: true
+                    }
+                ]),
+                errors: [
+                    {
+                        message:
+                            'The Go Button must always be present in the application filter bar, so the liveMode property should not be used.',
+                        line: 130,
+                        column: 15
+                    }
+                ],
+                output: getManifestAsCode(V4_MANIFEST, [
+                    {
+                        path: ['sap.ui5', 'routing', 'targets', 'IncidentsList', 'options', 'settings'],
+                        value: {
+                            // liveMode property removed
+                            entitySet: 'Incidents',
+                            variantManagement: 'Page',
+                            navigation: {
+                                Incidents: {
+                                    detail: {
+                                        route: 'IncidentsObjectPage'
+                                    }
+                                }
+                            },
+                            controlConfiguration: {
+                                '@com.sap.vocabularies.UI.v1.LineItem': {
+                                    tableSettings: {
+                                        type: 'ResponsiveTable',
+                                        selectionMode: 'Auto'
+                                    }
+                                }
+                            }
+                        }
+                    }
+                ])
+            },
+            []
+        )
+    ]
+});
