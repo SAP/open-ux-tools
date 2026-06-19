002d65371567f02c6bc089b963bf9011b08d6aaf
Vladislavs Silins <vladislavs.silins@sap.com>
2026-06-19 13:53:23 +0300

    test: add generateFragment test coverage

--- packages/fe-fpm-writer/test/unit/column.test.ts
+++ packages/fe-fpm-writer/test/unit/column.test.ts
@@ -349,4 +349,82 @@ describe('CustomAction', () => {
             });
         });
     });
+
+    describe('generateFragment', () => {
+        let fs: Editor;
+        const testFragmentDir = join(__dirname, 'sample/column');
+
+        beforeEach(() => {
+            fs = create(createStorage());
+        });
+
+        test('generate fragment with default options', async () => {
+            const { generateFragment } = await import('../../src/index.js');
+            const fragment = {
+                name: 'MyFragment'
+            };
+
+            await generateFragment(testFragmentDir, fragment, fs);
+
+            const expectedPath = join(testFragmentDir, 'webapp/ext/fragment/MyFragment.fragment.xml');
+            expect(fs.exists(expectedPath)).toBe(true);
+            expect(fs.read(expectedPath)).toMatchSnapshot();
+        });
+
+        test('generate fragment with custom folder', async () => {
+            const { generateFragment } = await import('../../src/index.js');
+            const fragment = {
+                name: 'CustomFragment',
+                folder: 'extensions/fragments'
+            };
+
+            await generateFragment(testFragmentDir, fragment, fs);
+
+            const expectedPath = join(testFragmentDir, 'webapp/extensions/fragments/CustomFragment.fragment.xml');
+            expect(fs.exists(expectedPath)).toBe(true);
+            expect(fs.read(expectedPath)).toMatchSnapshot();
+        });
+
+        test('generate fragment with custom content', async () => {
+            const { generateFragment } = await import('../../src/index.js');
+            const fragment = {
+                name: 'ContentFragment',
+                content: '<Label text="Custom Content" />'
+            };
+
+            await generateFragment(testFragmentDir, fragment, fs);
+
+            const expectedPath = join(testFragmentDir, 'webapp/ext/fragment/ContentFragment.fragment.xml');
+            expect(fs.exists(expectedPath)).toBe(true);
+            const content = fs.read(expectedPath);
+            expect(content).toContain('Custom Content');
+            expect(content).toMatchSnapshot();
+        });
+
+        test('generate fragment without fs parameter', async () => {
+            const { generateFragment } = await import('../../src/index.js');
+            const fragment = {
+                name: 'NoFsFragment'
+            };
+
+            const resultFs = await generateFragment(testFragmentDir, fragment);
+
+            const expectedPath = join(testFragmentDir, 'webapp/ext/fragment/NoFsFragment.fragment.xml');
+            expect(resultFs.exists(expectedPath)).toBe(true);
+        });
+
+        test('do not overwrite existing fragment', async () => {
+            const { generateFragment } = await import('../../src/index.js');
+            const fragment = {
+                name: 'ExistingFragment'
+            };
+
+            const expectedPath = join(testFragmentDir, 'webapp/ext/fragment/ExistingFragment.fragment.xml');
+            fs.write(expectedPath, '<FragmentDefinition xmlns="sap.ui.core">Existing</FragmentDefinition>');
+
+            await generateFragment(testFragmentDir, fragment, fs);
+
+            expect(fs.read(expectedPath)).toContain('Existing');
+        });
+    });
 });
