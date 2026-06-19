31d6c8a6f51c5f29298a2d55f9b5433733cfbc20
Alina Govoruhina <alina.govoruhina@sap.com>
2026-06-19 15:18:29 +0300

    test: add get loc unit tests

--- packages/eslint-plugin-fiori-tools/test/language/json/source-code.test.ts
+++ packages/eslint-plugin-fiori-tools/test/language/json/source-code.test.ts
@@ -1,3 +1,4 @@
+import type { MemberNode, StringNode } from '@humanwhocodes/momoa';
 import { parse } from '@humanwhocodes/momoa';
 import { FioriJSONSourceCode } from '../../../src/language/json/source-code.js';
 import type { ProjectContext } from '../../../src/project-context/project-context.js';
@@ -7,8 +8,7 @@ const DUMMY_PROJECT_CONTEXT: ProjectContext = {} as any;
 
 describe('FioriJSONSourceCode', () => {
     it('should get parent node if requested path array is empty', () => {
-        const jsonText =
-            '{"type": "Object", "name": "StartNode", "members": [{"name": {"type": "String", "value": "dummy_name"}}]}';
+        const jsonText = '{"type": "Object", "firstNode": {"secondNode": {"thirdNode": {}}}}';
         const ast = parse(jsonText);
         const sourceCode = new FioriJSONSourceCode({
             text: jsonText,
@@ -17,12 +17,11 @@ describe('FioriJSONSourceCode', () => {
             uri: ''
         });
 
-        expect(sourceCode.getNode(ast.body, [])).toBe(ast.body);
+        expect(sourceCode.getNode(ast.body, [])).toMatchObject(ast.body);
     });
 
     it('should get parent node if requested node not found', () => {
-        const jsonText =
-            '{"type": "Object", "name": "StartNode", "members": [{"name": {"type": "Boolean", "value": "true"}}]}';
+        const jsonText = '{"type": "Object", "firstNode":{ "secondNode": {"thirdNode": {}}}}';
         const ast = parse(jsonText);
         const sourceCode = new FioriJSONSourceCode({
             text: jsonText,
@@ -31,12 +30,11 @@ describe('FioriJSONSourceCode', () => {
             uri: ''
         });
 
-        expect(sourceCode.getNode(ast.body, ['path', 'to', 'node'])).toBe(ast.body);
+        expect(sourceCode.getNode(ast.body, ['path', 'to', 'node'])).toMatchObject(ast.body);
     });
 
-    it('should get found node', () => {
-        const dummyNodeText = '{"type": "Member", "name": {"type": "String", "value": "DummyNode"}}';
-        const jsonText = `{"type": "Object", "name": "StartNode", "members": [${dummyNodeText}]}`;
+    it('should get found first node', () => {
+        const jsonText = '{"type": "Object", "firstNode": {}}';
         const ast = parse(jsonText);
         const sourceCode = new FioriJSONSourceCode({
             text: jsonText,
@@ -45,17 +43,73 @@ describe('FioriJSONSourceCode', () => {
             uri: ''
         });
 
-        const foundNode = sourceCode.getNode(ast.body, ['DummyNode']);
+        const foundNode = sourceCode.getNode(ast.body, ['firstNode']);
+        expect(foundNode.type).toBe('Member');
+        expect(((foundNode as MemberNode).name as StringNode).value).toBe('firstNode');
         expect(foundNode.loc).toMatchObject({
             start: {
-                column: 1,
+                column: 20,
                 line: 1,
-                offset: 0
+                offset: 19
             },
             end: {
-                column: 123,
+                column: 35,
                 line: 1,
-                offset: 122
+                offset: 34
+            }
+        });
+    });
+
+    it('should get third found node', () => {
+        const jsonText = '{"type": "Object", "firstNode": {"secondNode": {"thirdNode": {}}}}';
+        const ast = parse(jsonText);
+        const sourceCode = new FioriJSONSourceCode({
+            text: jsonText,
+            ast,
+            projectContext: DUMMY_PROJECT_CONTEXT,
+            uri: ''
+        });
+
+        const foundNode = sourceCode.getNode(ast.body, ['firstNode', 'secondNode', 'thirdNode']);
+        expect(foundNode.type).toBe('Member');
+        expect(((foundNode as MemberNode).name as StringNode).value).toBe('thirdNode');
+        expect(foundNode.loc).toMatchObject({
+            start: {
+                column: 49,
+                line: 1,
+                offset: 48
+            },
+            end: {
+                column: 64,
+                line: 1,
+                offset: 63
+            }
+        });
+    });
+
+    it('should get second found node as third is not found', () => {
+        const jsonText = '{"type": "Object", "firstNode": {"secondNode": {}}}';
+        const ast = parse(jsonText);
+        const sourceCode = new FioriJSONSourceCode({
+            text: jsonText,
+            ast,
+            projectContext: DUMMY_PROJECT_CONTEXT,
+            uri: ''
+        });
+
+        const foundNode = sourceCode.getNode(ast.body, ['firstNode', 'secondNode', 'thirdNode']);
+        expect(foundNode.type).toBe('Member');
+        expect(((foundNode as MemberNode).name as StringNode).value).toBe('secondNode');
+        expect(foundNode.loc).toMatchObject({
+            start: {
+                column: 34,
+                line: 1,
+                offset: 33
+            },
+            end: {
+                column: 50,
+                line: 1,
+                offset: 49
             }
         });
     });
