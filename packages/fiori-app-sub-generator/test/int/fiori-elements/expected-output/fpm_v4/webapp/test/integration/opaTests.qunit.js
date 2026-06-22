31ceaa2181506b857cec13863faca95f6522553f
David O'Connor <david.o-connor@sap.com>
2026-06-22 11:07:27 +0100

    update snapshots

--- packages/fiori-app-sub-generator/test/int/fiori-elements/expected-output/fpm_v4/webapp/test/integration/opaTests.qunit.js
+++ packages/fiori-app-sub-generator/test/int/fiori-elements/expected-output/fpm_v4/webapp/test/integration/opaTests.qunit.js
@@ -22,7 +22,7 @@ sap.ui.require(
     "sap/ui/thirdparty/qunit-2",
     "sap/ui/qunit/qunit-junit",
     "sap/ui/qunit/qunit-coverage",
-    "testNameSpace/fpmv4/test/integration/FirstJourney",
+    "testNameSpace/fpmv4/test/integration/TravelMainJourney.gen",
 ], function (QUnit) {
     "use strict";
     QUnit.start();
