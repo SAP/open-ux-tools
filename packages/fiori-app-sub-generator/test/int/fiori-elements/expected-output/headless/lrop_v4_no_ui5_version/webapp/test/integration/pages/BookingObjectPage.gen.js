4d4e8d7fe13739af01d1b08212237eea284acf4f
I334706 <ondrej.sebela@sap.com>
2026-06-23 15:46:29 +0200

    test: extend migration fixture updates to worklist + headless tests

--- packages/fiori-app-sub-generator/test/int/fiori-elements/expected-output/headless/lrop_v4_no_ui5_version/webapp/test/integration/pages/BookingObjectPage.gen.js
+++ packages/fiori-app-sub-generator/test/int/fiori-elements/expected-output/headless/lrop_v4_no_ui5_version/webapp/test/integration/pages/BookingObjectPage.gen.js
@@ -15,18 +15,11 @@
  * ╚═══════════════════════════════════════════════════════════════════════╝ *
  ******************************************************************************/
 
-sap.ui.define(['sap/fe/test/ObjectPage', 'sap/ui/test/actions/Press'], function(ObjectPage, Press) {
+sap.ui.define(['sap/fe/test/ObjectPage'], function(ObjectPage) {
     'use strict';
 
     var CustomPageDefinitions = {
-        actions: {
-            iPressSectionIconTabFilterButton: function (section) {
-                return this.waitFor({
-                    id: new RegExp(`.*--fe::FacetSection::${section}-anchor$`),
-                    actions: new Press()
-                });
-            }
-        },
+        actions: {},
         assertions: {}
     };
 
