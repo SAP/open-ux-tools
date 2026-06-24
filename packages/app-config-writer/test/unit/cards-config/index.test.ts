ae78ab2bf4a2686b9cf18f490326dbed93a381b2
D048415 <dominik.heim@sap.com>
2026-06-24 12:05:22 +0200

    re-add tests

--- packages/app-config-writer/test/unit/cards-config/index.test.ts
+++ packages/app-config-writer/test/unit/cards-config/index.test.ts
@@ -1,6 +1,8 @@
 import { jest } from '@jest/globals';
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { create as createStorage } from 'mem-fs';
+import { create } from 'mem-fs-editor';
 
 const mockGetProjectType = jest.fn();
 const mockFindCapProjectRoot = jest.fn();
@@ -38,7 +40,108 @@ jest.unstable_mockModule('../../../src/cards-config/cap.js', () => ({
 
 const { enableCardGeneratorConfig } = await import('../../../src/cards-config/index.js');
 
+function createTestFs(basePath: string) {
+    const fs = create(createStorage());
+    fs.writeJSON(join(basePath, 'webapp/manifest.json'), {
+        'sap.app': {
+            id: 'test.id',
+            title: 'Test App'
+        }
+    });
+    fs.writeJSON(join(basePath, 'package.json'), {});
+    fs.write(join(basePath, 'ui5.yaml'), '');
+    return fs;
+}
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+describe('enableCardGenerator', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+        mockGetProjectType.mockResolvedValue('EDMXBackend');
+        mockFindCapProjectRoot.mockResolvedValue(null);
+        mockUpdateCapRootPackageJsonForCards.mockResolvedValue(undefined);
+    });
+
+    test('Valid LROP', async () => {
+        const basePath = join(__dirname, '../../fixtures/cards-config/lrop-v4');
+        const fs = createTestFs(basePath);
+        await enableCardGeneratorConfig(basePath, join(basePath, 'ui5.yaml'), undefined, fs);
+
+        expect(fs.read(join(basePath, 'package.json'))).toMatchSnapshot();
+        expect(fs.read(join(basePath, 'ui5.yaml'))).toMatchSnapshot();
+    });
+
+    test('V4 LROP with CLI 3.0', async () => {
+        const basePath = join(__dirname, '../../fixtures/cards-config/lrop-v4');
+        const fs = create(createStorage());
+        await enableCardGeneratorConfig(basePath, join(basePath, 'ui5.yaml'), undefined, fs);
+
+        if (process.env.UX_DEBUG) {
+            fs.commit(() => {});
+        }
+
+        expect(fs.read(join(basePath, 'package.json'))).toMatchSnapshot();
+        expect(fs.read(join(basePath, 'ui5.yaml'))).toMatchSnapshot();
+    });
+
+    test('Valid LROP without cardGenerator config', async () => {
+        const basePath = join(__dirname, '../../fixtures/cards-config/lrop-v4');
+        const fs = create(createStorage());
+        await enableCardGeneratorConfig(basePath, join(basePath, 'ui5-without-generator.yaml'), undefined, fs);
+
+        if (process.env.UX_DEBUG) {
+            fs.commit(() => {});
+        }
+
+        expect(fs.read(join(basePath, 'package.json'))).toMatchSnapshot();
+        expect(fs.read(join(basePath, 'ui5-without-generator.yaml'))).toMatchSnapshot();
+    });
+
+    test('Valid LROP with deprecated preview config', async () => {
+        const basePath = join(__dirname, '../../fixtures/cards-config/lrop-v4');
+        const fs = create(createStorage());
+        await enableCardGeneratorConfig(basePath, join(basePath, 'ui5-with-deprecated-config.yaml'), undefined, fs);
+
+        if (process.env.UX_DEBUG) {
+            fs.commit(() => {});
+        }
+
+        expect(fs.read(join(basePath, 'package.json'))).toMatchSnapshot();
+        expect(fs.read(join(basePath, 'ui5-with-deprecated-config.yaml'))).toMatchSnapshot();
+    });
+
+    test('Valid LROP with deprecated rta config', async () => {
+        const basePath = join(__dirname, '../../fixtures/cards-config/lrop-v4');
+        const fs = create(createStorage());
+        await enableCardGeneratorConfig(basePath, join(basePath, 'ui5-with-deprecated-rta-config.yaml'), undefined, fs);
+
+        if (process.env.UX_DEBUG) {
+            fs.commit(() => {});
+        }
+
+        expect(fs.read(join(basePath, 'package.json'))).toMatchSnapshot();
+        expect(fs.read(join(basePath, 'ui5-with-deprecated-rta-config.yaml'))).toMatchSnapshot();
+    });
+
+    test('Valid LROP with deprecated config with cards generator', async () => {
+        const basePath = join(__dirname, '../../fixtures/cards-config/lrop-v4');
+        const fs = create(createStorage());
+        await enableCardGeneratorConfig(
+            basePath,
+            join(basePath, 'ui5-with-deprecated-config-and-cards-generator.yaml'),
+            undefined,
+            fs
+        );
+
+        if (process.env.UX_DEBUG) {
+            fs.commit(() => {});
+        }
+
+        expect(fs.read(join(basePath, 'package.json'))).toMatchSnapshot();
+        expect(fs.read(join(basePath, 'ui5-with-deprecated-config-and-cards-generator.yaml'))).toMatchSnapshot();
+    });
+});
 
 describe('enableCardGeneratorConfig - CAP routing', () => {
     const basePath = join(__dirname, '../../fixtures/cards-config/lrop-v4');
