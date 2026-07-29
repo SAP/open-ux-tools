b318ece8e79ec64914fad219051b45f885a0a51b
Alina Govoruhina <alina.govoruhina@sap.com>
2026-07-29 14:29:14 +0300

    feat: add new rule definition

--- packages/eslint-plugin-fiori-tools/src/project-context/linker/fe-v2.ts
+++ packages/eslint-plugin-fiori-tools/src/project-context/linker/fe-v2.ts
@@ -18,6 +18,7 @@ export interface ApplicationSetting {
     statePreservationMode: string;
     flexibleColumnLayout: FlexibleColumnLayoutSettings;
     tableColumnVerticalAlignment: TableColumnVerticalAlignmentValues;
+    cloudDevAdaptationStatus: string;
 }
 export interface PageSetting {
     createMode: string;
@@ -742,6 +743,8 @@ function linkApplicationSettings(context: LinkerContext): LinkedFeV2App {
     const twoColumnLayoutValue = config.settings?.flexibleColumnLayout?.defaultTwoColumnLayoutType;
     const threeColumnLayoutValue = config.settings?.flexibleColumnLayout?.defaultThreeColumnLayoutType;
     const tableColumnVerticalAlignmentValue = config.settings?.tableColumnVerticalAlignment;
+    const fioriConfig = context.app.manifestObject['sap.fiori'] as { cloudDevAdaptationStatus?: string } | undefined;
+    const cloudDevAdaptationStatus = fioriConfig?.cloudDevAdaptationStatus;
     const linkedApp: LinkedFeV2App = {
         type: 'fe-v2',
         pages: [],
@@ -772,6 +775,11 @@ function linkApplicationSettings(context: LinkerContext): LinkedFeV2App {
                 values: ['Bottom', 'Middle', 'Top'],
                 configurationPath: ['sap.ui.generic.app', 'settings', 'tableColumnVerticalAlignment'],
                 valueInFile: tableColumnVerticalAlignmentValue
+            },
+            cloudDevAdaptationStatus: {
+                values: [],
+                configurationPath: ['sap.fiori', 'cloudDevAdaptationStatus'],
+                valueInFile: cloudDevAdaptationStatus
             }
         }
     };
