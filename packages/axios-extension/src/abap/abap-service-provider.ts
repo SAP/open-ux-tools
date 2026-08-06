601353f73444e784e4d04a231ef41a88bb939281
Oksana Korotkova <oksana.korotkova@sap.com>
2026-08-06 14:46:59 +0100

    fix(axios-extension): address code review feedback
    - Remove example service paths from comments
    - Change 'http://dummy' to 'http://placeholder' for clarity

--- packages/axios-extension/src/abap/abap-service-provider.ts
+++ packages/axios-extension/src/abap/abap-service-provider.ts
@@ -305,11 +305,8 @@ export class AbapServiceProvider extends ServiceProvider {
         const valueListReferences: ExternalService[] = [];
         const allPromises = references.map(async (reference) => {
             const { serviceRootPath, value } = reference;
-            // Properly resolve relative paths with '../' navigation
-            // Example: serviceRootPath='/sap/opu/odata4/dmo/sb_travel_mdsk_o4/srvd/dmo/sd_travel_mdsk/0001/'
-            //          value='../../../../srvd_f4/dmo/i_agency/0001/$metadata'
-            //          should resolve to '/srvd_f4/dmo/i_agency/0001'
-            const baseUrl = new URL(serviceRootPath, 'http://dummy');
+            // Properly resolve relative paths with '../' navigation using URL API
+            const baseUrl = new URL(serviceRootPath, 'http://placeholder');
             const resolvedUrl = new URL(value, baseUrl);
             const externalServicePath = resolvedUrl.pathname.replace('/$metadata', '');
             const externalService = this.service(externalServicePath);
